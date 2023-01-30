8161a903e9bc2695e5be494840718d3c5d5b71ea
gratisreise <104671487+gratisreise@users.noreply.github.com>
2023-01-30 18:06:54 +0900

    1.30

--- 3.반복문/11.A+b-4.cpp
+++ 3.반복문/11.A+b-4.cpp
@@ -18,7 +18,7 @@ int main(){
         cout<< a+b<<"\n";
     }
 
-    //풀이4[향샹된 입출력 + cin operator bool]
+    //풀이4[향샹된 입출력 + cin operator bool] -> 추천
     ios_base::sync_with_stdio(false); cin.tie(NULL);
     while(cin>>a>>b){
         cout<< a+b<<"\n";
@@ -26,3 +26,7 @@ int main(){
     
     return 0;
 }
+/*
+1.eof는 입력받은 후 다음의 값을 받을 때 나온다
+2.cin자체가 조건문에 들어갈 때는 연산자 오버로딩으로 인해 bool값으로 변한다.
+*/
--- 4.1차원배열/1.개수세기.cpp
+++ 4.1차원배열/1.개수세기.cpp
@@ -3,6 +3,7 @@ using namespace std;
 int n,v;
 int arr[101];
 int main(){
+    ios::sync_with_stdio(0);cin.tie(0);
     int cnt = 0;
     cin >> n;
     for(int i = 0; i<n; i++) cin>>arr[i];
