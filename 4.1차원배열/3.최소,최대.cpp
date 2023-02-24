4785e2e8f35854d8be514f6242fe4cdff7bffaa5
gratisreise <104671487+gratisreise@users.noreply.github.com>
2023-02-24 16:13:31 +0900

    02.24

--- 4.1차원배열/3.최소,최대.cpp
+++ 4.1차원배열/3.최소,최대.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int n;
 int a[1000004];
-
+// 정렬함수 이용
 int main(){
     ios_base::sync_with_stdio(false); cin.tie(NULL);
     cin>>n;
@@ -11,6 +11,26 @@ int main(){
     }
     sort(a, a + n); //
     cout<<a[0]<<" "<< a[n-1]<<"\n";
+
+    return 0;
+}
+//index이용
+int main(){
+    ios::sync_with_stdio(0);cin.tie(0);
+    int n,a;
+    int max=-2000000; //최대값 넣을 변수선언
+    int min=2000000; //최솟값 넣을 변수선언
+    cin>>n;
+    for(int i=0;i<n;i++){
+        cin>>a;
+        if(a>max){
+            max=a;
+        }
+        if(a<min){ 
+            min=a;
+        }
+    }
+    cout<<min<<' '<<max<<'\n';
 
     return 0;
 }
---
+++ 5.함수/1.정수n의합.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+long long sum(vector<int> &a){
+    ll ans =0;
+    for(int b:a) ans+=b;
+    
+    return ans;
+}
