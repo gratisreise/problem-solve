89d976d532edab95baea4ce7e5f08d40d844e09a
gratisreise <104671487+gratisreise@users.noreply.github.com>
2023-04-02 20:56:55 +0900

    2023.04.02

--- 3주차/3-B.보물섬.cpp
+++ 3주차/3-B.보물섬.cpp
@@ -1,9 +1,10 @@
 #include<bits/stdc++.h>
 using namespace std; 
-int n, m, mx, visited[54][54]; 
-const int dy[] = {-1, 0, 1, 0}; 
-const int dx[] = {0, 1, 0, -1}; 
-char a[54][54];
+int n, m, y, x, ret; 
+int visited[54][54]; char a[54][54];
+int dy[] = {-1, 0, 1, 0};
+int dx[] = {0, 1, 0, -1};
+// vector<pair<int, int>> Land;
 void bfs(int y, int x){
     memset(visited, 0, sizeof(visited)); 
     visited[y][x] = 1; 
@@ -15,11 +16,10 @@ void bfs(int y, int x){
             int ny = y + dy[i]; 
             int nx = x + dx[i]; 
             if(ny < 0 || ny >= n || nx < 0 || nx >= m) continue; 
-            if(visited[ny][nx]) continue; 
-            if(a[ny][nx] == 'W')continue;
+            if(visited[ny][nx] || a[ny][nx] == 'W') continue; 
             visited[ny][nx] = visited[y][x] + 1; 
             q.push({ny, nx});
-            mx = max(mx, visited[ny][nx]);
+            ret = max(ret, visited[ny][nx]);
         }
     }
     return;
@@ -31,13 +31,15 @@ int main(){
             cin >> a[i][j]; 
         }
     }
+    // for(pair<int, int> L : Land) bfs(L.first, L.second);
     for(int i = 0; i < n; i++){
         for(int j = 0; j < m; j++){
             if(a[i][j] == 'L') bfs(i, j); 
         }
     } 
-    cout << mx - 1 << "\n"; //거리를 구하는거니깐 -1 해주면 됨
+    cout << ret - 1 << "\n"; //거리를 구하는거니깐 -1 해주면 됨
 }
 /*
 bfs 탐색을 변형시켜서 최대값 찾기
+시간을 구하는 거니깐 -1을 해줘야 함
 */
--- 3주차/3-C.인구이동.cpp
+++ 3주차/3-C.인구이동.cpp
@@ -1,17 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
-int visited[54][54], a[54][54], n, l, r, sum, cnt; 
-int dy[] = {-1,0,1,0};
-int dx[] = {0,1,0,-1}; 
-vector<pair<int,int>> v; //v가 필요한 이유는 좌표를 저장해서 나중에 계산할라고
-void dfs(int y,int x,vector<pair<int,int>> &v){ 
+int n, l, r, sum, cnt; 
+int visited[54][54], a[54][54];
+int dy[] = {-1, 0, 1, 0};
+int dx[] = {0, 1, 0, -1}; 
+vector<pair<int,int>> v; //나중에 계산할 좌표들 저장하는 vector
+
+//DFS를 이용해 연합을 찾고 각 칸 수의 합을 해준다.
+void dfs(int y, int x, vector<pair<int,int>> &v){ 
     for(int i = 0; i < 4; i++){
         int ny = y + dy[i];
         int nx = x + dx[i];
         if(nx < 0 || nx >= n || ny < 0 || ny >= n || visited[ny][nx]) continue;
         if(abs(a[ny][nx]- a[y][x]) >= l && abs(a[ny][nx] - a[y][x]) <= r){
             visited[ny][nx] = 1;
-            v.push_back({ny, nx}); // 나중에 계산하는데 이용할려고
+            v.push_back({ny, nx}); // 나중에 계산된 값을 뿌려줄려고
             sum += a[ny][nx]; // 나중에 나눌려고
             dfs(ny, nx, v);
         }
@@ -27,25 +30,27 @@ int main(){
     }
     while(true){
         bool flag =0;
-        fill(&visited[0][0], &visited[0][0] + 54 * 54, 0); //초기화
+        fill(&visited[0][0], &visited[0][0] + 54 * 54, 0); //다시 방문되야할 필요성이 있으니깐
         for(int i = 0; i < n; i++){ 
             for(int j = 0; j < n; j++){ 
-                if(!visited[i][j]){
+                if(!visited[i][j]){ // 아직 방문하지 않은경우
                     v.clear();
                     visited[i][j] = 1;
                     v.push_back({i,j});
-                    sum = a[i][j]; // 
+                    sum = a[i][j]; // 여기서 더하기 초기화
                     dfs(i,j,v); // 
-                    if(v.size() == 1) continue;
-                    for(pair<int,int> b : v){
-                        a[b.first][b.second] = sum / v.size();
-                        flag = 1;
+
+                    if(v.size() == 1) continue; // 연합 크기가 1인 경우는 계산할 필요 없음
+
+                    for(pair<int,int> b : v){ // 연합 내 모든 좌표에 대해
+                        a[b.first][b.second] = sum / v.size(); // 인구 이동
+                        flag = 1; // 인구 이동이 일어났다는 표시
                     }
                 }
             }
         }
-        if(!flag) break;
-        cnt++;
+        if(!flag) break; // 더 이상 인구 이동이 일어나지 않은 경우 break
+        cnt++; // 인구이동이 일어난 횟수
      } 
     cout<< cnt << "\n";
     return 0;
