087e61c97d3a21c00a6a2fafeb8480fabec2c475
gratisreise <104671487+gratisreise@users.noreply.github.com>
2023-05-16 09:28:32 +0900

    2023.05.16

--- 2주차/9.수학숙제.cpp
+++ 2주차/9.수학숙제.cpp
@@ -14,8 +14,8 @@ void go(){
     v.push_back(ret); // ret을 벡터v에 넣어줌 
     ret = ""; // ret 초기화해서 숫자만 올 때 대비해줌
 }
-bool cmp(string a, string b){ //문자열 정렬은 아스키코드 순이기 때문에 커스텀 필요
-    if(a.size() == b.size()) return a < b; //자릿수가 같으면 큰 거 뒤에오게
+bool cmp(string a, string b){ // 문자열 정렬은 아스키코드 순이기 때문에 커스텀 필요
+    if(a.size() == b.size()) return a < b; // 자릿수가 같으면 큰 거 뒤에오게
     return a.size() < b.size(); // 자릿수가 더 큰걸 뒤에 오게 한다.
 }
 int main(){
@@ -25,14 +25,15 @@ int main(){
         ret = ""; // ret 초기화 -> 빼먹었었음
         for(int j = 0; j < s.size(); j++){ // 
             if(s[j] < 65) ret += s[j];
-            else if(ret.size()) go();
+            else if(ret.size()) go(); // 문자이면서 ret크기가 있을 때
         }
-        if(ret.size()) go(); //s에 숫자만 올 경우 대비한 로직
+        if(ret.size()) go(); //문자 다음 맨뒤에 숫자만 올걸 대비??
     }
     sort(v.begin(), v.end(), cmp);
     for(string i : v) cout << i << '\n';
     return 0;
 }
+
 //이거 왜 안돼지??
 #include<bits/stdc++.h>
 using namespace std;
