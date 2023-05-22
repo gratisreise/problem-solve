9c3a388c7c41224cb20555b728420beec3b8493d
gratisreise <104671487+gratisreise@users.noreply.github.com>
2023-05-22 20:41:10 +0900

    2023.05.22

--- Drill/1.재귀.cpp
+++ Drill/1.재귀.cpp
@@ -1,48 +1,19 @@
-// 순열 -> 원상복구 재귀 이용
-makePermuatation(3, 3, 0);
-void makePermutation(int n, int r, int depth){
-    if(depth == r){
-        printV(v);
-        return;
+string quard(int y, int x, int n){
+    if(n == 1) return string(1, a[y][x]); //기저사례 + 캐릭터를 스트링으로 바꾸는 함수
+    char b = a[y][x]; // char 
+    string ret = ""; // 빈문자열로 초기화
+    for(int i = y; i < y + n; i++){  // 분할정복 개념들어감
+        for(int j = x; j < x + n; j++){
+            if(b != a[i][j]){ // 같은 문자가 아닌게 있으면 바로 쪼개기
+                ret += '(';
+                ret += quard(y, x, n / 2); //왼쪽 위 
+                ret += quard(y, x + n / 2, n / 2); // 오른쪽 위
+                ret += quard(y + n / 2, x, n / 2); // 왼쪽 아래
+                ret += quard(y + n / 2, x + n / 2, n / 2); // 오른쪽 아래
+                ret += ')';
+                return ret;
+            }
+        }
     }
-    for(int i = depth; i < n; i++){
-        swap(v[i], v[depth]); // 교체 
-        makePermutation(n, r, depth + 1); //다르게 들어갈 매개변수
-        swap(v[i], v[depth]); // 원상복구
-    }
-    return;
-}
-
-//n개중 k개를 뽑는 조합코드
-void combi(int start, vector<int> b){
-    if(b.size() == k){ //뽑을 갯수 k
-        print(b);
-        return;
-    }
-    for(int i = start + 1; i < n; i++){ // n개중에서 
-        b.push_back(i);
-        combi(i, b);
-        b.pop_back();
-    }
-    return;
-}
-vector<int> b;
-combi(-1, b);
-
-//팩토리얼
-int fact(int n){
-    if(n == 1 || n == 0) return  1;
-    return n * fact(n - 1);
-}
-int fact1(int n){
-    int ret = 1;
-    for(int i = 1; i <= n; i++){
-        ret *= i;
-    }
-    return ret;
-}
-//피보나치
-int fibo(int n){
-    if(n == 0 || n == 1) return n;
-    return fibo(n - 1) + fibo(n - 2);
+    return string(1, a[y][x]); // 모두다 같은 문자일 때
 }
--- Drill/5.수학.cpp
+++ Drill/5.수학.cpp
@@ -7,6 +7,8 @@ int lcm(int a, int b){
     return (a * b) / gcd(a, b);
 }
 
+<에라토스테네스의 체>
+배열을 선언하고 사용해줘야해서 공간의 제약있음
 vector<int> era(int mx_n){
     vector<int> v;
     for(int i = 2; i <= mx_n; i++){
@@ -18,7 +20,7 @@ vector<int> era(int mx_n){
     for(int = 2; i <= mx_n; i++) if(che[i] == 0) v.push_back(i);
     return v;
 }
-
+<논리형 소수체크>
 bool check(int n){
     if(n <= 1) return 0;
     if(n == 2) return 1;
@@ -26,4 +28,21 @@ bool check(int n){
     for(int i = 2; i * i <= n; i++){
         if(n % i == 0) return 0;
     }
+}
+
+오름차순열 : next_permutation(v.begin(), v.end());
+내림차순열 : prev_permutation(v.begin(), v.end());
+
+nPr
+void makePermutation(int n, int r, int depth){
+    if(r == depth){
+        print();
+        return;
+    }
+    for(int i = depth; i < n; i++){
+        swap(a[i], a[depth]);
+        makePermutation(n, r, depth + 1);
+        swap(a[i], a[depth]);
+    }
+    return;
 }
