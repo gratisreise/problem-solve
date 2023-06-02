449a4f84b8e9a0d5660b4d0183a69a8302e7a542
gratisreise <104671487+gratisreise@users.noreply.github.com>
2023-06-02 10:48:18 +0900

    2023.06.02

--- 4주차/4-A.다이어트.cpp
+++ 4주차/4-A.다이어트.cpp
@@ -1,50 +1,47 @@
 #include<bits/stdc++.h>
 using namespace std;
 const int INF = 1e9;
-int n, mp, mf, ms, mv;
-int b, c, d, e, ret = INF, sum;
-struct A{
+int n, mp, mf, ms, mv, ret = INF;
+int p, f, s, v, c;
+map<int, vector<vector<int>>> mp1;
+struct A {
     int mp, mf, ms, mv, cost;
 }a[16];
-map<int, vector<vector<int>>> ret_v;
 int main(){
-    ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
-    cin >> n;
-    cin >> mp >> mf >> ms >> mv;
+    cin >> n >> mp >> mf >> ms >> mv;
     for(int i = 0; i < n; i++){
         cin >> a[i].mp >> a[i].mf >> a[i].ms >> a[i].mv >> a[i].cost;
     }
-    for(int i = 1; i < (1 << n); i++){ // 재료없는 경우는 없으니깐
-        b = c = d = e = sum = 0;
-        vector<int> v;
+    for(int i = 1; i < (1 << n); i++){ // 재료없는 경우는 제외 0001부터 시작
+        p = f = s = v = c = 0;
+        vector<int> vv;
         for(int j = 0; j < n; j++){
             if(i & (1 << j)){
-                v.push_back(j + 1);
-                b += a[j].mp;
-                c += a[j].mf;
-                d += a[j].ms;
-                e += a[j].mv;
-                sum += a[j].cost;
+                vv.push_back(j + 1);
+                p += a[j].mp;
+                f += a[j].mf;
+                s += a[j].ms;
+                v += a[j].mv;
+                c += a[j].cost;
             }
         }
-        if(b >= mp && c >= mf && d >= ms && e >= mv){
-            if(ret >= sum){
-                ret = sum;
-                ret_v[ret].push_back(v); //ret가격 키에 벡터 저장
-            }
+        if(c <= ret && p >= mp && f >= mf && s >= ms && v >= mv){
+            ret = c;
+            mp1[c].push_back(vv); // ret키에 벡터 저장
         }
     }
     if(ret == INF) cout << -1 << '\n';
     else{
-        sort(ret_v[ret].begin(), ret_v[ret].end()); //오름차순 정렬
-        cout << ret << '\n'; // 가격 출력
-        for(int a : ret_v[ret][0]){ // ret가격 값중 가장 첫 번째
-            cout << a << " ";
-        }
+        sort(mp1[ret].begin(), mp1[ret].end()); //오름차순 정렬
+        cout << ret << '\n'; 
+        for(int i : mp1[ret][0]) cout << i << ' '; // ret값중 사전순 첫 번째
     }
     return 0;
 }
+//맵
 /*
 비트 연산자 이용한 반복 돌리기 부분 잘 모름 -> j이용하기 -> 있다없다 정해주니깐
+
+Q1. 맵은 자동 오름차순 정렬 아닌가??
 
 */
--- 4주차/4-B.동전뒤집기.cpp
+++ 4주차/4-B.동전뒤집기.cpp
@@ -1,39 +1,34 @@
 #include<bits/stdc++.h>
 using namespace std;
-const int INF = 1e9;
-int n, ret = INF, a[44];
-string s;
-// 재귀를 통해 뒤집기 여부 경우의 수 나누고, 기저사례에서 열의 뒤집기 실행
-void go(int here){ 
-    if(here == n + 1){// 모든 행을 확인한 경우
+int n, ret = 1e9, a[44]; string s;
+// 동전 뒤집기 + 재뒤집 표현
+void go(int here){
+    if(here == n + 1){
         int sum = 0;
-        for(int i  = 1; i <= (1 << (n - 1)); i *= 2){ // 비트마스킹을 된 각 행을 표현 함
-            int cnt = 0; //동전의 앞면 갯수 카운팅
-            for(int j = 1; j <= n; j++){ 
-                if(a[j] & i) cnt++; // i번째 경우에서 앞면인 동전의 개수를 세서 cnt에 저장
+        for(int i = 1; i <= (1 << (n - 1)); i *= 2){ // 행
+            int cnt = 0; // 뒷면의 갯수 합 저장
+            for(int j = 0; j < n; j++){ // 열
+                if(a[j] & i) cnt++; // 뒷면 동전체크
             }
-            sum += min(cnt, n - cnt); // 앞뒤면중 작은수를 선택하므로 뒤집는 행위랑 같음
+            sum += min(cnt, n - cnt); // 앞과 뒤중 작은 수 선택
         }
-        ret = min(ret, sum); // 가능한 모든 경우에서 뒷면인 동전의 개수를 최소화한 값을 ret에 저장
+        ret = min(ret, sum); // 뒷면 동전 최소화한 값
         return;
     }
-    go(here + 1); // 재귀 홍출
-    a[here] = ~a[here]; // 뒤집고
-    go(here + 1);  //호출 
+    go(here + 1); // 재귀 호출
+    a[here] = ~a[here]; // 뒤집기
+    go(here + 1); // 뒤집은 걸로 들어가기
 }
-
 int main(){
     ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
     cin >> n;
-    for(int i = 1; i <= n; i++){ //a배열 1부터 배열설정
-        cin >> s; // 한 줄의 문자열을 입력 받습니다
-        int value = 1; // 각 동전의 앞면(T)에 해당하는 비트값을 의미합니다.
-        for(int j = 0; j < n; j++){// 각 자리에서 T를 1로 취급하고 넣어줌
-            if(s[j] == 'T') a[i] |= value; // 문자열에서 'T'가 나오면, 해당 비트를 1로 설정합니다.
-            value *= 2; // 다음 동전의 비트값을 계산하기 위해 2배씩 증가시킵니다.
+    for(int i = 0; i < n; i++){
+        cin >> s;
+        for(int j = 0; j < n; j++){
+            if(s[j] == 'T') a[i] += (1 << j);
         }
     }
-    go(1); //왜 1??
+    go(0);
     cout << ret << '\n';
 }
 /*
