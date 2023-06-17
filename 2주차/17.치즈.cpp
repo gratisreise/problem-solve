37f705877ea91a3027b22e1058394f062f85d175
gratisreise <104671487+gratisreise@users.noreply.github.com>
2023-06-17 15:21:57 +0900

    2023.06.17

--- 2주차/17.치즈.cpp
+++ 2주차/17.치즈.cpp
@@ -1,52 +1,46 @@
-#include <bits/stdc++.h>
-using namespace std; 
-int n,m,cnt,cnt2;
-int a[104][104], visited[104][104];
-int dy[] = {-1, 0, 1, 0};
-int dx[] = {0, 1, 0, -1};    
-vector <pair<int, int>> v; //녹아야할 치즈를 담는다
+#include<bits/stdc++.h>
+using namespace std;
+int n, m, cnt, cnt2, a[104][104], visited[104][104];
+const int dy[] = {-1, 0, 1, 0}, dx[] = {0, 1, 0, -1};
+vector<pair<int, int>> v;
 void go(int y, int x){
-	visited[y][x] = 1;
-    if(a[y][x] == 1){ //치즈양반 딱 걸렸수다!!
-        v.push_back({y, x});
-        return;
-    }
-    for(int i=0; i<4; i++){
+    visited[y][x] = 1;
+    if(a[y][x] == 1){v.push_back({y, x}); return;}
+    for(int i = 0; i < 4; i++){
         int ny = y + dy[i];
         int nx = x + dx[i];
-        if(ny < 0 || ny >= n || nx < 0 || nx >= m || visited[ny][nx])continue; 
+        if(ny < 0 || nx < 0 || ny >= n || nx >= m || visited[ny][nx]) continue;
         go(ny, nx);
     }
-    return;
 }
-
-
-int main(){ 
+int main(){
     cin >> n >> m;
     for(int i = 0; i < n; i++){
-        for(int j = 0; j < m; j++){
+        for(int j = 0 ; j < m; j++){
             cin >> a[i][j];
         }
     }
-    while(true){
+    while(1){
         cnt2 = 0;
-        fill(&visited[0][0], &visited[0][0] + 104 * 104,0);
-        v.clear(); 
-        go(0, 0); 
-        for(pair<int, int> b : v){
-			cnt2++; // 녹아지는 순간을 체크
-			a[b.first][b.second] = 0;
-		}   
-        bool flag = 0; // 치즈 있으면 1 없으면 0
+        memset(visited, 0, sizeof(visited));
+        v.clear();
+        go(0, 0);
+        // 마지막은 녹이는 수가 치즈 갯수
+        for(auto i : v){
+            cnt2++;
+            a[i.first][i.second] = 0;
+        }
+        bool flag = 0;
+        // 치즈있나 체크
         for(int i = 0; i < n; i++){
             for(int j = 0; j < m; j++){
                 if(a[i][j] != 0) flag = 1;
             }
         }
-        cnt++;
+        cnt++; // 시간체크
         if(!flag) break;
     }
-    cout << cnt << "\n" << cnt2 << '\n'; 
+    cout << cnt << '\n' << cnt2 << '\n';
 }
 
 #include<bits/stdc++.h>
--- 4주차/4-O.현욱.cpp
+++ 4주차/4-O.현욱.cpp
@@ -1,6 +1,5 @@
 #include<bits/stdc++.h>
 using namespace std;
-
 int n, d[200001], ret, cnt; 
 string s;
 stack<int> stk;
@@ -21,4 +20,29 @@ int main() {
 		}else cnt = 0;  
 	}
 	cout << ret << "\n"; 
+}
+
+<stack>
+#include<bits/stdc++.h>
+using namespace std;
+int n, cnt, ret;
+string str;
+stack<int> s;
+int main(){
+    cin >> n;
+    cin >> str;
+    s.push(-1); //숫자 맞춰주기
+    for(int i = 0; i < n; i++){
+        if(str[i] == '(') s.push(i);
+        if(str[i] == ')'){
+            s.pop();
+            if(!s.empty()){
+                ret = max(ret, i - s.top());
+            }else{
+                s.push(i); // 분기점 사용 max 맞춰주기
+            }
+        }
+    }
+    cout << ret << '\n';
+    return 0;
 }
