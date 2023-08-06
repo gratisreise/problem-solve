80c85ac989c58b819d6366a936c43b8457459dbd
Noah <104671487+gratisreise@users.noreply.github.com>
2023-08-06 10:21:02 +0900

    [Bronze II] Title: 단어의 개수, Time: 340 ms, Memory: 28760 KB -BaekjoonHub

---
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        sc.close();
+
+        StringTokenizer st = new StringTokenizer(s, " ");
+        System.out.println(st.countTokens());
+    }
+}
