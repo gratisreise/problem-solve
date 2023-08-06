8add06a2d3513ca9cc84c634f1067ff6c4c7463e
Noah <104671487+gratisreise@users.noreply.github.com>
2023-08-06 11:14:41 +0900

    [Bronze I] Title: 단어 공부, Time: 304 ms, Memory: 24236 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.java
+++ 백준/Bronze/1157. 단어 공부/단어 공부.java
@@ -3,28 +3,26 @@ import java.util.*;
 public class Main{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-
         int[] cnt = new int[26];
         String s = sc.next();
-        
+
         for(int i = 0; i < s.length(); i++){
             if(s.charAt(i) <= 'Z') cnt[s.charAt(i) - 'A']++;
             else cnt[s.charAt(i) - 'a']++;
         }
-
-        int max = -1; 
+        
+        int max = -1;
         char ch = '?';
 
         for(int i = 0; i < 26; i++){
             if(cnt[i] > max){
                 max = cnt[i];
                 ch = (char)(i + 65);
-            }
-            else if(cnt[i] == max){
+            }else if(cnt[i] == max){
                 ch = '?';
             }
         }
-        System.out.println(ch);
         
+        System.out.println(ch);
     }
 }
