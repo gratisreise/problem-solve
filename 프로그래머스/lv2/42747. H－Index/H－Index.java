ba46ea7248919c265078ac23122f9251ab11b7d5
Noah <104671487+gratisreise@users.noreply.github.com>
2023-08-08 10:10:44 +0900

    [level 2] Title: H-Index, Time: 0.36 ms, Memory: 82.6 MB -BaekjoonHub

--- 프로그래머스/lv2/42747. H－Index/H－Index.java
+++ 프로그래머스/lv2/42747. H－Index/H－Index.java
@@ -1,14 +1,16 @@
 import java.util.*;
 class Solution {
-    int ret;
     public int solution(int[] citations) {
+        int ret = 0;
         Arrays.sort(citations);
+        
         for(int i = 0; i < citations.length; i++){
-            if(citations.length - i <= citations[i] ) {
+            if(citations[i] >= citations.length - i){
                 ret = citations.length - i;
                 break;
             }
         }
+        
         return ret;
     }
 }
--- 프로그래머스/lv2/42747. H－Index/README.md
+++ 프로그래머스/lv2/42747. H－Index/README.md
@@ -1,10 +1,10 @@
 # [level 2] H-Index - 42747 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42747) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42747?language=java) 
 
 ### 성능 요약
 
-메모리: 74.9 MB, 시간: 0.32 ms
+메모리: 82.6 MB, 시간: 0.36 ms
 
 ### 구분
 
