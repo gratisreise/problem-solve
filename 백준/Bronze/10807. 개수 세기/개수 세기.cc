86a191abd983c1e81168d06b0451bf2b669d2187
Noah <104671487+gratisreise@users.noreply.github.com>
2023-08-31 22:46:44 +0900

    [Bronze V] Title: 개수 세기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10807. 개수 세기/개수 세기.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+int n,v;
+int arr[101];
+int main(){
+    int cnt = 0;
+    cin>>n;
+    for(int i =0;i<n;i++){
+        cin>>arr[i];
+    }
+    cin>>v;
+    for(int i=0;i<n;i++){
+        if(arr[i]==v) cnt++;
+    }
+    cout<<cnt<<"\n";
+}
