59a0bb57cdea8c6d3dc9e61f472abfb0151719de
Noah <104671487+gratisreise@users.noreply.github.com>
2023-09-11 11:04:16 +0900

    [Silver I] Title: 1로 만들기 2, Time: 4 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Silver/12852. 1로 만들기 2/1로 만들기 2.cc
+++ 백준/Silver/12852. 1로 만들기 2/1로 만들기 2.cc
@@ -2,29 +2,29 @@
 using namespace std;
 int dp[1000004], n;
 const int INF = 1e9;
-void go(int here){
-    if(here == 0) return;
-    printf("%d ", here);
-    if(here % 3 == 0 && dp[here] == (dp[here / 3] + 1)) 
-        go(here / 3);
-    else if(here % 2 == 0 && dp[here] == (dp[here / 2] + 1)) 
-        go(here / 2);
-    else if((here - 1 >= 0) && (dp[here] == (dp[here - 1] + 1))) 
-        go(here - 1);
+void go(int n){
+    if(n == 0) return;
+    cout << n << ' ';
+    if(n % 3 == 0 && dp[n] == (dp[n / 3] + 1))
+        go(n / 3);
+    else if(n % 2 == 0 && dp[n] == (dp[n / 2] + 1))
+        go(n / 2);
+    else if(n - 1 >= 0 && dp[n] == (dp[n - 1] + 1))
+        go(n - 1);
     return;
 }
 int main(){
-    scanf("%d", &n);
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    cin >> n;
     fill(dp, dp + 1000004, INF);
     dp[1] = 0;
     for(int i = 1; i <= n; i++){
-        if(!(i % 3)) 
-            dp[i] = min(dp[i / 3] + 1, dp[i]);
-        if(!(i % 2)) 
-            dp[i] = min(dp[i / 2] + 1, dp[i]);
+        if(!(i % 3)) dp[i] = min(dp[i / 3] + 1, dp[i]);
+        if(!(i % 2)) dp[i] = min(dp[i / 2] + 1, dp[i]);
         dp[i] = min(dp[i - 1] + 1, dp[i]);
     }
-    printf("%d\n", dp[n]);
+    cout << dp[n] << '\n';
     go(n);
     return 0;
 }
