81366c46d469598f6522ec8e5eef16faf2811a51
Noah <104671487+gratisreise@users.noreply.github.com>
2023-09-25 11:24:42 +0900

    [D3] Title: 조별과제, Time: 104 ms, Memory: 13,544 KB -BaekjoonHub

---
+++ SWEA/D3/13218. 조별과제/조별과제.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int t, n;
+int main(){
+    cin >> t;
+    for(int i = 1; i <= t; i++){
+        cin >> n;   
+        cout <<"#"<< i<< ' ' << n / 3 << '\n';
+
+    }
+}
