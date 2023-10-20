af7b669ad113b2534c587a6352485e21cb646e77
Noah <104671487+gratisreise@users.noreply.github.com>
2023-10-21 08:52:50 +0900

    [Bronze V] Title: 아스키 코드, Time: 104 ms, Memory: 12796 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        char c = sc.nextLine().charAt(0);
+        System.out.println((int)c);
+    }
+}
