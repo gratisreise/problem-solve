279ddb094c35f151e2555c22632a8d3e7e6ab5ba
Noah <104671487+gratisreise@users.noreply.github.com>
2023-10-20 11:10:35 +0900

    [Bronze V] Title: 사파리월드, Time: 108 ms, Memory: 12824 KB -BaekjoonHub

---
+++ 백준/Bronze/2420. 사파리월드/사파리월드.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        long n = sc.nextLong();
+        long m = sc.nextLong();
+        System.out.println(Math.abs(n - m));
+    }
+}
