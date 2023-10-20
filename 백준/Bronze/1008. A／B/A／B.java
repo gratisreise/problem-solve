78f3555d9f880435dd43f40d9e5fa98a2b8635c6
Noah <104671487+gratisreise@users.noreply.github.com>
2023-10-20 10:45:03 +0900

    [Bronze V] Title: A/B, Time: 124 ms, Memory: 14576 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        double a = sc.nextDouble(), b = sc.nextDouble();          
+        System.out.println(a / b);               
+    }
+}
