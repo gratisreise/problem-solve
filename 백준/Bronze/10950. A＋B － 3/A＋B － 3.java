dff4dc9f4d1f58dae6819854bb1f7c8fba4bfb18
Noah <104671487+gratisreise@users.noreply.github.com>
2023-10-20 15:09:55 +0900

    [Bronze V] Title: A+B - 3, Time: 132 ms, Memory: 13076 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        for(int i = 0; i < t; i++){
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            System.out.println(a + b);
+        }
+    }
+}
