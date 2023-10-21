0058aafec149f2510a942aa1269913e1ad02c2ed
Noah <104671487+gratisreise@users.noreply.github.com>
2023-10-21 09:15:56 +0900

    [Bronze V] Title: 그대로 출력하기, Time: 128 ms, Memory: 13888 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = 0;
+        while(sc.hasNextLine()){
+            String s = sc.nextLine();
+            System.out.println(s);
+        }
+    }
+}
