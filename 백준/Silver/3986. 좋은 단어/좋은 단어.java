665a6b523521255e3ffbd79d3887975f255a62a7
Noah <104671487+gratisreise@users.noreply.github.com>
2023-10-23 10:54:24 +0900

    [Silver IV] Title: 좋은 단어, Time: 404 ms, Memory: 26248 KB -BaekjoonHub

---
+++ 백준/Silver/3986. 좋은 단어/좋은 단어.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class Main {
+    static int ret;
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        
+        scanner.nextLine(); // Consume the newline character
+
+        for (int i = 0; i < n; i++) {
+            String s = scanner.nextLine();
+            Stack<Character> stack = new Stack<>();
+
+            for (char c : s.toCharArray()) {
+                if (!stack.isEmpty() && stack.peek() == c) {
+                    stack.pop();
+                } else {
+                    stack.push(c);
+                }
+            }
+            
+            if (stack.isEmpty()) {
+                ret++;
+            }
+        }
+
+        System.out.println(ret);
+    }
+}
