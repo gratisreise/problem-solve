3b133ba3423c17f6d31317944587e3827d90e639
Noah <104671487+gratisreise@users.noreply.github.com>
2023-11-02 13:19:52 +0900

    [Silver III] Title: 교수가 된 현우, Time: 2692 ms, Memory: 103752 KB -BaekjoonHub

--- 백준/Silver/3474. 교수가 된 현우/교수가 된 현우.java
+++ 백준/Silver/3474. 교수가 된 현우/교수가 된 현우.java
@@ -1,19 +1,18 @@
 import java.util.Scanner;
 
-public class Main {
-    public static void main(String[] args) {
+public class Main{
+    public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();
-        
-        for (int i = 0; i < t; i++) {
+
+        for(int i = 0; i < t; i++){
             int ret2 = 0, ret5 = 0;
             int n = sc.nextInt();
-            
-            for (int j = 5; j <= n; j *= 5) {
+
+            for(int j = 5; j <= n; j *= 5){
                 ret5 += n / j;
             }
-            
-            for (int j = 2; j <= n; j *= 2) {
+            for(int j = 2; j <= n; j *= 2){
                 ret2 += n / j;
             }
             
