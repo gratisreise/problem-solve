a519f92e5fb525e37cbe56c74917df2242cde3c1
Noah <104671487+gratisreise@users.noreply.github.com>
2023-11-03 12:57:05 +0900

    [Gold IV] Title: 오큰수, Time: 1012 ms, Memory: 203224 KB -BaekjoonHub

---
+++ 백준/Gold/17298. 오큰수/오큰수.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] A = new int[N];
+        int[] answer = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < N; i++) {
+            while (!stack.isEmpty() && A[i] > A[stack.peek()]) {
+                answer[stack.pop()] = A[i];
+            }
+            stack.push(i);
+        }
+
+        while (!stack.isEmpty()) {
+            answer[stack.pop()] = -1;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < N; i++) {
+            sb.append(answer[i]).append(" ");
+        }
+
+        System.out.println(sb.toString());
+    }
+}
