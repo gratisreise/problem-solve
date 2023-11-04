0d89926a2680450530e41a6234981ab906f2bc4d
Noah <104671487+gratisreise@users.noreply.github.com>
2023-11-04 21:02:22 +0900

    [Gold V] Title: 보물섬, Time: 528 ms, Memory: 293804 KB -BaekjoonHub

--- 백준/Gold/2589. 보물섬/README.md
+++ 백준/Gold/2589. 보물섬/README.md
@@ -4,21 +4,25 @@
 
 ### 성능 요약
 
-메모리: 2036 KB, 시간: 140 ms
+메모리: 293804 KB, 시간: 528 ms
 
 ### 분류
 
 너비 우선 탐색, 브루트포스 알고리즘, 그래프 이론, 그래프 탐색
 
+### 제출 일자
+
+2023년 11월 4일 21:02:06
+
 ### 문제 설명
 
 <p>보물섬 지도를 발견한 후크 선장은 보물을 찾아나섰다. 보물섬 지도는 아래 그림과 같이 직사각형 모양이며 여러 칸으로 나뉘어져 있다. 각 칸은 육지(L)나 바다(W)로 표시되어 있다. 이 지도에서 이동은 상하좌우로 이웃한 육지로만 가능하며, 한 칸 이동하는데 한 시간이 걸린다. 보물은 서로 간에 최단 거리로 이동하는데 있어 가장 긴 시간이 걸리는 육지 두 곳에 나뉘어 묻혀있다. 육지를 나타내는 두 곳 사이를 최단 거리로 이동하려면 같은 곳을 두 번 이상 지나가거나, 멀리 돌아가서는 안 된다.</p>
 
-<p style="text-align: center;"><img alt="" src="" style="width: 238px; height: 144px; "></p>
+<p style="text-align: center;"><img alt="" src="https://www.acmicpc.net/upload/images/c1bYIsKpI6m317EAx.jpg" style="width: 238px; height: 144px; "></p>
 
 <p>예를 들어 위와 같이 지도가 주어졌다면 보물은 아래 표시된 두 곳에 묻혀 있게 되고, 이 둘 사이의 최단 거리로 이동하는 시간은 8시간이 된다.</p>
 
-<p style="text-align: center;"><img alt="" src="" style="width: 238px; height: 144px; "></p>
+<p style="text-align: center;"><img alt="" src="https://www.acmicpc.net/upload/images/XqDkWCRUWbzZ.jpg" style="width: 238px; height: 144px; "></p>
 
 <p>보물 지도가 주어질 때, 보물이 묻혀 있는 두 곳 간의 최단 거리로 이동하는 시간을 구하는 프로그램을 작성하시오.</p>
 
---
+++ 백준/Gold/2589. 보물섬/보물섬.java
@@ -0,0 +1,72 @@
+import java.util.*;
+
+public class Main {
+    static int n, m, ret;
+    static int[][] visited;
+    static char[][] a;
+    static List<Pair> L;
+    static final int[] dy = {-1, 0, 1, 0};
+    static final int[] dx = {0, 1, 0, -1};
+
+
+    static class Pair {
+        int f, s;
+
+        Pair(int f, int s) {
+            this.f = f;
+            this.s = s;
+        }
+    }
+
+    public static void bfs(int y, int x) {
+        Queue<Pair> q = new LinkedList<>();
+        q.add(new Pair(y, x));
+        visited[y][x] = 1;
+
+        while (!q.isEmpty()) {
+            Pair p = q.poll();
+            y = p.f;
+            x = p.s;
+
+            for (int i = 0; i < 4; i++) {
+                int ny = y + dy[i];
+                int nx = x + dx[i];
+
+                if (ny < 0 || nx < 0 || ny >= n || nx >= m) continue;
+                if (a[ny][nx] == 'W' || visited[ny][nx] != 0) continue;
+
+                visited[ny][nx] = visited[y][x] + 1;
+                ret = Math.max(ret, visited[ny][nx]);
+                q.add(new Pair(ny, nx));
+            }
+        }
+    }
+
+    public static void solve() {
+        for (Pair it : L) {
+            visited = new int[n + 1][m + 1];
+            bfs(it.f, it.s);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        m = sc.nextInt();
+        a = new char[n + 1][m + 1];
+        visited = new int[n + 1][m + 1];
+        L = new ArrayList<>();
+        ret = 0;
+
+        for (int i = 0; i < n; i++) {
+            String s = sc.next();
+            for (int j = 0; j < m; j++) {
+                a[i][j] = s.charAt(j);
+                if (a[i][j] == 'L') L.add(new Pair(i, j));
+            }
+        }
+
+        solve();
+        System.out.println(ret - 1);
+    }
+}
