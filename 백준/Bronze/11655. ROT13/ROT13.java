4680fe8c5c0679b4eb6e1a08e286cb6359b8f874
Noah <104671487+gratisreise@users.noreply.github.com>
2023-11-06 12:29:52 +0900

    [Bronze I] Title: ROT13, Time: 104 ms, Memory: 12808 KB -BaekjoonHub

--- 백준/Bronze/11655. ROT13/ROT13.java
+++ 백준/Bronze/11655. ROT13/ROT13.java
@@ -1,30 +1,26 @@
 import java.util.*;
 
-public class Main {
-    public static void main(String[] args) {
+public class Main{
+
+    public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         String s = sc.nextLine();
         StringBuilder ret = new StringBuilder();
-
-        for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-            if (Character.isUpperCase(c)) { // 대문자이면
-                if (c + 13 > 'Z') {
-                    c = (char) (c - 13);
+        for(int i = 0; i < s.length(); i++){
+            if('a' <= s.charAt(i) && s.charAt(i) <= 'z'){
+                if(s.charAt(i) + 13 > 'z'){
+                    ret.append((char)(s.charAt(i) - 13));
                 } else {
-                    c = (char) (c + 13);
+                    ret.append((char)(s.charAt(i) + 13));
                 }
-            } else if (Character.isLowerCase(c)) { // 소문자이면
-                if (c + 13 > 'z') { //범위 벗어나면 -13
-                    c = (char) (c - 13);
-                } else { // 범위 안 벗어나면 그냥 +13
-                    c = (char) (c + 13);
+            } else if('A' <= s.charAt(i) && s.charAt(i) <= 'Z'){
+                if(s.charAt(i) + 13 > 'Z'){
+                    ret.append((char)(s.charAt(i) - 13));
+                } else {
+                    ret.append((char)(s.charAt(i) + 13));
                 }
-            }
-            // 바뀐 문자 ret객체에 투입
-            ret.append(c);
+            } else ret.append(s.charAt(i));
         }
-
         System.out.println(ret);
     }
 }
