a5d24671961fc72cf0743d1b7d71bd0fbe96c41e
Noah <104671487+gratisreise@users.noreply.github.com>
2023-11-06 12:19:44 +0900

    [Bronze II] Title: 농구 경기, Time: 120 ms, Memory: 12956 KB -BaekjoonHub

--- 백준/Bronze/1159. 농구 경기/농구 경기.java
+++ 백준/Bronze/1159. 농구 경기/농구 경기.java
@@ -1,27 +1,19 @@
 import java.util.*;
-
-public class Main {
-    public static int[] cnt = new int[26];
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
+public class Main{
+    static int n;
+    static int[] cnt = new int[26];
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
         StringBuilder ret = new StringBuilder();
-
-        for (int i = 0; i < n; i++) {
-            String s = scanner.next();
+        n = sc.nextInt();
+        for(int i = 0; i < n; i++){
+            String s = sc.next();
             cnt[s.charAt(0) - 'a']++;
         }
-
-        for (int i = 0; i < 26; i++) {
-            if (cnt[i] >= 5) {
-                ret.append((char)(i + 'a'));
-            }
+        for(int i = 0; i < 26; i++){
+            if(cnt[i] >= 5) ret.append((char)(i + 'a'));
         }
-
-        if (ret.length() > 0) {
-            System.out.println(ret);
-        } else {
-            System.out.println("PREDAJA");
-        }
+        if(ret.length() > 0) System.out.println(ret);
+        else System.out.println("PREDAJA");
     }
 }
