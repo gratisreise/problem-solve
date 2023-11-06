ade0bcf4c25aac5bce2746befc7b7f7d4be82977
Noah <104671487+gratisreise@users.noreply.github.com>
2023-11-06 11:44:15 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 108 ms, Memory: 12844 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
@@ -1,21 +1,18 @@
 import java.util.*;
-
 public class Main{
-    public static int a[] = new int[9];
-    public static int b, c;
+    static int[] a = new int[9];
+    static int b, c;
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int sum = 0;
-
         for(int i = 0; i < 9; i++){
             a[i] = sc.nextInt();
             sum += a[i];
         }
-
         for(int i = 0; i < 9; i++){
-            for(int j = 0; j< 9; j++){
+            for(int j = 0; j < i; j++){
                 if(sum - a[i] - a[j] == 100){
-                    b = a[i]; c = a[j]; break;
+                    b = a[i]; c = a[j];
                 }
             }
         }
@@ -25,4 +22,5 @@ public class Main{
             System.out.println(a[i]);
         }
     }
+
 }
