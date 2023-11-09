dcdd667dc77367024379dc13b88199b654a7b108
Noah <104671487+gratisreise@users.noreply.github.com>
2023-11-09 10:44:36 +0900

    [Gold V] Title: 보물섬, Time: 484 ms, Memory: 294172 KB -BaekjoonHub

--- 백준/Gold/2589. 보물섬/보물섬.java
+++ 백준/Gold/2589. 보물섬/보물섬.java
@@ -1,72 +1,66 @@
 import java.util.*;
 
-public class Main {
-    static int n, m, ret = 0;
-    static int[][] visited;
+public class Main{
+
+    static int n, m, ret = Integer.MIN_VALUE;
     static char[][] a;
-    static List<Pair> L;
-    static final int[] dy = {-1, 0, 1, 0};
-    static final int[] dx = {0, 1, 0, -1};
-
-
-    static class Pair {
+    static int visited[][];
+    static int[] dy = {-1, 0, 1, 0};
+    static int[] dx = {0, 1, 0, -1};
+    static List<Pair> L = new ArrayList<>();
+    static class Pair{
         int f, s;
-
-        Pair(int f, int s) {
+        public Pair(int f, int s){
             this.f = f;
             this.s = s;
         }
     }
+    static void solve(){
 
-    public static void bfs(int y, int x) {
+        for(Pair p : L){
+            visited = new int[n + 4][m + 4];
+            bfs(p.f, p.s);
+        }
+    }
+    static void bfs(int y, int x){
+        visited[y][x] = 1;
         Queue<Pair> q = new LinkedList<>();
         q.add(new Pair(y, x));
-        visited[y][x] = 1;
-
-        while (!q.isEmpty()) {
+        while(!q.isEmpty()){
             Pair p = q.poll();
-            y = p.f;
-            x = p.s;
-
-            for (int i = 0; i < 4; i++) {
-                int ny = y + dy[i];
-                int nx = x + dx[i];
-
-                if (ny < 0 || nx < 0 || ny >= n || nx >= m) continue;
-                if (a[ny][nx] == 'W' || visited[ny][nx] != 0) continue;
-
-                visited[ny][nx] = visited[y][x] + 1;
-                ret = Math.max(ret, visited[ny][nx]);
+            int f = p.f;
+            int s = p.s;
+            for(int i = 0; i < 4; i++){
+                int ny = f + dy[i];
+                int nx = s + dx[i];
+                if(ny < 0 || nx < 0 || ny >= n || nx >= m) continue;
+                if(a[ny][nx] == 'W' || visited[ny][nx] != 0) continue;
+                visited[ny][nx] = visited[f][s] + 1;
+                ret = Math.max(ret, visited[ny][nx] - 1);
                 q.add(new Pair(ny, nx));
             }
         }
     }
-
-    public static void solve() {
-        for (Pair it : L) {
-            visited = new int[n + 1][m + 1];
-            bfs(it.f, it.s);
-        }
-    }
-
-    public static void main(String[] args) {
+    public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         n = sc.nextInt();
         m = sc.nextInt();
-        a = new char[n + 1][m + 1];
-        visited = new int[n + 1][m + 1];
-        L = new ArrayList<>();
-
-
-        for (int i = 0; i < n; i++) {
+        a = new char[n + 4][m + 4];
+        for(int i = 0; i < n; i++){
             String s = sc.next();
-            for (int j = 0; j < m; j++) {
+            for(int j = 0; j < m; j++){
                 a[i][j] = s.charAt(j);
-                if (a[i][j] == 'L') L.add(new Pair(i, j));
+                if(a[i][j] == 'L') L.add(new Pair(i, j));
             }
         }
-
         solve();
-        System.out.println(ret - 1);
+        System.out.println(ret);
     }
 }
+
+/**
+ * 육지, 바다 저장
+ * 최단거리(bfs) 로직
+ * 지도의 최단거리중 max 판단 == ret
+ * 육지 좌표의 시작점마다 bfs최대 비교
+ */
