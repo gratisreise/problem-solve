8fefeceafa176052cbeb241c5236368f6e0b3a58
Noah <104671487+gratisreise@users.noreply.github.com>
2023-11-10 14:15:41 +0900

    [Gold III] Title: 괄호 추가하기, Time: 108 ms, Memory: 12880 KB -BaekjoonHub

--- 백준/Gold/16637. 괄호 추가하기/괄호 추가하기.java
+++ 백준/Gold/16637. 괄호 추가하기/괄호 추가하기.java
@@ -1,50 +1,40 @@
-import java.util.Scanner;
-
-public class Main {
-    static int n;
-    static int ret = Integer.MIN_VALUE;
+import java.util.*;
+public class Main{
+    static int n, ret = Integer.MIN_VALUE;
     static String s;
     static int[] num;
     static char[] operStr;
 
-    static int oper(char a, int b, int c) {
+    static int oper(char a, int b, int c){
         int result = 0;
-        if (a == '+') result = b + c;
-        if (a == '*') result = b * c;
-        if (a == '-') result = b - c;
+        if(a == '+') result = b + c;
+        if(a == '*') result = b * c;
+        if(a == '-') result = b - c;
         return result;
     }
-
-    static void go(int here, int _num) {
-        if (here == num.length - 1) {
+    static void go(int here, int _num){
+        if(here == operStr.length){
             ret = Math.max(ret, _num);
             return;
         }
         go(here + 1, oper(operStr[here], _num, num[here + 1]));
-        if (here + 2 <= num.length - 1) {
+        if(here + 2 <= operStr.length){
             int temp = oper(operStr[here + 1], num[here + 1], num[here + 2]);
-            go(here + 2, oper(operStr[here], _num, temp));
+            go(here + 2, oper(operStr[here],_num, temp));
         }
     }
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        n = scanner.nextInt();
-        s = scanner.next();
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        s = sc.next();
 
-        num = new int[s.length() / 2 + 1];
+        num = new int[s.length()/2 + 1];
         operStr = new char[s.length() / 2];
-
-        for (int i = 0, j = 0, k = 0; i < s.length(); i++) {
-            if (i % 2 == 0) {
-                num[j] = s.charAt(i) - '0';
-                j++;
-            } else {
-                operStr[k] = s.charAt(i);
-                k++;
-            }
+        for(int i = 0, j = 0, k = 0; i < s.length(); i++){
+            if(i % 2 == 0){num[j] = s.charAt(i) - '0'; j++;}
+            else {operStr[k] = s.charAt(i); k++;}
         }
-
         go(0, num[0]);
         System.out.println(ret);
     }
