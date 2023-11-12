505f126aea35e533b06265774d013f10f3c8dadd
Noah <104671487+gratisreise@users.noreply.github.com>
2023-11-12 15:13:27 +0900

    [Silver IV] Title: 주몽, Time: 492 ms, Memory: 30880 KB -BaekjoonHub

--- 백준/Silver/1940. 주몽/주몽.java
+++ 백준/Silver/1940. 주몽/주몽.java
@@ -1,30 +1,21 @@
 import java.util.*;
 
 public class Main{
-    static int[] a = new int[100004];
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int m = sc.nextInt();
-
-        for(int i = 0; i < n; i++){
-            a[i] = sc.nextInt();
-        }
-
-        if(m > 200000){
-            System.out.println(0);
-            return;
-        }
-
+        int[] a = new int[15004];
+        for(int i = 0; i < n; i++) a[i] = sc.nextInt();
         int ret = 0;
-        
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < i; j++){
-                if(a[i] + a[j] == m){
-                    ret++;
+        if(m > 200000) System.out.println(ret);
+        else{
+            for(int i = 0; i < n; i++){
+                for(int j = 0; j < i; j++){
+                    if(a[i] + a[j] == m) ret++;
                 }
             }
+            System.out.println(ret);
         }
-        System.out.println(ret);
     }
 }
