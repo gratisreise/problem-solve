f632f4a7df0430e1cbc4de2d545b53307c575979
Noah <104671487+gratisreise@users.noreply.github.com>
2023-11-12 14:56:51 +0900

    [Silver III] Title: 패션왕 신해빈, Time: 144 ms, Memory: 13776 KB -BaekjoonHub

--- 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.java
+++ 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.java
@@ -1,24 +1,20 @@
 import java.util.*;
 
 public class Main{
-
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();
-        while(t-- > 0){
-            int n = sc.nextInt();
+        int n = sc.nextInt();
+        for(int i = 0; i < n; i++){
+            int m = sc.nextInt();
             Map<String, Integer> mp = new HashMap<>();
-            for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
                 String s = sc.next();
-                String ss =sc.next();
-                if(mp.containsKey(ss)){
-                    mp.put(ss, mp.get(ss) + 1);
-                } else mp.put(ss, 1);
+                String ss = sc.next();
+                if(!mp.containsKey(ss)) mp.put(ss, 1);
+                else mp.put(ss, mp.get(ss)+1);
             }
             long ret = 1;
-            for(Map.Entry<String, Integer> e : mp.entrySet()){
-                ret *= (e.getValue() + 1);
-            }
+            for(String key : mp.keySet()) ret *= (mp.get(key)+1);
             ret--;
             System.out.println(ret);
         }
