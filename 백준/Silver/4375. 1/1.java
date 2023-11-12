39a24572c23e0b099127811dd101ca0acad4d2f3
Noah <104671487+gratisreise@users.noreply.github.com>
2023-11-12 15:35:56 +0900

    [Silver III] Title: 1, Time: 108 ms, Memory: 12832 KB -BaekjoonHub

--- 백준/Silver/4375. 1/1.java
+++ 백준/Silver/4375. 1/1.java
@@ -1,22 +1,22 @@
 import java.util.*;
 
 public class Main{
-    public static void main(String[] args){
-           Scanner sc = new Scanner(System.in);
 
-           while(sc.hasNextInt()){
-               int n = sc.nextInt();
-               int cnt = 1, ret = 1;
-               while(true){
-                   if(cnt % n == 0){
-                       System.out.println(ret);
-                       break;
-                   } else {
-                       cnt = (cnt * 10) + 1;
-                       cnt %= n;
-                       ret++;
-                   }
-               }
-           }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        while(sc.hasNextInt()){
+            int n = sc.nextInt();
+            int cnt = 1, ret = 1;
+            while(true){
+                if(cnt % n == 0){
+                    System.out.println(ret);
+                    break;
+                } else {
+                    cnt = cnt * 10 + 1;
+                    cnt %= n;
+                    ret++;
+                }
+            }
+        }
     }
 }
