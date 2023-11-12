de0a5926a25f78a9f87270216aaf41500b7f0545
Noah <104671487+gratisreise@users.noreply.github.com>
2023-11-12 13:40:49 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 104 ms, Memory: 12784 KB -BaekjoonHub

--- 백준/Bronze/10808. 알파벳 개수/알파벳 개수.java
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.java
@@ -1,14 +1,13 @@
 import java.util.*;
 
 public class Main{
-    public static int cnt[] = new int[26];
+    static int[] cnt = new int[26];
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         String s = sc.next();
-
-        for(int i = 0; i < s.length(); i++){
-            cnt[s.charAt(i) - 97]++;
+        for(char c : s.toCharArray()){
+            cnt[c - 'a']++;
         }
-        for(int i : cnt) System.out.print(i+ " ");
+        for(int i : cnt) System.out.print(i + " ");
     }
 }
