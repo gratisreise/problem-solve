b199dc5c5db2f366db0a39e703ea0cd91213f89b
Noah <104671487+gratisreise@users.noreply.github.com>
2023-11-13 20:13:48 +0900

    [Silver IV] Title: 수학숙제, Time: 308 ms, Memory: 19524 KB -BaekjoonHub

--- 백준/Silver/2870. 수학숙제/수학숙제.java
+++ 백준/Silver/2870. 수학숙제/수학숙제.java
@@ -1,50 +1,37 @@
 import java.util.*;
 
-
-public class Main {
-    static List<String> ret = new ArrayList<>();
-    static String num = "";
-
-    public static void go() {
-        // 0없애주는 포맷팅
-        while (!num.isEmpty() && num.charAt(0) == '0') {
-            num = num.substring(1);
+public class Main{
+    static List<String> l = new ArrayList<>();
+    static StringBuilder ret = new StringBuilder();
+    public static void go(){
+        while(ret.length() != 0 && ret.charAt(0) == '0'){
+            ret = ret.delete(0, 1);
         }
-        // 0밖에 없으면 0추가
-        if (num.isEmpty()) {
-            num = "0";
+        if(ret.length() == 0){
+            ret.append("0");
         }
-        //ret list에 추가
-        ret.add(num);
-        //다시 빈칸으로 초기화
-        num = "";
+        l.add(ret.toString());
+        ret.delete(0, ret.length());
     }
-    
-    public static void main(String[] args) {
+
+    public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        for (int i = 0; i < n; i++) {
+        for(int i = 0; i < n; i++){
             String s = sc.next();
-            for (char c : s.toCharArray()) {
-                //숫자면 num에 추가
-                if ('0' <= c && c <= '9') num += c;
-                // 숫자가 아니면 숫자 포매팅
-                else if (!num.isEmpty()) go();
+            for(char c : s.toCharArray()){
+                if('0' <= c && c <= '9') ret.append(c);
+                else if(ret.length() != 0) go();
             }
-            // 문자열 끝났을 때 처리
-            if (!num.isEmpty()) go();
+            if(ret.length() != 0) go();
         }
 
-        // 오름차순 커스텀 개조
-        Collections.sort(ret, (a, b) -> {
-            //길이가 같을 때 오름차순 정렬
-            if (a.length() == b.length()) {
+        Collections.sort(l, (a, b) ->{
+            if(a.length() == b.length()){
                 return a.compareTo(b);
             }
-            //길이가 짧은 문자열이 앞에 오도록
             return Integer.compare(a.length(), b.length());
         });
-
-        for (String ss : ret) System.out.println(ss);
+        for(String ss : l) System.out.println(ss);
     }
 }
