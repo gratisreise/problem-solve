da82c41553d36721e3dc4139f176b2c45e566475
Noah <104671487+gratisreise@users.noreply.github.com>
2023-11-14 13:41:38 +0900

    [Silver III] Title: 빈도 정렬, Time: 212 ms, Memory: 18284 KB -BaekjoonHub

--- 백준/Silver/2910. 빈도 정렬/빈도 정렬.java
+++ 백준/Silver/2910. 빈도 정렬/빈도 정렬.java
@@ -1,50 +1,43 @@
 import java.util.*;
-
-public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int c = sc.nextInt();
-
-        Map<Integer, Integer> mp1 = new HashMap<>();
-        Map<Integer, Integer> mp2 = new HashMap<>();
-        List<Pair> v = new ArrayList<>();
-
-        for (int i = 0; i < n; i++) {
-            int temp = sc.nextInt();
-            mp1.put(temp, mp1.getOrDefault(temp, 0) + 1);
-            if (!mp2.containsKey(temp)) {
-                mp2.put(temp, i + 1);
-            }
-        }
-
-        for (Map.Entry<Integer, Integer> entry : mp1.entrySet()) {
-            v.add(new Pair(entry.getValue(), entry.getKey()));
-        }
-
-        Collections.sort(v, new Comparator<Pair>() {
-            @Override
-            public int compare(Pair a, Pair b) {
-                if (a.first == b.first) {
-                    return mp2.get(a.second) - mp2.get(b.second);
-                }
-                return b.first - a.first;
-            }});
-
-        for (Pair pair : v) {
-            for (int j = 0; j < pair.first; j++) {
-                System.out.print(pair.second + " ");
-            }
+public class Main{
+    static class Pair{
+        int f, s;
+        Pair(int f, int s){
+            this.f = f;
+            this.s = s;
         }
     }
 
-    static class Pair {
-        int first;
-        int second;
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        Map<Integer, Integer> mp1 = new HashMap<>(); //빈도
+        Map<Integer, Integer> mp2 = new HashMap<>();//순서
+        List<Pair> l = new ArrayList<>();
+        int n = sc.nextInt();
+        int c = sc.nextInt();
+        for(int i = 0; i < n; i++){
+            int temp = sc.nextInt();
+             mp1.put(temp, mp1.getOrDefault(temp, 0) + 1);
+             if(!mp2.containsKey(temp)){
+                 mp2.put(temp, i + 1);
+             }
+        }
+        for(Map.Entry<Integer, Integer> e : mp1.entrySet()){
+            l.add(new Pair(e.getValue(), e.getKey()));
+        }
+        Collections.sort(l, new Comparator<Pair>(){
+            public int compare(Pair a, Pair b){
+                if(a.f == b.f){
+                    return mp2.get(a.s) - mp2.get(b.s); // 순서 오름차순
+                }
+                return b.f - a.f; //빈도 내림차순
+            }
+        });
 
-        public Pair(int first, int second) {
-            this.first = first;
-            this.second = second;
+        for(Pair p : l){
+            for(int j = 0; j < p.f; j++){
+                System.out.print(p.s + " ");
+            }
         }
     }
 }
