ad8aa83c30f4e61c78b6bd08937907ee39a48987
Noah <104671487+gratisreise@users.noreply.github.com>
2023-11-16 13:31:32 +0900

    [Silver IV] Title: 수학숙제, Time: 288 ms, Memory: 19724 KB -BaekjoonHub

--- 백준/Silver/2870. 수학숙제/수학숙제.java
+++ 백준/Silver/2870. 수학숙제/수학숙제.java
@@ -1,23 +1,23 @@
 import java.util.*;
 
+
+
 public class Main{
-    static List<String> l = new ArrayList<>();
     static StringBuilder ret = new StringBuilder();
-    public static void go(){
-        while(ret.length() != 0 && ret.charAt(0) == '0'){
-            ret = ret.delete(0, 1);
+    static List<String> l = new ArrayList<>();
+    static int n ;
+    static void go(){
+        while(ret.length() != 0 &&  ret.charAt(0) == '0'){
+            ret.delete(0, 1);
         }
-        if(ret.length() == 0){
-            ret.append("0");
-        }
+        if(ret.length() == 0) ret.append("0");
         l.add(ret.toString());
         ret.delete(0, ret.length());
     }
-
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        for(int i = 0; i < n; i++){
+        n = sc.nextInt();
+        while(n-- > 0){
             String s = sc.next();
             for(char c : s.toCharArray()){
                 if('0' <= c && c <= '9') ret.append(c);
@@ -32,6 +32,6 @@ public class Main{
             }
             return Integer.compare(a.length(), b.length());
         });
-        for(String ss : l) System.out.println(ss);
+        for(String i : l) System.out.println(i);
     }
 }
