e0b0ab01839a693035276654c2f604115d5ca9ee
Noah <104671487+gratisreise@users.noreply.github.com>
2023-11-16 15:30:10 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 436 ms, Memory: 32296 KB -BaekjoonHub

--- 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.java
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.java
@@ -1,35 +1,21 @@
 import java.util.*;
 
-public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        while (true) {
-            String s = scanner.nextLine();
-            if (".".equals(s)) {
-                break;
-            }
-            if (isBalanced(s)) {
-                System.out.println("yes");
-            } else {
-                System.out.println("no");
-            }
-        }
-    }
+public class Main{
 
-    public static boolean isBalanced(String s) {
-        Stack<Character> stack = new Stack<>();
-        for (char c : s.toCharArray()) {
-            if (c != '(' && c != ')' && c != '[' && c != ']') {
-                continue;
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        while(true){
+            String s = sc.nextLine();
+            if(s.equals(".")) break;
+            Stack<Character> stk = new Stack<>();
+            for(char c : s.toCharArray()){
+                if(c != '(' && c != ')' && c != '[' && c != ']') continue;
+                if(!stk.isEmpty() && stk.peek() == '(' && c == ')') stk.pop();
+                else if(!stk.isEmpty() && stk.peek() == '[' && c == ']') stk.pop();
+                else stk.push(c);
             }
-            if (!stack.isEmpty() && stack.peek() == '(' && c == ')') {
-                stack.pop();
-            } else if (!stack.isEmpty() && stack.peek() == '[' && c == ']') {
-                stack.pop();
-            } else {
-                stack.push(c);
-            }
+            if(stk.isEmpty()) System.out.println("yes");
+            else System.out.println("no");
         }
-        return stack.isEmpty();
     }
 }
