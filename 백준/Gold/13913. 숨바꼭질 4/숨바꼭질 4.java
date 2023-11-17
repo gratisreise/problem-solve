8ec5e1935511a495113869184a74c663c11acbe7
Noah <104671487+gratisreise@users.noreply.github.com>
2023-11-17 15:46:55 +0900

    [Gold IV] Title: 숨바꼭질 4, Time: 1128 ms, Memory: 51772 KB -BaekjoonHub

--- 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.java
+++ 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.java
@@ -1,40 +1,36 @@
 import java.util.*;
 
-public class Main {
+public class Main{
     static final int mx = 200000;
     static int n, k;
-    static int[] visited;
-    static int[] prev;
-    static ArrayList<Integer> v;
-
-    public static void main(String[] args) {
+    static int[] a = new int[mx + 4];
+    static int[] visited = new int[mx + 4];
+    static int[] prev = new int[mx + 4];
+    static Queue<Integer> q = new LinkedList<>();
+    static List<Integer> l = new ArrayList<>();
+    public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         n = sc.nextInt();
         k = sc.nextInt();
-        Queue<Integer> q = new LinkedList<>();
-        prev = new int[mx + 4];
-        visited = new int[mx + 4];
-        v = new ArrayList<>();
+
         visited[n] = 1;
         q.add(n);
-
-        while (!q.isEmpty()) {
+        while(!q.isEmpty()){
             int now = q.poll();
-            for (int next : new int[] { now - 1, now + 1, now * 2 }) {
-                if (next < 0 || next > mx) continue;
-                if (visited[next] == 0) {
-                    visited[next] = visited[now] + 1;
-                    q.add(next);
-                    prev[next] = now;
-                }
+            if(visited[k] != 0) break;
+            for(int next : new int[]{now - 1, now + 1, now * 2}){
+                if(next < 0 || next > mx) continue;
+                if(visited[next] != 0) continue;
+                visited[next] = visited[now] + 1;
+                q.add(next);
+                prev[next] = now;
             }
         }
-
-        for (int i = k; i != n; i = prev[i]) v.add(i);
-        v.add(n);
-        Collections.reverse(v);
-
+        for(int i = k; i != n; i = prev[i]) l.add(i);
+        l.add(n);
+        Collections.reverse(l);
         System.out.println(visited[k] - 1);
-        for (int i : v) System.out.print(i + " ");
+        for(int i : l) System.out.print(i + " ");
+
     }
 }
