dce0be03f07ebdece7e953b1de4b4c55d947acf3
Noah <104671487+gratisreise@users.noreply.github.com>
2023-11-18 19:06:44 +0900

    [Gold IV] Title: 인구 이동, Time: 492 ms, Memory: 129248 KB -BaekjoonHub

--- 백준/Gold/16234. 인구 이동/인구 이동.java
+++ 백준/Gold/16234. 인구 이동/인구 이동.java
@@ -2,10 +2,11 @@ import java.util.*;
 
 public class Main{
     static int n, l, r, sum, ret;
-    static int[][] a = new int[104][104];
-    static List<Pair> L = new ArrayList<>();
+    static int[][] a = new int[54][54];
     static int[] dy = {-1, 0, 1, 0};
     static int[] dx = {0, 1, 0, -1};
+    static int[][] visited;
+    static List<Pair> L = new ArrayList<>();
     static void dfs(int y, int x){
         visited[y][x] = 1;
         sum += a[y][x];
@@ -20,7 +21,6 @@ public class Main{
             dfs(ny, nx);
         }
     }
-    static int[][] visited;
     static class Pair{
         int f, s;
         Pair(int f, int s){
@@ -29,35 +29,34 @@ public class Main{
         }
     }
     public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        n = sc.nextInt();
-        l = sc.nextInt();
-        r = sc.nextInt();
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < n; j++){
-                a[i][j] = sc.nextInt();
-            }
-        }
-
-        while(true){
-            boolean flag = false;
-            visited = new int[n + 1][n + 1];
-            for(int i = 0; i < n; i++){
-                for(int j = 0; j < n; j++){
-                    if(visited[i][j] != 0) continue;
-                    L.clear();
-                    sum = 0;
-                    dfs(i, j);
-                    if(L.size() == 1) continue;
-                    for(Pair p : L){
-                        a[p.f][p.s] = sum / L.size();
-                        flag = true;
-                    }
-                }
-            }
-            if(!flag) break;
-            ret++;
-        }
-        System.out.println(ret);
+           Scanner sc = new Scanner(System.in);
+           n = sc.nextInt();
+           l = sc.nextInt();
+           r = sc.nextInt();
+           for(int i = 0; i < n; i++){
+               for(int j = 0; j < n; j++){
+                   a[i][j] = sc.nextInt();
+               }
+           }
+           while(true){
+               boolean flag = false;
+               visited = new int[n + 1][n + 1];
+               for(int i = 0; i < n; i++){
+                   for(int j = 0; j < n; j++){
+                       if(visited[i][j] != 0) continue;
+                       sum = 0;
+                       L.clear();
+                       dfs(i, j);
+                       if(L.size() == 1) continue;
+                       for(Pair p : L){
+                           a[p.f][p.s] = sum / L.size();
+                           flag = true;
+                       }
+                   }
+               }
+               if(!flag) break;
+               ret++;
+           }
+           System.out.println(ret);
     }
 }
