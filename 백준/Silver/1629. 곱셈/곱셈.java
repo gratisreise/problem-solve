c7b3f262e2041ebb94a7a76c214c5ffbc00bcabd
Noah <104671487+gratisreise@users.noreply.github.com>
2023-11-20 22:11:27 +0900

    [Silver I] Title: 곱셈, Time: 108 ms, Memory: 12880 KB -BaekjoonHub

--- 백준/Silver/1629. 곱셈/곱셈.java
+++ 백준/Silver/1629. 곱셈/곱셈.java
@@ -1,20 +1,18 @@
 import java.util.*;
 
-public class Main {
-    static long modPower(long a, long b, long c) {
-        if (b == 1) return a % c;
-        long ret = modPower(a, b / 2, c);
+public class Main{
+    static long go(long a, long b, long c){
+        if(b == 1) return a % c;
+        long ret = go(a, b/2, c);
         ret = (ret * ret) % c;
-        if (b % 2 == 1) ret = (ret * a) % c;
+        if(b % 2 != 0) ret = (ret * a) % c;
         return ret;
     }
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        long a = scanner.nextLong();
-        long b = scanner.nextLong();
-        long c = scanner.nextLong();
-
-        long result = modPower(a, b, c);
-        System.out.println(result);
-    }   
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        long a = sc.nextInt();
+        long b = sc.nextInt();
+        long c = sc.nextInt();
+        System.out.println(go(a, b, c));
+    }
 }
