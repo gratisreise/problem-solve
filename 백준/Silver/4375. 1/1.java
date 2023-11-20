ad49e8de5d6671bedd300e5a578beb8bbf3d28b4
Noah <104671487+gratisreise@users.noreply.github.com>
2023-11-20 22:13:43 +0900

    [Silver III] Title: 1, Time: 108 ms, Memory: 12792 KB -BaekjoonHub

--- 백준/Silver/4375. 1/1.java
+++ 백준/Silver/4375. 1/1.java
@@ -1,14 +1,15 @@
 import java.util.*;
 
-public class Main{
+public class Main {
+    static int n;
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        while(sc.hasNextInt()){
-            int n = sc.nextInt();
+        while (sc.hasNextInt()) {
+            n = sc.nextInt();
             int cnt = 1, ret = 1;
-            while(true){
-                if(cnt % n == 0){
+            while (true) {
+                if (cnt % n == 0) {
                     System.out.println(ret);
                     break;
                 } else {
