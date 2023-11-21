1e9e5dafe9adee49f1e542743a1003d24a515d02
Noah <104671487+gratisreise@users.noreply.github.com>
2023-11-21 16:22:17 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 424 ms, Memory: 29164 KB -BaekjoonHub

--- 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.java
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.java
@@ -1,20 +1,19 @@
 import java.util.*;
 
-public class Main{
-
-    public static void main(String[] args){
+public class Main {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        while(true){
+        while (true) {
             String s = sc.nextLine();
-            if(s.equals(".")) break;
+            if (s.equals(".")) break;
             Stack<Character> stk = new Stack<>();
-            for(char c : s.toCharArray()){
-                if(c != '(' && c != ')' && c != '[' && c != ']') continue;
-                if(!stk.isEmpty() && stk.peek() == '(' && c == ')') stk.pop();
-                else if(!stk.isEmpty() && stk.peek() == '[' && c == ']') stk.pop();
+            for (char c : s.toCharArray()) {
+                if (c != '(' && c != ')' && c != '[' && c != ']') continue;
+                if (!stk.isEmpty() && c == ')' && stk.peek() == '(') stk.pop();
+                else if (!stk.isEmpty() && c == ']' && stk.peek() == '[') stk.pop();
                 else stk.push(c);
             }
-            if(stk.isEmpty()) System.out.println("yes");
+            if (stk.isEmpty()) System.out.println("yes");
             else System.out.println("no");
         }
     }
