30407bc631b1bd3e7f7f001b4e7fca17399c26a0
Noah <104671487+gratisreise@users.noreply.github.com>
2023-11-23 15:26:08 +0900

    [Silver I] Title: 부등호, Time: 216 ms, Memory: 35500 KB -BaekjoonHub

--- 백준/Silver/2529. 부등호/부등호.java
+++ 백준/Silver/2529. 부등호/부등호.java
@@ -2,19 +2,19 @@ import java.util.*;
 
 public class Main {
     static int n;
-    static int[] check = new int[10]; 
-    static char[] a = new char[20]; 
-    static List<String> ret = new ArrayList<>(); 
+    static int[] check = new int[10];
+    static char[] a = new char[20];
+    static List<String> ret = new ArrayList<>();
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        n = sc.nextInt(); 
+        n = sc.nextInt();
 
         for (int i = 0; i < n; i++) {
             a[i] = sc.next().charAt(0);
         }
 
-        go(0, ""); 
+        go(0, "");
         Arrays.sort(ret.toArray());
 
         System.out.println(ret.get(ret.size() - 1));
@@ -28,13 +28,13 @@ public class Main {
     }
 
     static void go(int idx, String num) {
-        if (idx == n + 1) { 
+        if (idx == n + 1) {
             ret.add(num);
             return;
         }
 
         for (int i = 0; i <= 9; i++) {
-            if (check[i] != 0) continue; 
+            if (check[i] != 0) continue;
             if ((idx == 0) || (good(num.charAt(idx - 1), (char) (i + '0'), a[idx - 1]))) {
                 check[i] = 1;
                 go(idx + 1, num + Integer.toString(i));
