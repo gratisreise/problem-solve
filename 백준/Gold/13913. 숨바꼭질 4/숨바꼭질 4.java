4971f779580d8f217c1ceb1a8de5e7af7a3e6690
Noah <104671487+gratisreise@users.noreply.github.com>
2023-11-23 14:33:58 +0900

    [Gold IV] Title: 숨바꼭질 4, Time: 1044 ms, Memory: 48516 KB -BaekjoonHub

--- 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.java
+++ 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.java
@@ -1,36 +1,33 @@
 import java.util.*;
 
-public class Main{
+public class Main {
     static final int mx = 200000;
     static int n, k;
-    static int[] a = new int[mx + 4];
-    static int[] visited = new int[mx + 4];
     static int[] prev = new int[mx + 4];
+    static int[] visited = new int[mx + 4];
     static Queue<Integer> q = new LinkedList<>();
     static List<Integer> l = new ArrayList<>();
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         n = sc.nextInt();
         k = sc.nextInt();
-
         visited[n] = 1;
         q.add(n);
-        while(!q.isEmpty()){
+        while (!q.isEmpty()) {
             int now = q.poll();
-            if(visited[k] != 0) break;
-            for(int next : new int[]{now - 1, now + 1, now * 2}){
-                if(next < 0 || next > mx) continue;
-                if(visited[next] != 0) continue;
+            for (int next : new int[]{now - 1, now + 1, now * 2}) {
+                if (next < 0 || next > mx) continue;
+                if (visited[next] != 0) continue;
                 visited[next] = visited[now] + 1;
-                q.add(next);
                 prev[next] = now;
+                q.add(next);
             }
         }
-        for(int i = k; i != n; i = prev[i]) l.add(i);
+        for (int i = k; i != n; i = prev[i]) l.add(i);
         l.add(n);
         Collections.reverse(l);
         System.out.println(visited[k] - 1);
-        for(int i : l) System.out.print(i + " ");
-
+        for (int i : l) System.out.print(i + " ");
     }
 }
