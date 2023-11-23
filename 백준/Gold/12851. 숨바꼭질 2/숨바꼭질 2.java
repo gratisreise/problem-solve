d2941e8f480516386170f2fc050890729e532b57
Noah <104671487+gratisreise@users.noreply.github.com>
2023-11-23 14:23:05 +0900

    [Gold IV] Title: 숨바꼭질 2, Time: 192 ms, Memory: 31404 KB -BaekjoonHub

--- 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.java
+++ 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.java
@@ -1,33 +1,34 @@
 import java.util.*;
 
-public class Main{
-    static final int mx = 200000;
+public class Main {
+    static final int mx = 200004;
     static int n, k;
-    static int[] visited = new int[mx + 4];
-    static int[] cnt = new int[mx + 4];
+    static int[] a = new int[mx];
+    static int[] visited = new int[mx];
+    static int[] cnt = new int[mx];
     static Queue<Integer> q = new LinkedList<>();
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-         n = sc.nextInt();
-         k = sc.nextInt();
-         visited[n] = 1;
-         q.add(n);
-         cnt[n] = 1;
-         while(!q.isEmpty()){
-             int now = q.poll();
-             for(int next : new int[]{now - 1, now + 1, now * 2}){
-                 if(next < 0 || next > mx) continue;
-                 if(visited[next] == 0){
-                     visited[next] = visited[now] + 1;
-                     cnt[next] += cnt[now];
-                     q.add(next);
-                 } else if(visited[next] == visited[now] + 1){
-                     cnt[next] += cnt[now];
-                 }
-             }
-         }
-         System.out.println(visited[k] - 1);
-         System.out.println(cnt[k]);
+        n = sc.nextInt();
+        k = sc.nextInt();
+        visited[n] = 1;
+        cnt[n] = 1;
+        q.add(n);
+        while (!q.isEmpty()) {
+            int now = q.poll();
+            for (int next : new int[]{now - 1, now + 1, now * 2}) {
+                if (next < 0 || next >= mx) continue;
+                if (visited[next] == 0) {
+                    visited[next] = visited[now] + 1;
+                    cnt[next] += cnt[now];
+                    q.add(next);
+                } else if (visited[next] == visited[now] + 1) {
+                    cnt[next] += cnt[now];
+                }
+            }
+        }
+        System.out.println(visited[k] - 1);
+        System.out.println(cnt[k]);
     }
 }
