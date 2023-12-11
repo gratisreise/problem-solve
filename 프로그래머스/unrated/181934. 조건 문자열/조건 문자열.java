adb44819213c41c35ce5c20b128e524081a0524f
Noah <104671487+gratisreise@users.noreply.github.com>
2023-12-11 13:07:07 +0900

    [level unrated] Title: 조건 문자열, Time: 0.03 ms, Memory: 86.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181934. 조건 문자열/조건 문자열.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int solution(String ineq, String eq, int n, int m) {
+        int answer = 0;
+        if(ineq.equals(">")){
+            if(eq.equals("=")){
+                if(n >= m) answer = 1;
+                else answer = 0;
+            } else { // "!"
+                if(n > m) answer = 1;
+                else answer = 0;
+            }
+        } else { // "<"
+            if(eq.equals("=")){
+                if(n <= m) answer = 1;
+                else answer = 0;
+            } else { // "!"
+                if(n < m) answer = 1;
+                else answer = 0;
+            }
+        }
+        return answer;
+    }
+}
