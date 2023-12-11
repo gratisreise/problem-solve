8edaa06f5e29708d1c7659acabc817cfe69d8163
Noah <104671487+gratisreise@users.noreply.github.com>
2023-12-11 17:08:44 +0900

    [level unrated] Title: 등차수열의 특정한 항만 더하기, Time: 0.02 ms, Memory: 77 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int a, int d, boolean[] included) {
+        int answer = 0;
+        for(int i= 0; i < included.length; i++){
+            if(!included[i]) continue;
+            answer += (a + d * i);
+        }
+        return answer;
+    }
+}
