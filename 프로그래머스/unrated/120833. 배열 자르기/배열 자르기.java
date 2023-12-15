0a6459abeffd01db06550f45a480ad0850c679fe
Noah <104671487+gratisreise@users.noreply.github.com>
2023-12-15 14:42:11 +0900

    [level unrated] Title: 배열 자르기, Time: 0.04 ms, Memory: 76.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120833. 배열 자르기/배열 자르기.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Solution {
+    public List<Integer> solution(int[] numbers, int num1, int num2) {
+        List<Integer> ret = new ArrayList<>();
+        for(int i = num1; i <= num2; i++){
+            ret.add(numbers[i]);
+        }
+        
+        return ret;
+    }
+}
