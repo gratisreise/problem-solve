0d8d9bf82aacb6236ca76218c7541c535d852b22
Noah <104671487+gratisreise@users.noreply.github.com>
2023-12-16 01:02:22 +0900

    [level unrated] Title: 배열의 유사도, Time: 0.03 ms, Memory: 89.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120903. 배열의 유사도/배열의 유사도.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Solution {
+    public int solution(String[] s1, String[] s2) {
+        int answer = 0;
+        for(String s : s1){
+            for(String ss : s2){
+                if(s.equals(ss)) answer++;
+            }
+        }
+        return answer;
+    }
+}
