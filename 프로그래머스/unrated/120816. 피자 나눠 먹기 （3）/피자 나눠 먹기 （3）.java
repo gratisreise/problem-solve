b66136fcf295b163926e45e38ab272818b82ba8e
Noah <104671487+gratisreise@users.noreply.github.com>
2023-12-15 14:28:08 +0900

    [level unrated] Title: 피자 나눠 먹기 (3), Time: 0.01 ms, Memory: 74.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int solution(int slice, int n) {
+        int answer = 0;
+        answer += n / slice;
+        if(n % slice != 0) answer++;
+        return answer;
+    }
+}
