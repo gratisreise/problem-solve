a3878ba5a9d03e169ec51193795394ed24899e6e
Noah <104671487+gratisreise@users.noreply.github.com>
2023-12-16 01:07:26 +0900

    [level unrated] Title: 특정 문자 제거하기, Time: 10.89 ms, Memory: 80.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120826. 특정 문자 제거하기/특정 문자 제거하기.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String solution(String my_string, String letter) {
+        String answer = "";
+        for(char c : my_string.toCharArray()){
+            if(c == letter.charAt(0)) continue;
+            answer += c;
+        }
+        return answer;
+    }
+}
