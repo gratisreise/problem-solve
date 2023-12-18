b19f88a7bc0c490f669d7ec6ed61357e19867685
Noah <104671487+gratisreise@users.noreply.github.com>
2023-12-18 12:09:48 +0900

    [level unrated] Title: 배열 만들기 6, Time: 13.96 ms, Memory: 82.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181859. 배열 만들기 6/배열 만들기 6.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class Solution {
+    public Stack<Integer> solution(int[] arr) {
+        Stack<Integer> stk = new Stack<>();
+        int i = 0;
+        while(i < arr.length){
+        if(stk.isEmpty()){
+            stk.push(arr[i]);
+            i++;
+        } else {
+            if(stk.peek() == arr[i]){
+                stk.pop();
+                i++;
+            } else if(stk.peek() != arr[i]){
+                stk.push(arr[i]);
+                i++;
+            }
+        }
+        }
+        if(stk.isEmpty()) stk.push(-1);
+        return stk;
+    }
+}
