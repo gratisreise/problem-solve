7f4706fc9842ada9445c6f0c1e28dda8c978adfe
Noah <104671487+gratisreise@users.noreply.github.com>
2024-03-15 15:34:17 +0900

    [level 0] Title: 5명씩, Time: 0.02 ms, Memory: 78.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181886. 5명씩/5명씩.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Solution {
+    public List<String> solution(String[] names) {
+        List<String> ret = new ArrayList<>();
+        for(int i = 0; i < names.length; i += 5){
+            ret.add(names[i]);
+        }
+        
+        return ret;
+        
+    }
+}
