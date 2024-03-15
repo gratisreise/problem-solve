823b05186852ae0f549dd0ebda35c81f61ae0e55
Noah <104671487+gratisreise@users.noreply.github.com>
2024-03-15 17:52:39 +0900

    [level 0] Title: 꼬리 문자열, Time: 1.20 ms, Memory: 75 MB -BaekjoonHub

---
+++ 프로그래머스/0/181841. 꼬리 문자열/꼬리 문자열.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String solution(String[] str_list, String ex) {
+        String ret = "";
+        for(String s : str_list){
+            if(s.contains(ex)) continue;
+            ret += s;
+        }
+        return ret;
+    }
+}
