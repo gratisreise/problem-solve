eaa341b62d1957b03424c94070527c67e691b02f
Noah <104671487+gratisreise@users.noreply.github.com>
2024-03-15 10:50:26 +0900

    [level 0] Title: 세균 증식, Time: 0.03 ms, Memory: 77 MB -BaekjoonHub

---
+++ 프로그래머스/0/120910. 세균 증식/세균 증식.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int n, int t) {
+        int ret = n;
+        for(int i = 0; i < t; i++){
+            ret *= 2;
+        }
+        
+        return ret;
+    }
+}
