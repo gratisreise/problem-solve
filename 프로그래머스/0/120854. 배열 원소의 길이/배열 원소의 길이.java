3518c516632bfc2c424d7b2e839664952797a6f6
Noah <104671487+gratisreise@users.noreply.github.com>
2024-03-15 10:33:32 +0900

    [level 0] Title: 배열 원소의 길이, Time: 0.03 ms, Memory: 74.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120854. 배열 원소의 길이/배열 원소의 길이.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Solution {
+    public List<Integer> solution(String[] strlist) {
+        List<Integer> ret = new ArrayList<>();
+        for(String s : strlist){
+            ret.add(s.length());
+        }
+        return ret;
+    }
+}
