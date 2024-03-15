deee3474a609c2dcf7b90292f892cdf1e2e57bc8
Noah <104671487+gratisreise@users.noreply.github.com>
2024-03-15 14:47:38 +0900

    [level 0] Title: 카운트 업, Time: 0.03 ms, Memory: 74 MB -BaekjoonHub

---
+++ 프로그래머스/0/181920. 카운트 업/카운트 업.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Solution {
+    public List<Integer> solution(int start_num, int end_num) {
+        List<Integer> ret = new ArrayList<>();
+        for(int i = start_num; i <= end_num; i++){
+            ret.add(i);
+        }
+        return ret;
+    }
+}
