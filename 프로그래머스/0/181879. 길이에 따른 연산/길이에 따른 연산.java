cb54085b6954cdf78121cf48cce7c8c516d13aa2
Noah <104671487+gratisreise@users.noreply.github.com>
2024-03-15 15:11:20 +0900

    [level 0] Title: 길이에 따른 연산, Time: 0.02 ms, Memory: 73.8 MB -BaekjoonHub

--- 프로그래머스/0/181879. 길이에 따른 연산/길이에 따른 연산.java
+++ 프로그래머스/0/181879. 길이에 따른 연산/길이에 따른 연산.java
@@ -1,22 +1,13 @@
-import java.util.*;
 class Solution {
     public int solution(int[] num_list) {
-        int ret = 0;
-        
+        int ret;
         if(num_list.length > 10){
-            for(int i : num_list){
-                ret += i;
-            }
+            ret = 0;
+            for(int i : num_list) ret += i;
         } else {
-            int temp = 1;
-            for(int i : num_list){
-                temp *= i;
-            }
-            ret = temp;
+            ret = 1;
+            for(int i : num_list) ret *= i;
         }
         return ret;
     }
 }
-
-//num_list의 길이 11이상 -> 리스트 모든 원소의 합
-//num_list의 길이 10이하 -> 모든 원소의 곱
