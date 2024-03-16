2a88775bec079468be80748fe5dffda39905127e
Noah <104671487+gratisreise@users.noreply.github.com>
2024-03-16 09:31:43 +0900

    [level 0] Title: 문자열안에 문자열, Time: 0.02 ms, Memory: 77.8 MB -BaekjoonHub

--- 프로그래머스/0/120908. 문자열안에 문자열/문자열안에 문자열.java
+++ 프로그래머스/0/120908. 문자열안에 문자열/문자열안에 문자열.java
@@ -1,9 +1,7 @@
-import java.util.*;
 class Solution {
     public int solution(String str1, String str2) {
-        int ret = 0;
+        int ret = 2;
         if(str1.contains(str2)) ret = 1;
-        else ret = 2;
         return ret;
     }
 }
