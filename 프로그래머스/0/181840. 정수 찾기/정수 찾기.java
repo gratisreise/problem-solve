b54b079c2462ce603098622a83031f051b4cbda6
Noah <104671487+gratisreise@users.noreply.github.com>
2024-03-16 11:56:57 +0900

    [level 0] Title: 정수 찾기, Time: 0.02 ms, Memory: 77.4 MB -BaekjoonHub

--- 프로그래머스/0/181840. 정수 찾기/정수 찾기.java
+++ 프로그래머스/0/181840. 정수 찾기/정수 찾기.java
@@ -1,10 +1,11 @@
 class Solution {
     public int solution(int[] num_list, int n) {
         int ret = 0;
-        
         for(int i : num_list){
             if(i == n) ret = 1;
         }
+        
+        
         return ret;
     }
 }
