82cf3da72c58e4f763ae1094e0f26105bed05814
Noah <104671487+gratisreise@users.noreply.github.com>
2024-03-16 09:16:41 +0900

    [level 0] Title: 아이스 아메리카노, Time: 0.01 ms, Memory: 73.3 MB -BaekjoonHub

--- 프로그래머스/0/120819. 아이스 아메리카노/아이스 아메리카노.java
+++ 프로그래머스/0/120819. 아이스 아메리카노/아이스 아메리카노.java
@@ -1,14 +1,8 @@
-import java.util.*;
 class Solution {
-    public List<Integer> solution(int money) {
-        List<Integer> ret = new ArrayList<>();
-        int cup = money / 5500;
-        int surplus = money - (5500 * cup);
-        ret.add(cup);
-        ret.add(surplus);
+    public int[] solution(int money) {
+        int[] ret = {0, 0};
+        ret[0] = money / 5500;
+        ret[1] = money % 5500;
         return ret;
     }
 }
-//아메리카노 - 5500
-//가지고 있는 돈은 주어짐
-// 최대로 마실 수 있는 아메리카노 잔 수와 남는돈을 담은 배열 리턴
