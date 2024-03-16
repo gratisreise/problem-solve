767efaacd3bd9fc8e56dae28a614ded8d772a240
Noah <104671487+gratisreise@users.noreply.github.com>
2024-03-16 09:28:26 +0900

    [level 0] Title: 배열 자르기, Time: 0.03 ms, Memory: 72.6 MB -BaekjoonHub

--- 프로그래머스/0/120833. 배열 자르기/배열 자르기.java
+++ 프로그래머스/0/120833. 배열 자르기/배열 자르기.java
@@ -1,10 +1,8 @@
 import java.util.*;
 class Solution {
     public List<Integer> solution(int[] numbers, int num1, int num2) {
-        int start = num1 > num2 ? num2 : num1;
-        int end = num1 > num2 ? num1 : num2;
         List<Integer> ret = new ArrayList<>();
-        for(int i = start; i <= end; i++){
+        for(int i = num1; i <= num2; i++){
             ret.add(numbers[i]);
         }
         return ret;
