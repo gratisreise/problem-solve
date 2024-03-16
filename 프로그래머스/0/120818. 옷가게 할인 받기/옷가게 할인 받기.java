a097c4f7c36c1ece9fb46c415eb4594ecce11a4d
Noah <104671487+gratisreise@users.noreply.github.com>
2024-03-16 11:21:48 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.02 ms, Memory: 72.6 MB -BaekjoonHub

--- 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.java
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.java
@@ -1,10 +1,9 @@
 class Solution {
     public int solution(int price) {
-        int ret = 0;
-        if(price < 100000) ret = price;
-        else if(price < 300000) ret = price * 95 / 100;
-        else if(price < 500000) ret = price * 90 / 100;
-        else ret = price * 80 / 100;
+        int ret = price;
+        if(price >= 500000) ret = ret * 80 /100;
+        else if(price >= 300000) ret = ret * 90 / 100;
+        else if(price >= 100000) ret = ret * 95 / 100;
         return ret;
     }
 }
