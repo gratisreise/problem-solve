1fe40cf32f86169b79a444db1247f0e46c646eef
Noah <104671487+gratisreise@users.noreply.github.com>
2024-03-18 10:53:47 +0900

    [level 0] Title: 가위 바위 보, Time: 2.76 ms, Memory: 79.5 MB -BaekjoonHub

--- 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.java
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.java
@@ -1,11 +1,14 @@
 class Solution {
     public String solution(String rsp) {
         String ret = "";
-        for(int i = 0; i < rsp.length(); i++){
-            if(rsp.charAt(i) == '2') ret += "0";
-            else if(rsp.charAt(i) == '0') ret += "5";
-            else if(rsp.charAt(i) == '5') ret += "2";
+        
+        for(char c : rsp.toCharArray()){
+            if(c == '2') ret += '0';
+            else if(c == '0') ret += '5';
+            else if(c == '5') ret += '2';
         }
+        
         return ret;
     }
 }
+//
