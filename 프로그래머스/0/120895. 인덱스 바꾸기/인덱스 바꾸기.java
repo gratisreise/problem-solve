b97ee549580860e6ae55823c6764438ea5912754
Noah <104671487+gratisreise@users.noreply.github.com>
2024-03-18 12:31:49 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 9.24 ms, Memory: 74.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String solution(String my_string, int num1, int num2) {
+        String ret = "";
+        for(int i = 0; i < my_string.length(); i++){
+            if(i == num1) ret += my_string.charAt(num2);
+            else if(i == num2) ret += my_string.charAt(num1);
+            else ret += my_string.charAt(i);
+        }
+        
+        return ret;
+    }
+}
