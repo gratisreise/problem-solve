5dbd4663cdb696873894a1fded578bbcedd0fb36
Noah <104671487+gratisreise@users.noreply.github.com>
2024-03-18 10:48:23 +0900

    [level 0] Title: 암호 해독, Time: 14.01 ms, Memory: 76.9 MB -BaekjoonHub

--- 프로그래머스/0/120892. 암호 해독/암호 해독.java
+++ 프로그래머스/0/120892. 암호 해독/암호 해독.java
@@ -2,8 +2,9 @@ class Solution {
     public String solution(String cipher, int code) {
         String ret = "";
         for(int i = 0; i < cipher.length(); i++){
-            if((i + 1) % code == 0) ret += cipher.charAt(i);
+            if((i+1) % code == 0) ret += cipher.charAt(i);
         }
+        
         return ret;
     }
 }
