8d73b92d33e04c41ecb259c98edf64eefeb1a798
Noah <104671487+gratisreise@users.noreply.github.com>
2024-03-18 10:43:59 +0900

    [level 0] Title: 카운트 다운, Time: 0.04 ms, Memory: 70.2 MB -BaekjoonHub

--- 프로그래머스/0/181899. 카운트 다운/README.md
+++ 프로그래머스/0/181899. 카운트 다운/README.md
@@ -1,10 +1,10 @@
 # [level 0] 카운트 다운 - 181899 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181899?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181899) 
 
 ### 성능 요약
 
-메모리: 75.2 MB, 시간: 0.03 ms
+메모리: 70.2 MB, 시간: 0.04 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 15일 16:22:42
+2024년 03월 18일 10:44:01
 
 ### 문제 설명
 
--- 프로그래머스/0/181899. 카운트 다운/카운트 다운.java
+++ 프로그래머스/0/181899. 카운트 다운/카운트 다운.java
@@ -1,10 +1,10 @@
 import java.util.*;
 class Solution {
-    public List<Integer> solution(int start, int end) {
-        List<Integer> l = new ArrayList<>();
-        for(int i = start; i >= end; i--){
-            l.add(i);
+    public List<Integer> solution(int start, int end_num) {
+        List<Integer> ret = new ArrayList<>();
+        for(int i = start; i >= end_num; i--){
+            ret.add(i);
         }
-        return l;
+        return ret;
     }
 }
