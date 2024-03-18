895bb1257fddd25730d7dfaef93e657ed9c6161d
Noah <104671487+gratisreise@users.noreply.github.com>
2024-03-18 09:18:27 +0900

    [level 0] Title: rny_string, Time: 14.88 ms, Memory: 82.2 MB -BaekjoonHub

--- 프로그래머스/0/181863. rny＿string/rny＿string.java
+++ 프로그래머스/0/181863. rny＿string/rny＿string.java
@@ -1,12 +1,11 @@
 class Solution {
     public String solution(String rny_string) {
         String ret = "";
-        for(int i = 0; i < rny_string.length(); i++){
-            if(rny_string.charAt(i) == 'm'){
-                ret += "rn";
-            } else ret += rny_string.charAt(i);
+        for(char c : rny_string.toCharArray()){
+            if(c == 'm') ret += "rn";
+            else ret += c;
         }
+        
         return ret;
     }
-    
 }
