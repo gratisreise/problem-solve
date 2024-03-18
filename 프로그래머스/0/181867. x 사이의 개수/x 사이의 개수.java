4163168ff44d5de62bdc0e9a0a784c5d8d2bc425
Noah <104671487+gratisreise@users.noreply.github.com>
2024-03-18 11:03:28 +0900

    [level 0] Title: x 사이의 개수, Time: 5.10 ms, Memory: 83.1 MB -BaekjoonHub

--- 프로그래머스/0/181867. x 사이의 개수/x 사이의 개수.java
+++ 프로그래머스/0/181867. x 사이의 개수/x 사이의 개수.java
@@ -1,13 +1,14 @@
 import java.util.*;
 class Solution {
     public List<Integer> solution(String myString) {
-        String[] temp = myString.split("x");
-        
         List<Integer> ret = new ArrayList<>();
-        for(String s : temp){
-            ret.add(s.length());
+        String[] s = myString.split("x");
+        for(String ss : s){
+            ret.add(ss.length());
         }
         if(myString.charAt(myString.length() - 1) == 'x') ret.add(0);
         return ret;
     }
 }
+
+//x를 기준으로 나눴을 때 마지막 x는 마지막과 사이의 0으로 인식
