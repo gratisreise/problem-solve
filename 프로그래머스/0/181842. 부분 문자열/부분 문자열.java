e3678981708bca2c20bb2b9e88f4667126b3bb6c
Noah <104671487+gratisreise@users.noreply.github.com>
2024-03-18 09:36:56 +0900

    [level 0] Title: 부분 문자열, Time: 0.03 ms, Memory: 79.1 MB -BaekjoonHub

--- 프로그래머스/0/181842. 부분 문자열/부분 문자열.java
+++ 프로그래머스/0/181842. 부분 문자열/부분 문자열.java
@@ -1,9 +1,8 @@
 class Solution {
     public int solution(String str1, String str2) {
         int ret = 0;
-        if(str2.contains(str1)){
-            ret = 1;
-        }
+        if(str2.contains(str1)) ret = 1;
+        
         return ret;
     }
 }
