36c1ae61ff47782a1f6a00e753a27010ae1c374d
Noah <104671487+gratisreise@users.noreply.github.com>
2024-03-18 09:51:38 +0900

    [level 0] Title: 마지막 두 원소, Time: 0.02 ms, Memory: 74.2 MB -BaekjoonHub

--- 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.java
+++ 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.java
@@ -2,14 +2,17 @@ import java.util.*;
 class Solution {
     public List<Integer> solution(int[] num_list) {
         List<Integer> ret = new ArrayList<>();
-        int end = 0;
-        if(num_list[num_list.length - 1] > num_list[num_list.length - 2]){
-            end = num_list[num_list.length - 1] - num_list[num_list.length - 2];
+        int end = num_list.length;
+        int temp1 = num_list[end - 1];
+        int temp2 = num_list[end - 2];
+        if(temp1 > temp2){
+            for(int i : num_list) ret.add(i);
+            ret.add(temp1 - temp2);
         } else {
-            end = num_list[num_list.length - 1] * 2;
+            for(int i : num_list) ret.add(i);
+            ret.add(temp1 * 2);
         }
-        for(int i : num_list) ret.add(i);
-        ret.add(end);
         return ret;
     }
 }
+//마지막 > 그전 -> 마지막 - 그전 | 마지막 <= 그전 -> 마지막 *= 2;
