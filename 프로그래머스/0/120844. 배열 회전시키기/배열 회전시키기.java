90c7fc3d24f7c47f13dc794b6d98e36925ab993d
Noah <104671487+gratisreise@users.noreply.github.com>
2024-03-18 14:20:46 +0900

    [level 0] Title: 배열 회전시키기, Time: 0.04 ms, Memory: 86.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120844. 배열 회전시키기/배열 회전시키기.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Solution {
+    public List<Integer> solution(int[] numbers, String direction) {
+        List<Integer> ret = new ArrayList<>();
+        if(direction.equals("right")){
+            ret.add(numbers[numbers.length - 1]);
+            for(int i = 0; i < numbers.length - 1; i++){
+                ret.add(numbers[i]);
+            }
+            
+        } else {
+            for(int i = 1; i < numbers.length ; i++){
+                ret.add(numbers[i]);
+            }
+            ret.add(numbers[0]);
+        }
+        return ret;
+    }
+}
