20da602bd07be7cd3ad39e890739f22e8f9ba6f7
Noah <104671487+gratisreise@users.noreply.github.com>
2024-03-18 10:55:02 +0900

    [level 0] Title: 대문자와 소문자, Time: 12.57 ms, Memory: 86 MB -BaekjoonHub

--- 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.java
+++ 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.java
@@ -1,13 +1,9 @@
 class Solution {
     public String solution(String my_string) {
         String ret = "";
-        for(int i = 0; i < my_string.length(); i++){
-            char c = my_string.charAt(i);
-            if(c >= 97){
-                ret += (char)(c - 32);
-            } else {
-                ret += (char)(c + 32);
-            }
+        for(char c : my_string.toCharArray()){
+            if(c >= 97) ret += (char)(c - 32);
+            else ret += (char)(c + 32);
         }
         return ret;
     }
