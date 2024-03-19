6c5aeee7fbd8ea3010572f7ad5122dd2a6ec3e59
Noah <104671487+gratisreise@users.noreply.github.com>
2024-03-19 11:55:44 +0900

    [level 0] Title: 배열 만들기 4, Time: 5.87 ms, Memory: 78.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Solution {
+    public Stack<Integer> solution(int[] arr) {
+        Stack<Integer> stk = new Stack<>();
+        int i = 0;
+        while(i < arr.length){
+            if(stk.isEmpty()){
+                stk.push(arr[i]); i++;
+            } else if(!stk.isEmpty() && (stk.peek() < arr[i])){
+                stk.push(arr[i]); i++;
+            } else if(!stk.isEmpty() && (stk.peek() >= arr[i])){
+                stk.pop();
+            }
+        }
+        return stk;
+    }
+}
