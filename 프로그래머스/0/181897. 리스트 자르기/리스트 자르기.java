c9bb275047b29cdf9f1b6abacff4c00faa32ea0e
Noah <104671487+gratisreise@users.noreply.github.com>
2024-03-19 10:28:59 +0900

    [level 0] Title: 리스트 자르기, Time: 0.02 ms, Memory: 77 MB -BaekjoonHub

---
+++ 프로그래머스/0/181897. 리스트 자르기/리스트 자르기.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class Solution {
+    public List<Integer> solution(int n, int[] slicer, int[] num_list) {
+        List<Integer> ret = new ArrayList<>();
+        int a = slicer[0];
+        int b = slicer[1];
+        int c = slicer[2];
+        if(n == 1){
+            for(int i = 0; i <= b; i++) ret.add(num_list[i]);
+        } else if(n == 2){
+            for(int i = a; i < num_list.length; i++) ret.add(num_list[i]);
+        } else if(n == 3){
+            for(int i = a; i <= b; i++) ret.add(num_list[i]);
+        } else if(n == 4){
+            for(int i = a; i <= b; i += c) ret.add(num_list[i]);
+        }
+        return ret;
+    }
+}
+// n = 1 :: 0~b
+// n = 2 :: a~end
+// n = 3 :: a~b
+// n = 4 :: a~b c간격
