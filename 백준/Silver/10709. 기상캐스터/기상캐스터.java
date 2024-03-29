64e2434f7d031c09b7481e61bc739d55d734c41f
Noah <104671487+gratisreise@users.noreply.github.com>
2024-03-29 13:01:30 +0900

    [Silver V] Title: 기상캐스터, Time: 304 ms, Memory: 15948 KB -BaekjoonHub

--- 백준/Silver/10709. 기상캐스터/기상캐스터.java
+++ 백준/Silver/10709. 기상캐스터/기상캐스터.java
@@ -1,33 +1,33 @@
 import java.util.*;
 
-public class Main {
+public class Main{
     static int n, m;
     static int[][] a = new int[104][104];
-
-    public static void main(String[] args) {
+    public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         n = sc.nextInt();
         m = sc.nextInt();
-        for (int i = 0; i < n; i++) {
+        for(int i = 0; i < n; i++){
             String s = sc.next();
-            for (int j = 0; j < m; j++) {
-                if (s.charAt(j) == 'C') a[i][j] = 0;
-                if (s.charAt(j) == '.') a[i][j] = -1;
+            for(int j = 0; j < m; j++){
+                char c = s.charAt(j);
+                if(c == 'c') a[i][j] = 0;
+                else a[i][j] = -1;
             }
         }
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (a[i][j] == 0) {
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if(a[i][j] == 0){
                     int cnt = 1;
-                    while (a[i][j + 1] == -1) {
+                    while(a[i][j + 1] == -1){
                         a[i][j + 1] = cnt++;
                         j++;
                     }
                 }
             }
         }
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
                 System.out.print(a[i][j] + " ");
             }
             System.out.println();
