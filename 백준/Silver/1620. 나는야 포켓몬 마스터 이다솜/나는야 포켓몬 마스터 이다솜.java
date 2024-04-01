7cea70e6f51704eae9a3ca189f1ce1165799988b
Noah <104671487+gratisreise@users.noreply.github.com>
2024-04-01 10:58:37 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 2296 ms, Memory: 118140 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
@@ -1,25 +1,29 @@
 import java.util.*;
 
-public class Main{
-    public static void main(String[] args){
+public class Main {
+    static int n, m;
+    static String[] a = new String[100004];
+    static Map<String, Integer> mp = new HashMap<>();
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int m = sc.nextInt();
-        String[] a = new String[n + 4];
-        Map<String, Integer> mp = new HashMap<>();
-        for(int i = 1; i <= n; i++){
+        n = sc.nextInt();
+        m = sc.nextInt();
+        for(int i = 0; i < n; i++){
             String s = sc.next();
-            a[i] = s;
-            mp.put(s, i);
+            mp.put(s, i + 1);
+            a[i + 1] = s;
         }
         for(int i = 0; i < m; i++){
             String s = sc.next();
             char c = s.charAt(0);
-            if(Character.isDigit(c)){
-                System.out.println(a[Integer.parseInt(s)]);
-            }else{
+            //숫자
+            if('0' <= c && c <= '9'){
+                int num = Integer.parseInt(s);
+                System.out.println(a[num]);
+            } else {
                 System.out.println(mp.get(s));
             }
         }
+
     }
 }
