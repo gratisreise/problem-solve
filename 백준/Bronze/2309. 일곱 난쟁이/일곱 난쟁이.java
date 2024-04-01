0ecabbd9b3f1c52c0641744385a0d81e8245dc21
Noah <104671487+gratisreise@users.noreply.github.com>
2024-04-01 10:28:55 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 80 ms, Memory: 11540 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
@@ -1,26 +1,52 @@
-import java.util.*;
-public class Main{
-    static int[] a = new int[9];
-    static int b, c;
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int[] a = new int[9];
         int sum = 0;
-        for(int i = 0; i < 9; i++){
-            a[i] = sc.nextInt();
+
+        // 입력
+        for (int i = 0; i < 9; i++) {
+            st = new StringTokenizer(br.readLine());
+            a[i] = Integer.parseInt(st.nextToken());
             sum += a[i];
         }
-        for(int i = 0; i < 9; i++){
-            for(int j = 0; j < i; j++){
-                if(sum - a[i] - a[j] == 100){
-                    b = a[i]; c = a[j];
+
+        int num1 = 0;
+        int num2 = 0;
+
+        // 두 난쟁이의 키의 합이 100이 되는 경우 찾기
+        for (int i = 0; i < 9; i++) {
+            for (int j = i + 1; j < 9; j++) {
+                if (sum - a[i] - a[j] == 100) {
+                    num1 = i;
+                    num2 = j;
+                    break;
                 }
             }
         }
-        Arrays.sort(a);
-        for(int i = 0; i < 9; i++){
-            if(a[i] == b || a[i] == c) continue;
-            System.out.println(a[i]);
-        }
-    }
 
+        List<Integer> l = new ArrayList<>();
+
+        // 결과 리스트에 추가
+        for (int i = 0; i < 9; i++) {
+            if (i == num1 || i == num2) continue;
+            l.add(a[i]);
+        }
+
+        // 정렬
+        Collections.sort(l);
+
+        // 출력
+        for (int i : l) System.out.println(i);
+    }
 }
