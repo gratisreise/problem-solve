49088c3923cddc424ec55c2249c340399dd5c818
Noah <104671487+gratisreise@users.noreply.github.com>
2024-04-01 10:43:23 +0900

    [Bronze II] Title: 농구 경기, Time: 80 ms, Memory: 11580 KB -BaekjoonHub

--- 백준/Bronze/1159. 농구 경기/농구 경기.java
+++ 백준/Bronze/1159. 농구 경기/농구 경기.java
@@ -1,20 +1,35 @@
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-public class Main{
+public class Main {
+    static int[] cnt = new int[150];
 
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        int[] cnt = new int[200];
-        int n = sc.nextInt();
-        for(int i = 0; i < n; i++){
-            String s = sc.next();
-            cnt[s.charAt(0)]++;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int n = Integer.parseInt(br.readLine());
+        
+        for (int i = 0; i < n; i++) {
+            String s = br.readLine();
+            char c = s.charAt(0);
+            cnt[c]++;
         }
+
         StringBuilder ret = new StringBuilder();
-        for(int i = 97; i < 150; i++){
-            if(cnt[i] >= 5) ret.append((char)i);
+
+        for (int i = 97; i <= 122; i++) {
+            if (cnt[i] >= 5) {
+                ret.append((char) i);
+            }
         }
-        if(ret.length() == 0) System.out.println("PREDAJA");
-        else System.out.println(ret);
+
+        if (ret.length() > 0) {
+            System.out.println(ret);
+        } else {
+            System.out.println("PREDAJA");
+        }
     }
 }
