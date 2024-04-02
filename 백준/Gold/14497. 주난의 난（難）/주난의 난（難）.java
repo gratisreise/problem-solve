4dc15835ac7ff975b672585455dab6275cabbd66
Noah <104671487+gratisreise@users.noreply.github.com>
2024-04-02 17:48:44 +0900

    [Gold IV] Title: 주난의 난(難), Time: 244 ms, Memory: 24832 KB -BaekjoonHub

--- 백준/Gold/14497. 주난의 난（難）/주난의 난（難）.java
+++ 백준/Gold/14497. 주난의 난（難）/주난의 난（難）.java
@@ -1,60 +1,53 @@
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     static final int mx = 301;
+    static int[] dy ={-1, 0, 1, 0};
+    static int[] dx = {0, 1, 0, -1};
     static int n, m, y1, x1, y2, x2;
-    static int[][] visited = new int[mx][mx];
-    static char[][] a = new char[mx][mx];
-    static final int[] dy = { -1, 0, 1, 0 };
-    static final int[] dx = { 0, 1, 0, -1 };
-    static Queue<Integer> q = new LinkedList<>();
-
+    static int[][] visited;
+    static char[][] a;
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        n = scanner.nextInt();
-        m = scanner.nextInt();
-        y1 = scanner.nextInt();
-        x1 = scanner.nextInt();
-        y2 = scanner.nextInt();
-        x2 = scanner.nextInt();
-        y1--;
-        x1--;
-        y2--;
-        x2--;
-        for (int i = 0; i < n; i++) {
-            a[i] = scanner.next().toCharArray();
-        }
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        m = sc.nextInt();
+        y1 = sc.nextInt();
+        x1 = sc.nextInt();
+        y2 = sc.nextInt();
+        x2 = sc.nextInt();
+        y1--;x1--;y2--;x2--;
+        //y랑 x 체인지
+        visited = new int[n + 1][m + 1];
+        a = new char[n + 1][m + 1];
+        
+        Queue<Integer> q = new LinkedList<>();
+        for(int i = 0; i < n; i++) a[i] = sc.next().toCharArray();
 
         q.add(1000 * y1 + x1);
         visited[y1][x1] = 1;
         int cnt = 0;
 
-        while (a[y2][x2] != '0') {
+        while(a[y2][x2] != '0'){
             cnt++;
             Queue<Integer> temp = new LinkedList<>();
-            while (!q.isEmpty()) {
+            while(!q.isEmpty()){
                 int cur = q.poll();
                 int y = cur / 1000;
                 int x = cur % 1000;
-                for (int i = 0; i < 4; i++) {
+                for(int i = 0; i < 4; i++){
                     int ny = y + dy[i];
                     int nx = x + dx[i];
-                    if (ny < 0 || nx < 0 || ny >= n || nx >= m || visited[ny][nx] != 0)
-                        continue;
+                    if(ny < 0 || nx < 0 || ny >= n || nx >= m) continue;
+                    if(visited[ny][nx] != 0) continue;
                     visited[ny][nx] = cnt;
-                    if (a[ny][nx] != '0') {
+                    if(a[ny][nx] != '0'){
                         a[ny][nx] = '0';
                         temp.add(1000 * ny + nx);
-                    } else {
-                        q.add(1000 * ny + nx);
-                    }
+                    } else q.add(1000* ny + nx);
                 }
             }
             q = temp;
         }
-
         System.out.println(visited[y2][x2]);
     }
 }
