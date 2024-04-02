3fb9d534eb45deaf7e3f5f75d1f7a936b77adc6f
Noah <104671487+gratisreise@users.noreply.github.com>
2024-04-02 12:04:59 +0900

    [Gold V] Title: 치킨 배달, Time: 328 ms, Memory: 24528 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.java
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.java
@@ -1,68 +1,53 @@
 import java.util.*;
 
 public class Main {
-    static int n, m, ret;
-    static int[][] a;
-    static List<Pair> chi, home;
-    static List<List<Integer>> Chi;
-
-    static class Pair {
+    static int n, m, ret = Integer.MAX_VALUE;
+    static int[][] a = new int[54][54];
+    static class Pair{
         int f, s;
-
-        Pair(int f, int s) {
+        Pair(int f, int s){
             this.f = f;
             this.s = s;
         }
     }
-
-    public static void combi(int start, List<Integer> v) {
-        if (v.size() == m) {
-            Chi.add(new ArrayList<>(v));
+    static void combi(int start, List<Integer> l){
+        if(l.size() == m){
+            chi_combi.add(new ArrayList<>(l));
             return;
         }
-        for (int i = start + 1; i < chi.size(); i++) {
-            v.add(i);
-            combi(i, v);
-            v.remove(v.size() - 1);
+        for(int i = start + 1; i < Chi.size(); i++){
+            l.add(i);
+            combi(i, l);
+            l.remove(l.indexOf(i));
         }
     }
-
+    static List<Pair> home = new ArrayList<>();
+    static List<Pair> Chi = new ArrayList<>();
+    static List<List<Integer>> chi_combi = new ArrayList<>();
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         n = sc.nextInt();
         m = sc.nextInt();
-        a = new int[n][n];
-        chi = new ArrayList<>();
-        home = new ArrayList<>();
-        Chi = new ArrayList<>();
-        ret = Integer.MAX_VALUE;
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
                 a[i][j] = sc.nextInt();
-                if (a[i][j] == 1) {
-                    home.add(new Pair(i, j));
-                }
-                if (a[i][j] == 2) {
-                    chi.add(new Pair(i, j));
-                }
+                if(a[i][j] == 1) home.add(new Pair(i, j));
+                else if(a[i][j] == 2) Chi.add(new Pair(i,j));
             }
         }
-
-        List<Integer> v = new ArrayList<>();
-        combi(-1, v);
-
-        for (List<Integer> C : Chi) {
-            int result = 0;
-            for (Pair h : home) { 
-                int _min = Integer.MAX_VALUE;
-                for (int c : C) {
-                    int _dist = Math.abs(h.f - chi.get(c).f) + Math.abs(h.s - chi.get(c).s);
-                    _min = Math.min(_dist, _min);
+        List<Integer> l = new ArrayList<>();
+        combi(-1, l);
+        for(List<Integer> C : chi_combi){
+            int dis_sum = 0;
+            for(Pair h : home){
+                int min = Integer.MAX_VALUE;
+                for(int c : C){
+                    int dis =  Math.abs(h.f - Chi.get(c).f) + Math.abs(h.s - Chi.get(c).s);
+                    min = Math.min(min, dis);
                 }
-                result += _min;
+                dis_sum += min;
             }
-            ret = Math.min(ret, result);
+            ret = Math.min(dis_sum, ret);
         }
         System.out.println(ret);
     }
