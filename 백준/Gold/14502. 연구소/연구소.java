25d8256ab2978b21d63f5c1264b5d05bb565df6d
Noah <104671487+gratisreise@users.noreply.github.com>
2024-04-02 10:04:46 +0900

    [Gold IV] Title: 연구소, Time: 296 ms, Memory: 37352 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.java
+++ 백준/Gold/14502. 연구소/연구소.java
@@ -1,92 +1,68 @@
 import java.util.*;
 
 public class Main {
-    static int n, m, ret = -1;
+    static int n, m, ret = Integer.MIN_VALUE;
     static int[][] a = new int[10][10];
-    static int[][] visited = new int[10][10];
-    static int[][] tempArr = new int[10][10];
-    static ArrayList<Pair> vir = new ArrayList<>();
-    static ArrayList<Pair> wall = new ArrayList<>();
-    static int[] dy = {0, 1, 0, -1};
-    static int[] dx = {-1, 0, 1, 0};
-
-    static class Pair {
-        int first, second;
-        public Pair(int first, int second) {
-            this.first = first;
-            this.second = second;
+    static int[][] visited;
+    static int[] dy = {-1, 0, 1, 0};
+    static int[] dx = {0, 1, 0, -1};
+    static List<Pair> vir = new ArrayList<>();
+    static List<Pair> wall = new ArrayList<>();
+    static class Pair{
+        int f, s;
+        Pair(int f, int s){
+            this.f = f;
+            this.s = s;
         }
     }
-
-    static void copy0(int[][] arr){
-        for(int i = 0; i < 10; i++){
-            for(int j = 0; j < 10; j++){
-                arr[i][j] = 0;
-            }
-        }
-    }
-
-    static void dfs(int y, int x) {
+    static void dfs(int y, int x){ // 벽만나면 멈춤
         visited[y][x] = 1;
-        for (int i = 0; i < 4; i++) {
+        for(int i = 0; i < 4; i++){
             int ny = y + dy[i];
             int nx = x + dx[i];
-            if (ny < 0 || nx < 0 || ny >= n || nx >= m) continue;
-            if (a[ny][nx] == 1 || visited[ny][nx] != 0) continue;
+            if(ny < 0 || nx < 0 || ny >= n || nx >= m) continue;
+            if(a[ny][nx] == 1 || visited[ny][nx] != 0) continue;
+
             dfs(ny, nx);
         }
     }
-
     static void solve(){
+        visited = new int[10][10];
         int cnt = 0;
-        copy0(visited);
-        // 바이러스 퍼짐
+        for(Pair p : vir) dfs(p.f, p.s);
         for(int i = 0; i < n; i++){
             for(int j = 0; j < m; j++){
-                if(a[i][j] == 2 && visited[i][j] == 0){
-                    dfs(i, j);
-                }
-            }
-        }
-        // 바이러스 퍼진 후 안전지대 확인
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < m; j++){
-                if(a[i][j] == 0 && visited[i][j] == 0){
-                    cnt++;
-                }
+                if(a[i][j] != 0 || visited[i][j] != 0) continue;
+                cnt++;
             }
         }
         ret = Math.max(ret, cnt);
     }
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        n = sc.nextInt(); m = sc.nextInt();
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
+        n = sc.nextInt();
+        m = sc.nextInt();
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
                 a[i][j] = sc.nextInt();
-                if (a[i][j] == 2) vir.add(new Pair(i, j));
-                if (a[i][j] == 0) wall.add(new Pair(i, j));
+                if(a[i][j] == 2) vir.add(new Pair(i, j));
+                else if(a[i][j] == 0) wall.add(new Pair(i, j));
             }
         }
 
         for(int i = 0; i < wall.size(); i++){
-            for(int j = 0; j < i; j ++){
+            for(int j = 0; j < i; j++){
                 for(int k = 0; k < j; k++){
-                    a[wall.get(i).first][wall.get(i).second] = 1;
-                    a[wall.get(j).first][wall.get(j).second] = 1;
-                    a[wall.get(k).first][wall.get(k).second] = 1;
+                    a[wall.get(i).f][wall.get(i).s] = 1;
+                    a[wall.get(j).f][wall.get(j).s] = 1;
+                    a[wall.get(k).f][wall.get(k).s] = 1;
                     solve();
-                    a[wall.get(i).first][wall.get(i).second] = 0;
-                    a[wall.get(j).first][wall.get(j).second] = 0;
-                    a[wall.get(k).first][wall.get(k).second] = 0;
+                    a[wall.get(i).f][wall.get(i).s] = 0;
+                    a[wall.get(j).f][wall.get(j).s] = 0;
+                    a[wall.get(k).f][wall.get(k).s] = 0;
                 }
             }
         }
         System.out.println(ret);
     }
-
-    
-
-
 }
