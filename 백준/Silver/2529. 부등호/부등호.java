9758e495445ddc14abec94e846c4f06ec778b9b1
Noah <104671487+gratisreise@users.noreply.github.com>
2024-04-03 12:24:29 +0900

    [Silver I] Title: 부등호, Time: 164 ms, Memory: 25052 KB -BaekjoonHub

--- 백준/Silver/2529. 부등호/부등호.java
+++ 백준/Silver/2529. 부등호/부등호.java
@@ -2,44 +2,37 @@ import java.util.*;
 
 public class Main {
     static int n;
-    static int[] check = new int[10];
-    static char[] a = new char[20];
     static List<String> ret = new ArrayList<>();
+    static int[] cnt = new int[10];
+    static char[] oper = new char[10];
+    static boolean good(char a, char b, char op){
+        if((a > b) && (op == '>')) return true;
+        if((a < b) && (op == '<')) return true;
+        return false;
+    }
+    static void go(int idx, String num){
+        if(idx == n + 1){
+            ret.add(num);
+            return;
+        }
+        for(int i = 0; i <= 9; i++){
+            if(cnt[i] != 0) continue;
+            if((idx == 0) || good(num.charAt(idx - 1), (char)(i + '0'), oper[idx-1])){
+                cnt[i] = 1;
+                go(idx + 1, num + (""+i));
+                cnt[i] = 0;
+            }
+        }
+    }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         n = sc.nextInt();
-
-        for (int i = 0; i < n; i++) {
-            a[i] = sc.next().charAt(0);
+        for(int i = 0; i < n; i++){
+            oper[i] = sc.next().charAt(0);
         }
-
         go(0, "");
-        Arrays.sort(ret.toArray());
-
         System.out.println(ret.get(ret.size() - 1));
         System.out.println(ret.get(0));
-    }
-
-    static boolean good(char x, char y, char op) {
-        if ((x < y) && (op == '<')) return true;
-        if ((x > y) && (op == '>')) return true;
-        return false;
-    }
-
-    static void go(int idx, String num) {
-        if (idx == n + 1) {
-            ret.add(num);
-            return;
-        }
-
-        for (int i = 0; i <= 9; i++) {
-            if (check[i] != 0) continue;
-            if ((idx == 0) || (good(num.charAt(idx - 1), (char) (i + '0'), a[idx - 1]))) {
-                check[i] = 1;
-                go(idx + 1, num + Integer.toString(i));
-                check[i] = 0;
-            }
-        }
     }
 }
