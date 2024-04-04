d87e91f50ae5b44b394db04d40508453f559db21
Noah <104671487+gratisreise@users.noreply.github.com>
2024-04-04 15:10:47 +0900

    [Gold IV] Title: 미세먼지 안녕!, Time: 496 ms, Memory: 147580 KB -BaekjoonHub

--- 백준/Gold/17144. 미세먼지 안녕！/미세먼지 안녕！.java
+++ 백준/Gold/17144. 미세먼지 안녕！/미세먼지 안녕！.java
@@ -1,20 +1,21 @@
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Scanner;
-
+import java.util.*;
 public class Main {
-    static int n, m, t, y, x, ret;
+    static int n, m, t, ret;
     static int[][] a, temp;
     static ArrayList<Pair> v1, v2;
-
     static int[] dy1 = {0, -1, 0, 1};
     static int[] dx1 = {1, 0, -1, 0};
     static int[] dy2 = {0, 1, 0, -1};
     static int[] dx2 = {1, 0, -1, 0};
-
+    static class Pair {
+        int f, s;
+        Pair(int f, int s) {
+            this.f = f;
+            this.s = s;
+        }
+    }
     public static void mise_go(int[] dy, int[] dx) {
-        temp = new int[n][m];
+        temp = new int[n + 1][m + 1];
         Queue<Pair> q = new LinkedList<>();
 
         for (int i = 0; i < n; i++) {
@@ -27,10 +28,9 @@ public class Main {
 
         while (!q.isEmpty()) {
             Pair pair = q.poll();
-            int y = pair.x;
-            int x = pair.y;
+            int y = pair.f;
+            int x = pair.s;
             int spread = a[y][x] / 5;
-
             for (int i = 0; i < 4; i++) {
                 int ny = y + dy[i];
                 int nx = x + dx[i];
@@ -48,7 +48,6 @@ public class Main {
             }
         }
     }
-
     public static ArrayList<Pair> chung(int sy, int sx, int[] dy, int[] dx) {
         ArrayList<Pair> v = new ArrayList<>();
         int cnt = 0;
@@ -74,35 +73,30 @@ public class Main {
 
         return v;
     }
-
     public static void go(ArrayList<Pair> v) {
         for (int i = v.size() - 1; i > 0; i--) {
-            int ny = v.get(i).x;
-            int nx = v.get(i).y;
-            a[ny][nx] = a[v.get(i - 1).x][v.get(i - 1).y];
+            int ny = v.get(i).f;
+            int nx = v.get(i).s;
+            a[ny][nx] = a[v.get(i - 1).f][v.get(i - 1).s];
         }
-
-        a[v.get(0).x][v.get(0).y] = 0;
+        a[v.get(0).f][v.get(0).s] = 0;
     }
-
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        n = scanner.nextInt();
-        m = scanner.nextInt();
-        t = scanner.nextInt();
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        m = sc.nextInt();
+        t = sc.nextInt();
         boolean flag = true;
-        a = new int[n][m];
+        a = new int[n + 1][m + 1];
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                a[i][j] = scanner.nextInt();
+                a[i][j] = sc.nextInt();
                 if (a[i][j] == -1) {
                     if (flag) {
                         v1 = chung(i, j, dy1, dx1);
                         flag = false;
-                    } else {
-                        v2 = chung(i, j, dy2, dx2);
-                    }
+                    } else v2 = chung(i, j, dy2, dx2);
                 }
             }
         }
@@ -125,14 +119,5 @@ public class Main {
         }
 
         System.out.println(ret);
-    }
-
-    static class Pair {
-        int x, y;
-
-        Pair(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
     }
 }
