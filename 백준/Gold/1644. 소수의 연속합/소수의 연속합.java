00e92ee7af2479f27df71d67b797787e4897d93d
Noah <104671487+gratisreise@users.noreply.github.com>
2024-04-04 12:28:16 +0900

    [Gold III] Title: 소수의 연속합, Time: 172 ms, Memory: 25008 KB -BaekjoonHub

--- 백준/Gold/1644. 소수의 연속합/소수의 연속합.java
+++ 백준/Gold/1644. 소수의 연속합/소수의 연속합.java
@@ -1,33 +1,32 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     static int n, p, lo, hi, ret, sum;
-    static boolean[] che;
     static int[] a;
+    static boolean[] che;
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        n = scanner.nextInt();
-        che = new boolean[n + 1];
-        a = new int[n + 1];
-        
-        for (int i = 2; i <= n; i++) {
-            if (che[i]) continue;
-            for (int j = 2 * i; j <= n; j += i) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        che = new boolean[n + 2];
+        a = new int[n/2 + 4];
+        for(int i = 2; i <= n; i++){
+            if(che[i]) continue;
+            for(int j = 2 * i; j <= n; j += i){
                 che[j] = true;
             }
         }
-
-        for (int i = 2; i <= n; i++) {
-            if (!che[i]) a[p++] = i;
+        //p는 0이니깐 ++해주며 인덱스 이동
+        for(int i = 2; i <= n; i++){
+            if(!che[i]) a[p++] = i;
         }
-
-        while (true) {
-            if (sum >= n)  sum -= a[lo++];
-            else if (hi == p) break;
+        //lo는 0 hi는 0
+        while(true){
+            if(sum >= n) sum -= a[lo++];
+            else if(hi == p) break;
             else sum += a[hi++];
-            
-            if (sum == n) ret++;
+            if(sum == n) ret++;
         }
+
         System.out.println(ret);
     }
 }
