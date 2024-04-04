558220800ae6a9d2ef14cd5d945344d883b0dada
Noah <104671487+gratisreise@users.noreply.github.com>
2024-04-04 10:14:46 +0900

    [Silver I] Title: 회의실 배정, Time: 1200 ms, Memory: 168436 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.java
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.java
@@ -12,27 +12,21 @@ public class Main {
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         n = sc.nextInt();
-
         for(int i = 0; i < n; i++){
             int a = sc.nextInt();
             int b = sc.nextInt();
-            l.add(new Pair(b, a));
+            l.add(new Pair(a, b));
         }
-
         Collections.sort(l, (a,b)->{
-           if(a.f == b.f) return a.s - b.s;
-           return a.f - b.f;
+            if(a.s == b.s)return a.f - b.f;
+            return a.s - b.s;
         });
-
-        ret = l.get(0).f;
-
+        ret = l.get(0).s;
         for(int i = 1; i < n; i++){
-            if(l.get(i).s < ret) continue;
-            ret = l.get(i).f; cnt++;
+            if(l.get(i).f < ret) continue;
+            ret = l.get(i).s; cnt++;
         }
-
         System.out.println(cnt);
     }
 }
