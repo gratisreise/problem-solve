d36e59fce8db136f9912418858ee526ce72bd29a
Noah <104671487+gratisreise@users.noreply.github.com>
2024-04-06 12:18:48 +0900

    [Gold V] Title: 동전 2, Time: 92 ms, Memory: 11808 KB -BaekjoonHub

--- 백준/Gold/2294. 동전 2/동전 2.java
+++ 백준/Gold/2294. 동전 2/동전 2.java
@@ -1,24 +1,33 @@
-import java.util.Arrays;
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    static final int INF = (int)1e9;
+    static int n, k;
     static int[] dp = new int[10001];
-    static int n, k, coin;
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        n = sc.nextInt();
-        k = sc.nextInt();
+    static final int INF = 1000000000;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        k = Integer.parseInt(st.nextToken());
+
         Arrays.fill(dp, INF);
         dp[0] = 0;
+
         for (int i = 0; i < n; i++) {
-            coin = sc.nextInt();
+            int coin = Integer.parseInt(br.readLine());
             if (coin >= 10001) continue;
+
             for (int j = coin; j <= k; j++) {
                 dp[j] = Math.min(dp[j], dp[j - coin] + 1);
             }
         }
-        if (dp[k] == INF) System.out.println(-1);
-        else System.out.println(dp[k]);
+
+        if (dp[k] == INF) {
+            System.out.println(-1);
+        } else {
+            System.out.println(dp[k]);
+        }
     }
 }
