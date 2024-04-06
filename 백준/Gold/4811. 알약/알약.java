159f1c44308515435664a603dd4c8baf4890d383
Noah <104671487+gratisreise@users.noreply.github.com>
2024-04-06 12:04:16 +0900

    [Gold V] Title: 알약, Time: 120 ms, Memory: 12412 KB -BaekjoonHub

--- 백준/Gold/4811. 알약/알약.java
+++ 백준/Gold/4811. 알약/알약.java
@@ -1,24 +1,27 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    static long[][] dp;
+    static long[][] dp = new long[31][31];
 
     static long go(int whole, int half) {
         if (whole == 0 && half == 0) return 1;
         if (dp[whole][half] != 0) return dp[whole][half];
+
         long ret = 0;
         if (whole > 0) ret += go(whole - 1, half + 1);
         if (half > 0) ret += go(whole, half - 1);
-        return dp[whole][half] = ret;
+        dp[whole][half] = ret;
+        return ret;
     }
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         while (true) {
-            int n = sc.nextInt();
+            long n = Long.parseLong(br.readLine());
             if (n == 0) break;
-            dp = new long[n + 1][n + 1];
-            System.out.println(go(n, 0));
+            System.out.println(go((int) n, 0));
         }
     }
 }
