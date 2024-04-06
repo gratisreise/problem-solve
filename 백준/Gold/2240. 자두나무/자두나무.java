06986d9799a5035ec770c87513c37cabbb2aa0ff
Noah <104671487+gratisreise@users.noreply.github.com>
2024-04-06 12:01:00 +0900

    [Gold V] Title: 자두나무, Time: 84 ms, Memory: 12056 KB -BaekjoonHub

--- 백준/Gold/2240. 자두나무/자두나무.java
+++ 백준/Gold/2240. 자두나무/자두나무.java
@@ -1,39 +1,31 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Main {
     static int n, m;
-    static int[] b;
-    static int[][][] dp;
+    static int[] b = new int[1004];
+    static int[][][] dp = new int[1004][2][34];
     static final int MIN = Integer.MIN_VALUE;
 
     static int go(int idx, int tree, int cnt) {
         if (cnt < 0) return MIN;
         if (idx == n) return 0;
-
-        int ret = dp[idx][tree][cnt];
-        if (ret != -1) return ret;
-
-        ret = Math.max(go(idx + 1, tree ^ 1, cnt - 1), go(idx + 1, tree, cnt)) + (tree == (b[idx] - 1) ? 1 : 0);
-        return dp[idx][tree][cnt] = ret;
+        if (dp[idx][tree][cnt] != 0) return dp[idx][tree][cnt];
+        int ret = Math.max(go(idx + 1, tree ^ 1, cnt - 1), go(idx + 1, tree, cnt)) + (tree == b[idx] - 1 ? 1 : 0);
+        dp[idx][tree][cnt] = ret;
+        return ret;
     }
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        n = sc.nextInt();
-        m = sc.nextInt();
-        b = new int[n];
-        dp = new int[n][2][34];
-
-        for (int i = 0; i < n; i++) b[i] = sc.nextInt();
-
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < 2; j++) {
-                for (int k = 0; k < 34; k++) {
-                    dp[i][j][k] = -1;
-                }
-            }
+            st = new StringTokenizer(br.readLine());
+            b[i] = Integer.parseInt(st.nextToken());
         }
-
-        System.out.println(Math.max(go(0, 0, m), go(0, 1, m - 1)));
+        int ans = Math.max(go(0, 0, m), go(0, 1, m - 1));
+        System.out.println(ans);
     }
 }
