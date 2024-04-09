afb8d6adbb9a6a73c917725ad076fc1f68072a70
Noah <104671487+gratisreise@users.noreply.github.com>
2024-04-09 10:26:39 +0900

    [level 2] Title: 최솟값 만들기, Time: 1.93 ms, Memory: 53.1 MB -BaekjoonHub

--- 프로그래머스/2/12941. 최솟값 만들기/README.md
+++ 프로그래머스/2/12941. 최솟값 만들기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 최솟값 만들기 - 12941 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12941) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12941?language=java) 
 
 ### 성능 요약
 
-메모리: 52.7 MB, 시간: 1.51 ms
+메모리: 53.1 MB, 시간: 1.93 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 27일 14:37:18
+2024년 04월 09일 10:26:41
 
 ### 문제 설명
 
--- 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.java
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.java
@@ -1,16 +1,14 @@
 import java.util.*;
-class Solution
-{
-    public int solution(int []A, int []B)
-    {
+class Solution{
+    public int solution(int []A, int []B){
         int ret = 0;
+        int n = A.length;
         Arrays.sort(A);
         Arrays.sort(B);
-        
-        for(int i = 0; i < A.length; i++){
-            ret += A[i] * B[B.length - 1 - i];
-        }
-
+        for(int i = 0; i < n; i++) {
+            ret += A[i] * B[n - 1 - i];
+        }        
+        // System.out.println("Hello Java");
         return ret;
     }
 }
