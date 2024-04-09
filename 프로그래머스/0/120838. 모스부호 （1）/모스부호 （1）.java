dc13a3f15fb4a6ff73cbe0f64b732459560d29b0
Noah <104671487+gratisreise@users.noreply.github.com>
2024-04-09 16:26:04 +0900

    [level 0] Title: 모스부호 (1), Time: 0.31 ms, Memory: 75.1 MB -BaekjoonHub

--- 프로그래머스/0/120838. 모스부호 （1）/모스부호 （1）.java
+++ 프로그래머스/0/120838. 모스부호 （1）/모스부호 （1）.java
@@ -1,42 +1,18 @@
-import java.util.*;
 class Solution {
     public String solution(String letter) {
-        Map<String, Character> morseToChar = new HashMap<>();
-        
-        // 주어진 모스 부호를 Map에 저장
-        morseToChar.put(".-", 'a');
-        morseToChar.put("-...", 'b');
-        morseToChar.put("-.-.", 'c');
-        morseToChar.put("-..", 'd');
-        morseToChar.put(".", 'e');
-        morseToChar.put("..-.", 'f');
-        morseToChar.put("--.", 'g');
-        morseToChar.put("....", 'h');
-        morseToChar.put("..", 'i');
-        morseToChar.put(".---", 'j');
-        morseToChar.put("-.-", 'k');
-        morseToChar.put(".-..", 'l');
-        morseToChar.put("--", 'm');
-        morseToChar.put("-.", 'n');
-        morseToChar.put("---", 'o');
-        morseToChar.put(".--.", 'p');
-        morseToChar.put("--.-", 'q');
-        morseToChar.put(".-.", 'r');
-        morseToChar.put("...", 's');
-        morseToChar.put("-", 't');
-        morseToChar.put("..-", 'u');
-        morseToChar.put("...-", 'v');
-        morseToChar.put(".--", 'w');
-        morseToChar.put("-..-", 'x');
-        morseToChar.put("-.--", 'y');
-        morseToChar.put("--..", 'z');
-        
-        String ret = "";
-        String[] s = letter.split(" ");
-        for(String ss : s){
-            ret += morseToChar.get(ss);
+        String[] morse = {".-","-...","-.-.","-..",".","..-.",
+                "--.","....","..",".---","-.-",".-..","--","-.",
+                "---",".--.","--.-",".-.","...","-","..-","...-",
+                ".--","-..-","-.--","--.."};
+        String[] morseString;
+        morseString = letter.split(" ");
+
+        StringBuilder sb = new StringBuilder();
+        for (String word : morseString) {
+            for (int i = 0; i < morse.length; i++) {
+                if (word.equals(morse[i])) sb.append(Character.toString(i + 'a'));
+            }
         }
-        
-        return ret;
+        return sb.toString();
     }
 }
