7a520b132cb3e37fca1107cf25b8adfa07fc11e6
Noah <104671487+gratisreise@users.noreply.github.com>
2024-04-09 16:39:11 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 2.10 ms, Memory: 77.1 MB -BaekjoonHub

--- 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
@@ -1,9 +1,9 @@
 class Solution {
     public String solution(int n) {
         String ret = "";
-        for(int i = 1; i <= n; i++){
-            if(i % 2 == 0) ret += "박";
-            else ret += "수";
+        for(int i = 0; i < n; i++){
+            if(i % 2 == 0) ret += "수";
+            else ret += "박";
         }
         
         return ret;
