a9f9c07b147b68b8fc74ffae45b65e645b5cd087
Noah <104671487+gratisreise@users.noreply.github.com>
2024-04-09 16:13:15 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 5.90 ms, Memory: 80.1 MB -BaekjoonHub

--- 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
@@ -1,12 +1,16 @@
 class Solution {
     public String solution(String[] seoul) {
-        String ret = "" + "김서방은 ";
+        String ret = "";
         int idx = 0;
         for(int i = 0; i < seoul.length; i++){
-            if(seoul[i].equals("Kim")) idx = i;
+            if(seoul[i].equals("Kim")){
+                idx = i;
+                break;
+            }
         }
-        ret += idx;
-        ret += "에 있다";
+        ret = "김서방은 " +idx +"에 있다";
+        
+        
         return ret;
     }
 }
