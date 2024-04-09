c25256ad11fd512d18230f5060bfaffe21f46081
Noah <104671487+gratisreise@users.noreply.github.com>
2024-04-09 14:36:43 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.10 ms, Memory: 75.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public long solution(int n) {
+        long[] dp = new long[n + 4];
+        dp[0] = 0; dp[1] = 1; dp[2] = 2;
+        for(int i = 3; i <= n; i++){
+            dp[i] = (dp[i - 2] + dp[i - 1]) % 1234567;
+        }
+        
+        return dp[n];
+    }
+}
