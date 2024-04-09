c412a01a30f67451c4a329e82165d132fdc9bfea
Noah <104671487+gratisreise@users.noreply.github.com>
2024-04-09 16:20:43 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.02 ms, Memory: 74.3 MB -BaekjoonHub

--- 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -1,11 +1,10 @@
 class Solution {
     public int solution(int[] numbers) {
         int ret = 0;
-        int[] cnt = new int[10];
-        for(int i : numbers) cnt[i]++;
-        for(int i = 0; i < 10; i++){
-            if(cnt[i] == 0) ret += i;
-        }
+        int[] a = new int[10];
+        for(int i : numbers) a[i]++;
+        for(int i = 0; i < 10; i++) if(a[i] == 0) ret += i;
+        
         
         return ret;
     }
