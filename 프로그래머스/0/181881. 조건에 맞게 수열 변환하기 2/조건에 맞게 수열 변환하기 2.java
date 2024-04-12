c751929333c82c2d4fc2ab2f436253e263822262
Noah <104671487+gratisreise@users.noreply.github.com>
2024-04-12 14:44:56 +0900

    [level 0] Title: 조건에 맞게 수열 변환하기 2, Time: 0.02 ms, Memory: 75.4 MB -BaekjoonHub

--- 프로그래머스/0/181881. 조건에 맞게 수열 변환하기 2/조건에 맞게 수열 변환하기 2.java
+++ 프로그래머스/0/181881. 조건에 맞게 수열 변환하기 2/조건에 맞게 수열 변환하기 2.java
@@ -1,35 +1,28 @@
 import java.util.*;
 class Solution {
-    static boolean check(int[] arr1, int[] arr2){
-        int n = arr1.length;
-        boolean flag = true;
-        for(int i = 0; i < n; i++){
-            if(arr1[i] != arr2[i]) flag = false;
-        }
-        
-        return flag;
-    }
-    static void print(int[] arr){
-        for(int i : arr){
-            System.out.print(i + " ");
-        }
-    }
     public int solution(int[] arr) {
         int ret = 0;
-        int[] prev = new int[arr.length];
+        //작업 반복
         while(true){
-            prev = Arrays.copyOf(arr, arr.length);
+            //작업실행
+            int[] temp = Arrays.copyOf(arr, arr.length);
             for(int i = 0; i < arr.length; i++){
-                if(arr[i] % 2 == 0 && arr[i] >= 50){
-                    arr[i] /= 2;
-                } else if(arr[i] % 2 != 0 && arr[i] < 50){
-                    arr[i] = (arr[i] * 2) + 1;
+                if(arr[i] >= 50 && arr[i] % 2==0){
+                    arr[i] = arr[i] / 2;
+                } else if(arr[i] < 50 && arr[i] % 2 != 0){
+                    arr[i] = arr[i] * 2 + 1;
                 }
             }
+            boolean flag = false;
+            for(int i = 0; i < arr.length; i++){
+                if(arr[i] != temp[i]){
+                    flag = true;
+                    break;
+                }
+            }
+            if(!flag) break;
             ret++;
-            if(check(prev, arr)) break;
         }
-        ret--;
         return ret;
     }
 }
