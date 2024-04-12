de9c81a72fffc31ab35e576dc68a4ffa7b505c2b
Noah <104671487+gratisreise@users.noreply.github.com>
2024-04-12 15:25:12 +0900

    [level 1] Title: 행렬의 덧셈, Time: 8.80 ms, Memory: 108 MB -BaekjoonHub

--- 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.java
+++ 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.java
@@ -1,10 +1,10 @@
 class Solution {
     public int[][] solution(int[][] arr1, int[][] arr2) {
-        int row = arr1.length;
-        int col = arr1[0].length;
-        int[][] ret = new int[row][col];
-        for(int i = 0; i < row; i++){
-            for(int j = 0; j < col; j++){
+        int n = arr1.length;
+        int m = arr1[0].length;
+        int[][] ret = new int[n][m];
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
                 ret[i][j] = arr1[i][j] + arr2[i][j];
             }
         }
