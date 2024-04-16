eaa31034da0d35a9f575ee0cc69f04f98ab094a0
Noah <104671487+gratisreise@users.noreply.github.com>
2024-04-16 10:48:13 +0900

    [level 1] Title: 시저 암호, Time: 48.32 ms, Memory: 115 MB -BaekjoonHub

--- 프로그래머스/1/12926. 시저 암호/시저 암호.java
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.java
@@ -1,19 +1,17 @@
 class Solution {
     public String solution(String s, int n) {
         String ret = "";
+        
         for(char c : s.toCharArray()){
-            if(c == ' '){
-                ret += " ";
-            } else {
-                if('a' <= c && c <= 'z'){
-                    if(c + n > 'z') ret += (char)(c + n - 26);
-                    else ret += (char)(c + n);
-                } else if('A' <= c && c <= 'Z'){
-                    if(c + n > 'Z') ret += (char)(c + n - 26);
-                    else ret +=(char)(c + n);
-                }
-            }
+            if('a' <= c && c <= 'z'){
+                if(c + n > 'z') ret += (char)(c - 26 + n);
+                else ret += (char)(c + n);
+            } else if('A' <= c && c <= 'Z'){
+                if(c + n > 'Z') ret += (char)(c - 26 + n);
+                else ret += (char)(c + n);
+            } else ret += c;
         }
+        
         return ret;
     }
 }
