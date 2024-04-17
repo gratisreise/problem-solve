8618e21296782951369d1280162305e5b679901b
Noah <104671487+gratisreise@users.noreply.github.com>
2024-04-17 12:53:55 +0900

    [level 1] Title: 소수 만들기, Time: 0.48 ms, Memory: 77.4 MB -BaekjoonHub

--- 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
@@ -1,27 +1,29 @@
 import java.util.*;
 class Solution {
-    static boolean check(int n){
-        if(n <= 1) return false;
-        if(n == 2) return true;
-        if(n % 2 == 0) return false;
-        for(int i = 2; i * i <= n; i++){
-            if(n % i == 0) return false;
-        }
-        return true;
-    }
     public int solution(int[] nums) {
-        int ret = 0;        
+        int ret = 0;
+        Arrays.sort(nums);
+        int max = nums[nums.length - 1] + nums[nums.length - 2] + nums[nums.length - 3];
+        int[] che = new int[max + 4];
+        
+        for(int i = 2; i <= max; i++){
+            if(che[i] == 1) continue;
+            for(int j = i * 2; j <= max; j += i){
+                che[j] = 1;
+            }
+        }
         
         for(int i = 0; i < nums.length; i++){
             for(int j = 0; j < i; j++){
                 for(int k = 0; k < j; k++){
                     int sum = nums[i] + nums[j] + nums[k];
-                    if(check(sum)) ret++;
+                    if(che[sum] == 1) continue;
+                    ret++;
                 }
             }
         }
         
-            
+        
         return ret;
     }
 }
