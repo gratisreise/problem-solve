878d75221328c180ab5c3fce9f2ede302c58f858
Noah <104671487+gratisreise@users.noreply.github.com>
2024-04-17 09:54:43 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 11.94 ms, Memory: 73.5 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -1,14 +1,13 @@
 class Solution {
     public String solution(int[] food) {
-        String ret = "";
-        ret += "0";
-        for(int i = food.length - 1; i >= 1; i--){
-            int k = (food[i] / 2);
-            String temp = "" + i;
-            for(int j = 0; j < k; j++){
-                ret = temp + ret;
-                ret += temp;
-            } 
+        String ret = "0";
+        for(int i = food.length - 1 ; i > 0; i--){
+            food[i] -= food[i] % 2;
+            if(food[i] == 0) continue;
+            for(int j = 0; j < food[i]; j += 2){
+                ret = "" + i + ret;
+                ret = ret + "" + i;
+            }
         }
         return ret;
     }
