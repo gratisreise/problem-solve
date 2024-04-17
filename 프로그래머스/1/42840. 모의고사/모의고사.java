a043a41868a3c46a744caa7b19eaa7435442b936
Noah <104671487+gratisreise@users.noreply.github.com>
2024-04-17 11:47:44 +0900

    [level 1] Title: 모의고사, Time: 0.67 ms, Memory: 71.9 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -1,35 +1,23 @@
 import java.util.*;
 class Solution {
-    public  List<Integer> solution(int[] ans) {
-        int[] cnt1 = new int[ans.length];
-        int[] cnt2 = new int[ans.length];
-        int[] cnt3 = new int[ans.length];
-        int[] forCnt2 = {2, 1, 2, 3, 2, 4, 2, 5};
-        int[] forCnt3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
-        for(int i = 0; i < ans.length; i++){
-            cnt1[i] = i % 5 + 1;
-            cnt2[i] = forCnt2[i % forCnt2.length];
-            cnt3[i] = forCnt3[i % forCnt3.length];
-            System.out.println(cnt1[i] + "::" + cnt2[i] + "::" + cnt3[i]);
+    static List<Integer> l = new ArrayList<>();
+    public List<Integer> solution(int[] answers) {
+        int n = answers.length;
+        int[] cnt = new int[3];
+        int max = -1;
+        int[] num1 = {1, 2, 3, 4, 5};
+        int[] num2 = {2, 1, 2, 3, 2, 4, 2, 5};
+        int[] num3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+        int n1 = num1.length;
+        int n2 = num2.length;
+        int n3 = num3.length;
+        for(int i = 0; i < n; i++){
+            if(num1[i % n1] == answers[i]) cnt[0]++;
+            if(num2[i % n2] == answers[i]) cnt[1]++;
+            if(num3[i % n3] == answers[i]) cnt[2]++;
         }
-        //각자 개수 세기
-        int count1 = 0;
-        int count2 = 0;
-        int count3 = 0;
-        for(int i = 0; i < ans.length; i++){
-            if(ans[i] == cnt1[i]) count1++;
-            if(ans[i] == cnt2[i]) count2++;
-            if(ans[i] == cnt3[i]) count3++;
-        }
-        int[] countSet = {count1, count2, count3};
-        int countMx = -1;
-        for(int i : countSet) if(i > countMx) countMx = i;
-        List<Integer> ret = new ArrayList<>();
-        for(int i = 0; i < countSet.length; i++){
-            if(countSet[i] == countMx) ret.add(i + 1);
-        }
-        return ret;
-        
-        
+        for(int i = 0; i < 3; i++) max = Math.max(cnt[i], max);
+        for(int i = 0; i < 3; i++) if(cnt[i] == max) l.add(i + 1);
+        return l;
     }
 }
