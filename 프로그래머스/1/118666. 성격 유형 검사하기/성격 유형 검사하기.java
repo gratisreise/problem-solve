12142a4b1bc4f21b8d437e5dfe4527d581cf64ef
Noah <104671487+gratisreise@users.noreply.github.com>
2024-04-25 10:21:15 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 29.42 ms, Memory: 70.5 MB -BaekjoonHub

--- 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.java
+++ 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.java
@@ -1,37 +1,51 @@
 import java.util.*;
 class Solution {
-    
-    static Map<Character, Integer> mp = new HashMap<>();
+    static class Pair{
+        char f; int s;
+        Pair(char f, int s){
+            this.f = f;
+            this.s = s;
+        }
+    }
+    static void calc(Pair[] pArr, char c, int score){
+        for(Pair p : pArr){
+            if(p.f == c) {
+                p.s += score;
+                break;
+            }
+        }
+    } 
     public String solution(String[] survey, int[] choices) {
         String ret = "";
-        String[] temp1 = {"RT", "CF", "JM", "AN"};
-        for(String s : temp1) {
-            for(char c : s.toCharArray()){
-                mp.put(c, 0);
-            }
-        }
+        Pair[] pArr = new Pair[8];
+        pArr[0] = new Pair('R', 0);
+        pArr[1] = new Pair('T', 0);
+        pArr[2] = new Pair('C', 0);
+        pArr[3] = new Pair('F', 0);
+        pArr[4] = new Pair('J', 0);
+        pArr[5] = new Pair('M', 0);
+        pArr[6] = new Pair('A', 0);
+        pArr[7] = new Pair('N', 0);
         
         for(int i = 0; i < survey.length; i++){
-            char c0 = survey[i].charAt(0);
-            char c1 = survey[i].charAt(1);
-            if(choices[i] < 4){ //순서가 다르다?
-                mp.put(c0, mp.get(c0) + (4 - choices[i]));
-            } else if(choices[i] > 4){
-                mp.put(c1, mp.get(c1) + (choices[i] - 4));
+            int score = choices[i];
+            String s = survey[i];
+            if(score < 4) {
+                char c = s.charAt(0);
+                score = 4 - score;
+                calc(pArr, c, score);
+            } else if(score > 4){
+                char c = s.charAt(1);
+                score %= 4;
+                calc(pArr, c, score);
             }
         }
-        
-        for(Character c : mp.keySet()){
-            System.out.println(c + " :: "+ mp.get(c));
-        }
-        for(String s : temp1){
-            char c0 = s.charAt(0);
-            char c1 = s.charAt(1);
-            if(mp.get(c0) >= mp.get(c1)) ret += c0;
-            else ret += c1;
+        // for(Pair p : pArr){
+        //     System.out.println(p.f + "::" + p.s);
+        // }
+        for(int i = 0; i < 8; i += 2){
+            ret += (pArr[i].s >= pArr[i + 1].s) ? pArr[i].f : pArr[i + 1].f;
         }
         return ret;
     }
 }
-// char : int 맵에서 해당하는 문자에 따라 넣어주기
-// temp1을 순회하면서 map의 숫자를 순회
