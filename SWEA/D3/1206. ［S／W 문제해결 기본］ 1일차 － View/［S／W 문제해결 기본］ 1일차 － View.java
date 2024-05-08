b541ee6faca9decc895f238c036d1e1a50399f42
Noah <104671487+gratisreise@users.noreply.github.com>
2024-05-08 12:40:06 +0900

    [D3] Title: [S/W 문제해결 기본] 1일차 - View, Time: 186 ms, Memory: 45,320 KB -BaekjoonHub

--- SWEA/D3/1206. ［S／W 문제해결 기본］ 1일차 － View/［S／W 문제해결 기본］ 1일차 － View.java
+++ SWEA/D3/1206. ［S／W 문제해결 기본］ 1일차 － View/［S／W 문제해결 기본］ 1일차 － View.java
@@ -1,29 +1,39 @@
 import java.util.*;
 
 public class Solution {
+    static int n, ret;
     static int[] a;
-    static int ret;
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        int count = 10;
-        while(count-- > 0){
-            ret = 0;
-            int n = sc.nextInt();
-            a = new int[n + 4];
-            for(int i = 0; i < n; i++) a[i] = sc.nextInt();
-            for(int i = 2; i < n - 2; i++){
-                if(a[i] > a[i - 2] && a[i] > a[i - 1] && a[i] > a[i + 1] && a[i] > a[i + 2]){
-                    List<Integer> l = new ArrayList<>();
-                    for(int j = i - 2; j <= i + 2; j++){
-                        if(j == i) continue;
-                        l.add(a[i] - a[j]);
-                    }
-                    Collections.sort(l);
-                    ret += l.get(0);
-                }
-            }
-            System.out.println( "#" +(10-count)+" "+ ret);
+
+    static int check(int idx) {
+        int x = idx - 2;
+        int y = idx + 2;
+        int ans = 0;
+        for (int i = Math.max(x, 0); i <= Math.min(y, 1000); i++) {
+            if (i == idx) continue;
+            ans = Math.max(a[i], ans);
         }
+        if (a[idx] - ans <= 0) return 0;
+
+        return a[idx] - ans;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        for (int k = 1; k <= 10; k++) {
+            ret = 0;
+            a = new int[1004];
+            n = sc.nextInt();
+            for (int i = 0; i < n; i++) a[i] = sc.nextInt();
+            for (int i = 0; i < n; i++) ret += check(i);
+
+            System.out.println("#" + k + " " + ret);
+        }
+
     }
 }
 
+/*
+1. 해당 인덱스에서 양쪽 길이 2 이내에 자기보다 높은 건물이 없으면 ++
+
+ */
