fab8265b32bde06d00528a51d669b0a4114e429f
Noah <104671487+gratisreise@users.noreply.github.com>
2024-05-09 12:46:45 +0900

    [Silver IV] Title: 좋은 단어, Time: 268 ms, Memory: 22644 KB -BaekjoonHub

--- 백준/Silver/3986. 좋은 단어/좋은 단어.java
+++ 백준/Silver/3986. 좋은 단어/좋은 단어.java
@@ -1,24 +1,35 @@
+import java.io.*;
 import java.util.*;
 
-public class Main {
-    static int ret;
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
+public class Main{
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        int ret = 0;
         
-        scanner.nextLine(); // Consume the newline character
-
-        for (int i = 0; i < n; i++) {
-            String s = scanner.nextLine();
-            Stack<Character> stack = new Stack<>();
-
-            for (char c : s.toCharArray()) {
-                if (!stack.isEmpty() && stack.peek() == c) stack.pop();
-                else stack.push(c);
+        while(n-- > 0){
+            String s = br.readLine();
+            Stack<Character> stk = new Stack<>();
+            for(char c : s.toCharArray()){
+                if(!stk.isEmpty() && stk.peek() == c) stk.pop();
+                else stk.push(c);
             }
-            if (stack.isEmpty()) ret++;
+            if(stk.isEmpty()) ret++;
         }
 
-        System.out.println(ret);
+        bw.write(String.valueOf(ret)+'\n');
+        bw.flush();
     }
 }
+/*
+짝짓기 -> 스택사용
+1. 단어의 수 n 입력받기
+2. n번만큼 문자열 입력받기
+3. 문자열 순회함녀서 stack에넣기
+    3-1. 스택의 윗문자랑 넣어줘야하는 문자가 같으면 pop
+    3-2. 아니면 그냥 스택에 넣기
+4. 스택이 비워져 있으면 ret++
+5. ret 출력
+*/
