16ee66c972072d90a11afd55c060567834a4ec96
Noah <104671487+gratisreise@users.noreply.github.com>
2024-05-09 11:04:52 +0900

    [Silver III] Title: 한국이 그리울 땐 서버에 접속하지, Time: 80 ms, Memory: 11692 KB -BaekjoonHub

--- 백준/Silver/9996. 한국이 그리울 땐 서버에 접속하지/한국이 그리울 땐 서버에 접속하지.java
+++ 백준/Silver/9996. 한국이 그리울 땐 서버에 접속하지/한국이 그리울 땐 서버에 접속하지.java
@@ -1,28 +1,41 @@
+import java.io.*;
 import java.util.*;
 
-public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        sc.nextLine(); 
+public class Main{
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        String s = sc.nextLine();
+        int n = Integer.parseInt(br.readLine());
+        String s = br.readLine();
+
         int pos = s.indexOf('*');
-        String pre_s = s.substring(0, pos);
-        String suf_s = s.substring(pos + 1);
+        String preS = s.substring(0, pos);
+        String sufS = s.substring(pos + 1);
 
-        for (int i = 0; i < n; i++) {
-            s = sc.nextLine();
-            if (s.length() < pre_s.length() + suf_s.length()) {
-                System.out.println("NE");
-            } else {
-                if (s.startsWith(pre_s) && s.endsWith(suf_s)) {
-                    System.out.println("DA");
-                } else {
-                    System.out.println("NE");
-                }
+        for(int i = 0; i < n; i++){
+            s = br.readLine();
+            int pre = preS.length();
+            int suf = sufS.length();
+            if(s.length() < pre + suf) bw.write("NE\n");
+            else {
+                String temp1 = s.substring(0, pre);
+                String temp2 = s.substring(s.length() - suf);
+                if(temp1.equals(preS) && temp2.equals(sufS)){
+                    bw.write("DA\n");
+                } else bw.write("NE\n");
             }
         }
-        
+
+        bw.flush();
     }
 }
+/*
+1. 파일의 갯수 n 입력 받기
+2. 패턴 입력받고 앞 뒤 패턴 문자열에 저장
+3. n번만큼 문자열 입력받기
+    3-1. 문자열 길이가 앞 + 뒤 길이보다 작을때 NE
+    3-2. 문자열 길이가 앞 + 뒤 길이보다 길때
+        3-2-1. 패턴맞으면 DA
+        3-2-2. 안맞으면 NE
+ */
