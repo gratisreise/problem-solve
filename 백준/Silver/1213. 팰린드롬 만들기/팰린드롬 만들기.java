5e3d0f962fa7e5396ef393ffbb73fbb8cf404460
Noah <104671487+gratisreise@users.noreply.github.com>
2024-05-09 12:21:32 +0900

    [Silver III] Title: 팰린드롬 만들기, Time: 76 ms, Memory: 11584 KB -BaekjoonHub

--- 백준/Silver/1213. 팰린드롬 만들기/팰린드롬 만들기.java
+++ 백준/Silver/1213. 팰린드롬 만들기/팰린드롬 만들기.java
@@ -1,26 +1,52 @@
-import  java.util.*;
+import java.io.*;
 
 public class Main{
     static int[] cnt = new int[200];
-    static char mid = 0;
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        String s = sc.next();
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String s = br.readLine();
         for(char c : s.toCharArray()) cnt[c]++;
+
         int flag = 0;
-        StringBuilder ret = new StringBuilder();
+        char mid = ' ';
+        String ret = "";
+
         for(int i = 'Z'; i >= 'A'; i--){
-            if(cnt[i] % 2 != 0){
-                flag++;cnt[i]--;mid = (char)i;
+            if(cnt[i] % 2 == 1){ // 홀수이면
+                cnt[i]--; flag++;
+                mid = (char)i;
             }
-            if(flag == 2) break;
+            if(flag == 2) {
+                bw.write("I'm Sorry Hansoo\n");
+                bw.flush();
+                return;
+            }
             for(int j = 0; j < cnt[i]; j += 2){
-                ret.insert(0, (char)i);
-                ret.append((char)i);
+                ret = (char)i + ret;
+                ret += (char)i;
             }
         }
-        if(mid != 0) ret.insert(ret.length()/2, mid);
-        if(flag == 2) System.out.println("I'm Sorry Hansoo");
-        else System.out.println(ret);
+        if(mid != ' ') {
+            int pos = ret.length()/2;
+            ret = ret.substring(0, pos) + mid + ret.substring(pos);
+        }
+
+        bw.write(ret + '\n');
+        bw.flush();
     }
 }
+/*
+cnt배열 이용해서 출력
+1. 문자열 입력받기
+2. 문자열을 순회하면서 cnt에 문자별 갯수 저장
+3. Z->A순으로 순회하기
+    3-1.문자갯수 홀수
+        3-1-1.갯수-1, 홀수갯수+1, 삽입할 문자 에넣기
+        3-1-1.문자열 앞뒤에 문자넣어주기
+    3-2.문자갯수 짝수
+        3-2-1.문자열 앞뒤에 문자넣어주기
+4.펠린드롬 불가능 -> I'm Sorry Hansoo
+5.펠린드롬 가능 -> 펠린드롬 출력
+ */
