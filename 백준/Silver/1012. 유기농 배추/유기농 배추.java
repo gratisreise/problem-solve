47139659509b141dc9705593d600389f18ca8493
Noah <104671487+gratisreise@users.noreply.github.com>
2024-05-10 11:32:53 +0900

    [Silver II] Title: 유기농 배추, Time: 108 ms, Memory: 13504 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.java
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.java
@@ -1,12 +1,11 @@
+import java.io.*;
 import java.util.*;
 
 public class Main{
-    static int t, n, m, k, y, x;
-    static int[][] a;
-    static int[][] visited;
-    static final int[] dy = {-1, 0, 1, 0};
-    static final int[] dx = {0, 1, 0, -1};
-
+    static int t, n, m, k, y, x, ret;
+    static int[][] a, visited;
+    static int[] dy = {-1, 0, 1, 0};
+    static int[] dx = {0, 1, 0, -1};
     static void dfs(int y, int x){
         visited[y][x] = 1;
         for(int i = 0; i < 4; i++){
@@ -18,34 +17,47 @@ public class Main{
         }
     }
 
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        t = sc.nextInt();
-
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String rs;
+        StringTokenizer st;
+        t = Integer.parseInt(br.readLine());
         while(t-- > 0){
-            int ret = 0;
-            a = new int[54][54];
-            visited = new int[54][54];
-            
-            m = sc.nextInt();
-            n = sc.nextInt();
-            k = sc.nextInt();
+            st = new StringTokenizer(br.readLine());
+            m = Integer.parseInt(st.nextToken());
+            n = Integer.parseInt(st.nextToken());
+            k = Integer.parseInt(st.nextToken());
+            ret = 0;
+            a = new int[n + 4][m + 4];
+            visited = new int[n + 4][m + 4];
 
-            for(int i = 0; i < k; i++){
-                x = sc.nextInt();
-                y = sc.nextInt();
+            //배추심기
+            while(k-- > 0){
+                st = new StringTokenizer(br.readLine());
+                x = Integer.parseInt(st.nextToken());
+                y = Integer.parseInt(st.nextToken());
                 a[y][x] = 1;
             }
 
-            
             for(int i = 0; i < n; i++){
                 for(int j = 0; j < m; j++){
                     if(a[i][j] == 0 || visited[i][j] != 0) continue;
                     dfs(i, j); ret++;
                 }
             }
-            
-            System.out.println(ret);
-        }   
+
+            bw.write(String.valueOf(ret)+'\n');
+            bw.flush();
+        }
+
     }
 }
+/*
+t입력
+x: m, y : n, 배추위치:k
+k만큼 배추 >>x >>y 들어온다.
+a2차원 배열에 배추 심기
+각 테스트 케이스마다 dfs로 연결컴포넌트 갯수 구하기
+출력하기
+ */
