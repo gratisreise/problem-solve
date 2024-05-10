d190808b40423431df30188994a947fe7a7dedcf
Noah <104671487+gratisreise@users.noreply.github.com>
2024-05-10 10:45:39 +0900

    [Silver IV] Title: 주몽, Time: 348 ms, Memory: 15896 KB -BaekjoonHub

--- 백준/Silver/1940. 주몽/주몽.java
+++ 백준/Silver/1940. 주몽/주몽.java
@@ -1,21 +1,36 @@
+import java.io.*;
 import java.util.*;
 
-public class Main{
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int m = sc.nextInt();
-        int[] a = new int[15004];
-        for(int i = 0; i < n; i++) a[i] = sc.nextInt();
-        int ret = 0;
-        if(m > 200000) System.out.println(ret);
-        else{
-            for(int i = 0; i < n; i++){
-                for(int j = 0; j < i; j++){
-                    if(a[i] + a[j] == m) ret++;
+public class Main {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        int m = Integer.parseInt(br.readLine());
+        int[] a = new int[n + 4];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            a[i] = Integer.parseInt(st.nextToken());
+        }
+
+        if (m > 200000)  bw.write("0\n");
+        else {
+            int ret = 0;
+            for (int i = 0; i < n; i++) {
+                for (int j = i + 1; j < n; j++) {
+                    if (a[i] + a[j] == m) ret++;
                 }
             }
-            System.out.println(ret);
+            bw.write(String.valueOf(ret)+"\n");
         }
+        bw.flush();
     }
 }
+/*
+재료의 개수에서 2개를 선택해서 더했을 때 M이면 출력
+1. M이 20만보다 크면 그냥 1 출력하고 끝
+2. 아니면 2중 for문으로 조합을 돌린다.
+3. 선택된 두수를 더해서 m이랑 맞느지 체크
+4. 맞으면 ret++ 아니면 넘기기
+ */
