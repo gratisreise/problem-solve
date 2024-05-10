01baeb67e817c14f67d8e5e791e5428f2654f31c
Noah <104671487+gratisreise@users.noreply.github.com>
2024-05-10 10:31:09 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 76 ms, Memory: 11588 KB -BaekjoonHub

--- 백준/Bronze/10808. 알파벳 개수/알파벳 개수.java
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.java
@@ -1,13 +1,25 @@
+import java.io.*;
 import java.util.*;
 
 public class Main{
-    static int[] cnt = new int[26];
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        String s = sc.next();
-        for(char c : s.toCharArray()){
-            cnt[c - 'a']++;
+    static int[] cnt = new int[200];
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String rs;
+        StringTokenizer st;
+        rs = br.readLine();
+        for(char c : rs.toCharArray()) cnt[c]++;
+        for(int i = 'a'; i <= 'z'; i++){
+            bw.write(String.valueOf(cnt[i]) + " ");
         }
-        for(int i : cnt) System.out.print(i + " ");
+        bw.write('\n');
+        bw.flush();
     }
 }
+
+/*
+1. 문자열을 입력받는다.
+2. 문자열을 순회하면서 cnt배열에 ++해준다.
+3. 'a' ~ 'z' 까지 cnt배열을 순회하면서 숫자를 출력한다. 
+ */
