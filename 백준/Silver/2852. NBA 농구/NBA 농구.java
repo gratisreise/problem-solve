da02299f16163aa2a4f8c9bb027a4a2ffa34e301
Noah <104671487+gratisreise@users.noreply.github.com>
2024-05-11 16:08:02 +0900

    [Silver III] Title: NBA 농구, Time: 84 ms, Memory: 11880 KB -BaekjoonHub

--- 백준/Silver/2852. NBA 농구/NBA 농구.java
+++ 백준/Silver/2852. NBA 농구/NBA 농구.java
@@ -1,43 +1,62 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
-public class Main {
-    static String print(int sum) {
-        int hours = sum / 60;
-        int minutes = sum % 60;
-        return String.format("%02d:%02d", hours, minutes);
+public class Main{
+    static int A, B, team, n, retA, retB;
+    static String time, prev = "00:00";
+
+    static int sum(){
+        return mToS(time) - mToS(prev);
     }
-
-    static int change(String s) {
-        String[] parts = s.split(":");
-        return Integer.parseInt(parts[0]) * 60 + Integer.parseInt(parts[1]);
+    static int mToS(String s){
+        String[] temp = s.split(":");
+        int a = Integer.parseInt(temp[0]) * 60;
+        int b = Integer.parseInt(temp[1]);
+        return a + b;
     }
+    static String sToM(int n){
+        String a = ("00" + n / 60);
+        String b = ("00" + n % 60);
+        return a.substring(a.length()-2) + ":" + b.substring(b.length()-2);
+    }
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String rs;
+        StringTokenizer st;
+        st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
 
+        while(n-- > 0){
+            st = new StringTokenizer(br.readLine());
+            team = Integer.parseInt(st.nextToken());
+            time = st.nextToken();
+            if(A > B) retA += sum();
+            else if(B > A) retB += sum();
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int A = 0, B = 0;
-        int asum = 0, bsum = 0;
-        String T, prev = "00:00";
-
-        for (int i = 0; i < n; i++) {
-            int tnum = sc.nextInt();
-            T = sc.next();
-
-            if (A > B) asum += change(T) - change(prev);
-            if (B > A) bsum += change(T) - change(prev);
-            if (tnum == 1) {
-                A++;
-            } else {
-                B++;
-            }
-            prev = T;
+            if(team == 1) A++; else B++;
+            prev = time;
         }
-
-        if (A > B) asum += change("48:00") - change(prev);
-        if (B > A) bsum += change("48:00") - change(prev);
-
-        System.out.println(print(asum));
-        System.out.println(print(bsum));
-    }   
+        time = "48:00";
+        if(A > B) retA += sum();
+        else if(B > A) retB += sum();
+        bw.write(sToM(retA)+'\n');
+        bw.write(sToM(retB)+'\n');
+        bw.flush();
+    }
 }
+/*
+골이 들어간 시간 : 팀 이름
+입력: 골이 들어간 횟수 n, n줄에 팀번호 :: 시간
+누가 유리하지? -> 변수에 골수를 담아서 비교하자
+어떻게 계산하지? -> 골이 들어갈 때마다 유불리를 비교해서 체크하자
+언제 계산해야하지? -> 골이 들어간순간 유불리가 바뀔수 있으니 골이 입력되기전에 계산로직을넣자
+시작과 끝의 경계처리
+00:00부터 계산
+48:00끝 계산
+
+1. 골횟수 n 입력.
+2. n번 팀번호 - 시간 입력받기
+3. 입력되기 전에 이전 시간과 비교하여 유불리비교해서 시간 계산
+4. 끝나는 시간에 유불리 비교해서 시간계산
+ */
