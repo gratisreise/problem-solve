b00be23d6fd4700736c72b6f97e072f6c51d2529
Noah <104671487+gratisreise@users.noreply.github.com>
2024-05-11 21:06:49 +0900

    [Silver IV] Title: 괄호, Time: 88 ms, Memory: 11820 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.java
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -1,30 +1,37 @@
+import java.io.*;
 import java.util.*;
 
-public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        scanner.nextLine(); // Consume the newline character
-
-        for (int i = 0; i < n; i++) {
-            String s = scanner.nextLine();
-            if (isBalanced(s)) {
-                System.out.println("YES");
-            } else {
-                System.out.println("NO");
+public class Main{
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+        st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        while(n-- > 0){
+            Stack<Character> stk = new Stack<>();
+            String s = br.readLine();
+            for(char c : s.toCharArray()){
+                if(!stk.isEmpty() && stk.peek() == '(' && c == ')') stk.pop();
+                else stk.push(c);
             }
+            if(stk.isEmpty()) bw.write("YES\n");
+            else bw.write("NO\n");
+            bw.flush();
         }
-    }
 
-    public static boolean isBalanced(String s) {
-        Stack<Character> stack = new Stack<>();
-        for (char c : s.toCharArray()) {
-            if (!stack.isEmpty() && c == ')' && stack.peek() == '(') {
-                stack.pop();
-            } else {
-                stack.push(c);
-            }
-        }
-        return stack.isEmpty();
     }
 }
+/*
+괄호짝 맞는지 체크하는 문제
+1. n 입력받기
+2. 문자열 입력받기
+3. 문자열 순회
+    3-1. stack이 비어있으면
+        3-1-1. 문자 넣기
+    3-2. stack이 안 비어있으면
+        3-2-1. peek()==( 이고 c == ')' 이면 pop()
+4. stack이 체크
+    4-1. 비어있으면 YES
+    4-2. 안 비어있으면 NO
+*/
