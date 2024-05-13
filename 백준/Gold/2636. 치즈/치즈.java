1d0a9f145f4712f4dd09c1d73a26c9a4defc55e5
Noah <104671487+gratisreise@users.noreply.github.com>
2024-05-13 10:15:45 +0900

    [Gold IV] Title: 치즈, Time: 100 ms, Memory: 12948 KB -BaekjoonHub

--- 백준/Gold/2636. 치즈/치즈.java
+++ 백준/Gold/2636. 치즈/치즈.java
@@ -1,55 +1,60 @@
+import java.io.*;
 import java.util.*;
 
-public class Main {
-    static int n, m, retT, retC;
-    static int[][] a;
-    static int[][] visited;
+public class Main{
+    static int n, m;
+    static int[][] a, visited;
     static int[] dy = {-1, 0, 1, 0};
     static int[] dx = {0, 1, 0, -1};
-    static List<Pair> l = new ArrayList<>();
-    static class Pair{
-        int f, s;
-        Pair(int f, int s){
-            this.f = f;
-            this.s = s;
-        }
-    }
-    static void dfs(int y, int x){
+    static int dfs(int y, int x){
         visited[y][x] = 1;
+        int ret = 0;
         if(a[y][x] == 1){
-            l.add(new Pair(y, x));
-            return;
+            a[y][x] = 0; visited[y][x] = 1;
+            return 1;
         }
         for(int i = 0; i < 4; i++){
             int ny = y + dy[i];
             int nx = x + dx[i];
-            if(ny < 0 || nx < 0 || ny >= n|| nx >= m) continue;
+            if(ny < 0 || nx < 0 || ny >= n || nx >= m) continue;
             if(visited[ny][nx] != 0) continue;
-            dfs(ny, nx);
+            ret += dfs(ny, nx);
         }
+        return ret;
     }
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        n = sc.nextInt();
-        m = sc.nextInt();
-        a = new int[n + 1][m + 1];
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < m; j++){
-                a[i][j] = sc.nextInt();
-            }
-        }
-        while(true){
-            visited = new int[n + 1][m + 1];
-            l.clear();
-            dfs(0, 0);
-            if(l.size() == 0) break;
-            for(Pair p : l){
-                a[p.f][p.s] = 0;
-            }
-            retT++; retC = l.size();
-        }
-        System.out.println(retT + "\n" + retC);
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String rs;
+        StringTokenizer st;
 
+        st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        a = new int[n + 4][m + 4];
+        for(int i = 0; i < n; i++){
+            st = new StringTokenizer(br.readLine());
+            for(int j = 0; j < m; j++){
+                a[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        int prev = 0;
+        int ret = 0;
+        while(true){
+            visited = new int[n + 4][m + 4];
+            int sum = dfs(0, 0);
+            if(sum == 0) break;
+            ret++;
+            prev = sum;
+        }
+        bw.write(String.valueOf(ret)+'\n');
+        bw.write(String.valueOf(prev)+'\n');
+        bw.flush();
     }
 }
+/*
+1. 치즈녹이기 -> dfs 치즈 도달=> 치즈 지우고 방문처리
+2. 치즈조각 칸갯수 카운트 -> 리스트에 저장한다.
+3. 시간 카운트 -> while 돌리면서 카운트
+ */
