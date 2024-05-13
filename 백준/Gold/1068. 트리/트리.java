28d6339d07f636bf4ce0a7f940aac3083cc7b94d
Noah <104671487+gratisreise@users.noreply.github.com>
2024-05-13 11:24:05 +0900

    [Gold V] Title: 트리, Time: 80 ms, Memory: 11708 KB -BaekjoonHub

--- 백준/Gold/1068. 트리/트리.java
+++ 백준/Gold/1068. 트리/트리.java
@@ -1,46 +1,53 @@
-import java.util.ArrayList;
-import java.util.Scanner;
-import java.util.List;
+import java.io.*;
+import java.util.*;
 
-public class Main {
-    static int n, root, temp, del;
+public class Main{
+    static int n, root, delete;
     static List<Integer>[] tree;
-
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        n = scanner.nextInt();
-        tree = new List[n];
-        for (int i = 0; i < n; i++) {
-            tree[i] = new ArrayList<>();
-        }
-
-        for (int i = 0; i < n; i++) {
-            temp = scanner.nextInt();
-            if (temp == -1) {
-                root = i;
-            } else {
-                tree[temp].add(i);
-            }
-        }
-
-        del = scanner.nextInt();
-        if (del == root) {
-            System.out.println(0);
-        } else {
-            int result = go(root);
-            System.out.println(result);
-        }
-    }
-
-    static int go(int now) {
+    static int go(int now){
+        if(now == delete) return 0;
         int ret = 0;
         int child = 0;
-        for (int next : tree[now]) {
-            if (next == del) continue;
+        for(int next : tree[now]){
+            if(next == delete) continue;
             ret += go(next);
             child++;
         }
-        if (child == 0) return 1;
+        if(child == 0) return 1;
         return ret;
     }
+
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        tree = new List[n + 4];
+        for(int i = 0; i < n; i++) tree[i] = new ArrayList<>();
+
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < n; i++){
+            int temp = Integer.parseInt(st.nextToken());
+            if(temp == -1){
+                root = i; continue;
+            }
+            tree[temp].add(i);
+        }
+
+        st = new StringTokenizer(br.readLine());
+        delete = Integer.parseInt(st.nextToken());
+
+        bw.write(String.valueOf(go(root))+'\n');
+        bw.flush();
+    }
 }
+/*
+트리입력받기, 노드지우기, 리프노드 카운팅
+1. 노드를 입력받는다.
+2. 지울 노드를 입력받는다
+3. 트리를 탐색한다.
+4. 지운 노드는 건너 뛴다.
+5. 리프 노드를 카운팅 한다.
+*/
