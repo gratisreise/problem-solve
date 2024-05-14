c40300532c42ca6688b3e8a2a9331eb80f6ea4fa
Noah <104671487+gratisreise@users.noreply.github.com>
2024-05-14 12:07:41 +0900

    [Gold III] Title: 괄호 추가하기, Time: 84 ms, Memory: 11684 KB -BaekjoonHub

--- 백준/Gold/16637. 괄호 추가하기/괄호 추가하기.java
+++ 백준/Gold/16637. 괄호 추가하기/괄호 추가하기.java
@@ -1,41 +1,49 @@
+import java.io.*;
 import java.util.*;
+
 public class Main{
     static int n, ret = Integer.MIN_VALUE;
     static String s;
-    static int[] num;
-    static char[] operStr;
+    static List<Integer> num = new ArrayList<>();
+    static List<Character> operChar = new ArrayList<>();
 
+    //계산 메서드
     static int oper(char a, int b, int c){
-        int result = 0;
-        if(a == '+') result = b + c;
-        if(a == '*') result = b * c;
-        if(a == '-') result = b - c;
-        return result;
+        int ret = 0;
+        if(a == '+') ret = b + c;
+        else if(a == '*') ret = b * c;
+        else if(a == '-') ret = b - c;
+        return ret;
     }
-    static void go(int here, int _num){
-        if(here == operStr.length){
-            ret = Math.max(ret, _num);
+    static void go(int idx, int sum){
+        if(idx == operChar.size()){ // 연산자를 모두 사용했으면
+            ret = Math.max(ret, sum); //최댓값 찾아주기
             return;
         }
-        go(here + 1, oper(operStr[here], _num, num[here + 1]));
-        if(here + 2 <= operStr.length){
-            int temp = oper(operStr[here + 1], num[here + 1], num[here + 2]);
-            go(here + 2, oper(operStr[here],_num, temp));
-        }
+        // 순서대로 현재랑 바로 다음 수랑 계산해서 넘기기
+        go(idx + 1, oper(operChar.get(idx), sum, num.get(idx+1)));
+        // 2번 연산해야하므로 2를 더해서 size보다 크면 연산제외 시켜준다.
+        if(idx + 2 > operChar.size()) return;
+        int temp = oper(operChar.get(idx + 1), num.get(idx + 1), num.get(idx + 2));
+        go(idx + 2, oper(operChar.get(idx), sum, temp));
     }
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
 
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        n = sc.nextInt();
-        s = sc.next();
+        st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
 
-        num = new int[s.length()/2 + 1];
-        operStr = new char[s.length() / 2];
-        for(int i = 0, j = 0, k = 0; i < s.length(); i++){
-            if(i % 2 == 0){num[j] = s.charAt(i) - '0'; j++;}
-            else {operStr[k] = s.charAt(i); k++;}
+        s = br.readLine();
+        for(char c : s.toCharArray()){
+            if('0' <= c && c <= '9') num.add(c - '0');
+            else operChar.add(c);
         }
-        go(0, num[0]);
-        System.out.println(ret);
+
+        go(0, num.get(0));
+
+        bw.write(String.valueOf(ret)+'\n');
+        bw.flush();
     }
 }
