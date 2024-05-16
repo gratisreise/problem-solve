d3eb1f9e6f5c4c91e640598f3d06566ec513a3e3
Noah <104671487+gratisreise@users.noreply.github.com>
2024-05-16 12:42:09 +0900

    [Silver I] Title: 부등호, Time: 144 ms, Memory: 26584 KB -BaekjoonHub

--- 백준/Silver/2529. 부등호/부등호.java
+++ 백준/Silver/2529. 부등호/부등호.java
@@ -1,6 +1,7 @@
+import java.io.*;
 import java.util.*;
 
-public class Main {
+public class Main{
     static int n;
     static List<String> ret = new ArrayList<>();
     static int[] cnt = new int[10];
@@ -15,24 +16,28 @@ public class Main {
             ret.add(num);
             return;
         }
-        for(int i = 0; i <= 9; i++){
+        for(int i = 0; i < 10; i++){
             if(cnt[i] != 0) continue;
-            if((idx == 0) || good(num.charAt(idx - 1), (char)(i + '0'), oper[idx-1])){
+            if((idx == 0) || good(num.charAt(idx - 1), (char)(i + '0'), oper[idx - 1])){
                 cnt[i] = 1;
                 go(idx + 1, num + (""+i));
                 cnt[i] = 0;
             }
         }
     }
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        n = sc.nextInt();
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+        st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
         for(int i = 0; i < n; i++){
-            oper[i] = sc.next().charAt(0);
+            oper[i] = st.nextToken().charAt(0);
         }
         go(0, "");
-        System.out.println(ret.get(ret.size() - 1));
-        System.out.println(ret.get(0));
+        bw.write(String.valueOf(ret.get(ret.size() - 1))+'\n');
+        bw.write(String.valueOf(ret.get(0))+'\n');
+        bw.flush();
     }
 }
