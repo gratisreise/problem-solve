6d1d9af7541ef2e16f3f5ae9790026b153ccc189
Noah <104671487+gratisreise@users.noreply.github.com>
2024-05-17 14:24:01 +0900

    [Gold IV] Title: 주난의 난(難), Time: 164 ms, Memory: 19260 KB -BaekjoonHub

--- 백준/Gold/14497. 주난의 난（難）/주난의 난（難）.java
+++ 백준/Gold/14497. 주난의 난（難）/주난의 난（難）.java
@@ -1,39 +1,44 @@
+import java.io.*;
 import java.util.*;
 
-public class Main {
-    static final int mx = 301;
-    static int[] dy ={-1, 0, 1, 0};
+public class Main{
+    static int[] dy = {-1, 0, 1, 0};
     static int[] dx = {0, 1, 0, -1};
-    static int n, m, y1, x1, y2, x2;
     static int[][] visited;
     static char[][] a;
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        n = sc.nextInt();
-        m = sc.nextInt();
-        y1 = sc.nextInt();
-        x1 = sc.nextInt();
-        y2 = sc.nextInt();
-        x2 = sc.nextInt();
-        y1--;x1--;y2--;x2--;
-        //y랑 x 체인지
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
         visited = new int[n + 1][m + 1];
         a = new char[n + 1][m + 1];
-        
-        Queue<Integer> q = new LinkedList<>();
-        for(int i = 0; i < n; i++) a[i] = sc.next().toCharArray();
 
-        q.add(1000 * y1 + x1);
-        visited[y1][x1] = 1;
+        st = new StringTokenizer(br.readLine());
+        int y = Integer.parseInt(st.nextToken());
+        int x = Integer.parseInt(st.nextToken());
+        int ey = Integer.parseInt(st.nextToken());
+        int ex = Integer.parseInt(st.nextToken());
+        y--; x--; ey--; ex--;
+
+        for(int i = 0; i < n; i++) a[i] = br.readLine().toCharArray();
+
+        Queue<Integer> q = new LinkedList<>();
+        q.add(1000 * y + x);
+        visited[y][x] = 1;
         int cnt = 0;
 
-        while(a[y2][x2] != '0'){
+        while(visited[ey][ex] == 0){
             cnt++;
             Queue<Integer> temp = new LinkedList<>();
+
             while(!q.isEmpty()){
                 int cur = q.poll();
-                int y = cur / 1000;
-                int x = cur % 1000;
+                y = cur / 1000;
+                x = cur % 1000;
                 for(int i = 0; i < 4; i++){
                     int ny = y + dy[i];
                     int nx = x + dx[i];
@@ -43,11 +48,12 @@ public class Main {
                     if(a[ny][nx] != '0'){
                         a[ny][nx] = '0';
                         temp.add(1000 * ny + nx);
-                    } else q.add(1000* ny + nx);
+                    } else q.add(1000 * ny + nx);
                 }
             }
             q = temp;
         }
-        System.out.println(visited[y2][x2]);
+        bw.write(String.valueOf(visited[ey][ex])+'\n');
+        bw.flush();
     }
 }
