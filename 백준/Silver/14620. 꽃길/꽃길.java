87dc4d67c69958928926546446a1fec4804f71dc
Noah <104671487+gratisreise@users.noreply.github.com>
2024-05-17 16:43:03 +0900

    [Silver II] Title: 꽃길, Time: 132 ms, Memory: 15340 KB -BaekjoonHub

--- 백준/Silver/14620. 꽃길/꽃길.java
+++ 백준/Silver/14620. 꽃길/꽃길.java
@@ -1,75 +1,72 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
-public class Main {
-    static int n;
-    static int ret = 1000000000; // 최소에서 최대
-    static int[][] p; // 꽃
-    static int[][] v; // 꽃심은 것 확인
+public class Main{
+    static int n, ret = (int) 1e9;
+    static int[][] a, visited;
     static int[] dy = {-1, 0, 1, 0};
     static int[] dx = {0, 1, 0, -1};
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        n = scanner.nextInt();
-        p = new int[n][n];
-        v = new int[n][n];
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                p[i][j] = scanner.nextInt();
-            }
-        }
-
-        flower(0, 0);
-        System.out.println(ret);
-    }
-
-    static boolean check(int y, int x) { // 꽃 심어도 되는지 확인
-        if (v[y][x] != 0) return false; // 겹치면 안됨
-        for (int i = 0; i < 4; i++) { // 상하좌우 체크
+    static boolean check(int y, int x){
+        for(int i = 0; i < 4; i++){
             int ny = y + dy[i];
             int nx = x + dx[i];
-            if (v[ny][nx] != 0) return false; // 겹치면 안됨
+            if(visited[ny][nx] != 0) return false;
         }
-        return true; // 안 겹치면 괜찮음
+        return true;
     }
-
-    static int setFlower(int y, int x) { // 꽃의 땅값을 합산
-        v[y][x] = 1; // 땅에 꽃잎 자리 칠하기
-        int s = p[y][x]; // 땅값 합산
-        for (int i = 0; i < 4; i++) {
+    static int hap(int y, int x){
+        visited[y][x] = 1;
+        int sum = a[y][x];
+        for(int i = 0; i < 4; i++){
             int ny = y + dy[i];
             int nx = x + dx[i];
-            v[ny][nx] = 1;
-            s += p[ny][nx];
+            visited[ny][nx] = 1;
+            sum += a[ny][nx];
         }
-        return s;
+        return sum;
     }
-
-    static void eraseFlower(int y, int x) { // 원상복구용 방문 지우기
-        v[y][x] = 0;
-        for (int i = 0; i < 4; i++) {
+    static void clear(int y, int x){
+        visited[y][x] = 0;
+        for(int i = 0; i < 4; i++){
             int ny = y + dy[i];
             int nx = x + dx[i];
-            v[ny][nx] = 0;
+            visited[ny][nx] = 0;
         }
     }
-
-    static void flower(int cnt, int hap) {
-        if (cnt == 3) {
-            ret = Math.min(ret, hap);
+    static void go(int cnt, int sum){
+        if(cnt == 3){
+            ret = Math.min(ret, sum);
             return;
         }
-        // 어차피 화단 끝은 씨앗을 못 심으니 애초에 제거
-        for (int i = 1; i < n - 1; i++) {
-            for (int j = 1; j < n - 1; j++) {
-                if (check(i, j)) { // 자리 확인해서 가능하면
-                    // 다음으로
-                    flower(cnt + 1, hap + setFlower(i, j));
-                    // 원상복구
-                    eraseFlower(i, j);
+        for(int i = 1; i < n - 1; i++){
+            for(int j = 1; j < n - 1; j++){
+                if(check(i, j)){
+                    go(cnt + 1, sum + hap(i, j));
+                    clear(i, j);
                 }
             }
         }
+    }
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String rs;
+        StringTokenizer st;
+        st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        a = new int[n + 1][n + 1];
+        visited = new int[n + 1][n + 1];
+        for(int i = 0; i < n; i++){
+            st = new StringTokenizer(br.readLine());
+            for(int j = 0; j < n; j++){
+                a[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        go(0, 0);
+
+        bw.write(String.valueOf(ret)+'\n');
+        bw.flush();
     }
 }
