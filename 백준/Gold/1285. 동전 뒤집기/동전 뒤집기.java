bb8b6884b7d7ec7a592f91c64b1942efd3b6394b
Noah <104671487+gratisreise@users.noreply.github.com>
2024-05-21 11:44:08 +0900

    [Gold I] Title: 동전 뒤집기, Time: 472 ms, Memory: 12548 KB -BaekjoonHub

--- 백준/Gold/1285. 동전 뒤집기/동전 뒤집기.java
+++ 백준/Gold/1285. 동전 뒤집기/동전 뒤집기.java
@@ -2,12 +2,11 @@ import java.io.*;
 import java.util.*;
 
 public class Main{
-    static int n, ret = (int) 1e9;
+    static int n, ret = (int)1e9;
     static int[] a = new int[24];
-    static void go(int here){
-        if(here == n){
+    static void go(int idx){
+        if(idx == n){
             int sum = 0;
-            // 각 열 지정
             for(int i = 1; i <= (1 << (n - 1)); i *= 2){
                 int cnt = 0;
                 for(int j = 0; j < n; j++){
@@ -18,15 +17,10 @@ public class Main{
             ret = Math.min(ret, sum);
             return;
         }
-//        System.out.println("----------");
-//        System.out.println("here :: " + here);
-//        for(int i = 0; i < n; i++){
-//            System.out.println(a[i]);
-//        }
-//        System.out.println("---------");
-        go(here + 1);
-        a[here] = ~a[here];
-        go(here + 1);
+        go(idx + 1);
+        a[idx] = ~a[idx];
+        go(idx + 1);
+        a[idx] = ~a[idx];
     }
     public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -34,17 +28,25 @@ public class Main{
         StringTokenizer st;
         st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
-
         for(int i = 0; i < n; i++){
             char[] c = br.readLine().toCharArray();
             for(int j = 0; j < n; j++){
-                //앞은 1로 저장
                 if(c[j] == 'T') a[i] += (1 << j);
             }
         }
-
         go(0);
         bw.write(String.valueOf(ret)+'\n');
         bw.flush();
     }
 }
+/*
+1. 배열을 2진수의 int배열로 저장한다. (T는 1로)
+2. 완전탐색
+    2-1. 행기준 뒤집기
+    2-2. 행기준 안 뒤집기
+3. n 도달하면
+4. 열 기준으로 앞뒤 갯수 세기
+    4-1.앞이 더 많으면 sum += n - cnt;
+    4-2.뒤가 더 많으면 sum += n;
+5. ret = Math.min(ret, sum);
+ */
