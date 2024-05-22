f460c210a5ffba167b1bd4efdd80e3d5fa1ff877
Noah <104671487+gratisreise@users.noreply.github.com>
2024-05-22 11:27:12 +0900

    [Gold IV] Title: 가르침, Time: 264 ms, Memory: 12904 KB -BaekjoonHub

--- 백준/Gold/1062. 가르침/가르침.java
+++ 백준/Gold/1062. 가르침/가르침.java
@@ -1,47 +1,57 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    static int n, m;
+    static int n, k;
     static int[] words;
     static String s;
-
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        n = scanner.nextInt();
-        m = scanner.nextInt();
-
-        words = new int[51];
-
-        for (int i = 0; i < n; i++) {
-            s = scanner.next();
-            for (char c : s.toCharArray()) {
-                words[i] |= (1 << (c - 'a'));
-            }
-        }
-
-        System.out.println(go(0, m, 0));
-    }
 
     static int count(int mask) {
         int cnt = 0;
         for (int word : words) {
-            if (word != 0 && (word & mask) == word) {
+            if (word != 0 && ((word & mask) == word)) {
                 cnt++;
             }
         }
         return cnt;
     }
+    static boolean skip(int n){
+        return n == 'a' - 'a' || n == 'n' - 'a' || n == 't' - 'a' || n == 'i' - 'a' || n == 'c' - 'a';
+    }
+    
 
-    static int go(int index, int k, int mask) {
+    static int go(int idx, int k, int mask) {
         if (k < 0) return 0;
-        if (index == 26) return count(mask);
+        if (idx == 26) return count(mask);
 
-        int ret = go(index + 1, k - 1, mask | (1 << index));
-
-        if (index != 'a' - 'a' && index != 'n' - 'a' && index != 't' - 'a' && index != 'i' - 'a' && index != 'c' - 'a') {
-            ret = Math.max(ret, go(index + 1, k, mask));
-        }
+        int ret = go(idx + 1, k - 1, mask | (1 << idx));
+        if (!skip(idx)) ret = Math.max(ret, go(idx + 1, k, mask));
 
         return ret;
     }
+
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+        st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());;
+        k = Integer.parseInt(st.nextToken());;
+
+        words = new int[51];
+
+        for (int i = 0; i < n; i++) {
+            String s = br.readLine();
+            for (char c : s.toCharArray()) {
+                words[i] |= (1 << (c - 'a'));
+            }
+        }
+
+        bw.write(String.valueOf(go(0, k, 0))+'\n');
+        bw.flush();
+    }
 }
+/*
+<최대를 어떻게 구하는지 아이디어를 모르겠다.>
+*/
