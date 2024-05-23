10d22196f14619eabc81b8e0e6a16a293b7a4e03
Noah <104671487+gratisreise@users.noreply.github.com>
2024-05-23 12:46:25 +0900

    [Gold III] Title: 현욱은 괄호왕이야!!, Time: 192 ms, Memory: 21132 KB -BaekjoonHub

--- 백준/Gold/15926. 현욱은 괄호왕이야！！/현욱은 괄호왕이야！！.java
+++ 백준/Gold/15926. 현욱은 괄호왕이야！！/현욱은 괄호왕이야！！.java
@@ -1,29 +1,29 @@
-import java.util.Scanner;
-import java.util.Stack;
+import java.io.*;
+import java.util.*;
 
-public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        String str = scanner.next();
+public class Main{
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+        int n = Integer.parseInt(br.readLine());;
+        String str = br.readLine();
         Stack<Integer> stack = new Stack<>();
         int ret = 0;
         stack.push(-1);
-
+        char[] c = str.toCharArray();
         for (int i = 0; i < n; i++) {
-            if (str.charAt(i) == '(') {
-                stack.push(i);
-            }
-            if (str.charAt(i) == ')') {
+            if (c[i] == '(')  stack.push(i);
+            if (c[i] == ')') {
                 stack.pop();
                 if (!stack.isEmpty()) {
                     ret = Math.max(ret, i - stack.peek());
-                } else {
-                    stack.push(i);
-                }
+                } else  stack.push(i);
             }
         }
-
-        System.out.println(ret);
+        bw.write(String.valueOf(ret)+'\n');
+        bw.flush();
+        bw.close();
+        br.close();
     }
 }
