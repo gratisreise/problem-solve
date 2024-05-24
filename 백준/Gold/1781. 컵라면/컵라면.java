12c721ba7cd784b89b28beae7b91d4dd7af478a2
Noah <104671487+gratisreise@users.noreply.github.com>
2024-05-24 12:18:38 +0900

    [Gold II] Title: 컵라면, Time: 952 ms, Memory: 77328 KB -BaekjoonHub

--- 백준/Gold/1781. 컵라면/컵라면.java
+++ 백준/Gold/1781. 컵라면/컵라면.java
@@ -1,34 +1,39 @@
+import java.io.*;
 import java.util.*;
-
 public class Main {
     static int n, c, d, ret;
-    static List<Pair> l = new ArrayList<>();
-    static PriorityQueue<Integer> pq = new PriorityQueue<>();
-
     static class Pair{
-        int f, s;
-        Pair(int f, int s){
-            this.f = f;
-            this.s = s;
+        int d, c;
+        Pair(int d, int c){
+            this.d = d;
+            this.c = c;
         }
     }
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        n = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+        List<Pair> l = new ArrayList<>();
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        n = Integer.parseInt(br.readLine());
         for(int i = 0; i < n; i++){
-            d = sc.nextInt();
-            c = sc.nextInt();
+            st = new StringTokenizer(br.readLine());
+            d = Integer.parseInt(st.nextToken());
+            c = Integer.parseInt(st.nextToken());
             l.add(new Pair(d, c)); // 날짜, 컵수
         }
-        Collections.sort(l, (a, b) -> a.f - b.f);
+        Collections.sort(l, (a, b) -> Integer.compare(a.d, b.d));
         for(int i = 0; i < l.size(); i++){
-            ret += l.get(i).s;
-            pq.add(l.get(i).s);
-            while(pq.size() > l.get(i).f){
+            ret += l.get(i).c;  // 컵수 더하기
+            pq.add(l.get(i).c); // 컵 넣어서 day 세주기
+            // 컵
+            while(pq.size() > l.get(i).d){
                 ret -= pq.poll();
             }
         }
-        System.out.println(ret);
+        bw.write(String.valueOf(ret)+'\n');
+        bw.flush();
+        bw.close();
+        br.close();
     }
 }
