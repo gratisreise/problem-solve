81e4479bf6ced2854e98b1227b246a44e76819a4
Noah <104671487+gratisreise@users.noreply.github.com>
2024-05-24 11:24:59 +0900

    [Gold III] Title: 순회강연, Time: 380 ms, Memory: 22280 KB -BaekjoonHub

--- 백준/Gold/2109. 순회강연/순회강연.java
+++ 백준/Gold/2109. 순회강연/순회강연.java
@@ -1,46 +1,55 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.PriorityQueue;
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
-public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        int ret = 0;
-        ArrayList<Pair> v = new ArrayList<>();
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
-
-        for (int i = 0; i < n; i++) {
-            int p = scanner.nextInt();
-            int d = scanner.nextInt();
-            v.add(new Pair(d, p));
+public class Main{
+    static int n, p, d, ret;
+    static class Pair{
+        int d, p;
+        Pair(int d, int p){
+            this.d = d;
+            this.p = p;
         }
+    }
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+        st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        List<Pair> l = new ArrayList<>();
+        PriorityQueue<Integer> pq = new PriorityQueue();
+        for(int i = 0; i < n; i++){
+            st = new StringTokenizer(br.readLine());
+            int p = Integer.parseInt(st.nextToken());
+            int d = Integer.parseInt(st.nextToken());
+            l.add(new Pair(d, p));
+        }
+        
+        Collections.sort(l, (a, b) -> Integer.compare(a.d, b.d));
 
-        Collections.sort(v);
-        for (int i = 0; i < n; i++) {
-            ret += v.get(i).second;
-            pq.add(v.get(i).second);
-            while (pq.size() > v.get(i).first) {
+        for(Pair p : l){
+            ret += p.p;
+            pq.add(p.p);
+            while(pq.size() > p.d){
                 ret -= pq.poll();
             }
         }
-
-        System.out.println(ret);
-    }
-
-    static class Pair implements Comparable<Pair> {
-        int first;
-        int second;
-
-        public Pair(int first, int second) {
-            this.first = first;
-            this.second = second;
-        }
-
-        @Override
-        public int compareTo(Pair other) {
-            return Integer.compare(this.first, other.first);
-        }
+        bw.write(String.valueOf(ret)+'\n');
+        bw.flush();
+        bw.close();
+        br.close();
     }
 }
+/*
+1. 날짜 기준 오름차 정렬
+2. 리스트에 저장
+3. 리스트를 순회
+    3-1. 오름차 pq에 가격을 저장
+    3-2. ret에 가격 더하기
+    3-2. pq의 크기가 날짜보다 큰지 확인
+        3-2-1.크면
+            날짜에 해당하는 값중 최대가 아닌게 있으므로 ret에 가격 빼기
+        3-2-2.작거나같으면
+            날짜에 해당하는 값중 최대값이다.
+4. ret 출력!!
+*/
