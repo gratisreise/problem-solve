8d7b67622bab4b896d8bad64953c20f992e73faf
Noah <104671487+gratisreise@users.noreply.github.com>
2024-05-27 10:53:56 +0900

    [Gold II] Title: 보석 도둑, Time: 1668 ms, Memory: 144440 KB -BaekjoonHub

--- 백준/Gold/1202. 보석 도둑/보석 도둑.java
+++ 백준/Gold/1202. 보석 도둑/보석 도둑.java
@@ -1,58 +1,54 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
-public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        int k = scanner.nextInt();
-        long ret = 0;
+public class Main{
+    static int n, k;
+    static long ret;
 
-        List<Pair> v = new ArrayList<>();
-        List<Long> vv = new ArrayList<>();
+    static class Pair{
+        long m, v;
+        Pair(long m, long v){
+            this.m = m;
+            this.v = v;
+        }
+    }
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        k = Integer.parseInt(st.nextToken());
+
+        List<Pair> l = new ArrayList<>();
+        List<Long> g = new ArrayList<>();
         PriorityQueue<Long> pq = new PriorityQueue<>(Collections.reverseOrder());
 
-        for (int i = 0; i < n; i++) {
-            long M = scanner.nextLong();
-            long V = scanner.nextLong();
-            v.add(new Pair(M, V));
+        for(int i = 0; i < n; i++){
+            st = new StringTokenizer(br.readLine());
+            long a = Long.parseLong(st.nextToken());
+            long b = Long.parseLong(st.nextToken());
+            l.add(new Pair(a, b));
+        }
+        for(int i = 0; i < k; i++){
+            long num = Long.parseLong(br.readLine());
+            g.add(num);
         }
 
-        for (int i = 0; i < k; i++) {
-            long C = scanner.nextLong();
-            vv.add(C);
-        }
+        Collections.sort(l, (a, b) -> Long.compare(a.m, b.m));
+        Collections.sort(g);
 
-        Collections.sort(v);
-        Collections.sort(vv);
-
-        int j = 0;
-        for (int i = 0; i < k; i++) {
-            while (j < n && v.get(j).first <= vv.get(i)) {
-                pq.add(v.get(j).second);
+        for(int i = 0, j = 0; i < k; i++){
+            while(j < n && l.get(j).m <= g.get(i)){
+                pq.add(l.get(j).v);
                 j++;
             }
-
-            if (!pq.isEmpty()) {
-                ret += pq.poll();
-            }
+            if(!pq.isEmpty()) ret += pq.poll();
         }
-
-        System.out.println(ret);
-    }
-}
-
-class Pair implements Comparable<Pair> {
-    long first;
-    long second;
-
-    Pair(long first, long second) {
-        this.first = first;
-        this.second = second;
-    }
-
-    @Override
-    public int compareTo(Pair other) {
-        return Long.compare(this.first, other.first);
+        bw.write(String.valueOf(ret)+'\n');
+        bw.flush();
+        bw.close();
+        br.close();
     }
 }
