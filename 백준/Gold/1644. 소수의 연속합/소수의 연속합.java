77b1fdfe94f5978dd5aa2c7e6be2b7c418e4507a
Noah <104671487+gratisreise@users.noreply.github.com>
2024-05-27 11:28:37 +0900

    [Gold III] Title: 소수의 연속합, Time: 160 ms, Memory: 23932 KB -BaekjoonHub

--- 백준/Gold/1644. 소수의 연속합/소수의 연속합.java
+++ 백준/Gold/1644. 소수의 연속합/소수의 연속합.java
@@ -1,13 +1,16 @@
 import java.util.*;
+import java.io.*;
+
 
 public class Main {
-    static int n, p, lo, hi, ret, sum;
+    static int n, p, s, e, ret, sum;
     static int[] a;
     static boolean[] che;
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        n = sc.nextInt();
-        che = new boolean[n + 2];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        n = Integer.parseInt(br.readLine());;
+        che = new boolean[n + 4];
         a = new int[n/2 + 4];
         for(int i = 2; i <= n; i++){
             if(che[i]) continue;
@@ -16,17 +19,25 @@ public class Main {
             }
         }
         //p는 0이니깐 ++해주며 인덱스 이동
+        //p는 나중에도 사용되므로 소중하게대하자
         for(int i = 2; i <= n; i++){
             if(!che[i]) a[p++] = i;
         }
         //lo는 0 hi는 0
         while(true){
-            if(sum >= n) sum -= a[lo++];
-            else if(hi == p) break;
-            else sum += a[hi++];
+            // sum이 n보다 크면 시작점 소수 빼기
+            if(sum >= n) sum -= a[s++];
+            // 끝점이 종점과 같으면 break
+            else if(e == p) break;
+            // 끝점이 종점이
+            else sum += a[e++];
+            // sum이랑 같으면 갯수세주기
             if(sum == n) ret++;
         }
 
-        System.out.println(ret);
+        bw.write(String.valueOf(ret)+'\n');
+        bw.flush();
+        bw.close();
+        br.close();
     }
 }
