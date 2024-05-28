d50cb956b1b68b9f6c352d416cb261b99f4562f0
Noah <104671487+gratisreise@users.noreply.github.com>
2024-05-28 12:35:03 +0900

    [Gold I] Title: 멀티탭 스케줄링, Time: 76 ms, Memory: 11868 KB -BaekjoonHub

--- 백준/Gold/1700. 멀티탭 스케줄링/멀티탭 스케줄링.java
+++ 백준/Gold/1700. 멀티탭 스케줄링/멀티탭 스케줄링.java
@@ -2,48 +2,51 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static final int INF = 1000000000;
+    static int n, k, ret;
+    static int[] a;
+    static Set<Integer> visited = new HashSet<>();
+    static List<Integer> v = new ArrayList<>();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine()); // 입력을 받기 위한 StringTokenizer를 사용하여 첫 줄을 읽음
-        int k = Integer.parseInt(st.nextToken()); // k를 입력 받음
-        int n = Integer.parseInt(st.nextToken()); // n을 입력 받음
-        int cnt = 0; // 결과값을 저장할 변수
-        int[] a = new int[104]; // 입력된 수열을 저장할 배열
-        int[] visited = new int[104]; // 방문 여부를 저장할 배열
-        List<Integer> v = new ArrayList<>(); // 중복된 수를 저장할 리스트
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+        st = new StringTokenizer(br.readLine());
+        k = Integer.parseInt(st.nextToken());
+        n = Integer.parseInt(st.nextToken());
+        a = new int[n];
 
-        st = new StringTokenizer(br.readLine()); // 다음 줄을 읽어와 StringTokenizer에 저장
-        for (int i = 0; i < n; i++) { // n개의 수열을 입력 받음
-            a[i] = Integer.parseInt(st.nextToken()); // 수열을 배열에 저장
-        }
-
-        for (int i = 0; i < n; i++) { // 수열을 하나씩 탐색
-            if (visited[a[i]] == 0) { // 아직 방문하지 않은 수일 경우
-                if (v.size() == k) { // 리스트에 k개의 수가 저장되어 있는 경우(멀티탭다 쓰는 경우)
-                    int lastIdx = 0, pos = 0; // 마지막으로 등장하는 인덱스와 해당 수를 저장할 변수 초기화
-                    for (int j = 0; j < v.size(); j++) { // 리스트에 저장된 수에 대해 탐색
-                        int herePick = INF; // 해당 수가 마지막으로 등장하는 인덱스 초기화
-                        for (int l = i + 1; l < n; l++) { // 현재 위치 이후의 수열에 대해 탐색
-                            if (v.get(j) == a[l]) { // 리스트에 저장된 수와 동일한 수를 발견한 경우
-                                herePick = l; // 해당 수의 인덱스를 저장
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < n; i++) a[i] = Integer.parseInt(st.nextToken());
+        for(int i = 0; i < n; i++){
+            if(!visited.contains(a[i])){
+                if(v.size() == k){
+                    int lastIdx = 0;
+                    int pos = 0;
+                    for(int _a : v){
+                        int herePick = Integer.MAX_VALUE;
+                        for(int j = i + 1; j < n; j++){
+                            if(_a == a[j]){
+                                herePick = j;
                                 break;
                             }
                         }
-                        if (lastIdx < herePick) { // 마지막으로 등장하는 인덱스가 이전에 등장한 인덱스보다 큰 경우
-                            lastIdx = herePick; // 마지막으로 등장하는 인덱스 갱신
-                            pos = v.get(j); // 해당 수 갱신
+                        if(lastIdx < herePick){
+                            lastIdx = herePick;
+                            pos = _a;
                         }
                     }
-                    visited[pos] = 0; // 리스트에서 해당 수를 제거하기 위해 방문 여부를 0으로 변경
-                    cnt++; // 카운트 증가
-                    v.remove(v.indexOf(pos)); // 리스트에서 해당 수 제거
+                    visited.remove(pos);
+                    ret++;
+                    v.remove((Integer)pos);
                 }
-                v.add(a[i]); // 현재 수를 멀티탭에 꽂기
-                visited[a[i]] = 1; // 현재 수를 방문 처리
+                v.add(a[i]);
+                visited.add(a[i]);
             }
         }
-        System.out.println(cnt); // 결과 출력
+        bw.write("" + ret +'\n');
+        bw.flush();
+        bw.close();
+        br.close();
     }
 }
