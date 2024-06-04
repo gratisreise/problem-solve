c9ae4007eba42ca49546e91391d16390240c2b59
noah <104671487+gratisreise@users.noreply.github.com>
2024-06-04 17:48:16 +0900

    [Gold IV] Title: 뱀, Time: 88 ms, Memory: 12096 KB -BaekjoonHub

--- 백준/Gold/3190. 뱀/뱀.java
+++ 백준/Gold/3190. 뱀/뱀.java
@@ -1,83 +1,76 @@
-import java.util.ArrayDeque;
-import java.util.Deque;
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
-public class Main {
+public class Main{
     static int n, k, l, y, x, t, ret, idx, dir = 1;
-    static int[][] a;
-    static int[][] visited;
+    static int[][] a, visited;
     static int time;
-    static Deque<Pair> dq;
-    static Pair[] _time;
     static final int[] dy = {-1, 0, 1, 0};
     static final int[] dx = {0, 1, 0, -1};
-
-    static class Pair {
-        int first, second;
-
-        public Pair(int first, int second) {
-            this.first = first;
-            this.second = second;
+    static Deque<Pair> dq = new ArrayDeque<>();
+    static List<Pair> _time = new ArrayList<>();
+    static class Pair{
+        int f, s;
+        Pair(int f, int s){
+            this.f = f;
+            this.s = s;
         }
     }
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+        n = Integer.parseInt(br.readLine());
+        k = Integer.parseInt(br.readLine());
+        a = new int[n + 4][n + 4];
+        visited = new int[n + 4][n + 4];
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        n = scanner.nextInt();
-        k = scanner.nextInt();
-        a = new int[n][n];
-        visited = new int[n][n];
-        _time = new Pair[101];
-        dq = new ArrayDeque<>();
-
-        for (int i = 0; i < k; i++) {
-            y = scanner.nextInt() - 1;
-            x = scanner.nextInt() - 1;
+        //사과 위치 저장
+        for(int i = 0; i < k; i++){
+            st = new StringTokenizer(br.readLine());
+            y = Integer.parseInt(st.nextToken()) - 1;
+            x = Integer.parseInt(st.nextToken()) - 1;
             a[y][x] = 1;
         }
 
-        l = scanner.nextInt();
-        for (int i = 0; i < l; i++) {
-            t = scanner.nextInt();
-            char c = scanner.next().charAt(0);
-            if (c == 'D') {
-                _time[i] = new Pair(t, 1);
-            } else {
-                _time[i] = new Pair(t, 3);
-            }
+        l = Integer.parseInt(br.readLine());
+        //방향 전환 저장
+        for(int i = 0; i < l; i++){
+            st = new StringTokenizer(br.readLine());
+            int t = Integer.parseInt(st.nextToken());
+            char c = st.nextToken().charAt(0);
+            if(c == 'D') _time.add(new Pair(t, 1));
+            else _time.add(new Pair(t, 3));
         }
 
         dq.add(new Pair(0, 0));
-        time = 0;
-
-        while (!dq.isEmpty()) {
+        int time = 0;
+        while(!dq.isEmpty()){
             time++;
-            Pair pair = dq.peek();
-            y = pair.first;
-            x = pair.second;
+            Pair p = dq.peek();
+            y = p.f;
+            x = p.s;
             int ny = y + dy[dir];
             int nx = x + dx[dir];
-
-            if (ny < 0 || nx < 0 || ny >= n || nx >= n || visited[ny][nx] == 1) {
+            if(ny < 0 || nx < 0 || ny >= n || nx >= n || visited[ny][nx] == 1){
                 break;
             }
-
-            if (a[ny][nx] == 0) {
+            if(a[ny][nx] == 0){
                 Pair tail = dq.pollLast();
-                visited[tail.first][tail.second] = 0;
-            } else {
-                a[ny][nx] = 0;
-            }
+                visited[tail.f][tail.s] = 0;
+            } else a[ny][nx] = 0;
 
             visited[ny][nx] = 1;
             dq.addFirst(new Pair(ny, nx));
 
-            if (idx < l && time == _time[idx].first) {
-                dir = (dir + _time[idx].second) % 4;
+            if(idx < l && time == _time.get(idx).f){
+                dir = (dir + _time.get(idx).s) % 4;
                 idx++;
             }
         }
-
-        System.out.println(time);
+        bw.write(String.valueOf(time)+'\n');
+        bw.flush();
+        bw.close();
+        br.close();
     }
 }
