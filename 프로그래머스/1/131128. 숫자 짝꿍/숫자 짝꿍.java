6768ab6293c4707ed194ac7fa87fba592b139236
noah <104671487+gratisreise@users.noreply.github.com>
2024-07-08 16:29:02 +0900

    [level 1] Title: 숫자 짝꿍, Time: 0.04 ms, Memory: 76.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.java
@@ -0,0 +1,24 @@
+class Solution {
+    
+    public String solution(String X, String Y) {
+        String answer = "";
+        int[] cntx = new int[10];
+        int[] cnty = new int[10];
+        for(char c : X.toCharArray()) cntx[c - '0']++;
+        for(char c : Y.toCharArray()) cnty[c - '0']++;
+        int[] cnt = new int[10];
+        for(int i = 0; i < 10; i++) cnt[i] = Math.min(cntx[i], cnty[i]);
+        
+        StringBuilder temp = new StringBuilder();
+        for(int i = 9; i >= 0; i--){
+            if(i == 0 && temp.length() == 0 && cnt[i] > 0) temp.append(0);
+            else {
+                for(int j = 0; j < cnt[i]; j++) temp.append(i);
+            }
+        }
+        
+        if(temp.length() == 0) answer = "-1";
+        else answer = "" + temp;
+        return answer;
+    }
+}
