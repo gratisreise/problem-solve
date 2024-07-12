ad2053b9fc9c81ed2d1a22c2ee4f0d8ed59bdc70
noah <104671487+gratisreise@users.noreply.github.com>
2024-07-12 09:52:48 +0900

    [level 0] Title: 두 수의 합, Time: 14.42 ms, Memory: 83.5 MB -BaekjoonHub

--- 프로그래머스/0/181846. 두 수의 합/두 수의 합.java
+++ 프로그래머스/0/181846. 두 수의 합/두 수의 합.java
@@ -1,58 +1,20 @@
-import java.util.*;
 class Solution {
-    static int n;
     public String solution(String a, String b) {
-        String ret = "";
-        Stack<Character> stka = new Stack<>();
-        Stack<Character> stkb = new Stack<>();
-        n = (a.length() > b.length()) ? b.length() : a.length();
-        for(char c : a.toCharArray()) stka.push(c);
-        for(char c : b.toCharArray()) stkb.push(c);
+        String answer = "";
+        StringBuilder ret = new StringBuilder();
+        int n = Math.max(a.length(), b.length());
+        a = new StringBuilder(a).reverse().toString();
+        b = new StringBuilder(b).reverse().toString();
         int prev = 0;
         for(int i = 0; i < n; i++){
-            int sum = 0;
-            int aa = stka.pop() - '0';
-            int bb = stkb.pop() - '0';
-            sum = aa + bb;
-            if(prev == 1) {
-                sum++;
-                prev = 0;
-            }
-            if(sum > 9){
-                prev = 1;
-                sum -= 10;
-            }
-            ret = (""+sum ) + ret;
+            int aNum = a.length() >= i + 1 ? a.charAt(i) - '0' : 0;
+            int bNum = b.length() >= i + 1 ? b.charAt(i) - '0' : 0;
+            int sum = aNum + bNum + prev;
+            if(sum >= 10) {prev = 1; sum -= 10;}
+            else prev = 0;
+            ret.append(sum);
         }
-            while(stka.isEmpty() && !stkb.isEmpty()){
-                int sum = 0;
-                sum = stkb.pop() - '0';
-                if(prev == 1) {
-                    prev = 0;
-                    sum++;
-                }
-                if(sum > 9) {
-                    sum -= 10;
-                    prev = 1;
-                }
-                ret = ("" + sum) + ret;
-            }
-            while(stkb.isEmpty() && !stka.isEmpty()){
-                int sum = 0;
-                sum = stka.pop() - '0';
-                if(prev == 1) {
-                    prev = 0;
-                    sum++;
-                }
-                if(sum > 9) {
-                    sum -= 10;
-                    prev = 1;
-                }
-                ret = ("" + sum) + ret;
-            }
-        if(stka.isEmpty() && stkb.isEmpty()){
-            if(prev == 1) ret = "1" + ret;
-        }
-        return ret;
+        if(prev == 1) ret.append(1);
+        return ret.reverse().toString();
     }
 }
