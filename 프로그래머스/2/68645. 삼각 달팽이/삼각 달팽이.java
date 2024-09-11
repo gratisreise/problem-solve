83cf112ed7e42ea19eee5b96279c9a8b3dd14023
noah <104671487+gratisreise@users.noreply.github.com>
2024-09-11 09:58:08 +0900

    [level 2] Title: 삼각 달팽이, Time: 31.49 ms, Memory: 160 MB -BaekjoonHub

--- 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.java
+++ 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.java
@@ -1,36 +1,40 @@
-public class Solution {
-    private static final int[] dx = {0, 1, -1};
-    private static final int[] dy = {1, 0, -1};
-
-    public int[] solution(int n) {
-        int[][] triangle = new int[n][n];
-        int v = 1;
-        int x = 0;
-        int y = 0;
-        int d = 0;
-
-        while (true) {
-            triangle[y][x] = v++;
-            int nx = x + dx[d];
-            int ny = y + dy[d];
-            if (nx == n || ny == n || nx == -1 || ny == -1 || triangle[ny][nx] != 0) {
-                d = (d + 1) % 3;
-                nx = x + dx[d];
-                ny = y + dy[d];
-                if (nx == n || ny == n || nx == -1 || ny == -1 || triangle[ny][nx] != 0) break;
-            }
-            x = nx;
-            y = ny;
-        }
-
-        int[] result = new int[v - 1];
-        int index = 0;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j <= i; j++) {
-                result[index++] = triangle[i][j];
+import java.util.*;
+class Solution {
+    //방향
+    static int[] dy = {1, 0, -1};
+    static int[] dx = {0, 1, -1};
+    //2차원배열
+    static int[][] temp;
+    public List<Integer> solution(int n) {
+        temp = new int[n + 1][n + 1];
+        move(0, n, 1, 0, 0);
+        List<Integer> ret = new ArrayList<>();
+        for(int[] arr : temp){
+            for(int a : arr){
+                if(a == 0) continue;
+                ret.add(a);
             }
         }
-
-        return result;
+        return ret;
+    }
+    private void move(int d, int n, int startNum, int y, int x){
+        if(n == 0) return;
+        int cy = y;
+        int cx = x;
+        int number = startNum;
+        for(int i = 0; i < n; i++){
+            int ny = cy + dy[d % 3];
+            int nx = cx + dx[d % 3];
+            temp[cy][cx] = number;
+            number++;
+            if(i == n - 1) break;
+            cy = ny;
+            cx = nx;
+        }
+        // 방향 더하기, 크기 -1, 숫자는 더한거 그대로 가지고 하기, 시작점 방향 바꾼거 더하기
+        move(d + 1, n - 1, number, cy + dy[(d + 1) % 3], cx + dx[(d+1) % 3]);
     }
 }
+/*
+1.더하는 값은 고정, 방향과 크기는 변화
+*/
