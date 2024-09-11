914ead8ff7c553b1484e201c93b3a771635b9a5b
noah <104671487+gratisreise@users.noreply.github.com>
2024-09-11 11:45:49 +0900

    [level 2] Title: 거리두기 확인하기, Time: 0.07 ms, Memory: 77.3 MB -BaekjoonHub

--- 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.java
+++ 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.java
@@ -1,66 +1,74 @@
-import java.util.*;
 class Solution {
-    static int[][] visited;
-    static int[] dy = {-1, 0, 1, 0};
-    static int[] dx = {0, 1, 0, -1};
-    static boolean check;
-    static char[][] temp;
-    public List<Integer> solution(String[][] places) {
-        List<Integer> ret = new ArrayList<>();
-        for(String[] sArr : places){
-            int row = sArr.length;
-            int col = sArr[0].length();
-            temp = new char[row][col];
+    private static final int[] dx = {0, -1, 1, 0};
+    private static final int[] dy = {-1, 0, 0, 1};
+    
+    //다음 테이블에서 탐색
+    private boolean isNextToVolunteer(char[][] room, int x, int y, int exclude){
+        for(int d = 0; d < 4; d++){
+            if(d == exclude) continue;
             
-            for(int i = 0; i < row; i++){
-                temp[i] = sArr[i].toCharArray();
-            }
-            
-            //--------
-            
-            // 초기화 
-            check = false;
-            visited = new int[row][col];
-            
-            for(int i = 0; i < row; i++){
-                for(int j = 0; j < col; j++){
-                    if(temp[i][j] == 'P') {
-                        go(i, j, 2, row, col);
-                    }
-                    if(check) break;
-                }
-            }
-            if(check) ret.add(0);
-            else ret.add(1);
-            
-        }    
-        return ret;
-    }
-    private void go(int y, int x, int cnt, int n, int m){
-        if(cnt == 0) return;
-        if(check) return;
-        //방문처리
-        visited[y][x] = 1;
-        for(int i = 0; i < 4; i++){
-            int ny = y + dy[i];
-            int nx = x + dx[i];
-            // 경계값 처리
-            if(ny < 0 || nx < 0 || ny >= n || nx >= m) continue;
-            if(visited[ny][nx] != 0) continue;
-            // 파티션 있으면 더이상 못간다
-            if(temp[ny][nx] == 'X') continue;
-            // 범위 안에 사람 있으면 true
-            if(temp[ny][nx] == 'P') check = true;
-            go(ny, nx, cnt - 1, n, m);
+            int nx = x + dx[d];
+            int ny = y + dy[d];
+            if(ny < 0 || ny >= room.length || nx < 0 || nx >= room[ny].length) continue;
+            if(room[ny][nx] == 'P') return true;
         }
-        visited[y][x] = 0;
+        return false;
     }
     
+    // 해당 좌표에서 거리두기 지키는지 검사
+    private boolean isDistinced(char[][] room, int x, int y){
+        //room[y][x]가 거리두기를 지키는지 검사
+        for(int d = 0; d < 4; d++){
+            int ny = y + dy[d];
+            int nx = x + dx[d];
+            if(ny < 0 || ny >= room.length || nx < 0 || nx >= room[ny].length) continue;
+            //room[ny][nx]를 통해 다른 응시자에 도달할 수 있는지 검사
+            switch(room[ny][nx]){
+                case 'P': return false;
+                case 'O':
+                    //인접한 곳에 다른 응시자가 있는지 검사
+                    if(isNextToVolunteer(room, nx, ny, 3 - d)) return false;
+                    break;
+            }
+        }
+        return true;
+    }
+    
+     // 해당 대기실이 거리두기 지키는지 검사
+    private boolean isDistinced(char[][] room){
+        for(int y = 0; y < room.length; y++){
+            for(int x = 0; x < room[y].length; x++){
+                if(room[y][x] != 'P') continue;
+                //거리두기 검사
+                if(!isDistinced(room, x, y)) return false;
+            }
+        }
+        
+        return true;
+    }
+    
+    
+    public int[] solution(String[][] places) {
+        int[] answer = new int[places.length];
+        for(int i = 0; i < answer.length; i++){
+            String[] place = places[i];
+            char[][] room = new char[place.length][];
+            for(int j = 0; j < room.length; j++){
+                room[j] = place[j].toCharArray();
+            }
+            //거리두기 검사 후 answer에 기록
+            if(isDistinced(room)) answer[i] = 1;
+            else answer[i] = 0;
+        }
+        
+        return answer;
+    }
+   
 }
-
 /*
-- 대기실 5개, 5*5 크기
-- 맨해튼 거리(|y1-y2| + |x1-x2|) > 2
-- 파티션 있으면 허용
-크기2의 dfs 탐색을 할 동안 
+1. 대기실의 모든 응시자 위치에 대해 반복
+    1-1. 상하좌우 중 빈 테이블이 있는 방향에 대해 1-2로 진행
+    1-2. 빈 테이블과 인접한 위치 중 응시자가 있다면 거리두기를 지키지 않은 것
+2. 모든 응시자의 위치를 검사했으나 거리두기를 지키지 않은 경우를 발견하지 못했으면 거리두기를 지킨 것
+
 */
