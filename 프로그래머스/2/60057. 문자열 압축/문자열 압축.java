f8d447516ab89aa3a41c6c2e336060551943fd08
noah <104671487+gratisreise@users.noreply.github.com>
2024-09-12 17:10:24 +0900

    [level 2] Title: 문자열 압축, Time: 3.92 ms, Memory: 75.5 MB -BaekjoonHub

--- 프로그래머스/2/60057. 문자열 압축/문자열 압축.java
+++ 프로그래머스/2/60057. 문자열 압축/문자열 압축.java
@@ -1,25 +1,43 @@
 class Solution {
     public int solution(String s) {
-        int ans = 0;
-        int n = s.length();
-        int min = 1000000;
-        for(int i = 1; i <= n / 2 + 1; i++){
-            StringBuilder ret = new StringBuilder(); //결과 담을 것
+        int ret = 10000;
+        if(s.length() == 1) return 1;
+        for(int i = 1; i <= s.length() / 2; i++){
+            StringBuilder builder = new StringBuilder();
+            int index = 0;
+            int cnt = 1;
             String prev = "";
-            int size = 1;
-            for(int j = 0; j < n; j += i){
-                int end = Math.min(j + i, s.length());
-                String temp = s.substring(j, end);
-                if(temp.equals(prev)) size++;
-                else {
-                    ret.append(size > 1 ? size + prev : prev);
-                    size = 1;
+            while(index <= s.length() - i){
+                String cur = s.substring(index, index + i);
+                //이전이랑 같으면
+                if(cur.equals(prev)) cnt++;
+                else { // 같지않으면
+                    //반복수 한개인경우
+                    if(cnt == 1) builder.append(prev);
+                    else { //반복수 여러개인경우
+                        builder.append(cnt);
+                        builder.append(prev);
+                    }
+                    cnt = 1;
                 }
-                prev = temp;
+                index += i;
+                prev = cur;
             }
-            ret.append(size > 1 ? size + prev : prev);
-            min = Math.min(min, ret.length()); 
-        } 
-        return ans = min;
+            if(cnt == 1) builder.append(prev);
+            else {
+                builder.append(cnt);
+                builder.append(prev);
+            }
+            if(index < s.length()) builder.append(s.substring(index));
+            ret = Math.min(ret, builder.length());
+        }
+        
+        return ret;
     }
 }
+/*
+1. 압축 가능한 크기의 경우만큼 반복
+2. 모든 문자열에 대해 반복
+ 2-1. 압축하는 크기만큼 문자열 체크
+ 2-2. 문자열의 크기가 작으면 교체
+*/
