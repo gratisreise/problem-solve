365cc181360eef7e8d829030b86c7eae34577c00
noah <104671487+gratisreise@users.noreply.github.com>
2024-09-12 15:33:57 +0900

    [level 1] Title: 시저 암호, Time: 1.36 ms, Memory: 75.1 MB -BaekjoonHub

--- 프로그래머스/1/12926. 시저 암호/시저 암호.java
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.java
@@ -1,17 +1,34 @@
 class Solution {
     public String solution(String s, int n) {
-        String ret = "";
+        String answer = "";
+        StringBuilder ret = new StringBuilder();
+        char[] cArr = s.toCharArray();
         
-        for(char c : s.toCharArray()){
+        for(char c : cArr){
             if('a' <= c && c <= 'z'){
-                if(c + n > 'z') ret += (char)(c - 26 + n);
-                else ret += (char)(c + n);
+                ret.append(pushLower(c, n));
             } else if('A' <= c && c <= 'Z'){
-                if(c + n > 'Z') ret += (char)(c - 26 + n);
-                else ret += (char)(c + n);
-            } else ret += c;
+                ret.append(pushUpper(c, n));
+            } else ret.append(c);
         }
         
-        return ret;
+        return ret.toString();
     }
+    private char pushLower(char c, int n){
+        if(c + n <= 'z') return (char)(c + n);
+        else return (char)(c + n - 26);
+    }
+    private char pushUpper(char c, int n){
+        if(c + n <= 'Z') return (char)(c + n);
+        else return (char)(c + n - 26);
+    }
+    
 }
+/*
+1. 문자열을 문자의 배열로 만든다
+2. 문자의 배열을 순회
+    2-1. 소문자 => 밀기, 빌더에 넣기 
+    2-2. 대문차 => 밀기, 빌더에 넣기
+    2-3. 공백  => 배제, 빌더에 넣기
+3. 변환된 문자의 배열을 
+*/
