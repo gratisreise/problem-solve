6f6d5014a8f35d9a1f5d64fcefe03b7ef5d22855
noah <104671487+gratisreise@users.noreply.github.com>
2024-09-13 11:32:30 +0900

    [level 2] Title: 문자열 압축, Time: 6.18 ms, Memory: 78.9 MB -BaekjoonHub

--- 프로그래머스/2/60057. 문자열 압축/문자열 압축.java
+++ 프로그래머스/2/60057. 문자열 압축/문자열 압축.java
@@ -1,43 +1,55 @@
+import java.util.*;
 class Solution {
     public int solution(String s) {
-        int ret = 10000;
+        int min = Integer.MAX_VALUE;
         if(s.length() == 1) return 1;
-        for(int i = 1; i <= s.length() / 2; i++){
-            StringBuilder builder = new StringBuilder();
-            int index = 0;
-            int cnt = 1;
-            String prev = "";
-            while(index <= s.length() - i){
-                String cur = s.substring(index, index + i);
-                //이전이랑 같으면
-                if(cur.equals(prev)) cnt++;
-                else { // 같지않으면
-                    //반복수 한개인경우
-                    if(cnt == 1) builder.append(prev);
-                    else { //반복수 여러개인경우
-                        builder.append(cnt);
-                        builder.append(prev);
-                    }
-                    cnt = 1;
-                }
-                index += i;
-                prev = cur;
-            }
-            if(cnt == 1) builder.append(prev);
-            else {
-                builder.append(cnt);
-                builder.append(prev);
-            }
-            if(index < s.length()) builder.append(s.substring(index));
-            ret = Math.min(ret, builder.length());
+        for(int length = 1; length <= s.length() / 2; length++){
+            //문자열 압축 후 가장 짧은 길이 선택
+            int compressed = compress(s, length);
+            if(compressed < min) min = compressed;
         }
         
-        return ret;
+        return min;
+    }
+    
+    private int compress(String source, int length){
+        StringBuilder builder = new StringBuilder();
+        String last = "";
+        int count = 0;
+        
+        for(String token : split(source, length)){
+            //압축문자열 구성
+            if(token.equals(last)) {
+                count++;
+            } else { // 새로운 토큰 등장 처리
+                if(count > 1) builder.append(count);
+                builder.append(last);
+                last = token;
+                count = 1;
+            }
+        }
+        
+        if(count > 1) builder.append(count);
+        builder.append(last);
+        
+        return builder.length();
+    }
+    
+    private List<String> split(String source, int length){
+        List<String> tokens = new ArrayList<>();
+        //source를 length만큼씩 잘라 token 리스트에 추가
+        for(int startIndex = 0; startIndex < source.length(); startIndex += length){
+            int endIndex = startIndex + length;
+            if(endIndex > source.length()) endIndex = source.length();
+            //문자열을 startIndex부터 잘라 tokens 리스트에 추가
+            tokens.add(source.substring(startIndex, endIndex));
+        }
+        return tokens;
     }
 }
 /*
-1. 압축 가능한 크기의 경우만큼 반복
-2. 모든 문자열에 대해 반복
- 2-1. 압축하는 크기만큼 문자열 체크
- 2-2. 문자열의 크기가 작으면 교체
+1. 1부터 입력 문자열 s의 길이만큼 자를 문자열의 길이를 설정하며 반복
+2. 설정된 길이만큼 문자열을 잘라 낸 token의 배열 생성
+3. 문자열을 비교하며 token의 배열을 하나의 문자열로 압축
+4. 1~3 과정으로 압축된 문자열 중 가장 짧은 길이 반환
 */
