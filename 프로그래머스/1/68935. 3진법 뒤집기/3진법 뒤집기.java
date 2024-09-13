5a2c4311be0b8c5ce1c4f1528c8d02aacd3b0e58
noah <104671487+gratisreise@users.noreply.github.com>
2024-09-13 12:03:45 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.07 ms, Memory: 75 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -1,26 +1,7 @@
 class Solution {
     public int solution(int n) {
-        int ret = 0;
-        
-        int temp = n;
-        //3진법 변환
-        String s3 = "";
-        while(temp > 0){
-            s3 += ("" + temp % 3);
-            temp /= 3;
-        }
-        
-        //계산
-        for(int i = s3.length() - 1; i >= 0; i--){
-            int k = i;
-            if(s3.charAt(i) == '0') continue;
-            int n3 = 1;
-            for(int j = 0; j < s3.length() - 1 -k; j++){
-                n3 *= 3;
-            }
-            ret += (s3.charAt(i) - '0') * n3;
-        }
-        
+        String num3 = new StringBuilder(Integer.toString(n, 3)).reverse().toString();
+        int ret = Integer.parseInt(num3, 3);
         return ret;
     }
 }
