72c774b06368a90c69e88e512ffe22429a6d9fe7
noah <104671487+gratisreise@users.noreply.github.com>
2024-09-14 14:58:09 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.08 ms, Memory: 77.6 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -1,37 +1,16 @@
 class Solution {
-    static int stringToInt(String s){
-        
-        return switch(s){
-                case "zero" -> 0;
-                case "one" -> 1;
-                case "two" -> 2;
-                case "three" -> 3;
-                case "four" -> 4;
-                case "five" -> 5;
-                case "six" -> 6;
-                case "seven" -> 7;
-                case "eight" -> 8;
-                case "nine" -> 9;
-                default -> 10;
-        };
-    }
     public int solution(String s) {
-        int ret = 0;
-        String temp = "";
-        String ans = "";
-        for(char c : s.toCharArray()){
-            if('0' <= c && c <= '9') ans +=c;
-            else temp += c;
-            // System.out.println(temp);
-            if(temp.length() >= 3){
-                if(stringToInt(temp) == 10) continue;
-                else {
-                    ans += "" + stringToInt(temp);
-                    temp = "";
-                }
-            }
-        }
-        ret = Integer.parseInt(ans);
-        return ret;
+        s =  s.replace("zero", "0")
+              .replace("one", "1")
+              .replace("two", "2")
+              .replace("three", "3")
+              .replace("four", "4")
+              .replace("five", "5")
+              .replace("six", "6")
+              .replace("seven", "7")
+              .replace("eight", "8")
+              .replace("nine", "9");
+        
+        return Integer.parseInt(s);
     }
 }
