94100b2ffb968d278bb5b8bfa11f3b6a708a87f4
noah <104671487+gratisreise@users.noreply.github.com>
2024-09-16 12:01:32 +0900

    [level 2] Title: 쿼드압축 후 개수 세기, Time: 30.88 ms, Memory: 128 MB -BaekjoonHub

--- 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.java
+++ 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.java
@@ -1,29 +1,54 @@
-import java.util.*;
 class Solution {
-    private static int[] ret = new int[2];
-    private static int[][] arrs;
-    public int[] solution(int[][] arr) {
-        arrs = arr;
-        quad(0, 0, arr.length);
-        return ret;
-    }
-    private void quad(int y, int x, int n){
-        if(n == 1) {
-            ret[arrs[y][x]] += 1;
-            return;
+    private static class Count{
+        public final int zero;
+        public final int one;
+        
+        public Count(int zero, int one){
+            this.zero = zero;
+            this.one = one;
         }
-        int num = arrs[y][x];
-        for(int i = y; i < y + n; i++){
-            for(int j = x; j < x + n; j++){
-                if(arrs[i][j] != num){
-                    quad(y, x, n / 2);
-                    quad(y, x + n / 2, n / 2);
-                    quad(y + n / 2, x, n / 2);
-                    quad(y + n / 2, x + n / 2, n / 2);
-                    return;
+        public Count add(Count other){
+            return new Count(zero + other.zero, one + other.one);
+        }
+    }
+    private Count count(int offsetX, int offsetY, int size, int[][] arr){
+        int h = size / 2;
+        for(int x = offsetX; x < offsetX + size; x++){
+            for(int y = offsetY; y < offsetY + size; y++){
+                if(arr[y][x] != arr[offsetY][offsetX]){
+                    /*원소가 섞여있는 경우 반환값*/
+                    return count(offsetX, offsetY, h, arr)
+                            .add(count(offsetX + h, offsetY, h, arr))
+                            .add(count(offsetX, offsetY + h, h, arr))
+                            .add(count(offsetX + h, offsetY + h, h, arr));
+                        
                 }
             }
         }
-        ret[num] += 1;
+        //모든 원소가 같은 값인 경우
+        if(arr[offsetY][offsetX] == 1){
+            return new Count(0, 1);
+        }
+        return new Count(1, 0);
     }
+    public int[] solution(int[][] arr) {
+        Count count = count(0, 0, arr.length, arr);
+        return new int[]{count.zero, count.one};
+    }
+    
 }
+/*
+상태
+(offseX, offsetY, size) = 좌표 (offsetX, offsetY)에서 시작하여 가로 길이와 세로 길이가 size인
+정사각형을 압축했을 때 남아 있는 0과 1의 개수3
+종료조건
+범위 안의 모든 원소가 0또는 1 {0:zero, 1:one} 0개수 : 1개수
+{0:1, 1:0} -> 모든 원소 0
+{0:0, 1:1} -> 모든 원소 1
+점화식
+(offsetX, offsetY, size) = (offsetX, offsetY, size/2)
+                        + (offsetX + size/2, offsetY, size/2)
+                        + (offsetX , offsetY + size/2, size/2)
+                        + (offsetX + size/2, offsetY + size/2, size/2)
+                        
+*/
