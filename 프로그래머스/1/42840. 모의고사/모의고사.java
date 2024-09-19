6c2a3597b30b9d99e58539959218cd4d2aaaa5f8
noah <104671487+gratisreise@users.noreply.github.com>
2024-09-19 18:33:06 +0900

    [level 1] Title: 모의고사, Time: 5.65 ms, Memory: 77 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -1,23 +1,44 @@
-import java.util.*;
+
+import java.util.stream.*;
+
 class Solution {
-    static List<Integer> l = new ArrayList<>();
-    public List<Integer> solution(int[] answers) {
-        int n = answers.length;
-        int[] cnt = new int[3];
-        int max = -1;
-        int[] num1 = {1, 2, 3, 4, 5};
-        int[] num2 = {2, 1, 2, 3, 2, 4, 2, 5};
-        int[] num3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
-        int n1 = num1.length;
-        int n2 = num2.length;
-        int n3 = num3.length;
-        for(int i = 0; i < n; i++){
-            if(num1[i % n1] == answers[i]) cnt[0]++;
-            if(num2[i % n2] == answers[i]) cnt[1]++;
-            if(num3[i % n3] == answers[i]) cnt[2]++;
+    private static final int[][] RULES = {
+        {1, 2, 3, 4, 5},
+        {2, 1, 2, 3, 2, 4, 2, 5},
+        {3, 3, 1, 1, 2, 2, 4, 4, 5, 5}
+    };
+
+    //수포자의 해당 문제번호 답 구하는 메서드
+    private int getPicked(int person, int problem){
+        int[] rule = RULES[person];
+        int index = problem % rule.length;
+        return rule[index];
+    }
+
+    public int[] solution(int[] answers) {
+        //맞은개수
+        int[] corrects = new int[3];
+        //최대정답수
+        int max = 0;
+
+        for(int problem = 0; problem < answers.length; problem++){
+            int answer = answers[problem];
+
+            for(int person = 0; person < 3; person++){
+                int picked = getPicked(person, problem);
+                if(answer == picked){
+                    if(++corrects[person] > max){
+                        max = corrects[person];
+                    }
+                }
+            }
         }
-        for(int i = 0; i < 3; i++) max = Math.max(cnt[i], max);
-        for(int i = 0; i < 3; i++) if(cnt[i] == max) l.add(i + 1);
-        return l;
+
+        final int maxCorrects = max;
+        
+        return IntStream.range(0, 3)
+            .filter(i -> corrects[i] == maxCorrects)
+            .map(i -> i + 1)
+            .toArray();
     }
 }
