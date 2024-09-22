dc1d4ff63e7933d5df06a1c4da625b58533c0edd
noah <104671487+gratisreise@users.noreply.github.com>
2024-09-22 11:34:22 +0900

    [level 0] Title: 평행, Time: 0.03 ms, Memory: 76.3 MB -BaekjoonHub

--- 프로그래머스/0/120875. 평행/평행.java
+++ 프로그래머스/0/120875. 평행/평행.java
@@ -1,29 +1,34 @@
 import java.util.*;
 class Solution {
-    static boolean check(int a, int b, int[][] dots){
-        boolean ans = false;
-        List<Integer> l = new ArrayList<>();
-        for(int i = 0; i <  dots.length; i++){
-            if(i == a || i == b) continue;
-            l.add(i);
-        }
-        int c = l.get(0);
-        int d = l.get(1);
-        int tempincline1 = Math.abs(dots[a][0] - dots[b][0]) * 1000 / Math.abs(dots[a][1] - dots[b][1]);
-        int tempincline2 = Math.abs(dots[c][0] - dots[d][0]) * 1000 / Math.abs(dots[c][1] - dots[d][1]);
-        if(tempincline1 == tempincline2) ans = true;
-        if(tempincline1 == 0 && tempincline2 == 0) ans = false;
-        return ans;
-    }
+    private static final int[][][] cases = {
+        {{0, 1}, {2, 3}},
+        {{0, 2}, {1, 3}},
+        {{0, 3}, {1, 2}}
+    };
     public int solution(int[][] dots) {
-        int ret = 0;
-        boolean flag = false;
-        for(int i = 0; i < 4; i++){
-            for(int j = 0; j < i; j++){
-               if(check(i, j, dots)) flag = true;
+        int answer = 0;
+        for(int i = 0; i < 3; i++){
+            double inclination = 0;
+            double prev = 0;
+            for(int j = 0; j < 2; j++){
+                int dy = dots[cases[i][j][0]][0] - dots[cases[i][j][1]][0];
+                int dx = dots[cases[i][j][0]][1] - dots[cases[i][j][1]][1];
+                inclination = (double) dy / dx;
+                if(prev == inclination) return 1;
+                prev = inclination;
             }
         }
-        if(flag) ret = 1;
-        return ret;
+        return 0;
     }
 }
+/*
+두팀씩 짝 짓는 경우의 수를 구하여 저장
+해당 경우의 수를 돌면서 기울기 체크
+{{0, 1}, {2, 3}}
+{{0, 2}, {1, 3}}
+{{0, 3}, {1, 2}}
+겹친다? => 모든 점의 
+y = 2x + 1;
+{1, 3},{2, 5}, {3, 7}, {4, 9}
+
+*/
