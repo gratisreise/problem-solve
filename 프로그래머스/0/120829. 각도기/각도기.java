7de00f34cfc1b3fb846e146b1e363151a55cf815
noah <104671487+gratisreise@users.noreply.github.com>
2024-09-24 17:39:59 +0900

    [level 0] Title: 각도기, Time: 0.02 ms, Memory: 72.8 MB -BaekjoonHub

--- 프로그래머스/0/120829. 각도기/각도기.java
+++ 프로그래머스/0/120829. 각도기/각도기.java
@@ -2,9 +2,9 @@ class Solution {
     public int solution(int angle) {
         int ret = 0;
         if(angle < 90) ret = 1;
-        else if(angle == 90) ret = 2;
-        else if(angle < 180) ret = 3;
-        else if(angle == 180) ret = 4;
+        else if(angle == 90) ret =  2;
+        else if(angle < 180) ret =  3;
+        else if(angle == 180) ret =  4;
         return ret;
     }
 }
