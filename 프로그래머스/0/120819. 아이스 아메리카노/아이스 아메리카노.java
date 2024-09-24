93a7f6c4cf4b5648d32bdcc0d25c57d5782a1a98
noah <104671487+gratisreise@users.noreply.github.com>
2024-09-24 17:58:54 +0900

    [level 0] Title: 아이스 아메리카노, Time: 0.04 ms, Memory: 73.2 MB -BaekjoonHub

--- 프로그래머스/0/120819. 아이스 아메리카노/아이스 아메리카노.java
+++ 프로그래머스/0/120819. 아이스 아메리카노/아이스 아메리카노.java
@@ -1,8 +1,5 @@
 class Solution {
     public int[] solution(int money) {
-        int[] ret = {0, 0};
-        ret[0] = money / 5500;
-        ret[1] = money % 5500;
-        return ret;
+        return new int[]{money / 5500, money % 5500};
     }
 }
