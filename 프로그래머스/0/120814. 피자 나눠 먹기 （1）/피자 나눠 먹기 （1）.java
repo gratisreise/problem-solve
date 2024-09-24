4d04d683c459baa437f2eb27b6fb61e6a72dfc25
noah <104671487+gratisreise@users.noreply.github.com>
2024-09-24 17:56:07 +0900

    [level 0] Title: 피자 나눠 먹기 (1), Time: 0.03 ms, Memory: 78 MB -BaekjoonHub

--- 프로그래머스/0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.java
+++ 프로그래머스/0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.java
@@ -1,8 +1,6 @@
 class Solution {
     public int solution(int n) {
-        int ret = 0;
-        ret = n / 7;
-        if(n % 7 > 0) ret++;
-        return ret;
+            
+        return n % 7 == 0 ? n / 7 : n / 7 + 1;
     }
 }
