211c535e797b68a9763bfa7234805ecbdb125677
noah <104671487+gratisreise@users.noreply.github.com>
2024-09-24 11:21:41 +0900

    [level 1] Title: 옹알이 (2), Time: 31.97 ms, Memory: 78.5 MB -BaekjoonHub

--- 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.java
+++ 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.java
@@ -1,30 +1,21 @@
-import java.util.*;
 class Solution {
+    String[] arr = {"aya", "ye", "woo", "ma"};
     public int solution(String[] babbling) {
-        int ret = 0;
-        List<String> sl = new ArrayList<>();
-        sl.add("aya");
-        sl.add("ye");
-        sl.add("woo");
-        sl.add("ma");
+        int answer = 0;
         for(String s : babbling){
-            String temp = "";
-            String prev = "";
-            boolean flag = false;
-            for(char c : s.toCharArray()){
-                temp += c;
-                if(sl.contains(temp) && prev.equals(temp)){ 
-                    flag = true;
-                } else if(sl.contains(temp) && !prev.equals(temp)){
-                    prev = temp;
-                    temp = "";
-                }
-                
+            String temp = s;
+            for(String ss : arr){
+                temp = temp.replaceAll("(?<!"+ ss +")"+"("+ ss + ")" +"(?!" + ss + ")", " ");
             }
-            if(temp.length() > 0 && !sl.contains(temp)) flag = true;
-            if(temp.equals(s)) flag = true;
-            if(!flag) ret++;
+            temp = temp.replaceAll(" ", "");
+            System.out.println(temp);
+            if(temp.equals("")) answer++;
         }
-        return ret;
+        
+        return answer;
     }
 }
+/*
+같은 발음이 연속되지 않고 
+
+*/
