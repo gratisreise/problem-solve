e7c6dbf4e42eebc59f36b8236a182479cb619535
noah <104671487+gratisreise@users.noreply.github.com>
2024-09-24 17:54:42 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.04 ms, Memory: 72.7 MB -BaekjoonHub

--- 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -2,8 +2,8 @@ import java.util.*;
 class Solution {
     public List<Integer> solution(int n) {
         List<Integer> ret = new ArrayList<>();
-        for(int i = 1; i <= n; i += 2){
-            ret.add(i);
+        for(int i = 1; i <= n; i++){
+            if(i % 2 != 0) ret.add(i);
         }
         return ret;
     }
