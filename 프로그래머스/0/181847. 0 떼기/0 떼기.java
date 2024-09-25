0a57b70571bc106ef993c36abeb31385240b1850
noah <104671487+gratisreise@users.noreply.github.com>
2024-09-25 11:41:44 +0900

    [level 0] Title: 0 떼기, Time: 0.05 ms, Memory: 81 MB -BaekjoonHub

--- 프로그래머스/0/181847. 0 떼기/0 떼기.java
+++ 프로그래머스/0/181847. 0 떼기/0 떼기.java
@@ -1,15 +1,7 @@
 class Solution {
     public String solution(String n_str) {
-        String ret = "";
-        boolean flag = false;
-        for(char c : n_str.toCharArray()){
-            if(c == '0' && !flag) continue;
-            else {
-                ret += c;
-                flag = true;
-            }
-        }
-        
-        return ret;
+        StringBuilder ret = new StringBuilder(n_str);
+        while(ret.charAt(0) == '0') ret.deleteCharAt(0);
+        return ret.toString();
     }
 }
