08d9d784ebfbc6994b11b8909fa4b0555218e5e6
noah <104671487+gratisreise@users.noreply.github.com>
2024-09-25 11:16:35 +0900

    [level 0] Title: 원소들의 곱과 합, Time: 0.08 ms, Memory: 74.3 MB -BaekjoonHub

--- 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.java
+++ 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.java
@@ -1,15 +1,11 @@
 class Solution {
     public int solution(int[] num_list) {
-        int ret = 0;
-        int num1 = 1; // 모든 원소들의 곱
-        int num2 = 0; // 모든 원소들의 합의 제곱
+        int num1 = 1;
+        int num2 = 0;
         for(int i : num_list){
             num1 *= i;
             num2 += i;
         }
-        num2 *= num2;
-        System.out.println(num1 + " : " + num2);
-        if(num1 < num2) ret = 1;
-        return ret;
+        return num1 < (int)Math.pow(num2, 2) ? 1 : 0;
     }
 }
