ef301f173c9a9563d424fca244885048285842f0
noah <104671487+gratisreise@users.noreply.github.com>
2024-09-25 11:44:08 +0900

    [level 0] Title: 배열의 길이에 따라 다른 연산하기, Time: 0.06 ms, Memory: 73.1 MB -BaekjoonHub

--- 프로그래머스/0/181854. 배열의 길이에 따라 다른 연산하기/배열의 길이에 따라 다른 연산하기.java
+++ 프로그래머스/0/181854. 배열의 길이에 따라 다른 연산하기/배열의 길이에 따라 다른 연산하기.java
@@ -1,13 +1,11 @@
 class Solution {
     public int[] solution(int[] arr, int n) {
-        if(arr.length % 2 != 0){
-            for(int i = 0; i < arr.length; i++){
-                if(i % 2 == 0) arr[i] += n;
-            }
-        } else {
-            for(int i = 0; i < arr.length; i++){
-                if(i % 2 != 0) arr[i] += n;
-            }
+        int flag = 0;
+        if(arr.length % 2 != 0) flag = 1;
+        else flag = 0;
+        for(int i = 0 ; i < arr.length; i++){
+            if(flag == 1 && i % 2 == 0) arr[i] += n;
+            else if(flag == 0 && i % 2 != 0) arr[i] += n;
         }
         return arr;
     }
