933368df4f709b4432d28e434ab280b96ad225b3
noah <104671487+gratisreise@users.noreply.github.com>
2024-09-25 11:52:23 +0900

    [level 0] Title: 뒤에서 5등까지, Time: 0.52 ms, Memory: 71 MB -BaekjoonHub

--- 프로그래머스/0/181853. 뒤에서 5등까지/뒤에서 5등까지.java
+++ 프로그래머스/0/181853. 뒤에서 5등까지/뒤에서 5등까지.java
@@ -1,11 +1,7 @@
 import java.util.*;
 class Solution {
-    public List<Integer> solution(int[] num_list) {
-        List<Integer> ret = new ArrayList<>();
+    public int[] solution(int[] num_list) {
         Arrays.sort(num_list);
-        for(int i = 0; i < 5; i++){
-            ret.add(num_list[i]);
-        }
-        return ret;
+        return Arrays.copyOf(num_list, 5);
     }
 }
