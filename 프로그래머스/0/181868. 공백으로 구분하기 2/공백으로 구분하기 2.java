5e223988d7fe98eabe0164dbf0851b4f06764b2f
noah <104671487+gratisreise@users.noreply.github.com>
2024-09-25 12:16:30 +0900

    [level 0] Title: 공백으로 구분하기 2, Time: 1.37 ms, Memory: 78.2 MB -BaekjoonHub

--- 프로그래머스/0/181868. 공백으로 구분하기 2/공백으로 구분하기 2.java
+++ 프로그래머스/0/181868. 공백으로 구분하기 2/공백으로 구분하기 2.java
@@ -1,18 +1,5 @@
-import java.util.*;
 class Solution {
-    public List<String> solution(String my_string) {
-        List<String> ret = new ArrayList<>();
-        String temp = "";
-        for(char c : my_string.toCharArray()){
-            if(c != ' ') temp += c;
-            else if(c == ' ' && temp.length() > 0){
-                ret.add(temp);
-                temp = "";
-            }
-        }
-        if(temp.length() > 0) ret.add(temp);
-        return ret;
+    public String[] solution(String my_string) {
+        return my_string.trim().split("\\s+");
     }
 }
-
-// 순회 -> 빈칸x -> 빈문자열에 담기 -> 빈문자열 나오면 리스트에 추가 -> 남아있으면 리스트에 추가
