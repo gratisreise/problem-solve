6ff3226ecca43037d593cf8a82fe76d923375edd
noah <104671487+gratisreise@users.noreply.github.com>
2024-09-25 11:34:43 +0900

    [level 0] Title: 홀수 vs 짝수, Time: 0.03 ms, Memory: 75.1 MB -BaekjoonHub

--- 프로그래머스/0/181887. 홀수 vs 짝수/홀수 vs 짝수.java
+++ 프로그래머스/0/181887. 홀수 vs 짝수/홀수 vs 짝수.java
@@ -1,15 +1,11 @@
 class Solution {
     public int solution(int[] num_list) {
-        int ret = 0;
-        int temp1 = 0;
-        int temp2 = 0;
+        int numEven = 0;
+        int numOdd = 0;
         for(int i = 0; i < num_list.length; i++){
-            if((i+1) % 2 == 0) temp1 += num_list[i];
-            else temp2 += num_list[i];
+            if((i+1) % 2 == 0) numEven += num_list[i];
+            else numOdd += num_list[i];
         }
-        System.out.println(temp1 + " :: " + temp2);
-        if(temp1 > temp2) ret = temp1;
-        else ret = temp2;
-        return ret;
+        return numEven >= numOdd ? numEven : numOdd;
     }
 }
