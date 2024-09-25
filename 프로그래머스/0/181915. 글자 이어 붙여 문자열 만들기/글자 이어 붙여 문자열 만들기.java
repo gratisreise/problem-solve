a8167de6756ec8eecfedcd78a02520c9ce49f5ff
noah <104671487+gratisreise@users.noreply.github.com>
2024-09-25 11:00:26 +0900

    [level 0] Title: 글자 이어 붙여 문자열 만들기, Time: 0.14 ms, Memory: 96.6 MB -BaekjoonHub

--- 프로그래머스/0/181915. 글자 이어 붙여 문자열 만들기/글자 이어 붙여 문자열 만들기.java
+++ 프로그래머스/0/181915. 글자 이어 붙여 문자열 만들기/글자 이어 붙여 문자열 만들기.java
@@ -1,9 +1,8 @@
 class Solution {
     public String solution(String my_string, int[] index_list) {
-        String ret = "";
-        for(int i : index_list){
-            ret += my_string.charAt(i);
-        }
-        return ret;
+        char[] arr = my_string.toCharArray();
+        StringBuilder ret = new StringBuilder();
+        for(int i : index_list) ret.append(arr[i]);
+        return ret.toString();
     }
 }
