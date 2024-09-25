59dbc0f8c699aebcee11f503c555b03c2cc688d8
noah <104671487+gratisreise@users.noreply.github.com>
2024-09-25 11:47:39 +0900

    [level 0] Title: 뒤에서 5등 위로, Time: 3.65 ms, Memory: 73.7 MB -BaekjoonHub

--- 프로그래머스/0/181852. 뒤에서 5등 위로/뒤에서 5등 위로.java
+++ 프로그래머스/0/181852. 뒤에서 5등 위로/뒤에서 5등 위로.java
@@ -1,11 +1,10 @@
 import java.util.*;
+import java.util.stream.*;
 class Solution {
-    public List<Integer> solution(int[] num_list) {
+    public int[] solution(int[] num_list) {
         List<Integer> ret = new ArrayList<>();
         Arrays.sort(num_list);
-        for(int i = 5; i < num_list.length; i++){
-            ret.add(num_list[i]);
-        }
-        return ret;
+        for(int i = 5; i < num_list.length; i++) ret.add(num_list[i]);
+        return ret.stream().mapToInt(Integer::intValue).toArray();
     }
 }
