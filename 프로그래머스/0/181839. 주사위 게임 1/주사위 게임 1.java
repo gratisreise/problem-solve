bfdb15069cbf434932b5a0112eca1592e587ecee
noah <104671487+gratisreise@users.noreply.github.com>
2024-09-25 11:50:52 +0900

    [level 0] Title: 주사위 게임 1, Time: 0.05 ms, Memory: 86.4 MB -BaekjoonHub

--- 프로그래머스/0/181839. 주사위 게임 1/주사위 게임 1.java
+++ 프로그래머스/0/181839. 주사위 게임 1/주사위 게임 1.java
@@ -1,13 +1,9 @@
 class Solution {
     public int solution(int a, int b) {
         int ret = 0;
-        if(a % 2 != 0 && b % 2 != 0){
-            ret = a*a + b*b;
-        } else if(a % 2 != 0 || b % 2 != 0){
-            ret = 2 *(a + b);
-        } else if(a % 2 == 0 && b % 2 == 0){
-            ret = Math.abs(a - b);
-        }
+        if(a % 2 != 0 && b % 2 != 0) ret = a * a + b * b;
+        else if(a % 2 != 0 || b % 2 != 0) ret = 2 * (a +b);
+        else ret = Math.abs(a - b);
         return ret;
     }
 }
