74a9dcf3a7294a7890809c588c830d8f6f6086f3
noah <104671487+gratisreise@users.noreply.github.com>
2024-09-25 10:54:38 +0900

    [level 0] Title: 접두사인지 확인하기, Time: 0.08 ms, Memory: 81.3 MB -BaekjoonHub

--- 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.java
+++ 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.java
@@ -1,13 +1,5 @@
 class Solution {
     public int solution(String my_string, String is_prefix) {
-        int ret = 0;
-        String temp = "";
-        if(my_string.length() > is_prefix.length()){
-        for(int i = 0; i < is_prefix.length(); i++){
-            temp += my_string.charAt(i);
-        }
-            }
-        if(temp.equals(is_prefix)) ret = 1;
-        return ret;
+        return my_string.startsWith(is_prefix) ? 1 : 0;
     }
 }
