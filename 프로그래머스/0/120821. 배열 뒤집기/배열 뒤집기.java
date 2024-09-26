1f5252fd631b50fe29033fef8567b5e17c387c6e
noah <104671487+gratisreise@users.noreply.github.com>
2024-09-26 10:47:50 +0900

    [level 0] Title: 배열 뒤집기, Time: 2.74 ms, Memory: 76.1 MB -BaekjoonHub

--- 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.java
+++ 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.java
@@ -1,10 +1,9 @@
 import java.util.*;
+import java.util.stream.*;
 class Solution {
-    public List<Integer> solution(int[] num_list) {
+    public int[] solution(int[] num_list) {
         List<Integer> ret = new ArrayList<>();
-        for(int i = num_list.length - 1; i >= 0; i--){
-            ret.add(num_list[i]);
-        }
-        return ret;
+        for(int i = num_list.length - 1; i >= 0; i--) ret.add(num_list[i]);
+        return ret.stream().mapToInt(Integer::intValue).toArray();
     }
 }
