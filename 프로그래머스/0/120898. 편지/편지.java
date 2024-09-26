ca5a011d1cf02d342ec2ef5f62b78412709b661f
noah <104671487+gratisreise@users.noreply.github.com>
2024-09-26 11:20:33 +0900

    [level 0] Title: 편지, Time: 0.03 ms, Memory: 68.1 MB -BaekjoonHub

--- 프로그래머스/0/120898. 편지/편지.java
+++ 프로그래머스/0/120898. 편지/편지.java
@@ -1,7 +1,5 @@
 class Solution {
     public int solution(String message) {
-        int ret = 0;
-        ret = message.length() * 2;
-        return ret;
+        return message.length() * 2;
     }
 }
