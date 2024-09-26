5037e1d4d89b1e5aaf2e50bbaa51cf7ea33c5ad6
noah <104671487+gratisreise@users.noreply.github.com>
2024-09-26 16:06:09 +0900

    [level 0] Title: 피자 나눠 먹기 (2), Time: 0.03 ms, Memory: 72.7 MB -BaekjoonHub

--- 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.java
+++ 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.java
@@ -1,23 +1,6 @@
-
 class Solution {
-// 최대공약수를 구하는 메서드 (유클리드 호제법)
-    public static int gcd(int a, int b) {
-        while (b != 0) {
-            int temp = b;
-            b = a % b;
-            a = temp;
-        }
-        return a;
-    }
-
-    // 최소공배수를 구하는 메서드
-    public static int lcm(int a, int b) {
-        return (a * b) / gcd(a, b);
-    }
     public int solution(int n) {
-        int ret = lcm(n, 6) / 6;;
-        
-        
-        return ret;
+        for(int i = 1; i <= 100; i++) if((i * 6) % n == 0) return i;
+        return 0;
     }
 }
