beb03837499ebd69bb5182a0be86cd725876bf80
noah <104671487+gratisreise@users.noreply.github.com>
2024-09-26 16:49:30 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.38 ms, Memory: 78.5 MB -BaekjoonHub

--- 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.java
+++ 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.java
@@ -1,6 +1,6 @@
 class Solution {
     public int solution(int[] numbers) {
-        int ret =-100000001;
+        int ret = Integer.MIN_VALUE;
         for(int i = 0; i < numbers.length; i++){
             for(int j = 0; j < numbers.length; j++){
                 if(i == j) continue;
