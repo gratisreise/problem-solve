9ef129f524efb8f540ec15ae994f16bcd1c2de4b
noah <104671487+gratisreise@users.noreply.github.com>
2024-09-26 16:41:12 +0900

    [level 0] Title: 약수 구하기, Time: 7.62 ms, Memory: 79.1 MB -BaekjoonHub

--- 프로그래머스/0/120897. 약수 구하기/약수 구하기.java
+++ 프로그래머스/0/120897. 약수 구하기/약수 구하기.java
@@ -1,11 +1,15 @@
 import java.util.*;
+import java.util.stream.*;
 class Solution {
-    public List<Integer> solution(int n) {
+    public int[] solution(int n) {
         List<Integer> ret = new ArrayList<>();
-        
-        for(int i = 1; i <= n; i++){
-            if(n % i == 0) ret.add(i);
+        for(int i = 1; i * i <= n; i++){
+            if(n % i == 0) {
+                ret.add(i);
+                if(i * i == n) continue;
+                ret.add(n / i);
+            }
         }
-        return ret;   
+        return ret.stream().sorted().mapToInt(Integer::intValue).toArray();
     }
 }
