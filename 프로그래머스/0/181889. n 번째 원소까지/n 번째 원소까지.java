6b16f20c3fa4b1d7cdda279c072a036c8128f9ef
noah <104671487+gratisreise@users.noreply.github.com>
2024-09-26 12:00:04 +0900

    [level 0] Title: n 번째 원소까지, Time: 5.85 ms, Memory: 78.8 MB -BaekjoonHub

--- 프로그래머스/0/181889. n 번째 원소까지/n 번째 원소까지.java
+++ 프로그래머스/0/181889. n 번째 원소까지/n 번째 원소까지.java
@@ -1,11 +1,9 @@
 import java.util.*;
+import java.util.stream.*;
 class Solution {
-    public List<Integer> solution(int[] num_list, int n) {
+    public int[] solution(int[] num_list, int n) {
         List<Integer> ret = new ArrayList<>();
-        
-        for(int i = 0; i < n; i++){
-            ret.add(num_list[i]);
-        }
-        return ret;
+        for(int i = 0; i < n; i++) ret.add(num_list[i]);
+        return ret.stream().mapToInt(Integer::intValue).toArray();
     }
 }
