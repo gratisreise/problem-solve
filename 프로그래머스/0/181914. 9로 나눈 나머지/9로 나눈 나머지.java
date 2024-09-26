781bf439cd5f2721ecf90d7f6f4de8c663dab564
noah <104671487+gratisreise@users.noreply.github.com>
2024-09-26 15:52:26 +0900

    [level 0] Title: 9로 나눈 나머지, Time: 7.24 ms, Memory: 84.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181914. 9로 나눈 나머지/9로 나눈 나머지.java
@@ -0,0 +1,7 @@
+class Solution {
+    public int solution(String number) {
+        int answer = 0;
+        for(char c : number.toCharArray()) answer += c - '0';
+        return answer % 9;
+    }
+}
