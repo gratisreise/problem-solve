4d58801e6f5109e03f003be2ded3a904e1171414
noah <104671487+gratisreise@users.noreply.github.com>
2024-09-26 15:55:04 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.04 ms, Memory: 79.3 MB -BaekjoonHub

--- 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.java
+++ 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.java
@@ -1,14 +1,9 @@
 class Solution {
     public int solution(int a, int b, int c) {
-        int ret = 0;
-        if(a != b && b != c && c != a){ //3개 서로달라
-            ret = a + b + c;
-        }else if(a == b && b == c && c == a){ // 3개 서로같아
-            ret = (a + b + c)*(a*a + b*b + c*c)*(a*a*a + b*b*b + c*c*c);
-        } else { //2개는 같고 1개는 달라
-            ret = (a + b + c) * (a*a + b*b + c*c);
-        }
-        return ret;
+        int answer = 0;
+        if(a == b && b == c) answer = (a + b + c) * (a*a + b*b + c*c) *(a*a*a + b*b*b + c*c*c);
+        else if(a != b && b != c && c != a) answer = a + b + c;
+        else answer = (a + b + c) * (a*a + b*b + c*c);
+        return answer;
     }
 }
-//??
