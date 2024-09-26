11de73750d88bac7d3ea6aa301df92da289bb5bc
noah <104671487+gratisreise@users.noreply.github.com>
2024-09-26 11:42:54 +0900

    [level 0] Title: 첫 번째로 나오는 음수, Time: 0.03 ms, Memory: 73.7 MB -BaekjoonHub

--- 프로그래머스/0/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.java
+++ 프로그래머스/0/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.java
@@ -1,13 +1,8 @@
 class Solution {
     public int solution(int[] num_list) {
-        int ret = -1;
         for(int i = 0; i < num_list.length; i++){
-            if(num_list[i] < 0){
-                ret = i;
-                break;
-            }
+            if(num_list[i] < 0) return i;
         }
-        
-        return ret;
+        return -1;
     }
 }
