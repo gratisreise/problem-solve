869429bacab53f80a36f36a69c52e84c33f00c6d
noah <104671487+gratisreise@users.noreply.github.com>
2024-09-26 11:38:05 +0900

    [level 0] Title: 접미사 배열, Time: 28.41 ms, Memory: 80.5 MB -BaekjoonHub

--- 프로그래머스/0/181909. 접미사 배열/접미사 배열.java
+++ 프로그래머스/0/181909. 접미사 배열/접미사 배열.java
@@ -1,13 +1,16 @@
 import java.util.*;
 class Solution {
-    public List<String> solution(String my_string) {
-        List<String> ret = new ArrayList<>();
+    public String[] solution(String my_string) {
+        StringBuilder sb = new StringBuilder(my_string).reverse();
+        char[] spells = sb.toString().toCharArray();
+        String[] psum = new String[spells.length];
         String temp = "";
-        for(int i = my_string.length() - 1; i >= 0; i--){
-            temp = my_string.charAt(i) + temp;
-            ret.add(temp);
+        for(int i = 0; i < psum.length; i++){
+            StringBuilder builder = new StringBuilder();
+            if(i == 0) psum[i] = temp + spells[i];
+            else psum[i] = spells[i] + psum[i - 1];
         }
-        Collections.sort(ret);
-        return ret;
+        Arrays.sort(psum);
+        return psum;
     }
 }
