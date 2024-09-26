aadc750b0e0d9930e0120914bb070771483d68dd
noah <104671487+gratisreise@users.noreply.github.com>
2024-09-26 11:41:43 +0900

    [level 0] Title: 공배수, Time: 0.06 ms, Memory: 66.6 MB -BaekjoonHub

--- 프로그래머스/0/181936. 공배수/공배수.java
+++ 프로그래머스/0/181936. 공배수/공배수.java
@@ -1,8 +1,5 @@
 class Solution {
     public int solution(int number, int n, int m) {
-        int ret = 0;
-        if(number % n == 0 && number % m == 0) ret = 1;
-        
-        return ret;
+        return (number % n == 0) && (number % m == 0) ? 1 : 0;
     }
 }
