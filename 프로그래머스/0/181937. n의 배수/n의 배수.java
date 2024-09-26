95ee5e406ddd24d14511a51ed47c7d214529d7ab
noah <104671487+gratisreise@users.noreply.github.com>
2024-09-26 13:45:18 +0900

    [level 0] Title: n의 배수, Time: 0.04 ms, Memory: 77.8 MB -BaekjoonHub

--- 프로그래머스/0/181937. n의 배수/n의 배수.java
+++ 프로그래머스/0/181937. n의 배수/n의 배수.java
@@ -1,8 +1,5 @@
 class Solution {
     public int solution(int num, int n) {
-        int ret = 0;
-        if(num % n == 0) ret = 1;
-        
-        return ret;
+        return num % n == 0 ? 1 : 0;
     }
 }
