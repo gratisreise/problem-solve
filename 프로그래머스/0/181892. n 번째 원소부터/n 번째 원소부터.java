447b195beb31fb93e687d3d073387b195d23767f
noah <104671487+gratisreise@users.noreply.github.com>
2024-09-26 13:54:10 +0900

    [level 0] Title: n 번째 원소부터, Time: 7.22 ms, Memory: 79.9 MB -BaekjoonHub

--- 프로그래머스/0/181892. n 번째 원소부터/n 번째 원소부터.java
+++ 프로그래머스/0/181892. n 번째 원소부터/n 번째 원소부터.java
@@ -1,12 +1,9 @@
 import java.util.*;
+import java.util.stream.*;
 class Solution {
-    public List<Integer> solution(int[] num_list, int n) {
-        List<Integer> ret = new ArrayList<>();
-        for(int i = n - 1; i < num_list.length; i++){
-            ret.add(num_list[i]);
-        }
-        
-        
-        return ret;
+    public int[] solution(int[] num_list, int n) {
+        List<Integer> l = new ArrayList<>();
+        for(int i = n-1; i < num_list.length; i++) l.add(num_list[i]);
+        return l.stream().mapToInt(Integer::intValue).toArray();
     }
 }
