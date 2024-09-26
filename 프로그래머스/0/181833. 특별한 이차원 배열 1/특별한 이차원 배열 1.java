eeddbcd26ca48c35988dc923ff70459d99b11adf
noah <104671487+gratisreise@users.noreply.github.com>
2024-09-26 10:44:05 +0900

    [level 0] Title: 특별한 이차원 배열 1, Time: 0.27 ms, Memory: 85.4 MB -BaekjoonHub

--- 프로그래머스/0/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.java
+++ 프로그래머스/0/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.java
@@ -1,12 +1,12 @@
 class Solution {
     public int[][] solution(int n) {
-        int[][] ret = new int[n][n];
+        int[][] answer = new int[n][n];
         for(int i = 0; i < n; i++){
             for(int j = 0; j < n; j++){
-                if(i == j) ret[i][j] = 1;
-                else ret[i][j] = 0;
+                if(i == j) answer[i][j] = 1;
+                else answer[i][j] = 0;
             }
         }
-        return ret;
+        return answer;
     }
 }
