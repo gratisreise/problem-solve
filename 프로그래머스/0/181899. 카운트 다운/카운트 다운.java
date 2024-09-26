1b77560e0924e580e9780d6e242adda3cb60dcb9
noah <104671487+gratisreise@users.noreply.github.com>
2024-09-26 14:01:57 +0900

    [level 0] Title: 카운트 다운, Time: 0.04 ms, Memory: 76.7 MB -BaekjoonHub

--- 프로그래머스/0/181899. 카운트 다운/카운트 다운.java
+++ 프로그래머스/0/181899. 카운트 다운/카운트 다운.java
@@ -1,10 +1,10 @@
 class Solution {
     public int[] solution(int start_num, int end_num) {
-        int[] answer = new int[start_num - end_num + 1];
+        int[] ret = new int[start_num - end_num + 1];
         int temp = start_num;
         for(int i = 0; i < start_num - end_num + 1; i++){
-            answer[i] = temp--;
+            ret[i] = temp--;
         }
-        return answer;
+        return ret;
     }
 }
