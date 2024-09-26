775a68b1bc833b8c7e3925f0f68827cabf3600cd
noah <104671487+gratisreise@users.noreply.github.com>
2024-09-26 18:01:48 +0900

    [level 0] Title: 날짜 비교하기, Time: 0.05 ms, Memory: 74.8 MB -BaekjoonHub

--- 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.java
+++ 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.java
@@ -1,13 +1,9 @@
 class Solution {
     public int solution(int[] date1, int[] date2) {
-        int ret = 0;
-        if(date1[0] < date2[0]) ret = 1;
-        else if(date1[0] == date2[0]){
-            if(date1[1] < date2[1]) ret = 1;
-            else if(date1[1] == date2[1]){
-                if(date1[2] < date2[2]) ret = 1;
-            }
-        }
-        return ret;
+        int answer = 0;
+        if(date1[0] < date2[0]) return 1;
+        else if(date1[0] == date2[0] && date1[1] < date2[1]) return 1;
+        else if(date1[0] == date2[0] && date1[1] == date2[1] && date1[2] < date2[2]) return 1;
+        return answer;
     }
 }
