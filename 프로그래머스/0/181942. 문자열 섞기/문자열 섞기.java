c4e48a63e20cbef10b991bc904241ec955444f55
noah <104671487+gratisreise@users.noreply.github.com>
2024-09-26 17:07:59 +0900

    [level 0] Title: 문자열 섞기, Time: 0.07 ms, Memory: 77.2 MB -BaekjoonHub

--- 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.java
+++ 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.java
@@ -1,10 +1,10 @@
 class Solution {
     public String solution(String str1, String str2) {
-        String ret = "";
+        StringBuilder ret = new StringBuilder();
         for(int i = 0; i < str1.length(); i++){
-            ret += ("" + str1.charAt(i) + str2.charAt(i));
+            ret.append(str1.charAt(i));
+            ret.append(str2.charAt(i));
         }
-        
-        return ret;
+        return ret.toString();
     }
 }
