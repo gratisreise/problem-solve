ca3630382345893a2273deb624cbb29e1810252d
noah <104671487+gratisreise@users.noreply.github.com>
2024-09-27 13:52:46 +0900

    [level 0] Title: 가까운 수, Time: 6.55 ms, Memory: 76.5 MB -BaekjoonHub

--- 프로그래머스/0/120890. 가까운 수/가까운 수.java
+++ 프로그래머스/0/120890. 가까운 수/가까운 수.java
@@ -1,17 +1,23 @@
 import java.util.*;
 class Solution {
     public int solution(int[] array, int n) {
-        int ret = 0;
-        int min = (int)1e9;
-        Arrays.sort(array);
-        for(int i : array){
-            int cha = Math.abs(n - i);
-            min = Math.min(cha, min);
+        Map<Integer, Integer> mp = new HashMap<>();
+        List<Integer> l = new ArrayList<>();
+        for(int i = 0; i < array.length; i++){
+            int cha = Math.abs(array[i] - n);
+            l.add(i);
+            mp.put(i, cha);
         }
-        for(int i : array){
-            int cha = Math.abs(n - i);
-            if(cha == min) return i;
-        }
-        return ret;
+        Collections.sort(l, (a, b)->{
+            if(mp.get(a) == mp.get(b)){
+                return Integer.compare(array[a], array[b]);
+            } return Integer.compare(mp.get(a), mp.get(b));
+        });
+        return array[l.get(0)];
     }
 }
+/*
+mp에 인덱스별 차이값저장
+mp에 저장된 차이값이 작은 순으로 저장
+만약 차이값이 같으면 인덱스가 작은 값?
+*/
