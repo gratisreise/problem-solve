4be2c3eca73509dbf15581ce98f61e576c953103
noah <104671487+gratisreise@users.noreply.github.com>
2024-09-27 13:37:39 +0900

    [level 0] Title: 한 번만 등장한 문자, Time: 0.05 ms, Memory: 78.3 MB -BaekjoonHub

--- 프로그래머스/0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.java
+++ 프로그래머스/0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.java
@@ -1,13 +1,11 @@
 class Solution {
+    private int[] cnt = new int[26];
     public String solution(String s) {
-        String ret = "";
-        int[] cnt = new int[26];
-        for(char c : s.toCharArray()){
-            cnt[c - 'a']++;
+        StringBuilder ret = new StringBuilder();
+        for(char c : s.toCharArray()) cnt[c - 'a']++;
+        for(int i = 0; i < cnt.length; i++){
+            if(cnt[i] == 1) ret.append((char)(i + 'a'));
         }
-        for(int i = 0; i < 26; i++){
-            if(cnt[i] == 1) ret += (char)(i + 'a');
-        }
-        return ret;
+        return ret.toString();
     }
 }
