ed9aa7039c2c03b73e7e41e051811148ccc3dcbf
noah <104671487+gratisreise@users.noreply.github.com>
2024-09-27 12:20:28 +0900

    [level 0] Title: 문자열 뒤집기, Time: 24.42 ms, Memory: 76.2 MB -BaekjoonHub

--- 프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.java
+++ 프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.java
@@ -1,18 +1,8 @@
-import java.util.*;
 class Solution {
     public String solution(String my_string, int s, int e) {
-        String ret = "";
-        String temp = "";
-        for(int i = e; i >= s; i--){
-            temp += my_string.charAt(i);
-        }
-        for(int i = 0; i < s; i++){
-            ret += my_string.charAt(i);
-        }
-        ret += temp;
-        for(int i = e + 1; i < my_string.length(); i++){
-            ret += my_string.charAt(i);
-        }
-        return ret;
+        String s1 = my_string.substring(0, s);
+        String s2 = new StringBuilder(my_string.substring(s, e + 1)).reverse().toString();
+        String s3 = my_string.substring(e + 1);
+        return s1 + s2 + s3;
     }
 }
