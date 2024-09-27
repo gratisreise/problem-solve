85ab8557ef48703420dff4d88e4ef6ca7a72dbb5
noah <104671487+gratisreise@users.noreply.github.com>
2024-09-27 13:14:10 +0900

    [level 0] Title: 진료 순서 정하기, Time: 5.54 ms, Memory: 71.7 MB -BaekjoonHub

--- 프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.java
+++ 프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.java
@@ -1,11 +1,21 @@
 import java.util.*;
+import java.util.stream.*;
+
 class Solution {
-    public List<Integer> solution(int[] emergency) {
+    public int[] solution(int[] emergency) {
+        Map<Integer, Integer> mp = new HashMap<>();
         List<Integer> l = new ArrayList<>();
-        List<Integer> ret = new ArrayList<>();
-        for(int i : emergency) l.add(i);
-        Collections.sort(l, (a, b) -> b - a);
-        for(int i  : emergency) ret.add(l.indexOf(i) + 1);
+        for(int i = 0; i < emergency.length; i++){
+            l.add(i+1);
+            mp.put(i+1, emergency[i]);
+        }
+        Collections.sort(l, (a, b) -> Integer.compare(mp.get(b), mp.get(a)));
+        int[] ret = new int[l.size()];
+        for(int i = 0; i < ret.length; i++){
+            for(int j = 0; j < l.size(); j++){
+                if(i == l.get(j)-1) ret[i] = j+1;
+            }
+        }
         return ret;
     }
 }
