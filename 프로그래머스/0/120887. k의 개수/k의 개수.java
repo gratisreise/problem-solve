f4d5d56953296f6709ca2bbf17b151e0e67b91d1
noah <104671487+gratisreise@users.noreply.github.com>
2024-09-27 12:52:53 +0900

    [level 0] Title: k의 개수, Time: 15.51 ms, Memory: 80.2 MB -BaekjoonHub

--- 프로그래머스/0/120887. k의 개수/k의 개수.java
+++ 프로그래머스/0/120887. k의 개수/k의 개수.java
@@ -1,15 +1,16 @@
 class Solution {
+    private int count(int num, int k){
+        int cnt = 0;
+        for(char c : String.valueOf(num).toCharArray()){
+            if(c- '0' == k) cnt++;
+        }
+        return cnt;
+    }
     public int solution(int i, int j, int k) {
         int ret = 0;
-        
-        for(int x = i; x <= j; x++){
-            String s = "" + x;
-            for(char c : s.toCharArray()){
-                if((c - '0') == k) ret++;
-            }
+        for(int num = i; num <= j; num++){
+            ret += count(num, k);
         }
-        
-        
         return ret;
     }
 }
