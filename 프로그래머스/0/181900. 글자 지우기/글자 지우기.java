86ebec66264b82b5d369cffc725c3cc905dc855c
noah <104671487+gratisreise@users.noreply.github.com>
2024-09-27 12:17:43 +0900

    [level 0] Title: 글자 지우기, Time: 0.20 ms, Memory: 75.6 MB -BaekjoonHub

--- 프로그래머스/0/181900. 글자 지우기/글자 지우기.java
+++ 프로그래머스/0/181900. 글자 지우기/글자 지우기.java
@@ -1,14 +1,12 @@
 class Solution {
     public String solution(String my_string, int[] indices) {
-        String ret = "";
-        for(int i = 0; i < my_string.length(); i++){
-            boolean flag = false;
-            for(int j : indices){
-                if(i == j) flag = true;
-            }
-            if(!flag) ret += my_string.charAt(i);
+        StringBuilder ret = new StringBuilder();
+        char[] arr = my_string.toCharArray();
+        for(int i = 0; i < arr.length; i++){
+            boolean flag = true;
+            for(int j : indices) if(i == j) flag = false;
+            if(flag) ret.append(arr[i]);
         }
-        
-        return ret;
+        return ret.toString();
     }
 }
