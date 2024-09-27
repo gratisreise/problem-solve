dc48f3d803197810df9a79bfa125e0f037124ae3
noah <104671487+gratisreise@users.noreply.github.com>
2024-09-27 18:15:10 +0900

    [level 0] Title: 2의 영역, Time: 3.60 ms, Memory: 98.8 MB -BaekjoonHub

--- 프로그래머스/0/181894. 2의 영역/2의 영역.java
+++ 프로그래머스/0/181894. 2의 영역/2의 영역.java
@@ -1,25 +1,16 @@
-import java.util.*;
 class Solution {
-    public List<Integer> solution(int[] arr) {
-        List<Integer> ret = new ArrayList<>();
-        boolean flag =  false;
-        int start = 0;
-        int end = 0;
+    public int[] solution(int[] arr) {
+        int start = -1;
+        int end = -1;
         for(int i = 0; i < arr.length; i++){
-            if(arr[i] == 2) {start = i; break;}
+            if(start == -1 && arr[i] == 2) {start = i; end = i;}
+            if(start != -1 && arr[i] == 2) end = i;
         }
-        for(int i = arr.length - 1; i >= 0; i--){
-            if(arr[i] == 2) {end = i; break;}
+        int[] ret = new int[end - start + 1];
+        if(start == -1) return new int[]{-1};
+        for(int i = start; i <= end; i++){
+            ret[i-start] = arr[i];
         }
-        if(start == 0 && end == 0){
-            ret.add(-1);
-        } else {
-            for(int i = start; i <= end; i++){
-                ret.add(arr[i]);
-            }
-        }
-        
-        
         return ret;
     }
 }
