c61d6761bf3e133fa43c286cd2a2077e57857b37
noah <104671487+gratisreise@users.noreply.github.com>
2024-09-28 13:01:22 +0900

    [level 0] Title: 소인수분해, Time: 3.83 ms, Memory: 75.3 MB -BaekjoonHub

--- 프로그래머스/0/120852. 소인수분해/소인수분해.java
+++ 프로그래머스/0/120852. 소인수분해/소인수분해.java
@@ -1,19 +1,19 @@
 import java.util.*;
+import java.util.stream.*;
 class Solution {
-    static int[] np;
-    public List<Integer> solution(int n) {
-        List<Integer> ret = new ArrayList<>();
-        np = new int[n + 1];
+    public int[] solution(int n) {
+        int[] prime = new int[n + 1];
         for(int i = 2; i <= n; i++){
-            if(np[i] == 1) continue;
-            for(int j = i * 2; j <= n; j+= i){
-                np[j] = 1;
+            if(prime[i] == 1) continue;
+            if(i != 2 && i % 2 == 0) prime[i] = 1;
+            for(int j = 2*i; j <= n; j += i){
+                prime[j] = 1;
             }
         }
+        List<Integer> ret = new ArrayList<>();
         for(int i = 2; i <= n; i++){
-            if(np[i] == 1) continue;
-            if(n % i == 0) ret.add(i);
+            if(prime[i] == 0 && n % i == 0) ret.add(i);
         }
-        return ret;
+        return ret.stream().mapToInt(Integer::intValue).toArray();
     }
 }
