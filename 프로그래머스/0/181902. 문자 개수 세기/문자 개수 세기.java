329c4f02ae7022b156b47efa9ca76c5dc243623f
noah <104671487+gratisreise@users.noreply.github.com>
2024-09-28 13:17:54 +0900

    [level 0] Title: 문자 개수 세기, Time: 0.47 ms, Memory: 72.1 MB -BaekjoonHub

--- 프로그래머스/0/181902. 문자 개수 세기/문자 개수 세기.java
+++ 프로그래머스/0/181902. 문자 개수 세기/문자 개수 세기.java
@@ -2,11 +2,8 @@ class Solution {
     public int[] solution(String my_string) {
         int[] ret = new int[52];
         for(char c : my_string.toCharArray()){
-            if('A' <= c && c <= 'Z'){
-                ret[c - 'A']++;
-            } else if('a' <= c && c <= 'z'){
-                ret[c - 'a' + 26]++;
-            }
+            if(Character.isLowerCase(c)) ret[c - 'a' + 26] += 1;
+            else ret[c - 'A'] += 1;
         }
         return ret;
     }
