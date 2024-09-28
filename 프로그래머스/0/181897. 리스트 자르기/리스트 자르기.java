13d1c811ea18aa43aab4eff2c7499f1527fc2aa7
noah <104671487+gratisreise@users.noreply.github.com>
2024-09-28 12:30:56 +0900

    [level 0] Title: 리스트 자르기, Time: 13.76 ms, Memory: 77.3 MB -BaekjoonHub

--- 프로그래머스/0/181897. 리스트 자르기/리스트 자르기.java
+++ 프로그래머스/0/181897. 리스트 자르기/리스트 자르기.java
@@ -1,28 +1,25 @@
 import java.util.*;
+import java.util.stream.*;
 class Solution {
-    public List<Integer> solution(int n, int[] slicer, int[] num_list) {
-        List<Integer> ret = new ArrayList<>();
-        int a = slicer[0];
-        int b = slicer[1];
-        int c = slicer[2];
+    public int[] solution(int n, int[] slicer, int[] num_list) {
+        int s = 0;
+        int e = 0;
+        int len = 1;
         if(n == 1){
-            for(int i = 0; i <= b; i++){
-                ret.add(num_list[i]);
-            }
-            return ret;
+            s = 0; e = slicer[1];
         } else if(n == 2){
-            for(int i = a; i < num_list.length; i++){
-                ret.add(num_list[i]);
-            }
+            s = slicer[0]; e = num_list.length - 1;
         } else if(n == 3){
-            for(int i = a; i <= b; i++){
-                ret.add(num_list[i]);
-            }
+            s = slicer[0]; e = slicer[1];
         } else if(n == 4){
-            for(int i = a; i <= b; i += c){
-                ret.add(num_list[i]);
-            }
+            s = slicer[0]; e = slicer[1]; len = slicer[2];
         }
-        return ret;
+        List<Integer> ret = new ArrayList<>();
+        for(int i = s; i <= e; i += len) ret.add(num_list[i]);
+        return ret.stream().mapToInt(Integer::intValue).toArray();
     }
 }
+/*
+시작, 끝, 간격
+s, e, len
+*/
