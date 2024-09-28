08b9ac9d7abe8ceadfc5e7358b3222d660397dcd
noah <104671487+gratisreise@users.noreply.github.com>
2024-09-28 23:10:21 +0900

    [level 0] Title: 왼쪽 오른쪽, Time: 11.71 ms, Memory: 79.8 MB -BaekjoonHub

--- 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.java
+++ 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.java
@@ -1,21 +1,29 @@
 import java.util.*;
+import java.util.stream.*;
 class Solution {
-    public List<String> solution(String[] str_list) {
-        List<String> sl = new ArrayList<>();
-        boolean flag = false;
-        for(String s : str_list){
-            if(!flag && s.equals("l")){
-                flag = true;
+    public String[] solution(String[] str_list) {
+        List<String> ret = new ArrayList<>();
+        for(int i = 0; i < str_list.length; i++){
+            if(str_list[i].equals("l")) break;
+            ret.add(str_list[i]);
+            if(str_list[i].equals("r")) {
+                ret.clear();
+                for(int j = i + 1; j < str_list.length; j++){
+                    ret.add(str_list[j]);
+                }
                 break;
             }
-            else if(!flag && s.equals("r")) {
-                flag = true;
-                sl.clear();
-                continue;
-            }
-            sl.add(s);
         }
-        if(!flag) sl.clear();
-        return sl;
+        if(ret.size() == str_list.length) return new String[]{};
+        return ret.stream().toArray(String[]::new);
     }
 }
+
+
+/*
+
+
+자꾸 틀리네 다시 생각해보자!!
+
+
+*/
