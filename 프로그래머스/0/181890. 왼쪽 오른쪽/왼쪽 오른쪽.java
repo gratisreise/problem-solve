decd2d3bd68cc93ebccaf91a0a649609e8c242cb
noah <104671487+gratisreise@users.noreply.github.com>
2024-09-28 23:24:23 +0900

    [level 0] Title: 왼쪽 오른쪽, Time: 0.31 ms, Memory: 80.4 MB -BaekjoonHub

--- 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.java
+++ 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.java
@@ -1,21 +1,28 @@
 import java.util.*;
 import java.util.stream.*;
 class Solution {
+    private String[] makeL(int idx, String[] str_list){
+        String[] ans = new String[idx];
+        for(int i = 0; i < idx; i++) ans[i] = str_list[i];
+        return ans == null ? new String[]{} : ans;
+    }
+    private String[] makeR(int idx, String[] str_list){
+        String[] ans = new String[str_list.length-idx-1];
+        System.out.println(ans.length);
+        for(int i = idx + 1; i < str_list.length; i++){
+            ans[i - idx - 1] = str_list[i];
+        }
+        System.out.println(Arrays.toString(ans));
+        System.out.println(ans.length);
+        return ans == null ? new String[]{} : ans;
+    }
     public String[] solution(String[] str_list) {
         List<String> ret = new ArrayList<>();
         for(int i = 0; i < str_list.length; i++){
-            if(str_list[i].equals("l")) break;
-            ret.add(str_list[i]);
-            if(str_list[i].equals("r")) {
-                ret.clear();
-                for(int j = i + 1; j < str_list.length; j++){
-                    ret.add(str_list[j]);
-                }
-                break;
-            }
+            if(str_list[i].equals("l")) return makeL(i, str_list);
+            if(str_list[i].equals("r")) return makeR(i, str_list);
         }
-        if(ret.size() == str_list.length) return new String[]{};
-        return ret.stream().toArray(String[]::new);
+        return new String[]{};
     }
 }
 
