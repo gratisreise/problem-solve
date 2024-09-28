05cbe09da861366685592007eca86bace7a661ec
noah <104671487+gratisreise@users.noreply.github.com>
2024-09-28 13:36:52 +0900

    [level 0] Title: 공 던지기, Time: 0.76 ms, Memory: 72.1 MB -BaekjoonHub

--- 프로그래머스/0/120843. 공 던지기/공 던지기.java
+++ 프로그래머스/0/120843. 공 던지기/공 던지기.java
@@ -1,14 +1,12 @@
 import java.util.*;
 class Solution {
     public int solution(int[] numbers, int k) {
-        int ret = 0;
         Queue<Integer> q = new LinkedList<>();
         for(int i : numbers) q.add(i);
-        for(int i = 0; i < k; i++){
-            ret = q.peek();
+        while(--k > 0){
             q.add(q.poll());
             q.add(q.poll());
         }
-        return ret;
+        return q.peek();
     }
 }
