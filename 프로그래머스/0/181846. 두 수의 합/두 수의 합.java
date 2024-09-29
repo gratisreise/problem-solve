45259646813888fc4e9a761a859f8ac5bb9d10d0
noah <104671487+gratisreise@users.noreply.github.com>
2024-09-30 08:59:08 +0900

    [level 0] Title: 두 수의 합, Time: 18.59 ms, Memory: 86.1 MB -BaekjoonHub

--- 프로그래머스/0/181846. 두 수의 합/두 수의 합.java
+++ 프로그래머스/0/181846. 두 수의 합/두 수의 합.java
@@ -1,17 +1,16 @@
 class Solution {
     public String solution(String a, String b) {
-        String answer = "";
-        StringBuilder ret = new StringBuilder();
         int n = Math.max(a.length(), b.length());
-        a = new StringBuilder(a).reverse().toString();
-        b = new StringBuilder(b).reverse().toString();
+        char[] arr = new StringBuilder(a).reverse().toString().toCharArray();
+        char[] brr = new StringBuilder(b).reverse().toString().toCharArray();
         int prev = 0;
+        StringBuilder ret = new StringBuilder();
         for(int i = 0; i < n; i++){
-            int aNum = a.length() >= i + 1 ? a.charAt(i) - '0' : 0;
-            int bNum = b.length() >= i + 1 ? b.charAt(i) - '0' : 0;
+            int aNum = i + 1 > arr.length ? 0 : arr[i] - '0';
+            int bNum = i + 1 > brr.length ? 0 : brr[i] - '0';
             int sum = aNum + bNum + prev;
-            if(sum >= 10) {prev = 1; sum -= 10;}
-            else prev = 0;
+            prev = 0;
+            if(sum >= 10){prev = 1; sum -= 10;}
             ret.append(sum);
         }
         if(prev == 1) ret.append(1);
