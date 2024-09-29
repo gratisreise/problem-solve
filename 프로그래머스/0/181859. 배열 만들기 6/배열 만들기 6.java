b6fa5e271208e7e4f890f2e87ea310dedfa0017a
noah <104671487+gratisreise@users.noreply.github.com>
2024-09-30 08:31:15 +0900

    [level 0] Title: 배열 만들기 6, Time: 51.45 ms, Memory: 150 MB -BaekjoonHub

--- 프로그래머스/0/181859. 배열 만들기 6/배열 만들기 6.java
+++ 프로그래머스/0/181859. 배열 만들기 6/배열 만들기 6.java
@@ -1,24 +1,18 @@
 import java.util.*;
+import java.util.stream.*;
 class Solution {
-    public Stack<Integer> solution(int[] arr) {
+    public int[] solution(int[] arr) {
         Stack<Integer> stk = new Stack<>();
-        
-        int i = 0;
-        while(i < arr.length){
+        for(int i = 0; i < arr.length;){
             if(stk.isEmpty()){
-                stk.push(arr[i]);
-                i++;
-            } else if(!stk.isEmpty() && stk.peek() == arr[i]){
-                stk.pop();
-                i++;
-            } else if(!stk.isEmpty() && stk.peek() != arr[i]){
-                stk.push(arr[i]);
-                i++;
+                stk.push(arr[i]); i++;
+            } else if(stk.peek() == arr[i]){
+                stk.pop(); i++;
+            } else if(stk.peek() != arr[i]){
+                stk.push(arr[i]); i++;
             }
         }
-        
-        if(stk.isEmpty()) stk.push(-1);
-        
-        return stk;
+        if(stk.isEmpty()) return new int[]{-1};
+        return stk.stream().mapToInt(Integer::intValue).toArray();
     }
 }
