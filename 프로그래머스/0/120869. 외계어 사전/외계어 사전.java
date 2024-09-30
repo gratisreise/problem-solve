abec1af91b2138fbc963c8f8cc23cdd10520564a
noah <104671487+gratisreise@users.noreply.github.com>
2024-09-30 14:51:38 +0900

    [level 0] Title: 외계어 사전, Time: 0.06 ms, Memory: 82.8 MB -BaekjoonHub

--- 프로그래머스/0/120869. 외계어 사전/외계어 사전.java
+++ 프로그래머스/0/120869. 외계어 사전/외계어 사전.java
@@ -1,14 +1,9 @@
 class Solution {
-    private int[] cnt = new int[26];
     public int solution(String[] spell, String[] dic) {
-        int ret = 0;
-        for(String s : spell) cnt[s.charAt(0) - 'a']++;
         for(String s : dic){
-            int[] temp = new int[26];
             boolean flag = true;
-            for(char c : s.toCharArray()) temp[c- 'a']++;
-            for(int i = 0; i < 26; i++){
-                if(cnt[i] != temp[i]) flag = false;
+            for(String ss : spell){
+                if(!s.contains(ss)) flag = false;
             }
             if(flag) return 1;
         }
