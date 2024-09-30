d0d6b4f304ddf7b0365bb87994e47412ad05f38c
noah <104671487+gratisreise@users.noreply.github.com>
2024-09-30 09:08:31 +0900

    [level 0] Title: 문자열 여러 번 뒤집기, Time: 1.29 ms, Memory: 81.9 MB -BaekjoonHub

--- 프로그래머스/0/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.java
+++ 프로그래머스/0/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.java
@@ -1,19 +1,13 @@
 class Solution {
     public String solution(String my_string, int[][] queries) {
-        String ret = "";
-        char[] cArr = my_string.toCharArray();
+        String answer = "";
         for(int i = 0; i < queries.length; i++){
-            int j = queries[i][0];
-            int k = queries[i][1];
-            if(j == k) continue;
-            while(j <= k){
-                char temp = cArr[j];
-                cArr[j] = cArr[k];
-                cArr[k] = temp;
-                j++; k--;
-            }
+            String pre = my_string.substring(0, queries[i][0]);
+            String suf = my_string.substring(queries[i][1]+1);
+            StringBuilder temp = new StringBuilder(my_string.substring(queries[i][0], queries[i][1] + 1));
+            temp.reverse();
+            my_string = pre + temp.toString() + suf;
         }
-        for(char c : cArr) ret += c;
-        return ret;
+        return my_string;
     }
 }
