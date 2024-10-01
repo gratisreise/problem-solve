5cf15f0e2bf61879760773cd5beb1ccb38b5dd94
noah <104671487+gratisreise@users.noreply.github.com>
2024-10-01 11:15:39 +0900

    [level 0] Title: 그림 확대, Time: 0.48 ms, Memory: 82.4 MB -BaekjoonHub

--- 프로그래머스/0/181836. 그림 확대/그림 확대.java
+++ 프로그래머스/0/181836. 그림 확대/그림 확대.java
@@ -1,14 +1,15 @@
 import java.util.*;
 class Solution {
-    public List<String> solution(String[] picture, int k) {
-        List<String> ret = new ArrayList<>();
-        for(int i = 0; i < picture.length; i++){
-            String s = "";
-            int cnt = k;
-            for(char c : picture[i].toCharArray()){
-                for(int j = 0; j < k; j++) s += c;
+    public String[] solution(String[] picture, int k) {
+        String[] ret = new String[picture.length * k];
+        for(int i = 0; i < ret.length;){
+            StringBuilder sb = new StringBuilder();
+            for(char c : picture[i / k].toCharArray()){
+                for(int j = 0; j < k; j++) sb.append(c);
             }
-            while(cnt-- > 0) ret.add(s);
+            for(int j = 0; j < k; j++){
+                ret[i++] = sb.toString();
+            }
         }
         return ret;
     }
