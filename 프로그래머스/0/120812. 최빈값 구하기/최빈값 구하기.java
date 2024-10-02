05f63f4ef98f0011da04158dad70ca3f1916d18f
noah <104671487+gratisreise@users.noreply.github.com>
2024-10-02 19:19:40 +0900

    [level 0] Title: 최빈값 구하기, Time: 1.25 ms, Memory: 78.7 MB -BaekjoonHub

--- 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.java
+++ 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.java
@@ -1,18 +1,19 @@
+import java.util.*;
 class Solution {
+    class Pair{
+        int a, b;
+        Pair(int a, int b){
+            this.a = a;
+            this.b = b;
+        }
+    }
     public int solution(int[] array) {
-        int ret = 0;
-        int mx = -1;
-        int cnt = 0;
-        int[] arr = new int[1004]; // 빈도수
-        for(int i : array) arr[i]++;
-        for(int i : arr) mx = (i > mx) ? i : mx;
-        for(int i : arr) if(i == mx) cnt++;
-        if(cnt > 1) ret = -1;
-        else {
-            for(int i = 1; i <= 1000; i++)
-                if(arr[i] == mx) ret = i;
-            }
-        
-        return ret;
+        Map<Integer, Integer> mp = new HashMap<>();
+        List<Pair> l = new ArrayList<>();
+        for(int i : array) mp.put(i, mp.getOrDefault(i, 0) + 1);
+        for(int i : mp.keySet()) l.add(new Pair(i, mp.get(i)));
+        Collections.sort(l, (a, b) -> Integer.compare(b.b, a.b));
+        if(l.size() >= 2 && l.get(0).b == l.get(1).b) return -1;
+        return l.get(0).a;
     }
 }
