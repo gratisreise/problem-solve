27722a4f3ac9cbeb7ae306bd87475dcacf5d0f43
noah <104671487+gratisreise@users.noreply.github.com>
2024-10-03 18:00:39 +0900

    [level 0] Title: 겹치는 선분의 길이, Time: 0.51 ms, Memory: 72.8 MB -BaekjoonHub

--- 프로그래머스/0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.java
+++ 프로그래머스/0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.java
@@ -1,13 +1,14 @@
+import java.util.*;
 class Solution {
     public int solution(int[][] lines) {
-        int ret = 0;
-        int[] cnt = new int[210];
+        int answer = 0;
+        Map<Integer, Integer> cnt = new HashMap<>();
         for(int i = 0; i < lines.length; i++){
-            int a = lines[i][0] + 100;
-            int b = lines[i][1] + 100;
-            for(int j = a; j < b; j++) cnt[j]++;
+            for(int j = lines[i][0]; j < lines[i][1]; j++){
+                cnt.put(j, cnt.getOrDefault(j, 0) + 1);
+            }
         }
-        for(int i : cnt) if(i >= 2) ret++;
-        return ret;
+        for(int i : cnt.keySet()) if(cnt.get(i) >= 2) answer++;
+        return answer;
     }
 }
