49b28bf9ce4348087f7a155e89506a7988840aa4
noah <104671487+gratisreise@users.noreply.github.com>
2024-10-03 13:14:21 +0900

    [level 0] Title: 안전지대, Time: 0.04 ms, Memory: 64.6 MB -BaekjoonHub

--- 프로그래머스/0/120866. 안전지대/안전지대.java
+++ 프로그래머스/0/120866. 안전지대/안전지대.java
@@ -1,29 +1,24 @@
 class Solution {
-    static int n;
-    static int[][] vis;
-    static void filled(int y, int x){
-        for(int i = y - 1; i <= y + 1; i++){
-            for(int j = x - 1; j <= x + 1; j++){
-                if(i < 0 || j < 0 || i >= n || j >= n) continue;
-                
-                vis[i][j] = 1;
-            }
-        }
-    }
-    
-    public int solution(int[][] a) {
+    private int[] dy = {-1, -1, -1, 0, 1, 1, 1, 0};
+    private int[] dx = {-1, 0, 1, 1, 1, 0, -1, -1};
+    public int solution(int[][] board) {
         int ret = 0;
-        n = a.length;
-        vis = new int[n][n];
-        System.out.println(n);
+        int n = board.length;
+        int m = board[0].length;
         for(int i = 0; i < n; i++){
-            for(int j = 0; j < n; j++) {
-                if(a[i][j] == 1) filled(i, j);
+            for(int j = 0; j < m; j++){
+                if(board[i][j] == 0 || board[i][j] == -1) continue;
+                for(int d = 0; d < 8; d++){
+                    int ny = i + dy[d];
+                    int nx = j + dx[d];
+                    if(ny < 0 || nx < 0 || ny >= n || nx >= m || board[ny][nx] == 1) continue;
+                    board[ny][nx] = -1;
+                }
             }
         }
         for(int i = 0; i < n; i++){
-            for(int j = 0; j < n; j++) {
-                if(vis[i][j] == 0) ret++;
+            for(int j = 0; j < m; j++){
+                if(board[i][j] == 0) ret++;
             }
         }
         return ret;
