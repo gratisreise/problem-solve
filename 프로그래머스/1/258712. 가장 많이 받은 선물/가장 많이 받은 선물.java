af7bcac26bd2c0c28dcac84bf5f4676b33866cdf
noah <104671487+gratisreise@users.noreply.github.com>
2024-10-05 11:21:56 +0900

    [level 1] Title: 가장 많이 받은 선물, Time: 60.38 ms, Memory: 103 MB -BaekjoonHub

--- 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.java
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.java
@@ -1,48 +1,52 @@
 import java.util.*;
+
 class Solution {
+    static Map<String, List<String>> mp1 = new HashMap<>(); //준사라:받은사람 이름 저장
+    static Map<String, List<String>> mp2 = new HashMap<>(); //받은사람: 준사람 사람 이름 저장
+    static Map<String, Integer> mp3 = new HashMap<>(); //받을 선물 개수
+    static PriorityQueue<Integer> pq = new PriorityQueue<>((a,b) -> Integer.compare(b, a));
     public int solution(String[] friends, String[] gifts) {
-        List<String> frs = Arrays.asList(friends);
-        int ret = 0;
-        int[][] arr = new int[frs.size()][frs.size()];
-        for(String s : gifts){
-            String[] sArr = s.split(" ");
-            int y = frs.indexOf(sArr[0]);
-            int x = frs.indexOf(sArr[1]);
-            arr[y][x] += 1;
+        int ret = -10;
+        for (String s : friends) {
+            mp1.put(s, new ArrayList<>());
+            mp2.put(s, new ArrayList<>());
+            mp3.put(s, 0);
         }
-        
-        Map<String, Integer> gisu = new HashMap<>();
-        Map<String, Integer> give = new HashMap<>();
-        
-        //선물지수
-        for(int i = 0; i < frs.size(); i++){
-            int sum = 0;
-            int jisu = 0;
-            for(int j = 0; j < frs.size(); j++){
-                if(i == j) continue;
-                jisu += arr[i][j] - arr[j][i];
-                sum += arr[i][j];
-            }
-            give.put(frs.get(i), sum);
-            gisu.put(frs.get(i), jisu);
+        for (String s : gifts) {
+            String[] temp = s.split(" ");
+            String s1 = temp[0];
+            String s2 = temp[1];
+            mp1.get(s1).add(s2); // 준놈 : 받은놈
+            mp2.get(s2).add(s1); // 받은놈 : 준놈
         }
-        
-        for(int i = 0; i < frs.size(); i++){
-            int temp = 0;
-            for(int j = 0; j < frs.size(); j++){
-                if(i == j) continue;
-                String y = frs.get(i);
-                String x = frs.get(j);
-                if(arr[i][j] > arr[j][i]){
-                    temp++;
-                } else if(arr[i][j] == arr[j][i]){
-                    if(gisu.get(y) > gisu.get(x)) temp++;
+        for (int i = 0; i < friends.length; i++) {
+            for (int j = 0; j < i; j++) {
+                String A = friends[i];
+                String B = friends[j];
+                int Agive = 0;
+                int Bgive = 0;
+                
+                for (String s : mp1.get(A)) if (s.equals(B)) Agive++;
+                for (String s : mp1.get(B)) if (s.equals(A)) Bgive++;
+                
+                // a가 더많이 b한테 줬으면
+                if (Agive > Bgive) mp3.put(A, mp3.get(A) + 1);
+                // b가 더많이 a한테 줬으면
+                else if (Agive < Bgive) mp3.put(B, mp3.get(B) + 1);
+                // 서로 안줬거나, 서로 같거나
+                else { 
+                    //선물지수
+                    int Agi = mp1.get(A).size() - mp2.get(A).size();
+                    int Bgi = mp1.get(B).size() - mp2.get(B).size();
+                    // 선물지수 a가 더 크네
+                    if (Agi > Bgi) mp3.put(A, mp3.get(A) + 1);
+                    // 선물지수 b가 더 크네
+                    else if (Agi < Bgi) mp3.put(B, mp3.get(B) + 1);
                 }
             }
-            
-            ret = Math.max(temp, ret);       
         }
-        return ret;  
-            
-    } 
+        for(String s : mp3.keySet()) pq.add(mp3.get(s));
+        ret = pq.peek();
+        return ret;
+    }
 }
