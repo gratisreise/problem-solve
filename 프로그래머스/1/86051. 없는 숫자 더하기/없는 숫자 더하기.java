eb06970e87d1f5db50b773547a6be8cfad3d18de
noah <104671487+gratisreise@users.noreply.github.com>
2024-10-17 11:14:14 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.07 ms, Memory: 72.4 MB -BaekjoonHub

--- 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -1,11 +1,12 @@
+import java.util.*;
 class Solution {
     public int solution(int[] numbers) {
-        int ret = 0;
-        int[] a = new int[10];
-        for(int i : numbers) a[i]++;
-        for(int i = 0; i < 10; i++) if(a[i] == 0) ret += i;
-        
-        
-        return ret;
+        Set<Integer> st = new HashSet<>();
+        for(int v : numbers) st.add(v);
+        int sum = 0;
+        for(int i = 0; i <= 9; i++){
+            if(!st.contains(i)) sum += i;
+        }
+        return sum;
     }
 }
