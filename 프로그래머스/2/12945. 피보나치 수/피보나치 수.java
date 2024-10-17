b98a70f136fa17a6680ce5d00f9e0001ade64178
noah <104671487+gratisreise@users.noreply.github.com>
2024-10-17 14:45:27 +0900

    [level 2] Title: 피보나치 수, Time: 2.64 ms, Memory: 72.5 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
@@ -1,11 +1,17 @@
 class Solution {
-    static int[] dp = new int[100004];
     public int solution(int n) {
-        dp[0] = 0;
-        dp[1] = 1;
+        int[] ret = new int[100001];
+        ret[0] = 0; ret[1] = 1;
         for(int i = 2; i <= n; i++){
-            dp[i] = (dp[i - 1] + dp[i - 2]) % 1234567;
+            ret[i] = (ret[i - 1] + ret[i - 2])% 1234567;
         }
-        return dp[n];
+        
+        return ret[n];
     }
 }
+/*
+f(n) = f(n - 1) + f(n - 2);
+
+
+
+*/
