c537f66c96fa7374857a940f52288be99e475fc6
noah <104671487+gratisreise@users.noreply.github.com>
2024-10-17 11:04:19 +0900

    [level 0] Title: A로 B 만들기, Time: 0.16 ms, Memory: 70.7 MB -BaekjoonHub

--- 프로그래머스/0/120886. A로 B 만들기/A로 B 만들기.java
+++ 프로그래머스/0/120886. A로 B 만들기/A로 B 만들기.java
@@ -1,13 +1,13 @@
 class Solution {
-    private int[] cnt1 = new int[26];
-    private int[] cnt2 = new int[26];
     public int solution(String before, String after) {
+        int[] cntA = new int[26];
+        int[] cntB = new int[26];
         for(int i = 0; i < before.length(); i++){
-            cnt1[before.charAt(i) - 'a']++;
-            cnt2[after.charAt(i) - 'a']++;
+            cntA[after.charAt(i) - 'a']++;
+            cntB[before.charAt(i) - 'a']++;
         }
-        for(int i = 0; i < cnt1.length; i++){
-            if(cnt1[i] != cnt2[i]) return 0;
+        for(int i = 0; i < 26; i++){
+            if(cntA[i] != cntB[i]) return 0;
         }
         return 1;
     }
