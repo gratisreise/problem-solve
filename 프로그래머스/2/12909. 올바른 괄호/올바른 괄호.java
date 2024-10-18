4f7f12ba30eba4fce3e7fcca4ad2c161ee3eef3b
noah <104671487+gratisreise@users.noreply.github.com>
2024-10-18 10:41:44 +0900

    [level 2] Title: 올바른 괄호, Time: 16.65 ms, Memory: 53 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,13 +1,20 @@
 import java.util.*;
 class Solution {
     boolean solution(String s) {
-        boolean ret = false;
         Stack<Character> stk = new Stack<>();
+        
         for(char c : s.toCharArray()){
-            if(!stk.isEmpty() && stk.peek() == '(' && c == ')') stk.pop();
-            else stk.push(c);
+            switch(c) {
+                    case '(' -> stk.push(c);
+                    case ')' ->{
+                        if(stk.isEmpty()) return false;
+                        stk.pop();
+                    }
+            }
         }
-        if(stk.isEmpty()) ret = true;
-        return ret;
+        return stk.isEmpty();
     }
 }
+/*
+아이디어: 바구니에 넣고 비교하면서 팝
+*/
