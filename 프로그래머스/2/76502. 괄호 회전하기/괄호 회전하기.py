5ffbbea358e5d80f40b41761096fc869385079f9
noah <104671487+gratisreise@users.noreply.github.com>
2024-10-29 14:57:20 +0900

    [level 2] Title: 괄호 회전하기, Time: 147.49 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.py
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.py
@@ -1,19 +1,27 @@
-def check(s):
-    stk = []
-    for c in s:
-        if not stk and (c == ')' or c == '}' or c == ']'):
-             return False
-        if c == '(' or c == '[' or c == '{':
-            stk.append(c)
-        elif c == ')' and stk[len(stk)-1] == '(':
-            stk.pop()
-        elif c == ']' and stk[len(stk)-1] == '[':
-            stk.pop()
-        elif c == '}' and stk[len(stk)-1] == '{':
-            stk.pop()
-    return False if stk else True
 def solution(s):
-    ret = 0
-    for i in range(len(s)):
-        if(check(s[i:]+s[:i])): ret += 1
-    return ret
+  answer = 0
+  n = len(s)
+  for i in range(n):
+    stack = [ ]
+    for j in range(n):
+      # ➊ 괄호 문자열을 회전시키면서 참조
+      c = s[(i + j) % n]
+      if c == "(" or c == "[" or c == "{":  # ➋ 열린 괄호는 푸시
+        stack.append(c)
+      else:
+        if not stack:  # ➌ 짝이 맞지 않는 경우
+          break
+      
+         # ➍ 닫힌 괄호는 스택의 top과 짝이 맞는지 비교
+        if c == ")" and stack[-1] == "(":
+           stack.pop( ) 
+        elif c == "]" and stack[-1] == "[":
+           stack.pop( ) 
+        elif c == "}" and stack[-1] == "{":
+           stack.pop( ) 
+        else:
+             break
+    else:  # ➎ for문이 break에 의해 끝나지 않고 끝까지 수행된 경우
+      if not stack:
+        answer += 1
+  return answer
