447c98708be0095483deedc8f99d3a61ca7dba44
noah <104671487+gratisreise@users.noreply.github.com>
2024-10-31 13:31:23 +0900

    [level 2] Title: 예상 대진표, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/README.md
+++ 프로그래머스/2/12985. 예상 대진표/README.md
@@ -1,10 +1,10 @@
 # [level 2] 예상 대진표 - 12985 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12985) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12985#) 
 
 ### 성능 요약
 
-메모리: 10.2 MB, 시간: 0.04 ms
+메모리: 10.3 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 31일 13:27:55
+2024년 10월 31일 13:31:13
 
 ### 문제 설명
 
--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.py
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.py
@@ -1,16 +1,7 @@
 def solution(n,a,b):
-    ret = 1
-    while max(a,b)- min(a,b) != 1 or max(a,b) % 2 != 0:
-        print(a, b)
-        if a % 2 == 0: a //= 2
-        else: a = a//2 + 1
-        if b % 2 == 0: b //= 2
-        else: b = b//2 + 1
-        ret += 1
-    return ret
-"""
-1. 1~N번 배정
-2. 게임 이후 번호 다시 배정
-3. 임의의 A와 B 번호 몇번째 라운드에서 만나지?
-
-"""
+    answer = 0
+    while a != b:
+        a = (a + 1) // 2
+        b = (b + 1) // 2
+        answer += 1
+    return answer
