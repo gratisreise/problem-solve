a38e7b12c87a5a1bf85fa4d177b64c81399dfe84
noah <104671487+gratisreise@users.noreply.github.com>
2024-10-31 10:19:13 +0900

    [level 3] Title: 베스트앨범, Time: 0.08 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
@@ -1,30 +1,33 @@
 def solution(genres, plays):
-    genre_count = {g:0 for g in set(genres)}
-    genre_num = {g:[] for g in set(genres)}
-    genre = list(set(genres))
-    #장르 내림차
+    answer = [ ]
+    genres_dict = {}
+    play_dict = {}
+    
+    #장르별 총 재생 횟수와 각 곡의 재생 횟수 저장
+    for i in range(len(genres)):
+        genre = genres[i]
+        play = plays[i]
+        if genre not in genres_dict:
+            genres_dict[genre] = []
+            play_dict[genre] = 0
+        genres_dict[genre].append((i, play))
+        play_dict[genre] += play
+    
+    # 총 재생 횟수가 많은 장르순으로 정렬
+    sorted_genres = sorted(play_dict.items(), key=lambda x: x[1], reverse=True)
+    
+    # 각 장르 내에서 노래를 재생 횟수 순으로 정렬해 최대 2곡까지 선택
+    for genre, _ in sorted_genres:
+        sorted_songs = sorted(genres_dict[genre], key=lambda x:(-x[1], x[0]))
+        answer.extend([idx for idx,_ in sorted_songs[:2]])
+        
+    return answer
+    
+        
     
     
-    # 장르별 고유번호 리스트 저장
-    for i,g in enumerate(genres):
-        genre_count[g] += plays[i]
-        genre_num[g].append(i)
-    
-    # 장르별 고유번호 리스트 조회수 별로 내림차
-    for k in genre_num.keys():
-        arr = genre_num[k]
-        genre_num[k] = sorted(arr, key=lambda x: plays[x], reverse=True)
-    print(genre_num)
-    
-    genre = sorted(genre, key=lambda x: genre_count[x], reverse=True)
-    ret = []
-    for g in genre:
-        ret.extend(genre_num[g][:2])
-    return ret
-    
-    # return sorted(ret, key=lambda i:(genre_count[genres[i]], plays[i]), reverse=True)
-
-# 재생된 장르 내림차, 노래별 횟수 내림차, 고유번호 오름차
-# 장르-고유번호리스트, 장르이름 내림차, 장르 - 재생횟수
-# 장르이름 내림차
-# 
+"""
+- 총 재생 횟수를 기준으로 장르를 내림차순으로 정렬
+- 각 장르별로 2곡씩 선정해서 플레이리스트 만들기
+  **요소를 -붙여서 정렬의 순서를 반대로 가능**
+"""
