50d60b21ae44db0e2d99693c73cfdf3958355a1c
noah <104671487+gratisreise@users.noreply.github.com>
2024-11-08 17:17:56 +0900

    [level 2] Title: 짝지어 제거하기, Time: 84.48 ms, Memory: 12.2 MB -BaekjoonHub

--- 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.py
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.py
@@ -1,8 +1,10 @@
 def solution(s):
     stk = []
     for c in s:
-        if stk and c == stk[-1]:
-            stk.pop()
-        else:
-            stk.append(c)
-    return int(not stk)
+        if stk and stk[-1] == c: stk.pop()
+        else: stk.append(c)
+    return 0 if stk else 1
+"""
+1. 같은 문자열이 있으면 팝
+2. 문자열이 없으면 1 아니면 0 반환
+"""
