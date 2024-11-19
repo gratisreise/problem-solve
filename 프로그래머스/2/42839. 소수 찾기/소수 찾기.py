0e8bd00e1a568db7568e53363dede30c6f9d76fe
noah <104671487+gratisreise@users.noreply.github.com>
2024-11-19 15:11:13 +0900

    [level 2] Title: 소수 찾기, Time: 13.72 ms, Memory: 11.9 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
@@ -1,28 +1,22 @@
-import math
-def isprime(num):
-    if num == 1: return 0
-    if num == 2: return 1 
-    if num % 2 == 0: return 0
-    for i in range(num +1, int(math.sqrt(num))+1):
-        if num % i == 0: return 0
+from itertools import permutations
+
+def isprime(n):
+    if n < 2: return 0
+    for i in range(2, int(n ** 0.5)+1):
+        if n % i == 0: return 0
     return 1
 
-def count(numbers, number_set, number):
-    if len(number) == len(numbers):
-        return number_set.add(int(''.join(numbers[i] for i in number)))
-    if number: number_set.add(int(''.join(numbers[i] for i in number)))
-    for i in range(len(numbers)):
-        if i in number: continue
-        number.append(i)
-        count(numbers, number_set, number)
-        number.pop()
 def solution(numbers):
-    answer = 0
+    answer = []
     numbers = list(numbers)
-    number_set = set()
-    count(numbers, number_set, [])
-    return sum(1 for i in number_set if isprime(i))
-"""
-문자열의 숫자에 대해 순열
-
-"""
+    num = []
+    
+    for i in range(1, len(numbers)+1):
+        num.append(list(permutations(numbers, i)))  
+    num = [int(''.join(y)) for x in num for y in x]    
+    
+    for i in num:
+        if isprime(i):
+            answer.append(i)
+    
+    return len(set(answer))
