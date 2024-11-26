6f5a3462f804ccf27d46fb20ab100ed5e136a375
noah <104671487+gratisreise@users.noreply.github.com>
2024-11-26 17:03:35 +0900

    [level 3] Title: 불량 사용자, Time: 229.62 ms, Memory: 9.98 MB -BaekjoonHub

--- 프로그래머스/3/64064. 불량 사용자/불량 사용자.py
+++ 프로그래머스/3/64064. 불량 사용자/불량 사용자.py
@@ -1,19 +1,26 @@
 import re
 
-def search(idx, visit, userId, answer, banPatterns):
-    if idx == len(banPatterns):
+def search(idx, visit, answer, ban_patterns, user_id):
+    if idx == len(ban_patterns):
         answer.add(visit)
         return
-    
-    for i in range(len(userId)):
-        if (visit & (1 << i)) > 0 or not re.fullmatch(banPatterns[idx], userId[i]): continue
-        
-        search(idx + 1, visit | (1 << i), userId, answer, banPatterns)
+    for i in range(len(user_id)):
+        if (visit & (1 << i)) > 0 or not re.fullmatch(ban_patterns[idx], user_id[i]):
+            continue
+        search(idx + 1, visit | (1 << i), answer, ban_patterns, user_id)
 
-def solution(userId, bannedId):
+def solution(user_id, banned_id):
     answer = set()
-    banPatterns = [x.replace('*', '.') for x in bannedId]
-    search(0, 0, userId, answer, banPatterns)
-
+    ban_patterns = [s.replace('*','.') for s in banned_id];
+    search(0, 0, answer, ban_patterns, user_id)
     return len(answer)
 
+"""
+아이디어: 비트마스킹, 모든 문자열 연결 후 정렬
+1. 금지아이디별 가능한 경우를 구한다
+2. 중복제거
+f***f *****
+ffeef abcde
+feeff abcde
+
+"""
