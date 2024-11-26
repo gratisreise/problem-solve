795c163192e0708a3efbedec5378e5cf64585530
noah <104671487+gratisreise@users.noreply.github.com>
2024-11-26 15:11:38 +0900

    [level 2] Title: 소수 찾기, Time: 5.59 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
@@ -1,22 +1,25 @@
 from itertools import permutations
 
 def isprime(n):
-    if n < 2: return 0
+    if n < 2: 
+        return 0
     for i in range(2, int(n ** 0.5)+1):
-        if n % i == 0: return 0
+        if n % i == 0:
+            return 0
     return 1
-
+        
 def solution(numbers):
-    answer = []
-    numbers = list(numbers)
+    numbers = list(numbers);
     num = []
-    
     for i in range(1, len(numbers)+1):
-        num.append(list(permutations(numbers, i)))  
-    num = [int(''.join(y)) for x in num for y in x]    
+        num.append(permutations(numbers, i))
+    num = set([int(''.join(y)) for x in num for y in x])
+    return len([n for n in num if isprime(n)])
+    # ret = set(n for num in n if isprime(n))
+    # return len(ret)
     
-    for i in num:
-        if isprime(i):
-            answer.append(i)
-    
-    return len(set(answer))
+"""
+1. 뽑는 갯수마다 순열 구해서 숫자를 저장
+2. 숫자가 소수인지 확인해서 갯수반환
+
+"""
