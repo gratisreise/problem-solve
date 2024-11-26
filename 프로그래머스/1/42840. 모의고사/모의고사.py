cb2ac3cef0220562f32b932b3d1a37d6fceffe00
noah <104671487+gratisreise@users.noreply.github.com>
2024-11-26 14:45:50 +0900

    [level 1] Title: 모의고사, Time: 4.65 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -1,24 +1,22 @@
-patterns = [
-    [1, 2, 3, 4, 5],
-    [2, 1, 2, 3, 2, 4, 2, 5],
-    [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
-]
-
 def solution(answers):
-    correct = {i+1:0 for i in range(3)}
-    for i, answer in enumerate(answers):
-        for j,pattern in enumerate(patterns):
-            if pattern[i % len(pattern)] == answer:
-                correct[j+1] += 1
-    mx = max([correct[i] for i in correct.keys()])
-    return [i for i in correct.keys() if correct[i] == mx]
+    stu1 = [1, 2, 3, 4, 5];
+    stu2 = [2, 1, 2, 3, 2, 4, 2, 5];
+    stu3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
+    
+    correct = [0, 0, 0];
+    
+    for idx, answer in enumerate(answers):
+        if stu1[idx%len(stu1)] == answer:
+            correct[0] += 1
+        if stu2[idx%len(stu2)] == answer:
+            correct[1] += 1
+        if stu3[idx%len(stu3)] == answer:
+            correct[2] += 1
+    mx = max(correct)
+    
+    return [i+1 for i,n in enumerate(correct) if n == mx]
 """
-# 가장 많은 문제를 맞힌 사람의 번호를 출력
-
 1. 정답 번호에 대해 다음을 반복
-    1-1. 1,2,3의 정답 패턴에 맞느지 확인
-    1-2. 정답의 갯수를 저장
-2. 정답 갯수의 최댓값을 찾기
-3. 해당하는 정답의 갯수와 같은 번호를 담아서 리턴
-
+    1-1. 수포자들의 패턴과 비교하여 카운팅
+2. 카운팅된 숫자의 최대값을 비교하여 숫자번호저장
 """
