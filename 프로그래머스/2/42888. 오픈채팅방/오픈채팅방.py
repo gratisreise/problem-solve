14ade0b950b01972baa45e22fd24a3a3282f2949
noah <104671487+gratisreise@users.noreply.github.com>
2024-11-28 10:41:20 +0900

    [level 2] Title: 오픈채팅방, Time: 141.66 ms, Memory: 61.3 MB -BaekjoonHub

--- 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.py
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.py
@@ -1,25 +1,20 @@
 def solution(record):
-    answer = []
-    uid = {}
-    
-    for line in record: # record의 각 줄을 하나씩 처리
-        cmd = line.split(" ")
-        
-        if cmd[0] != "Leave": # Enter 또는 Change인 경우
-            uid[cmd[1]] = cmd[2]
-    for line in record: #record의 각 줄을 하나씩 처리
-        cmd = line.split(" ")
-        # 각 상태에 맞느 메세지를 answer에 저장
-        if cmd[0] == "Enter":
-            answer.append("%s님이 들어왔습니다." %uid[cmd[1]])
-        elif cmd[0] == "Change":
-            pass
+    actions = []
+    output_str = {"Enter":"님이 들어왔습니다.", "Leave":"님이 나갔습니다."}
+    names = {}
+    for r in record:
+        srr = r.split()
+        if len(srr) == 2:
+            actions.append((srr[0], srr[1]))
         else:
-            answer.append("%s님이 나갔습니다." %uid[cmd[1]])
-    return answer
-    
-# 1.최종적으로 구하고자 하는 건 뭐지? -> 최종메세지
-# 2.입력값 중 수정되지 않는 건 뭐지? -> 유저 아이디
-# 3.입력값 중 수정되는 건 뭐지? -> 닉네임
-#   3-1.입력값이 수정될 때 어디가 영향받지? -> 오픈 채팅방의 내용 변경
-#   3-2.입력값은 어느 조건에서 수정되지? -> Enter와 Change인 경우
+            names[srr[1]] = srr[2]
+            if srr[0] == "Enter":
+                actions.append((srr[0], srr[1]))
+    return [(names[action[1]] + output_str[action[0]]) for action in actions]
+"""
+아이디 - 이름
+행동 - 출력 문자열
+행동리스트 (행동, 아이디)
+
+
+"""
