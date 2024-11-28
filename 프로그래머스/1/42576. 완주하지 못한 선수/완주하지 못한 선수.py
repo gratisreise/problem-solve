d2ce14f93de83e7fbf1037d7fe674856ca7594d6
noah <104671487+gratisreise@users.noreply.github.com>
2024-11-28 09:58:58 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 45.05 ms, Memory: 37.6 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,11 +1,18 @@
 def solution(participant, completion):
-    people = {name:0  for name in participant}
-    for name in participant: people[name] += 1
-    for name in completion: people[name] -= 1
-    for name in people.keys():
-        if people[name] != 0: return name
+    value = 0
+    answer = {}
+    for part in participant:
+        answer[hash(part)] = part
+        value += hash(part)
+    for comp in completion:
+        value -= hash(comp)
+    return answer[value]
     
-# 모든 참가자들에 대해 
-# 참가자 이름에 대한 인원수 세기
-# 완주자에 대해 이름을 하나씩 빼기
-# 참가자가 0이 아니면 리턴
+"""
+완주하지 못한 선수를 찾아라
+동명이인 가능 
+1. 참가자 - 숫자 맵생성
+2. 완주자를 돌면서 숫자 빼기
+3. 값이 +인 key 찾기
+
+"""
