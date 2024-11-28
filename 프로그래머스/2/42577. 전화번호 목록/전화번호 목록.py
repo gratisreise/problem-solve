eabeafde8a8370c65ab5dc32a93ef8b291d0ab87
noah <104671487+gratisreise@users.noreply.github.com>
2024-11-28 10:05:21 +0900

    [level 2] Title: 전화번호 목록, Time: 88.87 ms, Memory: 30.7 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,18 +1,7 @@
-def solution(phone_book):
-    phone_book.sort()
-    for i in range(1, len(phone_book)):
-        if phone_book[i].startswith(phone_book[i-1]):
+def solution(phoneBook):
+    phoneBook.sort()
+    for p1, p2 in zip(phoneBook, phoneBook[1:]):
+        if p2.startswith(p1):
             return False
     return True
-        
-"""
-폰북의 번호를 돌면서
-번호를 정렬한다
 
-1. 현재가 뒤의 있는 번호의 접두어이면 
-    return False
-2. 아니면
-    return True
-
-
-"""
