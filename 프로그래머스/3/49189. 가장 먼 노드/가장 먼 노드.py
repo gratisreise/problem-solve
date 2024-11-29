355c491aa9145967b47703bc73f9fb2a275f0b8e
noah <104671487+gratisreise@users.noreply.github.com>
2024-11-29 15:12:49 +0900

    [level 3] Title: 가장 먼 노드, Time: 58.53 ms, Memory: 23.6 MB -BaekjoonHub

---
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.py
@@ -0,0 +1,24 @@
+from collections import deque
+
+def solution(n, edge):
+    vis = [0 for i in range(n)]
+    graph = [[] for _ in range(n)]
+    ret = 0
+    for v in edge:
+        a, b = v[0]-1, v[1]-1
+        graph[a].append(b)
+        graph[b].append(a)
+    dq, vis[0] = deque([0]), 1
+    
+    while dq:
+        now = dq.popleft()
+        for next in graph[now]:
+            if vis[next]: continue
+            vis[next] = vis[now] + 1
+            dq.append(next)
+    mx = max(vis)
+    return sum(1 for n in vis if n == mx)
+    
+            
+            
+        
