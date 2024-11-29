02273d961914b2a4025b45493bdf24af8b544291
noah <104671487+gratisreise@users.noreply.github.com>
2024-11-29 11:29:49 +0900

    [level 2] Title: 기능개발, Time: 0.60 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -1,26 +1,21 @@
-import math
-
 def solution(progresses, speeds):
-    answer = []
-    n = len(progresses)
-    
-    # 각 작업의 배포 가능일 계산
-    days_left = [math.ceil((100 - progresses[i])/ speeds[i]) for i in range(n)]
-    
-    count = 0 # 배포될 작업의 수 카운트
-    max_day = days_left[0] # 현재 작업중 가장 먼저 배포될 작업
-    
-    for i in range(n):
-        if days_left[i] <= max_day: # 배포 예정일이 기준 배포일보다 빠르면
-            count += 1
-        else: # 배포 예정일이 기준 배포일보다 느리면
-            answer.append(count)
-            count = 1
-            max_day = days_left[i]
-    
-    answer.append(count) # 마지막으로 카운트된 작업들을 함께 배포
-    return answer
-# 1. 각 작업의 배포 가능일을 구합니다.
-# 2. 작업을 진행하며 배포 가능일이 첫 번째 작업일 보다 빠른 작업들은 함께 배포합니다.
-# 3. 첫 번째 작업의 배포 가능일보다 늦은 작업이 나오면, 2단계와 유사하게 해당 
-#    작업의 배포일을 기준으로 뒤의 작업들을 배포합니다. 이를 모든 작업이 완료될 때까지 반복한다.
+    idx = 0
+    ret = []
+    while idx < len(speeds):
+        for i in range(idx, len(speeds)):
+            progresses[i] += speeds[i]
+        cnt = 0
+        while idx < len(speeds) and progresses[idx] >= 100:
+            cnt += 1
+            idx += 1
+        if cnt > 0: ret.append(cnt)
+    return ret
+"""
+진도가 100일 때 서비스 반영가능
+진도는 100미만
+속도는 100이하의 자연수
+배포는 하우레 한 번만 가능 
+
+"""
+
+
