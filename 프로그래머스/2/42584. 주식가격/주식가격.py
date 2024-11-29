f974b3f8f16a944ebc0436dff2b4262eeb2f5d8d
noah <104671487+gratisreise@users.noreply.github.com>
2024-11-29 10:28:41 +0900

    [level 2] Title: 주식가격, Time: 30.81 ms, Memory: 19.4 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.py
+++ 프로그래머스/2/42584. 주식가격/주식가격.py
@@ -1,22 +1,12 @@
 def solution(prices):
-    n = len(prices)
-    ret = [0] * n
+    ret = [0 for _ in range(len(prices))]
     stk = []
-    for i,num in enumerate(prices): 
-        
-        while stk and prices[stk[-1]] > num:
+    for i,p in enumerate(prices):
+        while stk and prices[stk[-1]] > p:
             ret[stk[-1]] = (i - stk[-1])
             stk.pop()
         stk.append(i)
-        
-    
     while stk:
-        ret[stk[-1]] = (n-1 - stk[-1])
+        ret[stk[-1]] = len(prices) - 1 - stk[-1]
         stk.pop()
-        
-    return ret
-# 0, 1, 2, 3, 4
-# 1, 2, 3, 2, 3
-# 스택에 각 숫자의 인덱스를 저장
-#  현재의 숫자가 이전의 숫자보다 작아졌을 때 -> stk에 있는 숫자중 보다 작거나 같은애 있을 때까지 시간초 계산해서 저장
-# 스택에 숫자가 남아 있으면 -> 마지막 인덱스를 가지고 계산해주기
+    return ret;
