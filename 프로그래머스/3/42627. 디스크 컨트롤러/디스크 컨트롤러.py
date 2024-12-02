d93710bfd7af87dad3c1b86a9f322f7d08ac9072
noah <104671487+gratisreise@users.noreply.github.com>
2024-12-02 14:22:10 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 12.88 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
@@ -1,23 +1,37 @@
 from heapq import heappush as push, heappop as pop
 
 def solution(jobs):
-    answer, now, i = 0, 0, 0
-    start = -1 
+    ret, now, i = 0, 0, 0
+    start = -1
     heap = []
     
     while i < len(jobs):
+        # 이전 시작 ~ 현재
         for job in jobs:
             if start < job[0] <= now:
                 push(heap, job[::-1])
-        
         if len(heap) > 0:
             cur = pop(heap)
-            start = now
-            now += cur[0]
-            answer += now - cur[1]
-            i += 1
+            start = now #시작 시간 = 현재로
+            now += cur[0] #현재 시간 + 소요시간
+            ret += now - cur[1] #현재 + 들어온 시작 시간
+            i += 1 #인덱스 ++ 
         else:
             now += 1
-                
-    return answer // len(jobs)
-
+        
+        
+    return ret // len(jobs)
+"""
+시간을 더할 때 중간에 시간이 스킵 되는 것은 이전 현재 시간과 현재의 현재시간을 범위 제한하여
+찾는 것으로 해결
+작업의 인덱스를 따로 두는 것으로 종료 설정
+1. 모든 작업에 대하여
+    1-1. 들어온 작업이 있는지 확인
+    1-2. 작업이 있으면 
+        1-2-1. 현재시간 + 소요시간
+        1-2-2. 결과에 현재 - 요청 더하기
+        1-2-3. 행동 인덱스 ++
+    1-3. 작업이 없으면
+        시간 +1
+평균 반환시간을 반환
+"""
