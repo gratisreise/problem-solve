2267836c5a8e51c98c783a6ad4b84b570ddd3309
noah <104671487+gratisreise@users.noreply.github.com>
2024-12-13 13:15:35 +0900

    [level 3] Title: 순위, Time: 6.78 ms, Memory: 10.7 MB -BaekjoonHub

--- 프로그래머스/3/49191. 순위/순위.py
+++ 프로그래머스/3/49191. 순위/순위.py
@@ -1,27 +1,20 @@
 from collections import defaultdict
-
 def solution(n, results):
-    answer = 0
+    ret = 0
     win, lose = defaultdict(set), defaultdict(set)
     
     for winner, loser in results:
-        win[loser].add(winner) # 자신에게 승리한 사람 목록
-        lose[winner].add(loser) # 자신에게 패배한 사람 목록
-    for i in range(1, n+1):  
-        # 나에게 이긴 사람들의
+        win[loser].add(winner)
+        lose[winner].add(loser)
+        
+    for i in range(1, n + 1):
         for winner in win[i]:
-            # 승리목록에 나한테 진사람 추가
             lose[winner].update(lose[i])
-        # 나에게 진 사람들의
         for loser in lose[i]:
-            # 이긴 사람목록에 나에게 이긴 사람들을 추가
             win[loser].update(win[i])
     
-    # 모든 기록에서
     for i in range(1, n+1):
-        # 합이 n-1이면 ++
-        if len(win[i]) + len(lose[i]) == n-1:
-            answer += 1
-
-    return answer
-
+        if len(win[i]) + len(lose[i]) == n - 1:
+            ret += 1
+        
+    return ret
