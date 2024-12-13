86e70a5389d207a242e10fdab5b9966a10602deb
noah <104671487+gratisreise@users.noreply.github.com>
2024-12-13 13:16:35 +0900

    [level 3] Title: 길 찾기 게임, Time: 523.64 ms, Memory: 25.8 MB -BaekjoonHub

--- 프로그래머스/3/42892. 길 찾기 게임/길 찾기 게임.py
+++ 프로그래머스/3/42892. 길 찾기 게임/길 찾기 게임.py
@@ -9,11 +9,11 @@ def preorder(y, x, answer):
     for i in range(1, len(y)):
         if node[0] > y[i][0]: right.append(y[i])
         else: left.append(y[i])
-    
+        
     answer.append(node[2])
     if len(right) > 0: preorder(right, x[:idx], answer)
     if len(left) > 0: preorder(left, x[idx + 1:], answer)
-
+    
 def postorder(y, x, answer):
     node = y[0]
     idx = x.index(node)
@@ -26,17 +26,17 @@ def postorder(y, x, answer):
     if len(right) > 0: postorder(right, x[:idx], answer)
     if len(left) > 0: postorder(left, x[idx + 1:], answer)
     answer.append(node[2])
-
-def solution(nodeinfo):    
-    nodeinfo = [[*info, idx + 1] for idx, info in enumerate(nodeinfo)]
-    y = sorted(nodeinfo, key=lambda x: -x[1])
-    x = sorted(nodeinfo)
+    
+def solution(nodeinfo):
+    nodeinfo = [[*info, idx + 1] for idx, info, in enumerate(nodeinfo)]
+    print(nodeinfo)
+    y = sorted(nodeinfo, key=lambda x: -x[1]) # 내림차 정렬
+    x = sorted(nodeinfo) # 오름차 정렬
     
     preorderList = []
     postorderList = []
-
+    
     preorder(y, x, preorderList)
     postorder(y, x, postorderList)
-
+    
     return [preorderList, postorderList]
-
