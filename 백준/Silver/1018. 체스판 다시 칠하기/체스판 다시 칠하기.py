ef6b21551fd10983e2ca34f2bef83bbe84279ff0
noah <104671487+gratisreise@users.noreply.github.com>
2025-01-24 11:47:54 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 52 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
@@ -1,6 +1,5 @@
 pattern1,pattern2 = "WBWBWBWB", "BWBWBWBW"
 board1 = [[*pattern1] if i % 2 == 0 else [*pattern2] for i in range(8)]
-board2 = [[*pattern2] if i % 2 == 0 else [*pattern1] for i in range(8)]
 n,m = map(int, input().split())
 board = [[0]*m for _ in range(n)]
 def count(y,x):
@@ -10,15 +9,11 @@ def count(y,x):
         for j in range(8):
             if temp[i][j] != board1[i][j]:
                 cnt1 += 1
-            elif temp[i][j] != board2[i][j]:
-                cnt2 += 1
+            else: cnt2 += 1
     return min(cnt1, cnt2)
-
-for i in range(n):
-    board[i] = [*input()]
+for i in range(n): board[i] = [*input()]
 ret = 65
 for i in range(n-7):
     for j in range(m-7):
-        # print(i,j)
         ret = min(count(i,j), ret)
 print(ret)
