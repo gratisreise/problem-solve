adfafffcce66a4dad5c5a2b249a04973df8f2c13
noah <104671487+gratisreise@users.noreply.github.com>
2025-02-17 13:09:01 +0900

    [Gold IV] Title: 다이어트, Time: 252 ms, Memory: 34992 KB -BaekjoonHub

--- 백준/Gold/19942. 다이어트/다이어트.py
+++ 백준/Gold/19942. 다이어트/다이어트.py
@@ -1,25 +1,19 @@
 import sys
 from collections import defaultdict
-
 ip, op = sys.stdin, sys.stdout
-
 def check(p, f, s, v):
     return p >= mp and f >= mf and s >= ms and v >= mv
-
 n = int(ip.readline().rstrip())
 mp, mf, ms, mv = map(int, ip.readline().rstrip().split())
-
 data = []
 for _ in range(n):
     data.append(tuple(map(int, ip.readline().rstrip().split())))
 
-ret = int(1e9)
+ret = 1e9
 ans = defaultdict(list)
-
 for i in range(1, 1 << n):
-    sump, sumf, sums, sumv, sumc = [0] * 5
+    sump, sumf, sums, sumv, sumc = [0]*5
     temp = []
-
     for j in range(n):
         if i & (1 << j):
             sump += data[j][0]
@@ -27,19 +21,13 @@ for i in range(1, 1 << n):
             sums += data[j][2]
             sumv += data[j][3]
             sumc += data[j][4]
-            temp.append(j + 1)
+            temp.append(j+1)
 
-    if check(sump, sumf, sums, sumv):
-        if sumc < ret:
-            ret = sumc
-            ans.clear()  # 이전 최적 해 삭제
-            ans[sumc].append(tuple(temp))
-        elif sumc == ret:
-            ans[sumc].append(tuple(temp))
-
-if ret == int(1e9):
-    op.write("-1\n")
+    if sumc <= ret and check(sump, sumf, sums, sumv) :
+        ret = sumc
+        ans[sumc].append(tuple(temp))
+if ret == 1e9: op.write("-1\n")
 else:
-    ans[ret] = sorted([sorted(arr) for arr in ans[ret]])  # 내부 리스트도 정렬
-    op.write(f"{ret}\n")
-    op.write(" ".join(map(str, ans[ret][0])) + "\n")  # 가장 작은 조합 출력
+    ans = sorted(ans[ret])
+    op.write(f'{ret}\n')
+    op.write(' '.join(map(str, sorted(ans[0])))+'\n')
