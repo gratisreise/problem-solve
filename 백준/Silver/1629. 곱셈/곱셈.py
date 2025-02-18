5566d6f418126c3a1e27931e9b931618846a8d71
noah <104671487+gratisreise@users.noreply.github.com>
2025-02-18 12:17:42 +0900

    [Silver I] Title: 곱셈, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1629. 곱셈/곱셈.py
+++ 백준/Silver/1629. 곱셈/곱셈.py
@@ -1,8 +1,2 @@
-def go(a, b, c):
-    if b == 1: return a % c
-    ret = go(a, b//2, c)
-    ret = (ret * ret) % c
-    if b % 2 == 1: ret *= a
-    return ret % c
 a, b, c = map(int, input().split())
-print(go(a, b, c))
+print(pow(a, b, c))
