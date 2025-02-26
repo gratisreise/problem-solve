99b794e7c005b294f5323e22aa3310853051ee9c
noah <104671487+gratisreise@users.noreply.github.com>
2025-02-26 12:01:45 +0900

    [Silver V] Title: 사과 담기 게임, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2828. 사과 담기 게임/사과 담기 게임.py
+++ 백준/Silver/2828. 사과 담기 게임/사과 담기 게임.py
@@ -1,25 +1,14 @@
-# 입력 받기
 n, m = map(int, input().split())
 j = int(input())
-l = 1
-ret = 0
-
-# j번 반복
+l, ret = 1, 0
 for _ in range(j):
-    r = l + m - 1
-    temp = int(input())
-    
-    # temp가 l과 r 사이에 있으면 넘어감
-    if l <= temp <= r:
-        continue
-    # temp가 범위 밖에 있는 경우
-    else:
-        if temp < l:
-            ret += (l - temp)
-            l = temp
-        else:
-            l += (temp - r)
-            ret += (temp - r)
-
-# 결과 출력
+    r = l + m  - 1
+    pos = int(input())
+    if l <= pos <= r: continue
+    elif pos < l:
+        ret += (l - pos)
+        l = pos
+    elif pos > r:
+        ret += (pos - r)
+        l += (pos - r)
 print(ret)
