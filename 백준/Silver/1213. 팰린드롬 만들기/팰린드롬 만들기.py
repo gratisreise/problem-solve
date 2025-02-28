4e7e96576c54d16c1474c00da86b6a2410138a24
noah <104671487+gratisreise@users.noreply.github.com>
2025-02-28 15:37:27 +0900

    [Silver III] Title: 팰린드롬 만들기, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1213. 팰린드롬 만들기/팰린드롬 만들기.py
+++ 백준/Silver/1213. 팰린드롬 만들기/팰린드롬 만들기.py
@@ -1,25 +1,24 @@
-from collections import deque
 s = input()
-cnt = [0]*26
-for c in s:
-    cnt[ord(c)-ord('A')] += 1
-flag = 0
-for num in cnt:
-    if num % 2 == 1: flag += 1
-if flag >= 2:
+counts = [0] * 26
+for char in s:
+    counts[ord(char) - ord('A')] += 1
+
+odd_count = 0
+odd_char = ''
+for i in range(26):
+    if counts[i] % 2 != 0:
+        odd_count += 1
+        odd_char = chr(i + ord('A'))
+        counts[i] -= 1
+
+if odd_count > 1:
     print("I'm Sorry Hansoo")
-    exit()
 else:
-    ret, mid = deque(), ''
-    for i in range(25, -1, -1):
-        c = chr(i + ord('A'))
-        if cnt[i] % 2 == 1:
-            cnt[i] -= 1
-            mid = c
-        for j in range(0, cnt[i], 2):
-            ret.append(c)
-            ret.appendleft(c)
-    if mid != '':
-        pos, ret  = len(ret)//2, list(ret)
-        print(''.join((ret[:pos] + [mid] + ret[pos:])))
-    else: print(''.join(ret))
+    result = ''
+    for i in range(26):
+        result += chr(i + ord('A')) * (counts[i] // 2)
+    
+    if odd_char:
+        print(result + odd_char + result[::-1])
+    else:
+        print(result + result[::-1])
