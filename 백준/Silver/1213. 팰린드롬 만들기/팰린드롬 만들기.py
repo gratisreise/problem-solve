75892f5ca57e6a6a9aa92bf3bce9ef9fcaab207a
noah <104671487+gratisreise@users.noreply.github.com>
2025-02-28 15:42:53 +0900

    [Silver III] Title: 팰린드롬 만들기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1213. 팰린드롬 만들기/팰린드롬 만들기.py
+++ 백준/Silver/1213. 팰린드롬 만들기/팰린드롬 만들기.py
@@ -1,24 +1,23 @@
 s = input()
-counts = [0] * 26
-for char in s:
-    counts[ord(char) - ord('A')] += 1
+cnt = [0] * 26
+for c in s:
+    cnt[ord(c) - ord('A')] += 1
 
 odd_count = 0
 odd_char = ''
+
 for i in range(26):
-    if counts[i] % 2 != 0:
+    if cnt[i] % 2 != 0:
         odd_count += 1
         odd_char = chr(i + ord('A'))
-        counts[i] -= 1
-
+        cnt[i] -= 1
 if odd_count > 1:
     print("I'm Sorry Hansoo")
 else:
-    result = ''
+    ret = ''
     for i in range(26):
-        result += chr(i + ord('A')) * (counts[i] // 2)
-    
+        ret += chr(i + ord('A')) * (cnt[i] // 2)
     if odd_char:
-        print(result + odd_char + result[::-1])
+        print(ret + odd_char + ret[::-1])
     else:
-        print(result + result[::-1])
+        print(ret + ret[::-1])
