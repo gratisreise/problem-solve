56f9844e99e48ebecda2943c55b1a6368ff1e3b8
noah <104671487+gratisreise@users.noreply.github.com>
2025-02-28 09:48:24 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
@@ -1,11 +1,10 @@
-import sys
-ip, op = sys.stdin, sys.stdout
-
-nums = [int(ip.readline()) for i in range(9)]
-a,b, sums = 0, 0, sum(nums)
+nums = []
+for _ in range(9):
+    nums.append(int(input()))
+total = sum(nums)
 for i in range(9):
     for j in range(i + 1, 9):
-        if sums - nums[i] - nums[j] == 100:
-            a,b = i, j
-nums = sorted([nums[i] for i in range(9) if i != a and i != b])
-op.write('\n'.join(map(str, nums))+'\n')
+        if total - nums[i] - nums[j] == 100:
+            ret = sorted([x for x in nums if x != nums[i] and x != nums[j]])
+            print(*ret, sep='\n')
+            exit()
