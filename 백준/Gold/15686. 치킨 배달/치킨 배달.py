e976c71e9a0e5f28d876217bb337cc4ccb1f6246
noah <104671487+gratisreise@users.noreply.github.com>
2025-03-01 15:17:23 +0900

    [Gold V] Title: 치킨 배달, Time: 524 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.py
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.py
@@ -1,25 +1,22 @@
-import sys
 from itertools import combinations
+n, m = map(int, input().split())
 
-ip, op = sys.stdin, sys.stdout
-dy, dx = [-1, 0, 1, 0], [0, 1, 0, -1]
-home, chi = [], []
-n, m = map(int, ip.readline().rstrip().split())
-mp = [0]*n
+a, chi, home = [], [], []
+
 for i in range(n):
-    mp[i] = list(map(int, ip.readline().rstrip().split()))
+    a.append(list(map(int, input().split())))
     for j in range(n):
-        if mp[i][j] == 1:
-            home.append((i,j))
-        elif mp[i][j] == 2:
-            chi.append((i,j))
-ret = 1e9
-for arr in list(combinations(chi, m)):
-    sum = 0
+        if a[i][j] == 1: home.append((i, j))
+        elif a[i][j] == 2: chi.append((i, j))
+combi = list(combinations(chi, m))
+ret = int(1e9)
+for arr in combi:
+    sum_dis = 0
     for h in home:
-        dis = 1e9
-        for c in arr:
-            dis = min(dis, abs(h[0]-c[0])+abs(h[1]-c[1]))
-        sum += dis
-    ret = min(sum, ret)
-op.write(f'{ret}\n')
+        mn = int(1e9)
+        for pos in arr:
+            dis = abs(h[0] - pos[0]) + abs(h[1] - pos[1])
+            mn = min(dis, mn)
+        sum_dis += mn
+    ret = min(sum_dis, ret)
+print(ret)
