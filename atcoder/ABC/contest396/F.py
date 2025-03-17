21f5b340831a80444d88fd495113f8cab64301f0
noah <jeonghogim318@gmail.com>
2025-03-17 10:47:26 +0900

    contest 397 setting

--- atcoder/ABC/contest396/F.py
+++ atcoder/ABC/contest396/F.py
@@ -1,3 +1,4 @@
+
 import sys
 
 input = sys.stdin.readline
@@ -5,20 +6,16 @@ input = sys.stdin.readline
 n, m = map(int, input().split())
 nums_a = list(map(int, input().split()))
 nums_a = [x%m for x in nums_a]
-for k in range(m):
-    
+
+sum = 0
+for i in range(n):
+    sum += i
 
 # 0~m-1
-
-
-
-
 
 
 """
 n,m과 길이 n인 수열 a가 주어진다
 인버전의 갯수
 앞원소 > 뒤원소
-
-
 """
