7d57c2fc2fa0aa55c4346da63d68b2b4c1f022a1
noah <104671487+gratisreise@users.noreply.github.com>
2025-03-30 16:44:41 +0900

    [level 1] Title: 조건에 부합하는 중고거래 댓글 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/1/164673. 조건에 부합하는 중고거래 댓글 조회하기/조건에 부합하는 중고거래 댓글 조회하기.sql
+++ 프로그래머스/1/164673. 조건에 부합하는 중고거래 댓글 조회하기/조건에 부합하는 중고거래 댓글 조회하기.sql
@@ -2,9 +2,10 @@
 - 2022년 10월 작성된 게시글
 - 게시글 제목, 게시글 ID, 댓글 ID, 댓글 작성자 ID, 댓글 내용, 댓글 작성일
 - 작성일 오름차, 게시글 제목 오름차
-1. 두테이블 조인
-2. 조건에 맞는 컬럼선택
-3. 작성일 asc, 제목 asc 정렬
+1. 두테이블 조인 설정
+2. 게시글 날짜 조건 설정
+3. 정렬 조건 설정
+4. 날짜형식 지정
 */
 SELECT b.title,
        b.board_id,
@@ -18,17 +19,3 @@ FROM   used_goods_board b
 WHERE b.created_date 
     BETWEEN '2022-10-01' AND '2022-10-31'
 ORDER  BY created_date, title
-          
-# SELECT 
-#     A.TITLE,
-#     A.BOARD_ID,
-#     B.REPLY_ID,
-#     B.WRITER_ID,
-#     B.CONTENTS,
-#     DATE_FORMAT(B.CREATED_DATE, '%Y-%m-%d') AS CREATED_DATE
-# FROM USED_GOODS_BOARD AS A
-#     INNER JOIN USED_GOODS_REPLY AS B
-#     ON A.BOARD_ID = B.BOARD_ID
-# WHERE A.CREATED_DATE BETWEEN '2022-10-01'
-#     AND '2022-10-31'
-# ORDER BY CREATED_DATE, TITLE
