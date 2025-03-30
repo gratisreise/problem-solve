472ae4b4e3ccc50721c049d0ad6d73ac27d5b06b
noah <104671487+gratisreise@users.noreply.github.com>
2025-03-30 16:52:21 +0900

    [level 1] Title: 가장 큰 물고기 10마리 구하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/1/298517. 가장 큰 물고기 10마리 구하기/가장 큰 물고기 10마리 구하기.sql
+++ 프로그래머스/1/298517. 가장 큰 물고기 10마리 구하기/가장 큰 물고기 10마리 구하기.sql
@@ -1,15 +1,5 @@
-/*
-- 가장 큰 물고기 10마리
-- ID, 길이
-- 길이 내림차, ID 오름차
-- 10cm 이하는 버리기
-- 컬럼명 ID, LENGTH 지정
-1. where 10보다 큰 조건 설정
-2. 길이 기준 내림차 정렬, ID 오름차 정렬
-*/
-SELECT id,
-       length
-FROM fish_info
-WHERE length > 10
-ORDER BY length DESC,
-         id LIMIT  10
+-- 가장큰 10마리 물고기
+-- 길이 내림차, 아이디 오름차, null인경우 없음
+select id, length from fish_info
+order by length desc, id
+limit 10
