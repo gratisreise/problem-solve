d039aeb256fbacbc7ba76c5736abdbce8cda221d
noah <104671487+gratisreise@users.noreply.github.com>
2025-03-31 10:19:34 +0900

    [level 2] Title: 입양 시각 구하기(1), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/59412. 입양 시각 구하기（1）/입양 시각 구하기（1）.sql
+++ 프로그래머스/2/59412. 입양 시각 구하기（1）/입양 시각 구하기（1）.sql
@@ -1,6 +1,6 @@
--- 코드를 입력하세요
-SELECT HOUR(DATETIME) as HOUR, COUNT(*) as COUNT
-FROM ANIMAL_OUTS
-WHERE HOUR(DATETIME) BETWEEN 9 and 19
-GROUP BY HOUR
-ORDER BY HOUR
+-- 09:00 ~ 19:59 시간대별로 몇건의 입양이 발생 했는지 조회
+-- 9~ 19 까지
+select hour(datetime) as hour, count(*) as count from ANIMAL_OUTS
+where hour(datetime) >= 9 and hour(datetime) <= 19
+group by hour
+order by hour
