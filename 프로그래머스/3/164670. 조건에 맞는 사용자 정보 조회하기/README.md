cf65b5a236699412ef36312ac44d94647bd967d2
noah <104671487+gratisreise@users.noreply.github.com>
2025-04-04 22:56:21 +0900

    [level 3] Title: 조건에 맞는 사용자 정보 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/164670. 조건에 맞는 사용자 정보 조회하기/README.md
+++ 프로그래머스/3/164670. 조건에 맞는 사용자 정보 조회하기/README.md
@@ -1,6 +1,6 @@
 # [level 3] 조건에 맞는 사용자 정보 조회하기 - 164670 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/164670?language=mysql) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/164670) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@ Empty
 
 ### 제출 일자
 
-2024년 09월 02일 13:20:16
+2025년 04월 04일 22:56:15
 
 ### 문제 설명
 
--- 프로그래머스/3/164670. 조건에 맞는 사용자 정보 조회하기/조건에 맞는 사용자 정보 조회하기.sql
+++ 프로그래머스/3/164670. 조건에 맞는 사용자 정보 조회하기/조건에 맞는 사용자 정보 조회하기.sql
@@ -1,18 +1,16 @@
-/*
-- 게시물 3건 이상
-- 사용자 id, 닉네임, 전체주소, 전화번호
-- 주소(시 + 도로명 주소 + 상세 주소), 전화번호(000-0000-0000)
-- 회원ID 내림차
-1. 서브쿼리 3건 이상인 조건 설정
-2. concat을 사용하여 컬럼 합치기, subtr '-' 넣기
-3. 회원 ID를 기준으로 내림차순
-*/
-SELECT USER_ID, NICKNAME,
-CONCAT(CITY,' ',STREET_ADDRESS1,' ',STREET_ADDRESS2) AS 전체주소,
-CONCAT(SUBSTR(TLNO, 1, 3), '-', SUBSTR(TLNO, 4, 4), '-', SUBSTR(TLNO, 8, 4)) AS 전화번호
-FROM USED_GOODS_USER
-WHERE (
-    SELECT COUNT(*) FROM USED_GOODS_BOARD
-    WHERE USER_ID = WRITER_ID) >= 3
-ORDER BY USER_ID DESC
+-- 중고 거래 게시물 3건 이상
+-- 사용자 ID, 닉네임, 전체주소, 전화번호
+-- 전체 주소는 시, 도로명 주소, 상세 주소가 함께 출력
+-- 전화번호 xxx-xxxx-xxxx 
+-- 회원 ID를 기준으로 내림차순
+select b.WRITER_ID,
+    u.NICKNAME,
+    concat(CITY, ' ', STREET_ADDRESS1, ' ', STREET_ADDRESS2) as '전체주소',
+    concat(left(TLNO, 3),'-',substr(TLNO, 4, 4), '-', right(TLNO, 4)) as '전화번호'
+from USED_GOODS_BOARD b
+join USED_GOODS_USER u on b.WRITER_ID = u.USER_ID
+group by b.WRITER_ID
+having count(b.WRITER_ID) >= 3
+order by b.WRITER_ID desc
 
+ 
