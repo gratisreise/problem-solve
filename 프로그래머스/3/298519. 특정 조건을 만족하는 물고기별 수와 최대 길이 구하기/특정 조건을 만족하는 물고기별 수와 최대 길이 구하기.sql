9ecfb34e18ee85880ea99465e27caca54b3b97fa
noah <104671487+gratisreise@users.noreply.github.com>
2025-04-05 10:02:13 +0900

    [level 3] Title: 특정 조건을 만족하는 물고기별 수와 최대 길이 구하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/3/298519. 특정 조건을 만족하는 물고기별 수와 최대 길이 구하기/특정 조건을 만족하는 물고기별 수와 최대 길이 구하기.sql
+++ 프로그래머스/3/298519. 특정 조건을 만족하는 물고기별 수와 최대 길이 구하기/특정 조건을 만족하는 물고기별 수와 최대 길이 구하기.sql
@@ -1,22 +1,12 @@
-/*
-- 평균길이 33cm이상 물고기
-- 종류별로 분류
-- 잡은 수, 최대 길이, 물고기의 종류
-- 물고기 종류 오름차, 10cm이하 10cm로 취급
-1. 물고기타입 그룹핑
-2. 물고기수 count, max 물고기 컬럼설정
-3. 10cm이하 = 10cm 조건설정
-4. type asc 정렬
-*/
-# SELECT Count(*) FISH_COUNT,
-#        Max(IFNULL(length, 10))   MAX_LENGTH,
-#        fish_type
-# FROM   fish_info
-# GROUP  BY fish_type
-# HAVING Avg(IFNULL(length, 10)) >= 33
-
-SELECT COUNT(*) FISH_COUNT, MAX(LENGTH) MAX_LENGTH, FISH_TYPE
-FROM FISH_INFO 
-GROUP BY FISH_TYPE
-HAVING avg(CASE WHEN LENGTH <= 10 or LENGTH is null THEN 10 ELSE LENGTH END) >= 33
-ORDER BY FISH_TYPE
+-- 평균길이 >= 33
+-- 종류별 
+-- 종류 오름차
+-- null은 10
+select 
+    count(*) as FISH_COUNT,
+    max(IFNULL(LENGTH, 10)) as MAX_LENGTH,
+    FISH_TYPE
+from FISH_INFO
+group by FISH_TYPE
+having avg(IFNULL(LENGTH, 10)) >= 33
+order by FISH_TYPE
