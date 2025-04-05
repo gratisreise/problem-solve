c2155cb1a6af7271731da9f4ad0e695d59996056
noah <104671487+gratisreise@users.noreply.github.com>
2025-04-05 09:54:40 +0900

    [level 3] Title: 헤비 유저가 소유한 장소, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/77487. 헤비 유저가 소유한 장소/헤비 유저가 소유한 장소.sql
+++ 프로그래머스/3/77487. 헤비 유저가 소유한 장소/헤비 유저가 소유한 장소.sql
@@ -1,12 +1,11 @@
-/*
-- 공간 >=2 '헤비 유저'
-- 아이디 오름차
-1. 공간 둘이상 조건 설정
-2. 아이디 오름차 조회
-*/
-select * from places
-where HOST_ID IN (select HOST_ID from places
-group by HOST_ID
-having count(*) >= 2)
-ORDER BY ID
-
+-- 공간 >= 2 : 헤비유저
+-- 헤비유저가 등록한 공간 정보 
+-- 아이디 오름차
+select id, name, host_id
+from PLACES
+where HOST_ID in (
+    select HOST_ID from PLACES 
+    group by HOST_ID
+    having count(*) >= 2
+)
+order by id 
