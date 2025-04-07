f5cc9b57a3449e91344531af0c048bfa06e7c8be
noah <104671487+gratisreise@users.noreply.github.com>
2025-04-07 22:48:10 +0900

    [level 3] Title: 물고기 종류 별 대어 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/3/293261. 물고기 종류 별 대어 찾기/물고기 종류 별 대어 찾기.sql
+++ 프로그래머스/3/293261. 물고기 종류 별 대어 찾기/물고기 종류 별 대어 찾기.sql
@@ -1,21 +1,13 @@
-/*
-- 물고기 종류별
-- max 크기 물고기의 ID, 물고기 이름, 길이
-- 물고기의 ID 오름차
-- 10cm 이하의 물고기가 가장 큰 경우 없음
-1. 두테이블 조인
-2. 그룹핑해서 물고기별 최대길이 서브쿼리 와 조인
-3. 서브쿼리와 같은 길이 같은 물고기 이름을 가진것 조인
-4. 출력
-물고기별 최대 길이 구하기
-*/
-SELECT I.id, N.FISH_NAME, I.length
-FROM FISH_INFO I
-JOIN FISH_NAME_INFO N ON I.FISH_TYPE = N.FISH_TYPE
-JOIN (
-    SELECT N.FISH_NAME, MAX(I.length) AS max_length
-    FROM FISH_INFO I
-    JOIN FISH_NAME_INFO N ON I.FISH_TYPE = N.FISH_TYPE
-    GROUP BY N.FISH_NAME
-) max_fish ON N.FISH_NAME = max_fish.FISH_NAME AND I.length = max_fish.max_length
-ORDER BY I.id;
+-- 물고기 종류별
+-- 가장 큰 물고기
+-- 물고기 id 오름차순
+
+select f1.ID, f2.FISH_NAME, f1.LENGTH
+from FISH_INFO f1
+join FISH_NAME_INFO f2 on f1.FISH_TYPE = f2.FISH_TYPE
+where (f1.fish_type, f1.LENGTH) in (
+    select fish_type, max(LENGTH)
+    from FISH_INFO
+    group by FISH_TYPE
+)
+order by f1.ID
