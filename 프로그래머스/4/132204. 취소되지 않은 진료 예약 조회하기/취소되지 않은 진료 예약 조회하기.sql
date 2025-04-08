3cf8611f71d51386a39f3e48eb3bddf4fad34703
noah <104671487+gratisreise@users.noreply.github.com>
2025-04-08 09:44:43 +0900

    [level 4] Title: 취소되지 않은 진료 예약 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/132204. 취소되지 않은 진료 예약 조회하기/취소되지 않은 진료 예약 조회하기.sql
+++ 프로그래머스/4/132204. 취소되지 않은 진료 예약 조회하기/취소되지 않은 진료 예약 조회하기.sql
@@ -1,18 +1,21 @@
-/*
-- 2022년 4월 13일
-- 취소X CS 진료 예약
-- 진료예약번호, 환자이름, 환자번호, 진료과코드, 의사이름, 진료예약일시
-- 진료예약일시 asc
-1. patient 조인 환자정보, doctor 조인 의사정보
-2. APNT_CNCL_YN = N 조건설정
-3. APNT_YMD ASC
-4. 조회
-*/
-SELECT APNT_NO, PT_NAME, a.PT_NO, a.MCDP_CD, DR_NAME, APNT_YMD
-FROM APPOINTMENT a 
-    JOIN PATIENT p ON a.PT_NO = p.PT_NO
-    JOIN DOCTOR d ON MDDR_ID = DR_ID
-WHERE APNT_CNCL_YN = 'N'
-    AND a.MCDP_CD = 'CS'
-    AND APNT_YMD LIKE '2022-04-13%'
-ORDER BY APNT_YMD
+-- 2022년 4월 13일 
+-- 취소x, CS 진료 예약 내역
+-- 진료예약일시 오름차
+select a.APNT_NO, PT_NAME, a.PT_NO, a.MCDP_CD, d.DR_NAME, a.APNT_YMD
+from APPOINTMENT a
+join PATIENT p on a.PT_NO = p.PT_NO
+join DOCTOR d on a.MDDR_ID = d.DR_ID
+where 
+    a.APNT_YMD like '2022-04-13%'
+    and a.MCDP_CD = 'CS'
+    and a.APNT_CNCL_YN = 'N'
+order by a.APNT_YMD
+
+# SELECT APNT_NO, PT_NAME, a.PT_NO, a.MCDP_CD, DR_NAME, APNT_YMD
+# FROM APPOINTMENT a 
+#     JOIN PATIENT p ON a.PT_NO = p.PT_NO
+#     JOIN DOCTOR d ON MDDR_ID = DR_ID
+# WHERE APNT_CNCL_YN = 'N'
+#     AND a.MCDP_CD = 'CS'
+#     AND APNT_YMD LIKE '2022-04-13%'
+# ORDER BY APNT_YMD
