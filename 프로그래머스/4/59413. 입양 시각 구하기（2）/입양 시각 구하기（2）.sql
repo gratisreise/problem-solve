71814a729bfcf92a4f00626eb0a47056a2afdff8
noah <104671487+gratisreise@users.noreply.github.com>
2025-04-14 12:59:28 +0900

    [level 4] Title: 입양 시각 구하기(2), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/59413. 입양 시각 구하기（2）/입양 시각 구하기（2）.sql
+++ 프로그래머스/4/59413. 입양 시각 구하기（2）/입양 시각 구하기（2）.sql
@@ -1,16 +1,19 @@
--- 코드를 입력하세요
-with recursive time
-as (
-    select 0 as hour
-    union all
-    select hour + 1
-    from time
-    where hour < 23
+-- 몇시에 입양 활발
+-- 0~23시 시간대별
+-- 입양건수 조회
+-- 시간대 오름차
+-- MySQL
+
+-- 0부터 23까지의 모든 시간을 생성하는 CTE (Common Table Expression)
+WITH RECURSIVE AllHours (HOUR) AS (
+    SELECT 0 -- 시작 시간
+    UNION ALL
+    SELECT HOUR + 1 FROM AllHours WHERE HOUR < 23 -- 23보다 작을 때까지 1씩 더하며 재귀 호출
 )
-select time.hour, count(animal_id)
-from time left join (
-        select *, hour(datetime) hour
-        from animal_outs) outs
-        on time.hour = outs.hour
-group by time.hour
-order by time.hour
+SELECT
+    ah.HOUR,
+    COUNT(ANIMAL_ID) AS COUNT        
+FROM AllHours ah 
+left join ANIMAL_OUTS o on ah.HOUR = HOUR(o.DATETIME)
+GROUP BY ah.HOUR           
+ORDER BY ah.HOUR           
