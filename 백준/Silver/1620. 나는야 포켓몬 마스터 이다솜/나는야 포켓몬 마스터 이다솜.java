87b1e7ec410fd4868ab596a9e41dedc9ff9ec5c7
noah <104671487+gratisreise@users.noreply.github.com>
2025-11-19 11:05:43 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 452 ms, Memory: 46608 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
@@ -1,47 +1,33 @@
 import java.io.*;
 import java.util.*;
 
-public class Main{
+public class Main {
+    public static void main(String[] args) throws IOException {
+        var in = new BufferedReader(new InputStreamReader(System.in));
+        var out = new PrintWriter(System.out);
 
-    public static void main(String args[]) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st;
-
-        st = new StringTokenizer(br.readLine());
+        var st = new StringTokenizer(in.readLine());
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
-        
-        Map<String, Integer> mp1 = new HashMap<>();
-        String[] arr = new String[n+ 4];
-        
+
+        Map<Integer, String> mp1 = new HashMap<>(); // 이름-> 번호
+        Map<String, Integer> mp2 = new HashMap<>(); // 번호-> 이름
         for(int i = 0; i < n; i++){
-            String s = br.readLine();
-            mp1.put(s, i + 1);
-            arr[i + 1] = s;
+            String s = in.readLine();
+            mp1.put(i+1, s);
+            mp2.put(s, i+1);
         }
-        
         for(int i = 0; i < m; i++){
-            String s = br.readLine();
-            char c = s.charAt(0);
-            if('0' <= c && c <= '9'){
+            String s = in.readLine();
+            if(s.charAt(0) >= '0' && s.charAt(0) <= '9'){
                 int num = Integer.parseInt(s);
-                bw.write(arr[num]+'\n');
-            } else bw.write(String.valueOf(mp1.get(s))+'\n');
+                out.println(mp1.get(num));
+            } else {
+                out.println(mp2.get(s));
+            }
         }
-        bw.flush();
+
+        out.flush();
+        out.close();
     }
 }
-
-/*
-이름 => 번호  :: 맵
-번호 => 이름  :: 배열, 맵
-
-1.포켓몬 갯수 n, 문제 갯수 m 입력받기
-2. n번만큼 포켓몬 이름 입력받기
-    2-1. 이름 => 번호 맵에 저장
-    2-2. 번호 => 이름 배열에 저장
-3. m번만큼 문제입력받기
-    3-1 숫자면 배열을 이용해서 출력
-    3-2. 이름이면 맵을 이용해서 출력
- */
