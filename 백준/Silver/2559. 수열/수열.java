a766bf2601cc245295f2cac08bce7d12d84c55b3
noah <104671487+gratisreise@users.noreply.github.com>
2025-11-19 10:54:34 +0900

    [Silver III] Title: 수열, Time: 236 ms, Memory: 23856 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.java
+++ 백준/Silver/2559. 수열/수열.java
@@ -1,36 +1,30 @@
 import java.io.*;
 import java.util.*;
 
-public class Main{
-    static int[] psum = new int[100004];
-    static int ret = -(int)1e9;
-    public static void main(String args[]) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        var in = new BufferedReader(new InputStreamReader(System.in));
+        var out = new PrintWriter(System.out);
 
-        st = new StringTokenizer(br.readLine());
+        var st = new StringTokenizer(in.readLine());
         int n = Integer.parseInt(st.nextToken());
-        int k = Integer.parseInt(st.nextToken())
-                ;
-        st = new StringTokenizer(br.readLine());
-        for(int i = 1; i <= n; i++){
-            int temp = Integer.parseInt(st.nextToken());
-            psum[i] = psum[i - 1] + temp;
-        }
-        for(int i = k; i <= n; i++){
-            ret = Math.max(ret, psum[i] - psum[i - k]);
-        }
+        int k = Integer.parseInt(st.nextToken());
 
-        bw.write(String.valueOf(ret)+'\n');
-        bw.flush();
+        st = new StringTokenizer(in.readLine());
+        int[] psum = new int[n+1];
+
+        for(int i = 1;  i <= n; i++){
+            int num = Integer.parseInt(st.nextToken());
+            psum[i] = psum[i-1] + num;
+        }
+        int mx = Integer.MIN_VALUE;
+        for(int i = k; i <= n; i++){
+            mx = Math.max(mx, psum[i] - psum[i-k]);
+        }
+        System.out.println(mx);
+
+
+        out.flush();
+        out.close();
     }
 }
-
-/*
-연속적인 날짜의 합-> 구간의 합
-구간합 -> 누적합 배열을 이용해서 구한다.
-1. 날짜 수 n과 연속날짜 k 입력받기
-2. psum 누적합배열 만들기
-3. 구간k만큼 순회하면서 최대값구하기
- */
