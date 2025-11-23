3c44a5226a69c8038d1704a298e49e502f4970a0
noah <104671487+gratisreise@users.noreply.github.com>
2025-11-23 12:49:00 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 112 ms, Memory: 15960 KB -BaekjoonHub

--- 백준/Bronze/10808. 알파벳 개수/알파벳 개수.java
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.java
@@ -1,25 +1,29 @@
 import java.io.*;
 import java.util.*;
 
-public class Main{
-    static int[] cnt = new int[200];
-    public static void main(String args[]) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        String rs;
-        StringTokenizer st;
-        rs = br.readLine();
-        for(char c : rs.toCharArray()) cnt[c]++;
-        for(int i = 'a'; i <= 'z'; i++){
-            bw.write(String.valueOf(cnt[i]) + " ");
+public class Main {
+    public static void main(String[] args) throws IOException {
+        var in = new BufferedReader(new InputStreamReader(System.in));
+        var out = new PrintWriter(System.out);
+
+
+        int[] cnt = new int[26];
+
+
+        String s = in.readLine();
+        for(char c : s.toCharArray()){
+            cnt[c-'a']++;
         }
-        bw.write('\n');
-        bw.flush();
+
+        for(int i : cnt){
+            out.print(i+" ");
+        }
+
+
+        out.flush();
+        out.close();
     }
 }
-
 /*
-1. 문자열을 입력받는다.
-2. 문자열을 순회하면서 cnt배열에 ++해준다.
-3. 'a' ~ 'z' 까지 cnt배열을 순회하면서 숫자를 출력한다. 
- */
+
+*/
