ea69c8b00b220357369b632e94c6fae1dd7561b8
noah <104671487+gratisreise@users.noreply.github.com>
2025-11-24 15:07:27 +0900

    [Silver V] Title: 비밀번호 발음하기, Time: 108 ms, Memory: 14356 KB -BaekjoonHub

--- 백준/Silver/4659. 비밀번호 발음하기/비밀번호 발음하기.java
+++ 백준/Silver/4659. 비밀번호 발음하기/비밀번호 발음하기.java
@@ -1,39 +1,47 @@
+import java.io.*;
 import java.util.*;
 
-public class Main{
-    static int jcnt, mcnt, cnt;
-    static boolean check(char c){
-        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
+public class Main {
+    static boolean isMo(char c){
+        return c == 'a' || c== 'i' || c =='o' || c == 'e' || c == 'u';
     }
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        while(true){
-            String s = sc.next();
-            if(s.equals("end")) break;
-            boolean flag = false;
-            jcnt = mcnt = cnt = 0;
-            char prev = '1';
-            for(char c : s.toCharArray()){
-                if(check(c)){
-                    mcnt++; jcnt = 0; cnt++;
-                } else {
-                    mcnt = 0; jcnt++;
-                }
-                if(jcnt == 3 || mcnt == 3) flag = true;
-                if(prev == c && (c != 'e' && c != 'o')){
-                    flag = true;
-                }
-                prev = c;
+    static boolean check(String s){
+        char[] crr = s.toCharArray();
+        boolean flag1 = false; //모음존재체크
+        boolean flag2 = true;//자모음3개연속 체크
+        boolean flag3 = true; // 같은글자연속체크
+        int cntJ = 0, cntM= 0;
+        char prev =  ' ' ;
+
+        for(char c : crr){
+            if(c == prev && (c != 'e' && c != 'o')) flag3 = false;
+            if(isMo(c)){
+                cntM++; cntJ = 0; flag1 = true;
+            } else {
+                cntJ++; cntM = 0;
             }
-            if(cnt == 0) flag = true;
-            if(flag) System.out.println("<" + s +"> is not acceptable.");
-            else System.out.println("<" + s + "> is acceptable.");
+            if(cntM >= 3 || cntJ >= 3) flag2 = false;
+            prev = c;
         }
+
+        return flag1 && flag2 && flag3;
+    }
+    public static void main(String[] args) throws IOException {
+        var in = new BufferedReader(new InputStreamReader(System.in));
+        var out = new PrintWriter(System.out);
+
+        while(true){
+            String s = in.readLine();
+            if(s.equals("end")) break;
+            if(check(s)){
+                out.printf("<%s> is acceptable.\n", s);
+            } else {
+                out.printf("<%s> is not acceptable.\n", s);
+            }
+        }
+
+
+        out.flush();
+        out.close();
     }
 }
-
-/** flag가 세워지는 경우
- * 모음 없을 때 -> cnt, 모음 체크
- * 자음, 모음 3개연속 -> jcnt, mcnt
- * e,o 제외 긑은 글자 연속이면  -> prev로 이전과 비교
- */
