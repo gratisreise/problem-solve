230b9cf47305c35887025691dd1d9fc7c27913d8
noah <104671487+gratisreise@users.noreply.github.com>
2025-12-02 17:42:23 +0900

    [Silver V] Title: 사과 담기 게임, Time: 100 ms, Memory: 14120 KB -BaekjoonHub

--- 백준/Silver/2828. 사과 담기 게임/사과 담기 게임.java
+++ 백준/Silver/2828. 사과 담기 게임/사과 담기 게임.java
@@ -1,26 +1,33 @@
+import java.io.*;
 import java.util.*;
 
-public class Main{
-    
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int m = sc.nextInt();
-        int j = sc.nextInt();
-        int r, l = 1, ret = 0;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        var in = new BufferedReader(new InputStreamReader(System.in));
+        var out = new PrintWriter(System.out);
 
+        var st = new StringTokenizer(in.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int j = Integer.parseInt(in.readLine());
+        int l= 1;
+        int ret = 0;
         for(int i = 0; i < j; i++){
-            r = l + m - 1; 
-            int temp = sc.nextInt();
-            if(l <= temp && temp <= r) continue;
-            if(temp < l){
-                ret += l - temp;
-                l = temp;
+            int apple = Integer.parseInt(in.readLine());
+            int r = l + m - 1;
+            if(l <= apple && apple <= r){
+                continue;
+            } else if(apple < l){
+                ret += l - apple;
+                l = apple;
             } else {
-                ret += temp - r;
-                l += temp - r;
+               ret +=  apple - r;
+               l = l + (apple - r);
             }
         }
-        System.out.println(ret);
+
+        out.println(ret);
+        out.flush();
+        out.close();
     }
 }
