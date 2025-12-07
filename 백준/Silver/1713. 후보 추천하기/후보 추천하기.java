e26b83cba4c961d5a23f17bf1e1d3bd7a068e8a6
noah <104671487+gratisreise@users.noreply.github.com>
2025-12-07 12:40:41 +0900

    [Silver I] Title: 후보 추천하기, Time: 128 ms, Memory: 14820 KB -BaekjoonHub

--- 백준/Silver/1713. 후보 추천하기/후보 추천하기.java
+++ 백준/Silver/1713. 후보 추천하기/후보 추천하기.java
@@ -6,58 +6,46 @@ public class Main {
         var in = new BufferedReader(new InputStreamReader(System.in));
         var out = new PrintWriter(System.out);
 
-        int n = Integer.parseInt(in.readLine()); // 사진틀 개수
-        int m = Integer.parseInt(in.readLine()); // 총 추천 횟수
+        int n = Integer.parseInt(in.readLine());
+        int m = Integer.parseInt(in.readLine());
+
+        int[] cnt1 = new int[101];//추천수
+        int[] cnt2 = new int[101];//시간
+
+        List<Integer> ret = new ArrayList<>();
         var st = new StringTokenizer(in.readLine());
-
-        int[] recommendCount = new int[101]; // 추천수 (기본값 0)
-        int[] recentTime = new int[101];     // 최근 추천받은 시간 (기본값 0)
-
-        // frame: 현재 사진틀에 걸려있는 학생들의 번호 (최대 N개)
-        List<Integer> frame = new ArrayList<>();
-
         for(int i = 0; i < m; i++){
-            int studentNum = Integer.parseInt(st.nextToken());
+            int now = Integer.parseInt(st.nextToken());
 
-            // 1. 이미 프레임에 걸려있는 학생인 경우
-            if (frame.contains(studentNum)) {
-                recommendCount[studentNum]++;
-            }
-            // 2. 새로운 학생인 경우
-            else {
-                // 2-A. 프레임에 빈자리가 있는 경우
-                if (frame.size() < n) {
-                    frame.add(studentNum);
-                    recommendCount[studentNum] = 1;
-                    recentTime[studentNum] = i; // 현재 시간으로 갱신
-                }
-                // 2-B. 프레임이 꽉 찬 경우 (교체 필요)
-                else {
-                    frame.sort((a, b) -> {
-                        if (recommendCount[a] == recommendCount[b]) {
-                            return recentTime[a] - recentTime[b];
-                        }
-                        return recommendCount[a] - recommendCount[b];
+            if(ret.contains(now)){ //있으면
+                cnt1[now]++;
+            } else { //없으면
+                if(ret.size() < n){ // 작으면
+                    cnt1[now] = 1;
+                    cnt2[now] = i;
+                } else { //크면
+                    ret.sort((a, b) ->{ //정렬
+                        if(cnt1[a] == cnt1[b]){
+                            return cnt2[a] - cnt2[b];
+                        } return cnt1[a] - cnt1[b];
                     });
 
-                    int evictedStudent = frame.remove(0);
+                    //제거
+                    int outed = ret.get(0);
+                    cnt1[outed] = 0;
+                    cnt2[outed] = 0;
+                    ret.remove(0);
 
-                    // 밀려난 학생의 추천수와 시간 기록 초기화
-                    recommendCount[evictedStudent] = 0;
-                    recentTime[evictedStudent] = 0;
-
-                    frame.add(studentNum);
-                    recommendCount[studentNum] = 1;
-                    recentTime[studentNum] = i;
+                    cnt1[now] = 1;
+                    cnt2[now] = i;
                 }
+                ret.add(now);
             }
+
         }
-
-
-        frame.sort(Comparator.naturalOrder());
-
-        for(int student : frame){
-            out.print(student + " ");
+        ret.sort(Comparator.naturalOrder());
+        for(int i : ret){
+            out.print(i + " ");
         }
 
         out.flush();
