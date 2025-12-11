69e1a1f7c5e6ad558510b6218971b52080e457a1
noah <104671487+gratisreise@users.noreply.github.com>
2025-12-11 11:15:48 +0900

    [Silver V] Title: 기상캐스터, Time: 112 ms, Memory: 14476 KB -BaekjoonHub

--- 백준/Silver/10709. 기상캐스터/기상캐스터.java
+++ 백준/Silver/10709. 기상캐스터/기상캐스터.java
@@ -1,53 +1,54 @@
+import com.sun.security.jgss.GSSUtil;
 import java.io.*;
 import java.util.*;
 
-public class Main{
+public class Main {
+    public static void main(String[] args) throws IOException {
+        var in = new BufferedReader(new InputStreamReader(System.in));
+        var out = new PrintWriter(System.out);
 
-    public static void main(String args[]) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-        StringTokenizer st;
-        st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-        int[][] a = new int[n + 4][m + 4];
-
-        for(int i = 0; i < n; i++){
-            char[] c = br.readLine().toCharArray();
-            for(int j = 0; j < m; j++){
-                if(c[j] == 'c') a[i][j] = 0;
-                else a[i][j] = -1;
-            }
+        var st = new StringTokenizer(in.readLine());
+        int h = Integer.parseInt(st.nextToken());
+        int w = Integer.parseInt(st.nextToken());
+        char[][] board = new char[h][w];
+        int[][] ret = new int[h][w];
+        for(int[] row : ret){
+            Arrays.fill(row, -1);
         }
 
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < m; j++){
-                if(a[i][j] == 0){
-                    int cnt = 1;
-                    while(a[i][j + 1] == -1){
-                        a[i][j + 1] = cnt++;
+        for(int i = 0; i < h; i++){
+            char[] row = in.readLine().toCharArray();
+            board[i] = row;
+        }
+
+
+        for(int i = 0; i < h; i++){
+            for(int j = 0; j < w; j++){
+                if(board[i][j] == 'c'){
+                    int now = 1;
+                    ret[i][j] = 0;
+                    while(j < w-1 && board[i][j+1] == '.'){
+                        ret[i][j + 1] = now;
+                        now++;
                         j++;
                     }
                 }
             }
         }
 
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < m; j++){
-                bw.write(a[i][j] + " ");
+        var sb = new StringBuilder();
+        for(int i = 0; i < h; i++){
+            for(int j=  0; j < w; j++){
+                sb.append(ret[i][j]);
+                if(j != w-1) sb.append(" ");
             }
-            bw.write('\n');
+            sb.append("\n");
         }
-        bw.flush();
+        out.println(sb);
+
+
+
+        out.flush();
+        out.close();
     }
 }
-/*
-가로세로 1킬로미터인 작은 구역으로 나뉘어진다.
-지금부터 몇 분뒤 처음으로 하늘에 구름이 오나?
-
-0.-1로 채워진 배열에 현재 구름 있는 곳은 0으로 만들기
-1.2차원 배열 순회
-2. 0이면 해당 좌표 기준으로 j++하면서 숫자 넣어주기
-3. 2차원 배열 출력
- */
