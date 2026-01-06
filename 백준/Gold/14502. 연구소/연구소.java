cae5ab41832f32f9901c4105a1cf72116311ee94
noah <104671487+gratisreise@users.noreply.github.com>
2026-01-06 10:56:03 +0900

    [Gold IV] Title: 연구소, Time: 400 ms, Memory: 83264 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.java
+++ 백준/Gold/14502. 연구소/연구소.java
@@ -1,68 +1,86 @@
+import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int n, m, ret = Integer.MIN_VALUE;
-    static int[][] a = new int[10][10];
-    static int[][] visited;
-    static int[] dy = {-1, 0, 1, 0};
+    static int n,m;
+    static int[][] board;
+    static boolean[][] visited;
+    static int[] dy = {-1 , 0, 1, 0};
     static int[] dx = {0, 1, 0, -1};
-    static List<Pair> vir = new ArrayList<>();
-    static List<Pair> wall = new ArrayList<>();
-    static class Pair{
-        int f, s;
-        Pair(int f, int s){
-            this.f = f;
-            this.s = s;
+    static class Point{
+        int y; int x;
+        Point(int y, int x){
+            this.y = y;
+            this.x = x;
         }
     }
-    static void dfs(int y, int x){ // 벽만나면 멈춤
-        visited[y][x] = 1;
-        for(int i = 0; i < 4; i++){
-            int ny = y + dy[i];
-            int nx = x + dx[i];
-            if(ny < 0 || nx < 0 || ny >= n || nx >= m) continue;
-            if(a[ny][nx] == 1 || visited[ny][nx] != 0) continue;
-
-            dfs(ny, nx);
+    static int count(List<Point> virus){
+        visited = new boolean[n][m];
+        Queue<Point> q = new ArrayDeque<>();
+        for(Point p : virus){
+            visited[p.y][p.x] = true;
+            q.offer(p);
         }
-    }
-    static void solve(){
-        visited = new int[10][10];
-        int cnt = 0;
-        for(Pair p : vir) dfs(p.f, p.s);
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < m; j++){
-                if(a[i][j] != 0 || visited[i][j] != 0) continue;
-                cnt++;
+        while(!q.isEmpty()){
+            Point now = q.poll();
+            int cy = now.y;
+            int cx = now.x;
+            for(int i = 0; i < 4; i++){
+                int ny = cy + dy[i];
+                int nx = cx + dx[i];
+                if(ny < 0 || nx < 0 || ny >= n || nx >= m) continue;
+                if(visited[ny][nx] || board[ny][nx] == 1) continue;
+                visited[ny][nx] = true;
+                q.offer(new Point(ny, nx));
             }
         }
-        ret = Math.max(ret, cnt);
-    }
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        n = sc.nextInt();
-        m = sc.nextInt();
+        int ret = 0;
         for(int i = 0; i < n; i++){
             for(int j = 0; j < m; j++){
-                a[i][j] = sc.nextInt();
-                if(a[i][j] == 2) vir.add(new Pair(i, j));
-                else if(a[i][j] == 0) wall.add(new Pair(i, j));
+                if(board[i][j] != 0 || visited[i][j]) continue;
+                ret++;
             }
         }
+        return ret;
+    }
+    public static void main(String[] args) throws IOException {
+        var in = new BufferedReader(new InputStreamReader(System.in));
+        var out = new PrintWriter(System.out);
 
-        for(int i = 0; i < wall.size(); i++){
-            for(int j = 0; j < i; j++){
-                for(int k = 0; k < j; k++){
-                    a[wall.get(i).f][wall.get(i).s] = 1;
-                    a[wall.get(j).f][wall.get(j).s] = 1;
-                    a[wall.get(k).f][wall.get(k).s] = 1;
-                    solve();
-                    a[wall.get(i).f][wall.get(i).s] = 0;
-                    a[wall.get(j).f][wall.get(j).s] = 0;
-                    a[wall.get(k).f][wall.get(k).s] = 0;
+        var st = new StringTokenizer(in.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        board = new int[n][m];
+        visited = new boolean[n][m];
+        List<Point> virus = new ArrayList<>();
+        List<Point> empty = new ArrayList<>();
+        for(int i = 0; i < n; i++){
+            st = new StringTokenizer(in.readLine());
+            for(int j = 0; j < m; j++){
+                board[i][j] = Integer.parseInt(st.nextToken());
+                if(board[i][j] == 0) empty.add(new Point(i, j));
+                else if(board[i][j] == 2) virus.add(new Point(i, j));
+            }
+        }
+        int emp = empty.size();
+
+        int ret = 0;
+        for(int i = 0; i < emp; i++){
+            for(int j = i+1; j < emp; j++){
+                for(int k = j + 1; k < emp; k++){
+                    board[empty.get(i).y][empty.get(i).x] = 1;
+                    board[empty.get(j).y][empty.get(j).x] = 1;
+                    board[empty.get(k).y][empty.get(k).x] = 1;
+                    ret = Math.max(ret, count(virus));
+                    board[empty.get(i).y][empty.get(i).x] = 0;
+                    board[empty.get(j).y][empty.get(j).x] = 0;
+                    board[empty.get(k).y][empty.get(k).x] = 0;
                 }
             }
         }
-        System.out.println(ret);
+
+        out.println(ret);
+        out.flush();
+        out.close();
     }
 }
