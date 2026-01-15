3dfe2e266fe9e634ab8d412fbbf35f398e23e3dc
noah <104671487+gratisreise@users.noreply.github.com>
2026-01-15 10:31:28 +0900

    [Gold IV] Title: 인구 이동, Time: 468 ms, Memory: 130552 KB -BaekjoonHub

--- 백준/Gold/16234. 인구 이동/인구 이동.java
+++ 백준/Gold/16234. 인구 이동/인구 이동.java
@@ -1,61 +1,82 @@
+import java.io.*;
 import java.util.*;
 
-public class Main{
-    static int n, l, r, sum, ret;
-    static List<Pair> L = new ArrayList<>();
-    static int[] dy = {-1, 0, 1, 0};
+public class Main {
+    static int n, L, R;
+    static int[][] board, visited;
+    static class Point{
+        int y; int x;
+        Point(int y, int x){
+            this.y = y;
+            this.x = x;
+        }
+    }
+    static int[] dy ={-1, 0, 1, 0};
     static int[] dx = {0, 1, 0, -1};
-    static int[][] a, visited;
-    static class Pair{
-        int f, s;
-        Pair(int f, int s){
-            this.f = f;
-            this.s = s;
+    static List<Point> points = new ArrayList<>();
+    static int dfs(int y, int x, int[][] visited){
+        int ret = 0;
+        ret += board[y][x];
+        points.add(new Point(y, x));
+        for(int d = 0; d < 4; d++){
+            int ny = y + dy[d];
+            int nx = x + dx[d];
+            if(ny < 0 || nx < 0 || ny >= n || nx >= n) continue;
+            int cha = Math.abs(board[ny][nx] - board[y][x]);
+            if(visited[ny][nx] != 0 || cha < L || cha > R) continue;
+            visited[ny][nx] = 1;
+            ret += dfs(ny, nx, visited);
         }
+        return ret;
     }
-    static void dfs(int y, int x){
-        visited[y][x] = 1;
-        sum += a[y][x];
-        L.add(new Pair(y, x));
-        for(int i = 0; i < 4; i++){
-            int ny = y + dy[i];
-            int nx = x + dx[i];
-            if(ny < 0 || nx < 0 || ny >= n || nx >= n || visited[ny][nx] != 0) continue;
-            int diff = Math.abs(a[ny][nx] - a[y][x]);
-            if(diff < l || diff > r) continue;
-            dfs(ny, nx);
-        }
-    }
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        n = sc.nextInt();
-        l = sc.nextInt();
-        r = sc.nextInt();
-        a = new int[n + 1][n + 1];
+    static boolean go(){
+        visited = new int[n][n];
+        boolean flag = false;
         for(int i = 0; i < n; i++){
             for(int j = 0; j < n; j++){
-                a[i][j] = sc.nextInt();
+                if(visited[i][j] != 0)continue;
+                visited[i][j] = 1;
+                int sum = dfs(i, j, visited);
+                if(points.size() == 1) {
+                    visited[i][j] = 0;
+                    points.clear();
+                    continue;
+                }
+                flag = true;
+                for(Point p : points) board[p.y][p.x] = sum / points.size();
+                points.clear();
             }
         }
-        while(true){
-            boolean flag = false;
-            visited = new int[n + 1][n + 1];
-            for(int i = 0 ; i < n; i++){
-                for(int j = 0; j < n; j++){
-                    if(visited[i][j] != 0) continue;
-                    sum = 0;
-                    L.clear();
-                    dfs(i, j);
-                    if(L.size() == 1) continue;
-                    for(Pair p : L){
-                        a[p.f][p.s] = sum / L.size();
-                        flag = true;
-                    }
-                }
+        return flag;
+    }
+    public static void main(String[] args) throws IOException {
+        var in = new BufferedReader(new InputStreamReader(System.in));
+        var out = new PrintWriter(System.out);
+
+        var st = new StringTokenizer(in.readLine());
+        n = Integer.parseInt(st.nextToken());
+        L = Integer.parseInt(st.nextToken());
+        R = Integer.parseInt(st.nextToken());
+        board = new int[n][n];
+        visited = new int[n][n];
+        for(int i = 0; i < n; i++){
+            st = new StringTokenizer(in.readLine());
+            for(int j = 0; j < n; j++){
+                board[i][j] = Integer.parseInt(st.nextToken());
             }
-            if(!flag) break;
+        }
+
+        int ret = 0;
+        while(true){
+            if(!go()) break;
             ret++;
         }
-        System.out.println(ret);
+
+
+        out.println(ret);
+
+
+        out.flush();
+        out.close();
     }
 }
