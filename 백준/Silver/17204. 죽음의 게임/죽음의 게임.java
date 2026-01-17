eb10cd7a67e280c4fa35a46dd0a30c5a4d1c58da
noah <104671487+gratisreise@users.noreply.github.com>
2026-01-17 11:00:36 +0900

    [Silver III] Title: 죽음의 게임, Time: 104 ms, Memory: 14260 KB -BaekjoonHub

--- 백준/Silver/17204. 죽음의 게임/죽음의 게임.java
+++ 백준/Silver/17204. 죽음의 게임/죽음의 게임.java
@@ -2,43 +2,44 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int[] graph, visited;
-    static int n, k;
-    static boolean check(int now, int cnt){
-        if(visited[now] == 1) return false; //순환체크
-        if(now == k) return true; //도착지점 도착
-        visited[now] = 1;
-        if(cnt == 0) return false;
-        return check(graph[now], cnt-1);
-    };
     public static void main(String[] args) throws IOException {
         var in = new BufferedReader(new InputStreamReader(System.in));
         var out = new PrintWriter(System.out);
 
         var st = new StringTokenizer(in.readLine());
-        n = Integer.parseInt(st.nextToken());
-        k = Integer.parseInt(st.nextToken());
-        graph = new int[n];
-        visited = new int[n];
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        int[] target = new int[n];
         for(int i = 0; i < n; i++){
-            graph[i] = Integer.parseInt(in.readLine());
+            target[i] = Integer.parseInt(in.readLine());
         }
 
-        int ret = 0;
+        var visited = new boolean[n];
+        int cnt = 0;
+        int cur = 0;
         while(true){
-            ret++;
-            Arrays.fill(visited, 0);
-            if(check(0, ret)) {
-                out.println(ret);
-                break;
-            }
-            if(ret >= n) {
+            if(visited[cur]){
                 out.println(-1);
                 break;
             }
+
+            visited[cur] = true;
+
+            if(cur == k){
+                out.println(cnt);
+                break;
+            }
+            
+            cnt++;
+            cur = target[cur];
         }
+
 
         out.flush();
         out.close();
     }
 }
+/*
+
+*/
