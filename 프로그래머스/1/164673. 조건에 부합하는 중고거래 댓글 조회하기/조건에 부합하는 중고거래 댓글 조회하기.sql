f795c995c0b7ef596628ba09f7efa24ea2f84eaf
noah <104671487+gratisreise@users.noreply.github.com>
2026-01-18 13:06:49 +0900

    [level 1] Title: 조건에 부합하는 중고거래 댓글 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/1/164673. 조건에 부합하는 중고거래 댓글 조회하기/조건에 부합하는 중고거래 댓글 조회하기.sql
+++ 프로그래머스/1/164673. 조건에 부합하는 중고거래 댓글 조회하기/조건에 부합하는 중고거래 댓글 조회하기.sql
@@ -1,21 +1,20 @@
+select 
+    b.title,
+    b.board_id,
+    r.reply_id, 
+    r.writer_id, 
+    r.contents, 
+    date_format(r.created_date, '%Y-%m-%d') as created_date
+from used_goods_board as b
+join used_goods_reply as r
+    on b.board_id = r.board_id
+where b.created_date between '2022-10-01' and '2022-10-31'
+order by created_date, b.title
+
+
 /*
-- 2022년 10월 작성된 게시글
-- 게시글 제목, 게시글 ID, 댓글 ID, 댓글 작성자 ID, 댓글 내용, 댓글 작성일
-- 작성일 오름차, 게시글 제목 오름차
-1. 두테이블 조인 설정
-2. 게시글 날짜 조건 설정
-3. 정렬 조건 설정
-4. 날짜형식 지정
+used_goods_board, used_goods_reply
+제목, 게시글id, 댓글id, 댓글 작성자 id, 댓글 내용, 댓글 작성일
+2022년 10월 작성
+댓글 작성일 오름차, 게시글 제목 오름차 
 */
-SELECT b.title,
-       b.board_id,
-       r.reply_id,
-       r.writer_id,
-       r.contents,
-       DATE_FORMAT(r.created_date, '%Y-%m-%d') as created_date
-FROM   used_goods_board b
-       JOIN used_goods_reply r
-         ON b.board_id = r.board_id
-WHERE b.created_date 
-    BETWEEN '2022-10-01' AND '2022-10-31'
-ORDER  BY created_date, title
