a496c822abd8b090c936f42017047196fa9af440
noah <104671487+gratisreise@users.noreply.github.com>
2026-01-21 11:10:49 +0900

    [level 4] Title: 오프라인/온라인 판매 데이터 통합하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/131537. 오프라인／온라인 판매 데이터 통합하기/오프라인／온라인 판매 데이터 통합하기.sql
+++ 프로그래머스/4/131537. 오프라인／온라인 판매 데이터 통합하기/오프라인／온라인 판매 데이터 통합하기.sql
@@ -1,14 +1,33 @@
--- 2022년 3월 
--- 상품판매 데이터
--- 판매일 오름차, 상품id 오름차, 유저id 오름차
+select
+    date_format(sales_date, '%Y-%m-%d') as sales_date,
+    product_id,
+    user_id,
+    sales_amount
+from online_sale
+where year(sales_date) = 2022 and month(sales_date) = 3
 
-select SALES_DATE, PRODUCT_ID, USER_ID, SALES_AMOUNT
-from ONLINE_SALE
-where SALES_DATE like '2022-03%'
 union all
-select 
-date_format(SALES_DATE, "%Y-%m-%d") as SALES_DATE,
-PRODUCT_ID, null, SALES_AMOUNT
-from OFFLINE_SALE
-where SALES_DATE like '2022-03%'
-order by SALES_DATE, PRODUCT_ID, USER_ID
+
+select
+    date_format(sales_date, '%Y-%m-%d') as sales_date,
+    product_id,
+    NULL as user_id,
+    sales_amount
+from offline_sale
+where year(sales_date) = 2022 and month(sales_date) = 3
+
+order by 
+    sales_date asc,
+    product_id asc,
+    user_id asc
+    
+
+
+/*
+online_sale, offline_sale
+2022년 3월 온/오프 상품 판매데이터
+판매날짜, 상품id, 유저id, 판매량
+오프란인 user_id는 null로 표시
+판매일 오름차, 상품ID 오르마, 유저ID 오름차
+
+*/
