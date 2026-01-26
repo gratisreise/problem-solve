8ad86bec4db233253c020370c9e7f2fa1b415dc5
noah <104671487+gratisreise@users.noreply.github.com>
2026-01-26 11:43:28 +0900

    [level 2] Title: 중성화 여부 파악하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/59409. 중성화 여부 파악하기/중성화 여부 파악하기.sql
+++ 프로그래머스/2/59409. 중성화 여부 파악하기/중성화 여부 파악하기.sql
@@ -1,9 +1,10 @@
-# NULL: name
-# SEX_UPON_INTAKE = Neutered or Spayed는 중성화, 아이디 오름차, 중성화 OX 표시
-
-SELECT ANIMAL_ID, NAME,
-    CASE 
-        WHEN (SEX_UPON_INTAKE REGEXP ('Neutered|Spayed')) 
-        THEN 'O' ELSE 'X' END
-        AS 중성화
-FROM ANIMAL_INS
+select 
+    animal_id,
+    name,
+    case
+        when sex_upon_intake like 'Neutered%' then 'O'
+        when sex_upon_intake like 'Spayed%' then 'O'
+        else 'X'
+    end as '중성화'
+from animal_ins
+order by animal_id
