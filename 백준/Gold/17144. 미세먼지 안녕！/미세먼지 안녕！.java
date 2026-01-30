4f82cf14bf55c32b99b2957a57435c1e05df360f
noah <104671487+gratisreise@users.noreply.github.com>
2026-01-30 10:40:01 +0900

    [Gold IV] Title: 미세먼지 안녕!, Time: 348 ms, Memory: 38276 KB -BaekjoonHub

--- 백준/Gold/17144. 미세먼지 안녕！/미세먼지 안녕！.java
+++ 백준/Gold/17144. 미세먼지 안녕！/미세먼지 안녕！.java
@@ -1,143 +1,167 @@
 import java.io.*;
 import java.util.*;
+
 public class Main {
-    static int n, m, t, ret;
-    static int[][] a, temp;
-    static ArrayList<Pair> v1, v2;
+    static int r, c, t;
+    static int[][] board;
+    static int[][] cleaner = new int[2][2];
     static int[] dy1 = {0, -1, 0, 1};
     static int[] dx1 = {1, 0, -1, 0};
     static int[] dy2 = {0, 1, 0, -1};
     static int[] dx2 = {1, 0, -1, 0};
 
-    static class Pair {
-        int f, s;
-
-        Pair(int f, int s) {
-            this.f = f;
-            this.s = s;
-        }
+    static boolean notDust(int y, int x){
+        return board[y][x] == -1 || board[y][x] == 0;
     }
-
-    public static void mise_go(int[] dy, int[] dx) {
-        temp = new int[n + 1][m + 1];
-
-        //미세먼지 확산
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (a[i][j] >= 5) {
-                    int spread = a[i][j] / 5;
-                    for (int d = 0; d < 4; d++) {
-                        int ny = i + dy[d];
-                        int nx = j + dx[d];
-                        if (ny < 0 || ny >= n || nx < 0 || nx >= m || a[ny][nx] == -1) continue;
-                        temp[ny][nx] += spread;
-                        a[i][j] -= spread;
-                    }
+    static boolean isOut(int y, int x){
+        return y < 0 || y >= r || x < 0 || x >= c;
+    }
+    static void print(int[][] arr){
+        for(int[] row : arr){
+            System.out.println(Arrays.toString(row));
+        }
+        System.out.println();
+    }
+    static void diffusion(){
+        //임시배열 생성
+        int[][] temp = new int[r][c];
+        //먼지 4방향 확산
+        for(int i = 0; i < r; i++){
+            for(int j = 0; j < c; j++){
+                if(notDust(i, j)) continue;
+                int cnt = 0; int value = board[i][j] / 5;
+                for(int d = 0; d < 4; d++){
+                    int ny = i + dy1[d];
+                    int nx = j + dx1[d];
+                    if(isOut(ny, nx) || board[ny][nx] == -1) continue;
+                    temp[ny][nx] += value;
+                    cnt++;
                 }
+                board[i][j] -= cnt* value;
             }
         }
 
-        //확산된거 적용
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                a[i][j] += temp[i][j];
+        //해당 배열을 board배열에 적용
+        for(int i = 0; i < r; i++){
+            for(int j = 0; j < c; j++){
+                board[i][j] = board[i][j] + temp[i][j];
             }
         }
     }
-    // 공청기의 작동흐름을 저장
-    public static ArrayList<Pair> chung(int sy, int sx, int[] dy, int[] dx) {
-        ArrayList<Pair> v = new ArrayList<>();
-        int cnt = 0;
-        int y = sy;
-        int x = sx;
-
-
-        //한바퀴 돌려버리기
-        while (true) {
-            int ny = y + dy[cnt];
-            int nx = x + dx[cnt];
-
-            // 시작점으로 돌아왔으면 브레이크
-            if (ny == sy && nx == sx) break;
-            // 경계에 도달 했으면 방향바꿔주기
-            if (ny < 0 || ny >= n || nx < 0 || nx >= m) {
-                cnt = (cnt + 1) % 4; // 방향바꿔주기
-                ny = y + dy[cnt];
-                nx = x + dx[cnt];
+    static void move1(int[][] temp){
+        //현재위치에 먼지가 있으면 다음 위치로 먼지 이동
+        //반시계
+        int[] cleaner1 = cleaner[0];
+        int d = 0;
+        int y = cleaner1[0] + dy1[d];
+        int x = cleaner1[1] + dx1[d];
+        while(y != cleaner1[0] || x != cleaner1[1]){
+            //바람이동
+            int ny = y + dy1[d];
+            int nx = x + dx1[d];
+            if(isOut(ny, nx)) {
+                d++;
+                ny = y + dy1[d];
+                nx = x + dx1[d];
             }
-
-            // 방향이 바뀌면 다음좌표로 시작점이면 브레이크
-            if (ny == sy && nx == sx) break;
-            y = ny;
-            x = nx;
-            // 다음으로
-            v.add(new Pair(ny, nx));
+            //먼지이동
+            temp[ny][nx] = board[y][x];
+            //먼지제거
+            if(ny == cleaner1[0] && nx == cleaner1[1]) temp[ny][nx] = -1;
+            y = ny; x = nx;
         }
-
-        return v;
+        //보드 변경
+        d = 0;
+        y += dy1[d]; x += dx1[d];
+        while(y != cleaner1[0] || x != cleaner1[1]){
+            int ny = y + dy1[d];
+            int nx = x + dx1[d];
+            if(isOut(ny, nx)) {
+                d++;
+                ny = y + dy1[d];
+                nx = x + dx1[d];
+            }
+            board[y][x] = temp[y][x];
+            y = ny; x = nx;
+        }
     }
-
-    public static void go(ArrayList<Pair> v) {
-        //미세먼지 이동
-        for (int i = v.size() - 1; i > 0; i--) {
-            int ny = v.get(i).f;
-            int nx = v.get(i).s;
-            a[ny][nx] = a[v.get(i - 1).f][v.get(i - 1).s];
+    static void move2(int[][] temp){
+        //시계
+        int[] cleaner2 = cleaner[1];
+        int d = 0;
+        int y = cleaner2[0] + dy2[d];
+        int x = cleaner2[1] + dx2[d];
+        while(y != cleaner2[0] || x != cleaner2[1]){
+            //바람이동
+            int ny = y + dy2[d];
+            int nx = x + dx2[d];
+            if(isOut(ny, nx)) {
+                d++;
+                ny = y + dy2[d];
+                nx = x + dx2[d];
+            }
+            //먼지이동
+            if(board[y][x] != 0) temp[ny][nx] = board[y][x];
+            //먼지제거
+            if(ny == cleaner2[0] && nx == cleaner2[1]) temp[ny][nx] = -1;
+            y = ny; x = nx;
         }
-        //첫 좌표는 0
-        a[v.get(0).f][v.get(0).s] = 0;
+        //보드변경
+        d = 0;
+        y += dy2[d]; x += dx2[d];
+        while(y != cleaner2[0] || x != cleaner2[1]){
+            int ny = y + dy2[d];
+            int nx = x + dx2[d];
+            if(isOut(ny, nx)) {
+                d++;
+                ny = y + dy2[d];
+                nx = x + dx2[d];
+            }
+            board[y][x] = temp[y][x];
+            y = ny; x = nx;
+        }
+    }
+    static void move(){
+        //임시배열 필요
+        int[][] temp = new int[r][c];
+        move1(temp);
+        move2(temp);
     }
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st;
-        st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
+        var in = new BufferedReader(new InputStreamReader(System.in));
+        var out = new PrintWriter(System.out);
+
+        var st = new StringTokenizer(in.readLine());
+        r = Integer.parseInt(st.nextToken());
+        c = Integer.parseInt(st.nextToken());
         t = Integer.parseInt(st.nextToken());
-
-        // 위아래 구분하기 위한 flag
-        boolean flag = true;
-        a = new int[n + 1][m + 1];
-
-        for (int i = 0; i < n; i++) {
-            st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < m; j++) {
-                a[i][j] = Integer.parseInt(st.nextToken());
-                if (a[i][j] == -1) {
-                    //공청기의 이동방향을 순차적으로 저장
-                    if (flag) {
-                        v1 = chung(i, j, dy1, dx1);
-                        flag = false;
-                    } else v2 = chung(i, j, dy2, dx2);
+        int idx = 0;
+        board = new int[r][c];
+        for(int i = 0; i < r; i++){
+            st = new StringTokenizer(in.readLine());
+            for(int j = 0; j < c; j++){
+                board[i][j] = Integer.parseInt(st.nextToken());
+                if(board[i][j] == -1){
+                    cleaner[idx][0] = i;
+                    cleaner[idx][1] = j;
+                    idx++;
                 }
             }
         }
 
-        while (t-- > 0) {
-            mise_go(dy1, dx1);
-            go(v1);
-            go(v2);
+        while(t-- > 0){
+            diffusion();
+            move();
         }
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (a[i][j] != -1) ret += a[i][j];
+        int ret = 0;
+        for(int i = 0; i < r; i++){
+            for(int j = 0; j < c; j++){
+                if(notDust(i, j)) continue;
+                ret += board[i][j];
             }
         }
-
-        bw.write(String.valueOf(ret)+'\n');
-        bw.flush();
-        bw.close();
-        br.close();
+        out.println(ret);
+        out.flush();
+        out.close();
     }
 }
-/*
-1. 미세먼지 확산
-  1-1. 확산할 미세먼지의 양을 임시배열에 저장한다.
-  1-2. 확산할 미세먼지의 양만큼 미세먼지를 빼준다.
-  1-3. 임시배열에 저장된 먼지양을 더해준다.
-2. 공청기 작동
-  2-1. 공청기의 이동할 좌표를 list에 저장한다.
-  2-2. t번 만큼 공청기의 이동방향으로 먼지를 이동시켜준다.
-*/
