0cc234e9f6a4d8da696b981af2f088ec468010fc
noah <104671487+gratisreise@users.noreply.github.com>
2026-03-30 08:15:54 +0900

    [Silver III] Title: 수강신청, Time: 968 ms, Memory: 64732 KB -BaekjoonHub

--- 백준/Silver/13414. 수강신청/수강신청.java
+++ 백준/Silver/13414. 수강신청/수강신청.java
@@ -6,37 +6,22 @@ public class Main {
         var in = new BufferedReader(new InputStreamReader(System.in));
         var out = new PrintWriter(System.out);
 
-        var st = new StringTokenizer(in.readLine());
-        // K: 수강 가능 인원, L: 대기 목록 길이 (클릭 횟수)
+        var st  = new StringTokenizer(in.readLine());
         int k = Integer.parseInt(st.nextToken());
         int l = Integer.parseInt(st.nextToken());
-
-        // LinkedHashSet: 삽입 순서를 유지하며 중복을 허용하지 않음
-        // 학번은 8자리 문자열이므로 String으로 처리
-        LinkedHashSet<String> waitingList = new LinkedHashSet<>();
-
+        Map<String, Integer> mp = new HashMap<>();
         for(int i = 0; i < l; i++){
-            // 학번을 String으로 입력받음
-            String studentId = in.readLine();
-
-            // 이미 대기열에 있으면: 기존 요소를 삭제하고 (맨 앞에서 밀려남)
-            if(waitingList.contains(studentId)){
-                waitingList.remove(studentId);
-            }
-
-            // 요소를 맨 뒤에 추가 (대기열 맨 뒤로 밀려남)
-            waitingList.add(studentId);
+            String num = in.readLine();
+            mp.put(num, i);
         }
+        var pq = new PriorityQueue<String>((a, b) -> mp.get(a) - mp.get(b));
+        pq.addAll(mp.keySet());
+        int limit = Math.min(k, pq.size());
+        for(int i = 0; i < limit; i++) out.println(pq.poll());
+        
 
-        // 최종 대기열을 List로 변환하여 K명까지만 출력
-        int count = 0;
-        for(String studentId : waitingList){
-            if(count >= k){
-                break;
-            }
-            out.println(studentId);
-            count++;
-        }
+
+
 
         out.flush();
         out.close();
