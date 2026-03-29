d6f3626d343d8076065969d236d839059b0f0898
noah <104671487+gratisreise@users.noreply.github.com>
2026-03-29 11:39:58 +0900

    [Silver III] Title: 팰린드롬 만들기, Time: 100 ms, Memory: 14264 KB -BaekjoonHub

--- 백준/Silver/1213. 팰린드롬 만들기/팰린드롬 만들기.java
+++ 백준/Silver/1213. 팰린드롬 만들기/팰린드롬 만들기.java
@@ -6,38 +6,32 @@ public class Main {
         var in = new BufferedReader(new InputStreamReader(System.in));
         var out = new PrintWriter(System.out);
 
+        int[] cnt = new int[300];
         String s = in.readLine();
-        int[] cnt = new int[26];
         for(char c : s.toCharArray()){
-            cnt[c - 'A']++;
+            cnt[c]++;
         }
-        int cnt1 = 0;
-        String mid = "";
-
-        for(int i = 0; i < 26; i++){
-            if(cnt[i] % 2 == 1){
-                cnt1++; mid = ""+ (char)(i + 'A');
-                cnt[i]--;
-            }
-        }
-        if(cnt1 > 1) {
-            out.println("I'm Sorry Hansoo\n");
-            out.flush();
-            out.close();
-            return;
-        }
-
-        var ret = new StringBuilder();
+        int cut = 0, mid = -1;
+        var pre = new StringBuilder();
+        var suf = new StringBuilder();
         for(int i = 'Z'; i >= 'A'; i--){
-            for(int j = 0; j < cnt[i - 'A']; j += 2){
-                ret.append((char)i);
-                ret= new StringBuilder(""+(char)i).append(ret);
+            if(cnt[i] % 2 == 1){
+                cut++; cnt[i]--; mid = i;
+            }
+            for(int j = 0; j < cnt[i]; j += 2){
+                pre.append((char)i);
+                suf.append((char)i);
             }
         }
-        if(cnt1 > 0){
-            ret.insert(ret.length()/2, mid);
+
+        if(cut > 1) out.println("I'm Sorry Hansoo");
+        else{
+            var ret = new StringBuilder();
+            ret.append(pre.reverse());
+            if(cut == 1) ret.append((char)mid);
+            ret.append(suf);
+            out.println(ret);
         }
-        System.out.println(ret);
 
         out.flush();
         out.close();
