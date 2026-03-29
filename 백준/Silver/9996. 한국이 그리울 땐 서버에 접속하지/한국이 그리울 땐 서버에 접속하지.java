dcf8a15ec49544374de522e3b950f1dc4c528bc6
noah <104671487+gratisreise@users.noreply.github.com>
2026-03-29 11:42:04 +0900

    [Silver III] Title: 한국이 그리울 땐 서버에 접속하지, Time: 100 ms, Memory: 14340 KB -BaekjoonHub

--- 백준/Silver/9996. 한국이 그리울 땐 서버에 접속하지/한국이 그리울 땐 서버에 접속하지.java
+++ 백준/Silver/9996. 한국이 그리울 땐 서버에 접속하지/한국이 그리울 땐 서버에 접속하지.java
@@ -1,41 +1,32 @@
 import java.io.*;
 import java.util.*;
 
-public class Main{
-    public static void main(String args[]) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+public class Main {
+    public static void main(String[] args) throws IOException {
+        var in = new BufferedReader(new InputStreamReader(System.in));
+        var out = new PrintWriter(System.out);
 
-        int n = Integer.parseInt(br.readLine());
-        String s = br.readLine();
-
-        int pos = s.indexOf('*');
-        String preS = s.substring(0, pos);
-        String sufS = s.substring(pos + 1);
+        int n = Integer.parseInt(in.readLine());
+        String s = in.readLine();
+        int pos = s.indexOf("*");
+        String prefix = s.substring(0, pos);
+        String suffix = s.substring(pos + 1);
 
         for(int i = 0; i < n; i++){
-            s = br.readLine();
-            int pre = preS.length();
-            int suf = sufS.length();
-            if(s.length() < pre + suf) bw.write("NE\n");
-            else {
-                String temp1 = s.substring(0, pre);
-                String temp2 = s.substring(s.length() - suf);
-                if(temp1.equals(preS) && temp2.equals(sufS)){
-                    bw.write("DA\n");
-                } else bw.write("NE\n");
+            String now = in.readLine();
+            if(now.length() < prefix.length() + suffix.length()){
+                out.println("NE");
+                continue;
+            }
+            if(now.startsWith(prefix) && now.endsWith(suffix)) {
+                out.println("DA");
+            } else{
+                out.println("NE");
             }
         }
 
-        bw.flush();
+
+        out.flush();
+        out.close();
     }
 }
-/*
-1. 파일의 갯수 n 입력 받기
-2. 패턴 입력받고 앞 뒤 패턴 문자열에 저장
-3. n번만큼 문자열 입력받기
-    3-1. 문자열 길이가 앞 + 뒤 길이보다 작을때 NE
-    3-2. 문자열 길이가 앞 + 뒤 길이보다 길때
-        3-2-1. 패턴맞으면 DA
-        3-2-2. 안맞으면 NE
- */
