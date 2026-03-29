adb68089fed294e33da8996edcec39c45fa9d8b5
noah <104671487+gratisreise@users.noreply.github.com>
2026-03-29 11:13:11 +0900

    [Silver III] Title: 수열, Time: 248 ms, Memory: 24312 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.java
+++ 백준/Silver/2559. 수열/수열.java
@@ -5,24 +5,30 @@ public class Main {
     public static void main(String[] args) throws IOException {
         var in = new BufferedReader(new InputStreamReader(System.in));
         var out = new PrintWriter(System.out);
+        StringTokenizer st;
 
-        var st = new StringTokenizer(in.readLine());
+        st = new StringTokenizer(in.readLine());
         int n = Integer.parseInt(st.nextToken());
         int k = Integer.parseInt(st.nextToken());
 
+        int[] nums = new int[n + 1];
         st = new StringTokenizer(in.readLine());
-        int[] psum = new int[n+1];
-
-        for(int i = 1;  i <= n; i++){
-            int num = Integer.parseInt(st.nextToken());
-            psum[i] = psum[i-1] + num;
+        for(int i = 0; i < n; i++){
+            nums[i] = Integer.parseInt(st.nextToken());
         }
-        int mx = Integer.MIN_VALUE;
-        for(int i = k; i <= n; i++){
-            mx = Math.max(mx, psum[i] - psum[i-k]);
-        }
-        System.out.println(mx);
 
+
+        int sum = 0;
+        for(int i = 0; i < k; i++){
+            sum += nums[i];
+        }
+        int ret = sum;
+
+        for(int i = k; i < n; i++){
+            sum = sum - nums[i - k] + nums[i];
+            ret = Math.max(ret, sum);
+        }
+        out.println(ret);
 
         out.flush();
         out.close();
