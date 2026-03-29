793bbf2d6f6c1d17b67472c40ec2cdbeca983e61
noah <104671487+gratisreise@users.noreply.github.com>
2026-03-29 10:39:16 +0900

    [Bronze II] Title: 트럭 주차, Time: 108 ms, Memory: 14172 KB -BaekjoonHub

--- 백준/Bronze/2979. 트럭 주차/트럭 주차.java
+++ 백준/Bronze/2979. 트럭 주차/트럭 주차.java
@@ -1,23 +1,31 @@
+import java.io.*;
 import java.util.*;
 
-public class Main{
-    static int[] cnt = new int[104];
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        int A = sc.nextInt();
-        int B = sc.nextInt();
-        int C = sc.nextInt();
-        for(int i = 0 ; i < 3; i++){
-            int a = sc.nextInt();
-            int b = sc.nextInt();
+public class Main {
+    public static void main(String[] args) throws IOException {
+        var in = new BufferedReader(new InputStreamReader(System.in));
+        var out = new PrintWriter(System.out);
+
+        int[] cnt = new int[104];
+        String[] nums = in.readLine().split(" ");
+        int A = Integer.parseInt(nums[0]);
+        int B = Integer.parseInt(nums[1]);
+        int C = Integer.parseInt(nums[2]);
+
+        for(int i = 0; i < 3; i++){
+            String[] times = in.readLine().split(" ");
+            int a = Integer.parseInt(times[0]);
+            int b = Integer.parseInt(times[1]);
             for(int j = a; j < b; j++) cnt[j]++;
         }
         int ret = 0;
-        for(int i = 1; i <= 100; i++){
+        for(int i = 1; i < 100; i++){
             if(cnt[i] == 1) ret += A;
             else if(cnt[i] == 2) ret += B * 2;
             else if(cnt[i] == 3) ret += C * 3;
         }
-        System.out.println(ret);
+        out.println(ret);
+        out.flush();
+        out.close();
     }
 }
