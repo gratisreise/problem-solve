3e9e5034f7fe4993964442604376ae45e63870a8
noah <104671487+gratisreise@users.noreply.github.com>
2026-03-30 09:03:27 +0900

    [Silver II] Title: 싸이버개강총회, Time: 612 ms, Memory: 72212 KB -BaekjoonHub

--- 백준/Silver/19583. 싸이버개강총회/싸이버개강총회.java
+++ 백준/Silver/19583. 싸이버개강총회/싸이버개강총회.java
@@ -2,51 +2,51 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int hourToMinute(String time){
+    static int convert(String time){
         String[] times = time.split(":");
-        int hour  = Integer.parseInt(times[0]) * 60;
-        int minute = Integer.parseInt(times[1]);
-        return hour + minute;
+        int t = Integer.parseInt(times[0]) * 60;
+        int m = Integer.parseInt(times[1]);
+        return t + m;
     }
     public static void main(String[] args) throws IOException {
         var in = new BufferedReader(new InputStreamReader(System.in));
         var out = new PrintWriter(System.out);
 
-
         var st = new StringTokenizer(in.readLine());
-        int s = hourToMinute(st.nextToken());
-        int e = hourToMinute(st.nextToken());
-        int q = hourToMinute(st.nextToken());
-        Map<String, Set<Integer>> mp = new HashMap<>();//출석확인
-
+        int S = convert(st.nextToken());
+        int E = convert(st.nextToken());
+        int Q = convert(st.nextToken());
+        Map<String, int[]> mp = new HashMap<>();
         String line;
-        while((line = in.readLine()) != null){
-            String[] words = line.split(" ");
-            String name = words[1];
-            int time = hourToMinute(words[0]);
-            if(!mp.containsKey(name)){
-                mp.put(name, new HashSet<>());;
-            }
-            if(time <= s){
-                Set<Integer> st1 = mp.get(name);
-                st1.add(-1);
-                mp.put(name, st1);
-            } else if(time >= e && time <= q){
-                Set<Integer> st1 = mp.get(name);
-                st1.add(1);
-                mp.put(name, st1);
+        while((line = in.readLine()) != null){ // 10만
+            if(line.isEmpty()) break;
+            st = new StringTokenizer(line);
+            int time = convert(st.nextToken());
+            String name = st.nextToken();
+            if(!mp.containsKey(name)) mp.put(name, new int[]{0, 0});
+            if(time >= 0 && time <= S){
+                mp.get(name)[0] = 1;
+            } else if(time >= E && time <= Q){
+                mp.get(name)[1] = 1;
             }
         }
         int ret = 0;
-        for(String name : mp.keySet()){
-            if(mp.get(name).size() == 2){
-                ret++;
-            }
+        for(var entry : mp.entrySet()){
+            int[] arr = entry.getValue();
+            if(arr[0] == 1 && arr[1] == 1) ret++;
         }
-
         out.println(ret);
 
         out.flush();
         out.close();
     }
 }
+/*
+개강총회 시작전: 입장여부확인 => 시작전 대화이름으로 확인, 시작시간==채팅시간 인정
+총회끝 - 스트리밍끝: 퇴장여부확인 => 끝시간자체도인정, 00:00부터는 다음 걸로 간주
+입장부터 퇴장까지 모두 확인된 학회원 입장 확인 퇴장확인 된넘들
+숫자변환메서드, 이걸로 대소비교
+입장은 +1 퇴장은 -1
+돌면서 0인놈들만 세주기
+map 이름 -> 숫자 ,
+*/
