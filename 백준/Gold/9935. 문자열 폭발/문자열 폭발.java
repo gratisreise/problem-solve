e9c6b5590f8ffbf4c7a93eb9f194f87eecf1f7b8
noah <104671487+gratisreise@users.noreply.github.com>
2026-03-31 11:56:51 +0900

    [Gold IV] Title: 문자열 폭발, Time: 324 ms, Memory: 25452 KB -BaekjoonHub

--- 백준/Gold/9935. 문자열 폭발/문자열 폭발.java
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.java
@@ -1,23 +1,50 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String s = sc.next();
-        String b = sc.next();
-        StringBuilder ret = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        var in = new BufferedReader(new InputStreamReader(System.in));
+        var out = new PrintWriter(System.out);
 
-        for (char a : s.toCharArray()) {
-            ret.append(a);
-            if (ret.length() >= b.length() && ret.substring(ret.length() - b.length()).equals(b)) {
-                ret.delete(ret.length() - b.length(), ret.length());
+        String str = in.readLine();
+        String bomb = in.readLine();
+        int strLen = str.length();
+        int bombLen = bomb.length();
+
+        // char 배열을 스택처럼 사용 (손가락 보호 + 성능 최강)
+        char[] result = new char[strLen];
+        int size = 0;
+
+        for (int i = 0; i < strLen; i++) {
+            result[size++] = str.charAt(i); // 일단 넣는다!
+
+            // 스택에 쌓인 글자가 폭발 문자열보다 길면 검사 시작
+            if (size >= bombLen) {
+                boolean isBomb = true;
+                for (int j = 0; j < bombLen; j++) {
+                    // 뒤에서부터 비교
+                    if (result[size - bombLen + j] != bomb.charAt(j)) {
+                        isBomb = false;
+                        break;
+                    }
+                }
+
+                if (isBomb) {
+                    size -= bombLen; // 폭발! 포인터만 뒤로 밀면 끝 (O(1))
+                }
             }
         }
 
-        if (ret.length() > 0) {
-            System.out.println(ret);
+        if (size == 0) {
+            out.println("FRULA");
         } else {
-            System.out.println("FRULA");
+            // 남은 만큼만 빌더에 담기
+            var sb = new StringBuilder();
+            for (int i = 0; i < size; i++) sb.append(result[i]);
+            out.println(sb);
         }
+
+        out.flush();
+        out.close();
     }
 }
