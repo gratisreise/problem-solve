888908bb1cbf08d9c6245c25fbefbcfd7682391e
noah <104671487+gratisreise@users.noreply.github.com>
2026-03-31 09:37:41 +0900

    [Silver I] Title: 미로 탐색, Time: 112 ms, Memory: 14860 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -2,51 +2,50 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int n, m;
-    static int[][] a, visited;
-    static int[] dy = {-1, 0, 1, 0};
-    static int[] dx = {0, 1, 0, -1};
-    static class Pair {
-        int y, x;
-        Pair(int y, int x) {
+    static class Pair{
+        int y; int x;
+        Pair(int y, int x){
             this.y = y;
             this.x = x;
         }
     }
-    public static void main(String args[]) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static void main(String[] args) throws IOException {
+        var in = new BufferedReader(new InputStreamReader(System.in));
+        var out = new PrintWriter(System.out);
 
-        //입력
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
-        a = new int[n + 1][m + 1];
-        visited = new int[n + 1][m + 1];
-        for (int i = 0; i < n; i++) {
-            char[] c = br.readLine().toCharArray();
-            for (int j = 0; j < m; j++) {
-                a[i][j] = c[j] - '0';
+        var st = new StringTokenizer(in.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int[][] board = new int[n][m];
+        int[][] visited = new int[n][m];
+        for(int i = 0; i < n; i++){
+            char[] line = in.readLine().toCharArray();
+            for(int j = 0; j < m; j++){
+                board[i][j] = line[j] - '0';
             }
         }
-        //bfs 최단거리 방문배열
-        Queue<Pair> q = new LinkedList<>();
-        q.add(new Pair(0, 0));
         visited[0][0] = 1;
-        while (!q.isEmpty()) {
+        Queue<Pair> q = new LinkedList<>();
+        q.offer(new Pair(0, 0));
+        int[] dy = {-1, 0, 1, 0}, dx = {0, 1, 0, -1};
+        while(!q.isEmpty()){
             Pair p = q.poll();
             int y = p.y;
             int x = p.x;
-            for (int i = 0; i < 4; i++) {
+            for(int i = 0; i < 4; i++){
                 int ny = y + dy[i];
                 int nx = x + dx[i];
-                if (ny < 0 || nx < 0 || ny >= n || nx >= m) continue;
-                if (a[ny][nx] == 0 || visited[ny][nx] != 0) continue;
+                if(ny < 0 || nx < 0 || ny >= n || nx >= m) continue;
+                if(visited[ny][nx] != 0 || board[ny][nx] == 0) continue;
+                q.offer(new Pair(ny, nx));
                 visited[ny][nx] = visited[y][x] + 1;
-                q.add(new Pair(ny, nx));
             }
         }
-        bw.write(String.valueOf(visited[n - 1][m - 1]) + '\n');
-        bw.flush();
+        out.println(visited[n-1][m-1]);
+
+
+        out.flush();
+        out.close();
     }
 }
