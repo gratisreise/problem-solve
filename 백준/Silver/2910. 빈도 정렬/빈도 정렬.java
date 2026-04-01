a6ecc38f6b473ded4f24371f7fabf46b25af5472
noah <104671487+gratisreise@users.noreply.github.com>
2026-04-01 10:01:10 +0900

    [Silver III] Title: 빈도 정렬, Time: 144 ms, Memory: 14944 KB -BaekjoonHub

--- 백준/Silver/2910. 빈도 정렬/빈도 정렬.java
+++ 백준/Silver/2910. 빈도 정렬/빈도 정렬.java
@@ -1,43 +1,36 @@
+import java.io.*;
 import java.util.*;
-public class Main{
-    static class Pair{
-        int f, s;
-        Pair(int f, int s){
-            this.f = f;
-            this.s = s;
-        }
-    }
 
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        Map<Integer, Integer> mp1 = new HashMap<>(); //빈도
-        Map<Integer, Integer> mp2 = new HashMap<>();//순서
-        List<Pair> l = new ArrayList<>();
-        int n = sc.nextInt();
-        int c = sc.nextInt();
+public class Main {
+    public static void main(String[] args) throws IOException {
+        var in = new BufferedReader(new InputStreamReader(System.in));
+        var out = new PrintWriter(System.out);
+
+        var st = new StringTokenizer(in.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+
+        Map<Integer, Integer> mp1 = new HashMap<>(); // 빈도
+        Map<Integer, Integer> mp2 = new HashMap<>(); // 등장
+        st = new StringTokenizer(in.readLine());
         for(int i = 0; i < n; i++){
-            int temp = sc.nextInt();
-             mp1.put(temp, mp1.getOrDefault(temp, 0) + 1);
-             if(!mp2.containsKey(temp)){
-                 mp2.put(temp, i + 1);
-             }
+            int now = Integer.parseInt(st.nextToken());
+            mp1.put(now, mp1.getOrDefault(now, 0) + 1);
+            if(!mp2.containsKey(now)) mp2.put(now, i);
         }
-        for(Map.Entry<Integer, Integer> e : mp1.entrySet()){
-            l.add(new Pair(e.getValue(), e.getKey()));
-        }
-        Collections.sort(l, new Comparator<Pair>(){
-            public int compare(Pair a, Pair b){
-                if(a.f == b.f){
-                    return mp2.get(a.s) - mp2.get(b.s); // 순서 오름차순
-                }
-                return b.f - a.f; //빈도 내림차순
-            }
+        var nums = new ArrayList<>(mp1.keySet());
+        nums.sort((a, b) -> {
+            if(mp1.get(a) == mp1.get(b)) return mp2.get(a) - mp2.get(b);
+            return mp1.get(b) - mp1.get(a);
         });
-
-        for(Pair p : l){
-            for(int j = 0; j < p.f; j++){
-                System.out.print(p.s + " ");
+        for(int num : nums){
+            for(int i = 0; i < mp1.get(num); i++){
+                out.print(num + " ");
             }
         }
+
+
+        out.flush();
+        out.close();
     }
 }
