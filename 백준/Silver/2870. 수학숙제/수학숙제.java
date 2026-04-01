eb3766f24ebc46042382b135519a101aad654c18
noah <104671487+gratisreise@users.noreply.github.com>
2026-04-02 08:15:53 +0900

    [Silver IV] Title: 수학숙제, Time: 120 ms, Memory: 14412 KB -BaekjoonHub

--- 백준/Silver/2870. 수학숙제/수학숙제.java
+++ 백준/Silver/2870. 수학숙제/수학숙제.java
@@ -1,57 +1,49 @@
 import java.io.*;
 import java.util.*;
 
-public class Main{
-    static PriorityQueue<String> ret = new PriorityQueue<>(
-            (a1, a2) -> {
-                if(a1.length() == a2.length()){
-                    return a1.compareTo(a2);
-                }
-                return Integer.compare(a1.length(), a2.length());
-            }
-    );
-
-    static void deleteZero(StringBuilder sb){
-        while(sb.length()> 1 && sb.charAt(0) == '0'){
-            sb.deleteCharAt(0);
-        }
-        ret.add(sb.toString());
+public class Main {
+    static boolean isNumber(char c){
+        return c >= '0' && c <= '9';
     }
-    public static void main(String args[]) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st;
-        st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        while(n-- > 0){
-            String s = br.readLine();
-            StringBuilder temp = new StringBuilder("");
-            for(char c : s.toCharArray()){
-                if('0' <= c && c <= '9') temp.append(c);
-                else if(temp.length() > 0) {
-                    deleteZero(temp);
-                    temp = new StringBuilder("");
-                }
-            }
-            if(temp.length() > 0) deleteZero(temp);
+    static String cut(String s){
+        int idx = 0;
+        while(idx < s.length() && s.charAt(idx) == '0'){
+            idx++;
         }
+        return idx == s.length() ? "0" : s.substring(idx);
+    }
+    public static void main(String[] args) throws IOException {
+        var in = new BufferedReader(new InputStreamReader(System.in));
+        var out = new PrintWriter(System.out);
 
-        while(!ret.isEmpty()) bw.write(ret.poll() + '\n');
-        bw.flush();
+        int n = Integer.parseInt(in.readLine());
+        List<String> l = new ArrayList<>();
+        for(int i = 0; i < n; i++){
+            String s = in.readLine();
+            int len = s.length();
+            for(int j = 0; j < len; j++) {
+                int temp = j;
+                var sb = new StringBuilder();
+                while (temp < len && isNumber(s.charAt(temp))) {
+                    sb.append(s.charAt(temp++));
+                }
+                j = temp;
+                if(sb.length() > 0) l.add(sb.toString());
+            }
+        }
+        var ret = new ArrayList<String>();
+        for(String s : l){
+            if(s.charAt(0) == '0') ret.add(cut(s));
+            else ret.add(s);
+        }
+        ret.sort((a, b) -> {
+            if(a.length() == b.length()) return a.compareTo(b);
+            return a.length() - b.length();
+        });
+        for(String s : ret) out.println(s);
+
+
+        out.flush();
+        out.close();
     }
 }
-/*
-숫자 + 소문자 -> 숫자 찾아서 오름차순 정렬 숫자앞 0은 생략해줌
-100글자 -> int, long 불가 -> 문자열로 저장하고 대소비교
-앞의 0이있으면
-
-1.테스트케이스의 갯수 n 입력받기
-2. 문자열을 입력받는다.
-3. 문자열을 순회한다.
-    3-1. 숫자이면 temp문자열에 저장
-    3-2. 숫자가 아닐때
-        3-2-1. temp가 있으면 0제거 메서드 실행하고 리스트 저장
-        3-2-2. temp가 없으면 다음으로
-4. 문자열 순회 완료 후 temp 있으면 0제거 메서드 실행하고 리스트 저장
-5. 리스트 문자열기준 오름차순 정렬 compareTo메서드 활용
- */
