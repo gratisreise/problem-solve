d5d1234adf63f238689cdc0213deb1152d57ea61
noah <104671487+gratisreise@users.noreply.github.com>
2026-04-02 09:00:40 +0900

    [Silver V] Title: 영화감독 숌, Time: 280 ms, Memory: 86100 KB -BaekjoonHub

--- 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
@@ -2,15 +2,21 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String args[]) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st;
-        st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
+    public static void main(String[] args) throws IOException {
+        var in = new BufferedReader(new InputStreamReader(System.in));
+        var out = new PrintWriter(System.out);
+
+        
+        int n = Integer.parseInt(in.readLine());
+        int cnt = 0;
         int i = 666;
-        for (; n != 0; i++) if (("" + i).contains("666")) n--;
-        bw.write(String.valueOf(i-1) + '\n');
-        bw.flush();
+        for(; ; i++){
+            if((""+i).contains("666")) cnt++;
+            if(cnt == n) break;
+        }
+        out.println(i);
+
+        out.flush();
+        out.close();
     }
 }
