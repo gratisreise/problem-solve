7ec664cb4bb0c5195d3591869663d7f796d92f73
noah <104671487+gratisreise@users.noreply.github.com>
2026-04-02 09:18:09 +0900

    [Silver IV] Title: 괄호, Time: 112 ms, Memory: 14328 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.java
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -1,37 +1,26 @@
 import java.io.*;
 import java.util.*;
 
-public class Main{
-    public static void main(String args[]) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st;
-        st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        while(n-- > 0){
-            Stack<Character> stk = new Stack<>();
-            String s = br.readLine();
+public class Main {
+    public static void main(String[] args) throws IOException {
+        var in = new BufferedReader(new InputStreamReader(System.in));
+        var out = new PrintWriter(System.out);
+
+        int n = Integer.parseInt(in.readLine());
+        for(int i = 0; i < n; i++){
+            String s = in.readLine();
+            var stk = new ArrayDeque<Character>();
             for(char c : s.toCharArray()){
-                if(!stk.isEmpty() && stk.peek() == '(' && c == ')') stk.pop();
-                else stk.push(c);
+                if(!stk.isEmpty() && stk.peek() == '(' && c == ')'){
+                    stk.pollLast();
+                } else stk.addLast(c);
             }
-            if(stk.isEmpty()) bw.write("YES\n");
-            else bw.write("NO\n");
-            bw.flush();
+            if(stk.isEmpty()) out.println("YES");
+            else out.println("NO");
         }
 
+
+        out.flush();
+        out.close();
     }
 }
-/*
-괄호짝 맞는지 체크하는 문제
-1. n 입력받기
-2. 문자열 입력받기
-3. 문자열 순회
-    3-1. stack이 비어있으면
-        3-1-1. 문자 넣기
-    3-2. stack이 안 비어있으면
-        3-2-1. peek()==( 이고 c == ')' 이면 pop()
-4. stack이 체크
-    4-1. 비어있으면 YES
-    4-2. 안 비어있으면 NO
-*/
