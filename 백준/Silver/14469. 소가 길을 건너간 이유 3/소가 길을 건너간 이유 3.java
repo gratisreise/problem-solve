f3ee002c13b3acd07986bb66459484eeb28229cc
noah <104671487+gratisreise@users.noreply.github.com>
2026-04-03 09:22:05 +0900

    [Silver IV] Title: 소가 길을 건너간 이유 3, Time: 112 ms, Memory: 14192 KB -BaekjoonHub

--- 백준/Silver/14469. 소가 길을 건너간 이유 3/소가 길을 건너간 이유 3.java
+++ 백준/Silver/14469. 소가 길을 건너간 이유 3/소가 길을 건너간 이유 3.java
@@ -1,46 +1,31 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        ArrayList<Pair> v = new ArrayList<>();
+    public static void main(String[] args) throws IOException {
+        var in = new BufferedReader(new InputStreamReader(System.in));
+        var out = new PrintWriter(System.out);
 
-        for (int i = 0; i < n; i++) {
-            int a = scanner.nextInt();
-            int b = scanner.nextInt();
-            v.add(new Pair(a, b));
+        int n = Integer.parseInt(in.readLine());
+        var l = new ArrayList<int[]>();
+        for(int i = 0; i < n; i++){
+            var st = new StringTokenizer(in.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            l.add(new int[]{a, b});
         }
-
-        Collections.sort(v);
-
-        int time = v.get(0).first + v.get(0).second;
-
-        for (int i = 1; i < n; i++) {
-            if (time > v.get(i).first) {
-                time += v.get(i).second;
-            } else {
-                time = v.get(i).first + v.get(i).second;
-            }
+        l.sort((a, b) -> a[0] - b[0]);
+        int now = 0;
+        for(int[] arr : l){
+            int arrive = arr[0];
+            int takes = arr[1];
+            if(arrive > now) now = arrive;
+            now += takes;
         }
+        out.println(now);
 
-        System.out.println(time);
-    }
-}
 
-class Pair implements Comparable<Pair> {
-    int first;
-    int second;
-
-    Pair(int first, int second) {
-        this.first = first;
-        this.second = second;
-    }
-
-    @Override
-    public int compareTo(Pair other) {
-        return Integer.compare(this.first, other.first);
+        out.flush();
+        out.close();
     }
 }
