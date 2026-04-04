87a839b28fac9b1ab910a31a7a8ca7a6d0387541
noah <104671487+gratisreise@users.noreply.github.com>
2026-04-04 10:23:09 +0900

    [Silver II] Title: 파닭파닭, Time: 592 ms, Memory: 76296 KB -BaekjoonHub

--- 백준/Silver/14627. 파닭파닭/파닭파닭.java
+++ 백준/Silver/14627. 파닭파닭/파닭파닭.java
@@ -1,39 +1,46 @@
+import java.io.*;
 import java.util.*;
 
 public class Main {
-    static long s, c, sum, ret;
-    static long[] a;
-
-    static boolean check(long mid) {
-        long cnt = 0;
-        for (int i = 0; i < s; i++) {
-            cnt += a[i] / mid;
+    static int s, c;
+    static int[] arr;
+    static boolean check(long mid){
+        int cnt = 0;
+        for(int i = 0; i < s; i++){
+            if(arr[i] < mid) continue;
+            cnt += arr[i] / mid;
         }
         return cnt >= c;
     }
+    public static void main(String[] args) throws IOException {
+        var in = new BufferedReader(new InputStreamReader(System.in));
+        var out = new PrintWriter(System.out);
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        s = sc.nextLong();
-        c = sc.nextLong();
-        a = new long[(int) s];
-        sum = 0;
-        for (int i = 0; i < s; i++) {
-            a[i] = sc.nextLong();
-            sum += a[i];
+        var st = new StringTokenizer(in.readLine());
+        s = Integer.parseInt(st.nextToken());
+        c = Integer.parseInt(st.nextToken());
+        arr = new int[s];
+        long l = 1, r = -1;
+        long sum =0;
+        for(int i = 0; i < s; i++) {
+            arr[i] = Integer.parseInt(in.readLine());
+            r = Math.max(r, arr[i]);
+            sum += arr[i];
         }
-
-        long lo = 1, hi = (long) 1e9;
-        while (lo <= hi) {
-            long mid = (lo + hi) / 2;
-            if (check(mid)) {
-                lo = mid + 1;
-                ret = mid;
-            } else {
-                hi = mid - 1;
-            }
+        long mid = 0;
+        long temp = 0;
+        while(l <= r){
+            mid = (l + r) / 2;
+            if(check(mid)){
+                l = mid + 1;
+                temp = mid;
+            } else r = mid - 1;
         }
+        sum -= c * temp;
+        out.println(sum);
 
-        System.out.println(sum - ret * c);
+
+        out.flush();
+        out.close();
     }
 }
