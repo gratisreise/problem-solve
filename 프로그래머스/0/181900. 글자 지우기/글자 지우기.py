ad1ec3682e85b9c383359ea0850d29054fce91c8
noah <104671487+gratisreise@users.noreply.github.com>
2026-05-18 08:33:03 +0900

    [level 0] Title: 글자 지우기, Time: 0.05 ms, Memory: 9.13 MB -BaekjoonHub

--- 프로그래머스/0/181900. 글자 지우기/글자 지우기.py
+++ 프로그래머스/0/181900. 글자 지우기/글자 지우기.py
@@ -1,2 +1,2 @@
-def solution(my_string, indices): return ''.join(c for i, c in enumerate(my_string) if not indices.count(i))
-        
+def solution(my_string, indices):
+    return ''.join(c for i,c in enumerate(my_string) if i not in indices)
