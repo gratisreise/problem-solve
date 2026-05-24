f13fa74cd11c2b84474941b147852cdd1ed30f9c
noah <104671487+gratisreise@users.noreply.github.com>
2026-05-24 13:31:23 +0900

    [level 0] Title: 소인수분해, Time: 1.49 ms, Memory: 9.3 MB -BaekjoonHub

--- 프로그래머스/0/120852. 소인수분해/README.md
+++ 프로그래머스/0/120852. 소인수분해/README.md
@@ -1,10 +1,10 @@
 # [level 0] 소인수분해 - 120852 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120852) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120852?language=python3) 
 
 ### 성능 요약
 
-메모리: 10.4 MB, 시간: 5.58 ms
+메모리: 9.3 MB, 시간: 1.49 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 21일 14:53:54
+2026년 05월 24일 13:31:19
 
 ### 문제 설명
 
--- 프로그래머스/0/120852. 소인수분해/소인수분해.py
+++ 프로그래머스/0/120852. 소인수분해/소인수분해.py
@@ -1,21 +1,13 @@
-def solution(n): 
-    che = [0 for _ in range(n + 1)]
+def solution(n):
+    isprime = [True] * (n+1)
+    
     for i in range(2, n+1):
-        if che[i]: continue
+        if not isprime[i]: continue
         for j in range(i*2, n+1, i):
-            if che[j]: continue
-            che[j] = 1
-    prime = [i for i in range(2, n+1) if not che[i]]
-    ret = []
-    for i in prime:
-        while n % i == 0: 
-            ret.append(i)
-            n = int(n/i)
-    ret = list(set(ret))
-    ret.sort()
-    return ret
+            isprime[j] = False
+
+    prime = [x for x in range(2, n+1) if isprime[x]]
+    return [x for x in prime if n % x == 0]
+        
+         
     
-    
-    
-    # 소수판단배열 
-    # 그 소수 배열을 돌면서 나누기 반복
