4d44c632d5a9768d7a234a6b83710ea65d1f14e8
noah <104671487+gratisreise@users.noreply.github.com>
2026-05-25 11:33:22 +0900

    [level 0] Title: 전국 대회 선발 고사, Time: 0.04 ms, Memory: 9.22 MB -BaekjoonHub

--- 프로그래머스/0/181851. 전국 대회 선발 고사/README.md
+++ 프로그래머스/0/181851. 전국 대회 선발 고사/README.md
@@ -1,10 +1,10 @@
 # [level 0] 전국 대회 선발 고사 - 181851 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181851) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181851?language=python3) 
 
 ### 성능 요약
 
-메모리: 10 MB, 시간: 0.03 ms
+메모리: 9.22 MB, 시간: 0.04 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 22일 23:33:50
+2026년 05월 25일 11:33:18
 
 ### 문제 설명
 
--- 프로그래머스/0/181851. 전국 대회 선발 고사/전국 대회 선발 고사.py
+++ 프로그래머스/0/181851. 전국 대회 선발 고사/전국 대회 선발 고사.py
@@ -1,6 +1,19 @@
 def solution(rank, attendance):
+    idxs = [x for x in range(len(rank))]
+    print(idxs)
+    idxs.sort(key=lambda x: rank[x])
     ret = []
-    for i, f in enumerate(attendance):
-        if f: ret.append(i)
-    ret = sorted(ret, key=lambda x: rank[x])
-    return 10000 * ret[0] + 100 * ret[1] + ret[2]
+    idx = 0
+    print(idxs)
+    while len(ret) < 3 and idx < len(idxs):
+        if attendance[idxs[idx]]:
+            ret.append(idxs[idx] )
+        idx += 1
+    a, b, c = ret
+    
+    return 10000 * a + 100 * b + c
+    
+"""
+참여가능 중 3명 선발
+idx를 숫자 높은 순으로 정렬해서 참석가능한 3명뽑고 거기서 계산하기
+"""
