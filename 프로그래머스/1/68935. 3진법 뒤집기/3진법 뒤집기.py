7626356e66589ff82ed3715fa9602a21b8da3eeb
noah <104671487+gratisreise@users.noreply.github.com>
2026-06-01 09:17:38 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.02 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.py
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.py
@@ -1,15 +1,27 @@
-from collections import deque
-def third(n):
-    ans = deque()
-    while n > 0:
-        ans.appendleft(str(n%3))
-        n //= 3
-    return ''.join(ans)
+def ten_to_3(num):
+    ret = ''
+    while num > 0:
+        ret += str(num % 3)
+        num //= 3
+    return ret[::-1]
+    
+    
+
+def three_to_10(num):
+    ret = 0
+    for i,x in enumerate(num):
+        ret += (3**i) * (ord(x) - ord('0'))
+    return ret
+
 
 def solution(n):
-    ret = 0
-    third_str = third(n)
-    for i in range(len(third_str)):
-        ret += int(third_str[i]) * (3 ** i)
+    ret = ten_to_3(n)
+    print(ret)
+    ret = three_to_10(ret)
     return ret
     
+"""
+1.3진법만들기
+2.뒤집기
+3.10진법변환
+"""
--- 프로그래머스/1/68935. 3진법 뒤집기/README.md
+++ 프로그래머스/1/68935. 3진법 뒤집기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 3진법 뒤집기 - 68935 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/68935) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/68935?language=python3) 
 
 ### 성능 요약
 
-메모리: 10.4 MB, 시간: 0.04 ms
+메모리: 11.5 MB, 시간: 0.02 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 11월 10일 09:25:43
+2026년 06월 01일 09:17:35
 
 ### 문제 설명
 
