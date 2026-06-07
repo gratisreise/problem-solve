4983736a9b8ae367b988a46fa752ea80343cbb48
noah <104671487+gratisreise@users.noreply.github.com>
2026-06-07 12:32:21 +0900

    [level 1] Title: 모의고사, Time: 1.15 ms, Memory: 11.6 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/README.md
+++ 프로그래머스/1/42840. 모의고사/README.md
@@ -1,10 +1,10 @@
 # [level 1] 모의고사 - 42840 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840?language=python3) 
 
 ### 성능 요약
 
-메모리: 10.3 MB, 시간: 4.65 ms
+메모리: 11.6 MB, 시간: 1.15 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 11월 26일 14:45:40
+2026년 06월 07일 12:32:17
 
 ### 문제 설명
 
--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -1,22 +1,17 @@
 def solution(answers):
-    stu1 = [1, 2, 3, 4, 5];
-    stu2 = [2, 1, 2, 3, 2, 4, 2, 5];
-    stu3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
+    p1= [1, 2, 3, 4,5]
+    p2 = [ 2, 1, 2, 3, 2, 4, 2, 5]
+    p3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
+    cnt = [0, 0, 0]
+    for i, x in enumerate(answers):
+        if x == p1[i%len(p1)]: cnt[0] += 1
+        if x == p2[i%len(p2)]: cnt[1] += 1
+        if x == p3[i%len(p3)]: cnt[2] += 1
+    mx = max(cnt)
+    return [i+1 for i,x in enumerate(cnt) if x == mx]
     
-    correct = [0, 0, 0];
     
-    for idx, answer in enumerate(answers):
-        if stu1[idx%len(stu1)] == answer:
-            correct[0] += 1
-        if stu2[idx%len(stu2)] == answer:
-            correct[1] += 1
-        if stu3[idx%len(stu3)] == answer:
-            correct[2] += 1
-    mx = max(correct)
-    
-    return [i+1 for i,n in enumerate(correct) if n == mx]
 """
-1. 정답 번호에 대해 다음을 반복
-    1-1. 수포자들의 패턴과 비교하여 카운팅
-2. 카운팅된 숫자의 최대값을 비교하여 숫자번호저장
+
+
 """
