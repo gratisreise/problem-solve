d721a6cde24494d3b60c556ff1d8beb6f85b650a
noah <104671487+gratisreise@users.noreply.github.com>
2026-06-07 14:00:52 +0900

    [level 1] Title: 실패율, Time: 2830.02 ms, Memory: 19.6 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/README.md
+++ 프로그래머스/1/42889. 실패율/README.md
@@ -1,10 +1,10 @@
 # [level 1] 실패율 - 42889 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42889) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42889?language=python3) 
 
 ### 성능 요약
 
-메모리: 11.5 MB, 시간: 28.43 ms
+메모리: 19.6 MB, 시간: 2830.02 ms
 
 ### 구분
 
@@ -16,13 +16,13 @@
 
 ### 제출 일자
 
-2024년 10월 29일 13:38:19
+2026년 06월 07일 14:00:48
 
 ### 문제 설명
 
 <h2>실패율</h2>
 
-<p><img src="https://grepp-programmers.s3.amazonaws.com/files/production/bde471d8ac/48ddf1cc-c4ea-499d-b431-9727ee799191.png" title="" alt="failture_rate1.png"></p>
+<p><img src="https://asset.programmers.co.kr/files/production/bde471d8ac/48ddf1cc-c4ea-499d-b431-9727ee799191.png" title="" alt="failture_rate1.png"></p>
 
 <p>슈퍼 게임 개발자 오렐리는 큰 고민에 빠졌다. 그녀가 만든 프랜즈 오천성이 대성공을 거뒀지만, 요즘 신규 사용자의 수가 급감한 것이다. 원인은 신규 사용자와 기존 사용자 사이에 스테이지 차이가 너무 큰 것이 문제였다.</p>
 
--- 프로그래머스/1/42889. 실패율/실패율.py
+++ 프로그래머스/1/42889. 실패율/실패율.py
@@ -1,19 +1,34 @@
-def solution(N, stages):
-    ret = [i for i in range(1, N+1)]
-    cur_stage = [0] * (N+2)
+def solution(n, stages):
+    clear = [0] * (n+2)
+    for num in stages:
+        for i in range(1, num+1):
+            clear[i] += 1
     
-    for i in stages: cur_stage[i] += 1
-    total_stage = [i for i in cur_stage]
-    for i in range(N, 0, -1):
-        total_stage[i] = cur_stage[i] + total_stage[i + 1]
-    defat = {x:cur_stage[x]/total_stage[x] if total_stage[x] else 0 for x in range(1, N+1)}
-    return sorted(ret, key = lambda x: defat[x], reverse=True)
-# 실패율이 높은 스테이지부터 내림차순으로 스테이지의 번호 1 ~ 500
-# 실패율 내림차 번호 오름차
-# 도달한 유저 0이면 0
+    failed_rate = {x:0 for x in range(1, n+1)}
+    for i in range(1, n+1):
+        if clear[i] == 0:
+            failed_rate[i] = 0
+        else:
+            failed_rate[i] = (clear[i] - clear[i+1]) / clear[i]
+    
+    return sorted(failed_rate.keys(), key = lambda x: (-failed_rate[x], x))
+    
+    
+"""
+실패율 = (도달o and 클리어x 플레이어수) / (도달플레이어수)
+N: 전체 스테이지수
+stages: 사용자가 현재멈춰있는 스테이지 수 배열
+n+1은 마지막까지 클리어
+실패율이 높은 순으로 스테이지 번호 담긴 배열 반환, 실패율 같으면 작은번호
+도달 유저수 0은 실패율0
 
-# 현재 멈춰있는 스테이지 번호가 담긴 배열을 입력받는다 
-# 마지막 스테이지부터 다음 스테이지 도전하는 인원을 더하여 스테이지에 도달한 플레이어 수를 구한다
-# 맵에 스테이지마다 실패율을 저장한다
-# 스테이지의 번호가 담긴 배열을 정렬한다.
+각 스테이지 마다 실패율을 구하기
+해당 배열을 기준으로 임의의 스테이지 번호 배열 정렬한 후 반환
+도달한 사람은 그냥 전부 더하기 
+도달o 클리x 플레이어 = 현재단계 - 다음단계 도달수 
 
+1. 도달 배열 구하기
+2. 도달 배열을 이용해 실패율 구하기
+3. 실패율 내림차, 숫자 오름차 정렬
+4. 반환
+"""
