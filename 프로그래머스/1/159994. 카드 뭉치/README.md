84697e977d3138b1f8fda868675d5471ad3d317b
noah <104671487+gratisreise@users.noreply.github.com>
2026-06-07 11:25:46 +0900

    [level 1] Title: 카드 뭉치, Time: 0.01 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/README.md
+++ 프로그래머스/1/159994. 카드 뭉치/README.md
@@ -1,10 +1,10 @@
 # [level 1] 카드 뭉치 - 159994 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/159994) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/159994?language=python3) 
 
 ### 성능 요약
 
-메모리: 10.3 MB, 시간: 0.01 ms
+메모리: 11.4 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 30일 15:11:40
+2026년 06월 07일 11:25:42
 
 ### 문제 설명
 
--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.py
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.py
@@ -1,18 +1,19 @@
 def solution(cards1, cards2, goal):
-    idx1 = 0
-    idx2 = 0
-    for c in goal:
-        flag = 0
-        if idx1 < len(cards1) and cards1[idx1] == c:
-            idx1 += 1
-            flag = 1
-        elif idx2 < len(cards2) and cards2[idx2] == c:
-            idx2 += 1
-            flag = 1
-        if not flag: return "No"
+    len1, len2, leng = len(cards1), len(cards2), len(goal)
+    i1, i2, ig = 0, 0, 0
+    while ig < leng:
+        if i1 < len1 and goal[ig] == cards1[i1]:
+            i1 += 1
+        elif i2 < len2 and goal[ig] == cards2[i2]:
+            i2 += 1
+        else:
+            return "No"
+        ig += 1
     return "Yes"
-        
-# 카드를 순서대로 한 장씩 사용
-# 재사용 불가
-# 스킵 불가
-# 순서 변경 불가
+
+"""
+카드 순서대로 한 장씩 사용, 재사용 불가, 
+1.둘의 요소 합친게 같은 갯수인지 확인
+2.만들 수 있는지는 어떻게 확인하지??
+
+"""
