6b153a1ce5bdb0e0729235fbb7a111459ee90b0e
noah <104671487+gratisreise@users.noreply.github.com>
2026-06-09 21:42:50 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 53.87 ms, Memory: 30.4 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/README.md
+++ 프로그래머스/1/42576. 완주하지 못한 선수/README.md
@@ -1,10 +1,10 @@
 # [level 1] 완주하지 못한 선수 - 42576 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576?language=python3) 
 
 ### 성능 요약
 
-메모리: 37.6 MB, 시간: 45.05 ms
+메모리: 30.4 MB, 시간: 53.87 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 11월 28일 09:58:50
+2026년 06월 09일 21:42:45
 
 ### 문제 설명
 
--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,18 +1,25 @@
 def solution(participant, completion):
-    value = 0
-    answer = {}
-    for part in participant:
-        answer[hash(part)] = part
-        value += hash(part)
-    for comp in completion:
-        value -= hash(comp)
-    return answer[value]
+    name_dict = {}
+    for p in participant:
+        if p not in name_dict.keys():
+            name_dict[p] = 0
+        name_dict[p] += 1
     
-"""
-완주하지 못한 선수를 찾아라
-동명이인 가능 
-1. 참가자 - 숫자 맵생성
-2. 완주자를 돌면서 숫자 빼기
-3. 값이 +인 key 찾기
+    for name in completion:
+        name_dict[name] -= 1
+    
+    for name in name_dict.keys():
+        if name_dict[name] > 0:
+            return name
+    
 
+"""
+한명제외 전부 완주
+완주x 이름 반환
+동명이인 가능
+각각의 이름을 맵으로 반환하고 각각의 이름이
+
+1. 이름에 1더하기 10만
+2. 나오는 이름 -= 1 하기 10만
+3. 이제 이름이 양수인놈 찾기 10만
 """
