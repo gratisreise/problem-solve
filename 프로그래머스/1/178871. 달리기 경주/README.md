048d760807c31ebfa8df57eea7dc7c746734483d
noah <104671487+gratisreise@users.noreply.github.com>
2026-06-13 15:47:40 +0900

    [level 1] Title: 달리기 경주, Time: 363.62 ms, Memory: 73.2 MB -BaekjoonHub

--- 프로그래머스/1/178871. 달리기 경주/README.md
+++ 프로그래머스/1/178871. 달리기 경주/README.md
@@ -1,10 +1,10 @@
 # [level 1] 달리기 경주 - 178871 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/178871#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/178871?language=python3) 
 
 ### 성능 요약
 
-메모리: 91.4 MB, 시간: 919.40 ms
+메모리: 73.2 MB, 시간: 363.62 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 27일 15:24:52
+2026년 06월 13일 15:47:35
 
 ### 문제 설명
 
--- 프로그래머스/1/178871. 달리기 경주/달리기 경주.py
+++ 프로그래머스/1/178871. 달리기 경주/달리기 경주.py
@@ -1,16 +1,22 @@
 def solution(players, callings):
-    # 선수 이름을 키로, 순위를 값으로 하는 딕셔너리 생성
-    rank = {name: i for i, name in enumerate(players)}
-
+    rank = {x:i for i,x in enumerate(players)}        
     for call in callings:
-        current_position = rank[call]          # 현재 순위 가져오기
-        new_position = current_position - 1    # 추월 후의 새로운 순위 계산
-        
-        # 추월된 선수 이름과 위치 가져오기
-        swapped_player = players[new_position]
-
-        # 순위 및 players 리스트 업데이트
-        players[new_position], players[current_position] = players[current_position], players[new_position]
-        rank[call], rank[swapped_player] = new_position, current_position
-
+        now = rank[call] # 추월한놈 현재등수
+        temp = players[now-1] # 추월 당할 놈 이름
+        players[now-1] = players[now]
+        players[now] = temp
+        rank[call] = now-1
+        rank[temp] = now
     return players
+    
+    
+"""
+[조건]
+바로 앞 추월할 때 추월한 선수 이름 부름 
+이름을 부르면 바로 앞에 놈이랑 자리를 바꾼다
+
+[아이디어]
+이름-등수 맵에 저장 이름 부를 때마다 나보다 하나 앞에 있는 놈이랑 스왑하면서 이름 - 등수 바꾸기
+
+
+"""
