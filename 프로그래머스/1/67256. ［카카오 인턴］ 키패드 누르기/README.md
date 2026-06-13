2ab8b127a8bf5834ef7f4001c8ade439d68c33ec
noah <104671487+gratisreise@users.noreply.github.com>
2026-06-13 14:01:08 +0900

    [level 1] Title: [카카오 인턴] 키패드 누르기, Time: 0.15 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/README.md
+++ 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/README.md
@@ -1,10 +1,10 @@
 # [level 1] [카카오 인턴] 키패드 누르기 - 67256 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/67256) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/67256?language=python3) 
 
 ### 성능 요약
 
-메모리: 10.2 MB, 시간: 1.06 ms
+메모리: 11.4 MB, 시간: 0.15 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 12월 13일 14:07:29
+2026년 06월 13일 14:01:03
 
 ### 문제 설명
 
--- 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.py
+++ 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.py
@@ -1,30 +1,55 @@
-def dist(target, pos):
-    return abs(target[0] - pos[0]) + abs(target[1] - pos[1])
+def distance(arr1, arr2):
+    return abs(arr1[0] - arr2[0]) + abs(arr1[1] - arr2[1])
 
 def solution(numbers, hand):
-    pattern = {'L': (1, 4, 7), 'R': (3, 6, 9)}
-    pos = {'L': [0, 3], 'R': [2, 3]}
-    result = []
-    
-    def press(which, coord):
-        result.append(which)
-        pos[which] = coord
-    
-    for number in numbers:
-        choose = 'L'
-        target = [0, (number - 1) // 3]
-        
-        if number in pattern['L']: pass
-        elif number in pattern['R']: choose = 'R'
+    lr = "LR"
+    like = 0 if hand == 'left' else 1
+    pos = [[3,0], [3,2]]
+    dic = {x:1 if x % 3 == 0 else 0 if (x-1) % 3 == 0 else -1 for x in range(10)}
+    dic[0] = -1
+    # print(dic)
+    num_pos = {x:[(x-1)//3, (x-1) % 3] for x in range(1, 10)}
+    num_pos[0] = [3, 1]
+    # print(num_pos)
+    ret = ''
+    for num in numbers:
+        cur_hand = dic[num]
+        cur_pos = num_pos[num]
+        cur_ret = ''
+        # print(cur_hand)
+        if cur_hand == -1:
+            ldis = distance(pos[0], cur_pos)
+            rdis = distance(pos[1], cur_pos)
+            if ldis < rdis:
+                pos[0] = cur_pos
+                ret += lr[0]    
+            elif ldis > rdis:
+                pos[1] = cur_pos
+                ret += lr[1]
+            else:
+                pos[like] = cur_pos
+                ret += lr[like]
         else:
-            target = [1, 3 if number == 0 else (number - 1) // 3]
-            left = dist(target, pos['L'])
-            right = dist(target, pos['R'])
-            
-            if right < left: choose = 'R'
-            elif right == left and hand == 'right': choose = 'R'
+            pos[cur_hand] = cur_pos
+            ret += lr[cur_hand]
+    return ret
         
-        press(choose, target)
+                
+            
     
-    return ''.join(result)
+"""
+왼엄:*, 오엄:# 에서 시작
+1. 상하좌우 이동, 거리한칸 = 1
+2. 1,4,7 = 왼
+3. 3,6,9 = 오
+4. 2,5,8,0 = 가까운놈, 거리같은면 우수,좌수 각자 우선
 
+[자료구조]
+엄지 좌표배열, 위치별 우선순위 손 맵, "LR", 숫자별 좌표
+[로직]
+1. 숫자를 순회
+    1-1. 숫자마다 우선하는 손가락 선정
+    1-2. 해당 위치의 손가락 움직이기
+    1-3. ret에 += 로 위치 더해주기
+
+"""
