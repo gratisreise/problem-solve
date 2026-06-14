147ade17d248f21d00ded9e9ae8b56891d675d95
noah <104671487+gratisreise@users.noreply.github.com>
2026-06-14 15:10:50 +0900

    [level 1] Title: 중요한 단어를 스포 방지, Time: 1.58 ms, Memory: 11.7 MB -BaekjoonHub

--- 프로그래머스/1/468370. 중요한 단어를 스포 방지/중요한 단어를 스포 방지.py
+++ 프로그래머스/1/468370. 중요한 단어를 스포 방지/중요한 단어를 스포 방지.py
@@ -1,76 +1,54 @@
-def solution(ss, spoiler_ranges):
-    st = set() # 스포방지 단어
-    spo_idx = []
-    ttemp = list(ss)
-    for ran in spoiler_ranges:
-        s,e = ran
-        for i in range(s, e+1):
-            if ttemp[i] != " ":
-                ttemp[i] = '#'
-    ttemp = ''.join(ttemp)
-    non_spo = set(x for x in ttemp.split())
-    l = len(ss)
-    ret = 0
-    for spran in spoiler_ranges:
-        s,e = spran
-        temp = ss[s:e+1]
-        flag1, flag2 = False, False
-        if temp[0] != " ":
-            while s > 0:
-                s -= 1
-                if ss[s] == " ":
-                    break
-                temp = ss[s] + temp
-                flag1 = True
-        if temp[-1] != " ":
-            while e < l-1:
-                e += 1
-                if ss[e] == " ":
-                    break
-                temp += ss[e]
-                flag2 = True
-        temp = temp.split()
-        
-        for word in temp:
-            if word not in st and word not in non_spo:
-                ret += 1
-            st.add(word)
-    return ret
-        
-                
-        
-            
-            
-        
+from collections import defaultdict
 
+def solution(message, spoiler_ranges):
+    n = len(message)
 
-"""
-[조건]
-메세지의 일부만 가림 => 스포방지
-왼 -> 오른 하나씩 클릭하면 공개되는 단어들 중 중요한 단어의 갯수
-- 공백구분, 알파벳 소문자 + 숫자
-- 스포 방지 구간 포함 => 스포 방지 단어,
-- 한 단어가 여러 구간에 포함가능, 여러 단어 한 구간에 포함가능
-- 스포방지가 풀렸을 때 다음 조건 중요한 단어
-    1. 스포 방지 단어
-    2. 단어의 모든 문자가 스포방지 구간
-    3. 이전에 공개된 스포 방지 단어와 중복x
-    4. 여러 단어 동시 공개 => 왼쪽부터 순서대로 하나씩 판단(중복되는거는 왼쪽부터 판단해라)
-스포방지 단어중 중요한 단어의 수 구하기
-메세지 <= 2만, 스포기간 길이 <= 천
+    # 각 문자 위치가 몇 번째 스포 구간에 속하는지 기록
+    reveal_idx = [0] * n
+    for idx, (s, e) in enumerate(spoiler_ranges, start=1):
+        for i in range(s, e + 1):
+            reveal_idx[i] = idx
 
-[아이디어]
-스포구간을 순회하면서 스포구간 걸친단어가 중요한 단어인지 확인하기
-이게 완결된 단어인지 어떻게 판단하지??
-split 한다음 모든 단어를 딕셔너리에 숫자를 넣고 이게 처음인게 다른 곳에 있는 것들은
-[자료구조]
-스포방지 단어 set, 중요단어 변수
+    normal_words = set()      # 스포 구간이 아닌 영역에서 등장한 단어
+    spoiler_words = []        # (공개시점, 시작위치, 단어)
 
-[로직]
-1. 스포구간을 순회한다
- 1-1. 해당 구간에서 공백기준으로 나뉘는지 확인
- 1-2. 맨왼쪽부터 단어 확인 
- 1-3. 중요한단어 체크 후 중요한 단어에 변수에 숫자 증가시키기
- 1-3. 스포방지집합에 단어를 넣기
-2. 중요한 단어 
-"""
+    i = 0
+    while i < n:
+        if message[i] == ' ':
+            i += 1
+            continue
+
+        start = i
+        while i < n and message[i] != ' ':
+            i += 1
+        end = i - 1
+
+        word = message[start:i]
+
+        reveal_time = 0
+        is_spoiler = False
+
+        for pos in range(start, end + 1):
+            if reveal_idx[pos] > 0:
+                is_spoiler = True
+                reveal_time = max(reveal_time, reveal_idx[pos])
+
+        if is_spoiler:
+            spoiler_words.append((reveal_time, start, word))
+        else:
+            normal_words.add(word)
+
+    # 공개 순서: 스포 구간 클릭 순서 -> 같은 시점이면 왼쪽부터
+    spoiler_words.sort(key=lambda x: (x[0], x[1]))
+
+    revealed = set()
+    answer = 0
+
+    for _, _, word in spoiler_words:
+        if word not in normal_words and word not in revealed:
+            answer += 1
+
+        # 중요 여부와 상관없이 이미 공개된 스포 단어로 취급
+        revealed.add(word)
+
+    return answer
