2b241c2b87aa16180062120be39b57b6f7a71f18
noah <104671487+gratisreise@users.noreply.github.com>
2026-06-20 13:02:02 +0900

    [level 3] Title: 가장 먼 노드, Time: 20.73 ms, Memory: 21.3 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/README.md
+++ 프로그래머스/3/49189. 가장 먼 노드/README.md
@@ -4,7 +4,7 @@
 
 ### 성능 요약
 
-메모리: 21.3 MB, 시간: 53.56 ms
+메모리: 21.3 MB, 시간: 20.73 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 12월 10일 11:38:18
+2026년 06월 20일 13:01:56
 
 ### 문제 설명
 
@@ -51,7 +51,7 @@
 
 <p>예제의 그래프를 표현하면 아래 그림과 같고, 1번 노드에서 가장 멀리 떨어진 노드는 4,5,6번 노드입니다.</p>
 
-<p><img src="https://grepp-programmers.s3.amazonaws.com/files/ybm/fadbae38bb/dec85ab5-0273-47b3-ba73-fc0b5f6be28a.png" title="" alt="image.png"></p>
+<p><img src="https://asset.programmers.co.kr/files/ybm/fadbae38bb/dec85ab5-0273-47b3-ba73-fc0b5f6be28a.png" title="" alt="image.png"></p>
 
 
 > 출처: 프로그래머스 코딩 테스트 연습, https://school.programmers.co.kr/learn/challenges
--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.py
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.py
@@ -1,26 +1,42 @@
-from collections import defaultdict
-
+from collections import deque
 def solution(n, edge):
-    dic = defaultdict(list)
-    tree = {i+1:[] for i in range(n)}
-    for arr in edge:
-        a,b = arr
-        tree[a].append(b)
-        tree[b].append(a)
-    mx, q, vis = -1, [1], [0, 1, *[0 for i in range(n-1)]]
+    graph = [[] for _ in range(n+1)]
+    for e in edge:
+        a, b = e
+        graph[a].append(b)
+        graph[b].append(a)
+    
+    q = deque()
+    visited = [0] * (n + 1)
+    q.append(1)
+    visited[1] = 1
+    mmax = -1
     while q:
-        now = q.pop(0)
-        for next in tree[now]:
-            if vis[next]: continue
-            vis[next] = vis[now] + 1
-            q.append(next)
-            dic[vis[next]].append(next)
-            mx = max(mx, vis[next])
-    return len(dic[mx])
+        now = q.popleft()
+        mmax = max(mmax, visited[now])
+        for nex in graph[now]:
+            if visited[nex]:
+                continue
+            q.append(nex)
+            visited[nex] = visited[now] + 1
+    return sum(1 for x in visited if x == mmax)
+        
+
 """
-가장 멀리 떨어진 노드의 갯수
-높이가 가장 큰것
-탐색을 하면서 
-맵에 해당하는 길이의 점을 저장하고, 길이의 최댓값 저장
-가장 큰 길이의 
+[조건]
+n개의 노드 1~n, 
+1번노드 기준 가장 멀리 떨어진 노드의 갯수
+가장 멀리 = 최단경로 간선 갯수 가장 많음
+
+
+[아이디어]
+리프 노드를 구하는 것 => 
+방문에서 1번을 기준으로 bfs로 채워주면 되지 않을까??
+
+[자료구조]
+graph, 방문배열
+[로직]
+1. vertex를 순회하면서 그래프 생성 
+2. bfs 돌면서 visited 채우기
+3. 출력 
 """
