a234e15b5f163bca01ce6f4d25db31e207fda003
noah <104671487+gratisreise@users.noreply.github.com>
2026-06-26 09:45:03 +0900

    [level 3] Title: 오랜 기간 보호한 동물(2), Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/3/59411. 오랜 기간 보호한 동물（2）/오랜 기간 보호한 동물（2）.sql
+++ 프로그래머스/3/59411. 오랜 기간 보호한 동물（2）/오랜 기간 보호한 동물（2）.sql
@@ -1,9 +1,14 @@
--- 입양간 동물
--- 보호 기간 가장 긴 동물
--- 두마리
--- 보호 기간 긴순
-select o.animal_id, o.name
-from ANIMAL_OUTS o
-left join ANIMAL_INS i on i.ANIMAL_ID = o.ANIMAL_ID
-order by datediff(o.datetime, i.datetime) desc
+select i.animal_id, i.name
+from animal_ins i 
+ join animal_outs o
+  on i.animal_id = o.animal_id
+order by datediff(o.datetime, i.datetime) desc, i.name
 limit 2
+
+
+/*
+입양간 동물, 보호기간 가장김, 두마리,
+보호기간순 조회
+아이디, 이름
+
+*/
