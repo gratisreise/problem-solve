ca10eff15d5a80d9c5a592435bac2ec299f8448e
noah <104671487+gratisreise@users.noreply.github.com>
2026-06-28 11:31:35 +0900

    [level 3] Title: 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/3/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
+++ 프로그래머스/3/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
@@ -1,34 +1,17 @@
--- 테이블 조회수 가장 높은 중고거래
--- file id 내림차
--- 파일경로 /home/grep/src/
--- 
-select 
-    concat('/home/grep/src/', f.BOARD_ID, '/', f.FILE_ID, f.FILE_NAME, f.FILE_EXT)
-    as FILE_PATH
-from USED_GOODS_BOARD b
-join USED_GOODS_FILE f on b.BOARD_ID = f.BOARD_ID
-where b.views = (select max(views) from USED_GOODS_BOARD)
-order by f.file_id desc
-
-# # select * from USED_GOODS_BOARD
-# # order by views desc
+select concat_ws('/', '/home/grep/src', board_id, concat_ws('',file_id, file_name,file_ext))
+ as file_path
+from used_goods_file
+where board_id = (
+    select board_id  
+    from used_goods_board
+    order by views desc
+    limit 1
+)
+order by file_id desc
 
 /*
-- 조회수 max 첨부파일 경로 조회
-- FILE ID 내림차
-- 기본파일경로 /home/grep/src/ 
-- 게시글 ID 기준 디렉토리 구분
-- 파일이름: 파일 ID, 파일 이름, 파일 확장자로 구성
-- !조회수가 가장 높은 게시물은 하나만 존재!
-1. 테이블 조인
-2. max 게시물 조건 설정
-3. 해당하는 게시물 첨부파일 출력
-4. FILE_ID desc
+조회수max, 중고거래 게시물에 대한 첨부파일 경로 
+fileid 내림차, 
+게시글id 기준 디렉토리 구분, 파일이름 = 파일id + 파일이름 + 파일 확장자 
+조회수 max는 하나만 존재
 */
-# SELECT CONCAT('/home/grep/src/', F.BOARD_ID, 
-#               '/',FILE_ID, FILE_NAME, FILE_EXT) FILE_PATH
-# FROM USED_GOODS_BOARD B JOIN USED_GOODS_FILE F
-# ON B.BOARD_ID = F.BOARD_ID
-# WHERE VIEWS = (select max(VIEWS) from USED_GOODS_BOARD)
-# ORDER BY FILE_ID DESC
-
