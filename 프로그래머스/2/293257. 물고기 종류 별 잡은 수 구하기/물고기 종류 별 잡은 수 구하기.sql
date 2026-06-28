d1bcde6d00576c03f8acd6e71d956b654dc4c95f
noah <104671487+gratisreise@users.noreply.github.com>
2026-06-28 09:25:22 +0900

    [level 2] Title: 물고기 종류 별 잡은 수 구하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/293257. 물고기 종류 별 잡은 수 구하기/물고기 종류 별 잡은 수 구하기.sql
+++ 프로그래머스/2/293257. 물고기 종류 별 잡은 수 구하기/물고기 종류 별 잡은 수 구하기.sql
@@ -1,16 +1,15 @@
+select count(*) as fish_count, fish_name
+from fish_info i
+ join fish_name_info n
+  on i.fish_type = n.fish_type
+group by fish_name 
+order by fish_count desc
+
+
 /*
-- 물고기의 종류별
-- 물고기의 이름, 포획수
-- 잡은 수 기준 내림차순
-1. 물고기의 fishtype 조인
-2. fishtype 그룹핑
-3. 물고기 이름 설정
+물고기종류별, 물고기 이름, 잡은수
+fish_name, fish_count
+잡은수 내림차 
+
+
 */
-
-SELECT COUNT(*) AS FISH_COUNT, FISH_NAME
-FROM FISH_INFO i JOIN FISH_NAME_INFO n
-ON i.FISH_TYPE = n.FISH_TYPE
-GROUP BY FISH_NAME
-ORDER BY FISH_COUNT DESC
-
-
