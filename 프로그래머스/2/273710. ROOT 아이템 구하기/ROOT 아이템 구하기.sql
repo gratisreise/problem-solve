ef7705f0dedd66b9c18073d345d8d4b97315523b
noah <104671487+gratisreise@users.noreply.github.com>
2026-06-28 09:58:30 +0900

    [level 2] Title: ROOT 아이템 구하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/273710. ROOT 아이템 구하기/ROOT 아이템 구하기.sql
+++ 프로그래머스/2/273710. ROOT 아이템 구하기/ROOT 아이템 구하기.sql
@@ -1,7 +1,11 @@
--- root 아이템의 정보출력
--- 아이템ID 오름차
-select i.ITEM_ID, i.ITEM_NAME
-from ITEM_INFO i
-left join ITEM_TREE t on i.ITEM_ID = t.ITEM_ID
-where t.PARENT_ITEM_ID is null
-order by i.ITEM_ID
+select i.item_id, item_name
+from item_info i
+ join item_tree t
+  on i.item_id = t.item_id 
+where t.parent_item_id is null
+
+
+/*
+
+
+*/
