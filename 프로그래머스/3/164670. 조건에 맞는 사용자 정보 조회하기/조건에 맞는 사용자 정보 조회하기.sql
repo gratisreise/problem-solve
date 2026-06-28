cbb4b8ec6e924ad4e3ec92c1485a493c0798971e
noah <104671487+gratisreise@users.noreply.github.com>
2026-06-28 10:18:44 +0900

    [level 3] Title: 조건에 맞는 사용자 정보 조회하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/3/164670. 조건에 맞는 사용자 정보 조회하기/조건에 맞는 사용자 정보 조회하기.sql
+++ 프로그래머스/3/164670. 조건에 맞는 사용자 정보 조회하기/조건에 맞는 사용자 정보 조회하기.sql
@@ -1,16 +1,19 @@
--- 중고 거래 게시물 3건 이상
--- 사용자 ID, 닉네임, 전체주소, 전화번호
--- 전체 주소는 시, 도로명 주소, 상세 주소가 함께 출력
--- 전화번호 xxx-xxxx-xxxx 
--- 회원 ID를 기준으로 내림차순
-select b.WRITER_ID,
-    u.NICKNAME,
-    concat(CITY, ' ', STREET_ADDRESS1, ' ', STREET_ADDRESS2) as '전체주소',
-    concat(left(TLNO, 3),'-',substr(TLNO, 4, 4), '-', right(TLNO, 4)) as '전화번호'
-from USED_GOODS_BOARD b
-join USED_GOODS_USER u on b.WRITER_ID = u.USER_ID
-group by b.WRITER_ID
-having count(b.WRITER_ID) >= 3
-order by b.WRITER_ID desc
+select 
+    user_id, nickname, 
+    concat_ws(' ', city, street_address1, street_address2) as 전체주소,
+    concat_ws('-', substring(tlno, 1, 3),substring(tlno, 4, 4), substring(tlno, 8, 4)) as 전화번호
+from used_goods_board b
+    join used_goods_user u
+    on b.writer_id = u.user_id 
+group by user_id 
+having count(*) >= 3
+order by user_id desc 
 
- 
+
+/*
+중고거래 게시물 3건이상 등록한 사용자
+사용자id, 닉네임, 전체주소(시,도로명주소,상세주소), 전화번호(xxx-xxxx-xxxx)
+회원id 내림차
+
+
+*/
