ac25e888dc7a06d6693f93d7a2fd363054d277c9
noah <104671487+gratisreise@users.noreply.github.com>
2026-07-03 21:25:44 +0900

    [level 2] Title: 예상 대진표, Time: 0.01 ms, Memory: 11.6 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/README.md
+++ 프로그래머스/2/12985. 예상 대진표/README.md
@@ -1,10 +1,10 @@
 # [level 2] 예상 대진표 - 12985 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12985#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12985?language=python3) 
 
 ### 성능 요약
 
-메모리: 10.3 MB, 시간: 0.01 ms
+메모리: 11.6 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 31일 13:31:13
+2026년 07월 03일 21:25:40
 
 ### 문제 설명
 
--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.py
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.py
@@ -1,7 +1,23 @@
 def solution(n,a,b):
-    answer = 0
-    while a != b:
-        a = (a + 1) // 2
-        b = (b + 1) // 2
-        answer += 1
-    return answer
+    ret = 1
+    while True :
+        a = a//2 if a % 2 == 0 else a//2 +1
+        b = b//2 if b % 2 == 0 else b//2 +1
+        if a == b:
+            break
+        ret += 1
+    return ret
+
+"""
+1~n, n명참가,
+12, 34, 56 ... 이렇게 바로 양옆에서 대결
+A가 B와 몇번째 라운드에서 만나는지 반환
+12, 34, 56,
+
+
+12 34 56 78
+
+1, 2, 3, 4
+ㅂ1, 2
+
+"""
