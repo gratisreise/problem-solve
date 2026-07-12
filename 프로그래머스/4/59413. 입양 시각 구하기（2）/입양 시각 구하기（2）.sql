6f8966b47c013834b8d4d76bb5441e59d6054e6b
noah <104671487+gratisreise@users.noreply.github.com>
2026-07-12 11:33:32 +0900

    [level 4] Title: 입양 시각 구하기(2), Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/59413. 입양 시각 구하기（2）/입양 시각 구하기（2）.sql
+++ 프로그래머스/4/59413. 입양 시각 구하기（2）/입양 시각 구하기（2）.sql
@@ -1,19 +1,31 @@
--- 몇시에 입양 활발
--- 0~23시 시간대별
--- 입양건수 조회
--- 시간대 오름차
--- MySQL
-
--- 0부터 23까지의 모든 시간을 생성하는 CTE (Common Table Expression)
-WITH RECURSIVE AllHours (HOUR) AS (
-    SELECT 0 -- 시작 시간
-    UNION ALL
-    SELECT HOUR + 1 FROM AllHours WHERE HOUR < 23 -- 23보다 작을 때까지 1씩 더하며 재귀 호출
+with recursive time_slots as (
+    select 0 as hour
+    union all
+    select hour + 1
+    from time_slots
+    where hour + 1 <= 23
 )
-SELECT
-    ah.HOUR,
-    COUNT(ANIMAL_ID) AS COUNT        
-FROM AllHours ah 
-left join ANIMAL_OUTS o on ah.HOUR = HOUR(o.DATETIME)
-GROUP BY ah.HOUR           
-ORDER BY ah.HOUR           
+
+select t.hour, ifnull(count, 0) as count
+from (
+    select hour(datetime) as hour, count(*) as count
+    from animal_outs 
+    group by hour(datetime)
+) as ao
+right join time_slots t
+on ao.hour = t.hour
+order by t.hour
+
+
+
+/*
+몇시에 입양이 가장 활발?
+
+0~ 23
+시간대별로 입양이 몇 건이나 발생하는지 조회
+
+0~23 까지의 테이블
+해당 테이블에 맞게 group by 해서 조인
+
+
+*/
