9ecd0eba0d45a95d63f2ebfbc05e8991dae74732
noah <104671487+gratisreise@users.noreply.github.com>
2026-07-12 12:20:12 +0900

    [level 4] Title: 자동차 대여 기록 별 대여 금액 구하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/151141. 자동차 대여 기록 별 대여 금액 구하기/자동차 대여 기록 별 대여 금액 구하기.sql
+++ 프로그래머스/4/151141. 자동차 대여 기록 별 대여 금액 구하기/자동차 대여 기록 별 대여 금액 구하기.sql
@@ -1,22 +1,46 @@
+with 
+car_rental as (
+    select 
+        history_id, 
+        datediff(end_date, start_date) + 1 as days,
+        car_type,
+        daily_fee
+    from car_rental_company_rental_history h
+    join car_rental_company_car c 
+    on h.car_id = c.car_id
+    where car_type = '트럭'
+), 
+plan_type as (
+    select 
+        car_type, 
+        trim(replace(duration_type, '일 이상', '')) + 0 as day_type,
+        1- (trim(replace(discount_rate, '%', '')) + 0)/100 as discount
+    from car_rental_company_discount_plan
+    where car_type = '트럭'
+)
+
+select 
+    history_id,
+    ifnull((
+        daily_fee*days* min(discount) 
+    ), daily_fee * days) as fee
+from plan_type p
+right join car_rental c
+on c.days >= p.day_type
+group by history_id
+order by fee desc, history_id desc
+
+# select * from CAR_RENTAL_COMPANY_DISCOUNT_PLAN where car_type = '트럭'
+
+
+
 /*
-- 종류 트럭
-- 대여 기록 별 대여 금액, 대여 기록 ID, 대여 금액
-- 대여 금액 내림차
+자동차 종류 = 트럭
+대여기록별-대여금액
+
+대여기록id, 대여금액리스트
+대여금액 내림차, id 내림차
+3개를 조인해서
+
+
 */
-SELECT history_id,
-       round(daily_fee * (datediff(end_date, start_date) + 1) * (100 - ifnull(discount_rate, 0)) /
-             100, 0) fee
-FROM (SELECT *,
-             CASE
-                 WHEN datediff(end_date, start_date) + 1 < 7 THEN NULL
-                 WHEN datediff(end_date, start_date) + 1 < 30 THEN '7일 이상'
-                 WHEN datediff(end_date, start_date) + 1 < 90 THEN '30일 이상'
-                 ELSE '90일 이상'
-                 END duration_type
-      FROM car_rental_company_rental_history) a
-         JOIN car_rental_company_car b
-              ON a.car_id = b.car_id
-         LEFT JOIN car_rental_company_discount_plan c
-                   ON c.car_type = b.car_type AND a.duration_type = c.duration_type
-WHERE b.car_type = '트럭'
-ORDER BY 2 DESC, 1 DESC
