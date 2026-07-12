3293d53523f86fa8bb9eebe4b4908900766a2461
noah <104671487+gratisreise@users.noreply.github.com>
2026-07-12 13:54:31 +0900

    [level 5] Title: 상품을 구매한 회원 비율 구하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/5/131534. 상품을 구매한 회원 비율 구하기/상품을 구매한 회원 비율 구하기.sql
+++ 프로그래머스/5/131534. 상품을 구매한 회원 비율 구하기/상품을 구매한 회원 비율 구하기.sql
@@ -1,47 +1,21 @@
+select year(sales_date) as year,
+    month(sales_date) as month,
+    count(distinct(o.user_id)) as purchased_users,
+    round(count(distinct(o.user_id)) 
+          / (select count(*) from user_info where year(joined) =2021), 1)
+    as purchased_ratio
+from user_info u
+join online_sale o
+on u.user_id = o.user_id and year(u.joined) = 2021
+group by year(sales_date), month(sales_date)
+order by year, month
+
 /*
-- 2021년에 가입한 회원들 중
-- 상품을 구매한 회원수/ 가입한 전체 회원수, 소수점 둘째 반올림
-- 년, 월 별로 출력
-- 년 오름차, 월 오름차
-1. 2021년에 가입한 회원의 정보를 뽑는 쿼리
-2. 가입한 전체 회원수, 해당 
+2021년에 가입
+상품 구매한 회원수, 상품구매한 회원 비율(가입+구매/가입) => 소수 두번째 반올림,
+년,월별 
+년 오름차, 월 오름차
+
+2021년에 가입 수
+
 */
-# SELECT year(SALES_DATE) year, month(sales_date) month, count(a.user_id), 
-#     YEAR (sales_date) YEAR, MONTH (sales_date) MONTH, ROUND(COUNT (DISTINCT a.user_id)/(
-#     SELECT COUNT (*)
-#     FROM USER_INFO
-#     WHERE JOINED LIKE '2021%'
-#     ), 2) PURCHASED_USERS
-# FROM ONLINE_SALE a JOIN USER_INFO b
-# ON a.USER_ID = b.USER_ID
-# WHERE JOINED LIKE '2021%'
-# GROUP BY YEAR, month
-
-
-# 2021년에 가입한 사람 수
-# (select count(*)
-# from USER_INFO
-# where JOINED like '2021%')
-
-# select 
-#     year(SALES_DATE) year,
-#     month(SALES_DATE) month,
-#     count(distinct a.USER_ID) PURCHASED_USERS,
-#     round(count(distinct a.USER_ID)/(select count(distinct USER_ID) from USER_INFO where JOINED like '2021%'), 1) as PUCHASED_RATIO
-# from ONLINE_SALE a join USER_INFO b on a.USER_ID = b.USER_ID
-# group by year, month
-# order by year, month
-
-
-WITH TOTAL_USER AS (SELECT * FROM USER_INFO WHERE YEAR(JOINED) = 2021)
-
-
-
-SELECT YEAR(OS.SALES_DATE) YEAR, MONTH(OS.SALES_DATE) MONTH, COUNT(DISTINCT UI.USER_ID) PURCHASED_USERS, ROUND(COUNT(DISTINCT UI.USER_ID) / (SELECT COUNT(*) FROM TOTAL_USER), 1) PUCHASED_RATIO
-FROM USER_INFO UI
-JOIN ONLINE_SALE OS
-ON UI.USER_ID = OS.USER_ID
-
-WHERE YEAR(UI.JOINED) = 2021
-GROUP BY 1, 2
-ORDER BY 1, 2
