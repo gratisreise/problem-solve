d1227f91b544c47fda0d0fb0755184d9719a01ec
noah <104671487+gratisreise@users.noreply.github.com>
2026-07-15 22:15:37 +0900

    [level 3] Title: 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/3/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
+++ 프로그래머스/3/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
@@ -1,8 +1,8 @@
-select concat_ws('/', '/home/grep/src', board_id, concat_ws('',file_id, file_name,file_ext))
- as file_path
+select 
+    concat_ws('/', '/home/grep/src', board_id, concat_ws('', file_id, file_name, file_ext)) as file_path
 from used_goods_file
 where board_id = (
-    select board_id  
+    select board_id 
     from used_goods_board
     order by views desc
     limit 1
@@ -10,8 +10,7 @@ where board_id = (
 order by file_id desc
 
 /*
-조회수max, 중고거래 게시물에 대한 첨부파일 경로 
-fileid 내림차, 
-게시글id 기준 디렉토리 구분, 파일이름 = 파일id + 파일이름 + 파일 확장자 
-조회수 max는 하나만 존재
+조회수 max, 첨부파일 경로,
+
+
 */
