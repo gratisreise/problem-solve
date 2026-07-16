e3db238a1ab5070838f7a1bd570fc7e2f72ba334
noah <104671487+gratisreise@users.noreply.github.com>
2026-07-16 22:15:41 +0900

    [level 4] Title: 취소되지 않은 진료 예약 조회하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/132204. 취소되지 않은 진료 예약 조회하기/취소되지 않은 진료 예약 조회하기.sql
+++ 프로그래머스/4/132204. 취소되지 않은 진료 예약 조회하기/취소되지 않은 진료 예약 조회하기.sql
@@ -1,18 +1,17 @@
-select a.apnt_no, p.pt_name, p.pt_no, d.mcdp_cd, d.dr_name, a.apnt_ymd
+select a.apnt_no, p.pt_name, p.pt_no, a.mcdp_cd, d.dr_name, a.apnt_ymd
 from appointment a
- join patient p
- on a.pt_no = p.pt_no
- join doctor d
- on a.mddr_id = d.dr_id
-where a.apnt_cncl_yn = 'N' and d.mcdp_cd = 'CS' and a.apnt_ymd like '2022-04-13%'
-order by a.apnt_ymd 
+join doctor d
+on d.dr_id = a.mddr_id 
+join patient p
+on p.pt_no = a.pt_no 
+where a.apnt_ymd like '2022-04-13%' and a.apnt_cncl_ymd is null and a.mcdp_cd = 'CS'
+order by apnt_ymd asc 
 
 
 /*
-2022년 4월 13일
-취소되지 않은, CS, 진료예약내역
-진료예약번호, 환자이름, 환자번호, 진료과코드, 의사이름, 진료예약일시
-진료예약일시 오름차
+2022년 4월 13일 
+취소x, CS, 
+진료예약일시 asc
 
 
 */
