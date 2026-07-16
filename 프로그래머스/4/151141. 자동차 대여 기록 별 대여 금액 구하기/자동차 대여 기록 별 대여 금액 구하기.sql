1cc66a96d0a1e472411c6b2bd09f409437102bdc
noah <104671487+gratisreise@users.noreply.github.com>
2026-07-16 22:05:17 +0900

    [level 4] Title: 자동차 대여 기록 별 대여 금액 구하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/151141. 자동차 대여 기록 별 대여 금액 구하기/자동차 대여 기록 별 대여 금액 구하기.sql
+++ 프로그래머스/4/151141. 자동차 대여 기록 별 대여 금액 구하기/자동차 대여 기록 별 대여 금액 구하기.sql
@@ -1,46 +1,30 @@
 with 
-car_rental as (
-    select 
-        history_id, 
-        datediff(end_date, start_date) + 1 as days,
-        car_type,
-        daily_fee
+counts as (
+    select h.history_id, h.car_id, ifnull(max(discount_rate), 0) as discount
     from car_rental_company_rental_history h
-    join car_rental_company_car c 
-    on h.car_id = c.car_id
-    where car_type = '트럭'
-), 
-plan_type as (
-    select 
-        car_type, 
-        trim(replace(duration_type, '일 이상', '')) + 0 as day_type,
-        1- (trim(replace(discount_rate, '%', '')) + 0)/100 as discount
-    from car_rental_company_discount_plan
-    where car_type = '트럭'
+    left join car_rental_company_discount_plan p
+    on datediff(end_date, start_date) + 1 >= trim(replace(duration_type, '일 이상', '')) + '0' and car_type = '트럭'
+    group by h.history_id, h.car_id
+    order by h.history_id
 )
 
+
 select 
-    history_id,
-    ifnull((
-        daily_fee*days* min(discount) 
-    ), daily_fee * days) as fee
-from plan_type p
-right join car_rental c
-on c.days >= p.day_type
-group by history_id
-order by fee desc, history_id desc
-
-# select * from CAR_RENTAL_COMPANY_DISCOUNT_PLAN where car_type = '트럭'
-
+    h.history_id,
+    cc.daily_fee * (datediff(end_date, start_date) + 1) * (1 - discount/100) as fee
+from car_rental_company_rental_history h
+join counts c
+on h.history_id = c.history_id and h.car_id = c.car_id
+join car_rental_company_car cc
+on h.car_id = cc.car_id
+where cc.car_type = '트럭'
+order by fee desc, h.history_id desc 
 
 
 /*
-자동차 종류 = 트럭
-대여기록별-대여금액
-
-대여기록id, 대여금액리스트
-대여금액 내림차, id 내림차
-3개를 조인해서
+자동차 종류 = 트럭 
+대여기록id, 대여금액
+대여금액 내림차, 기록id 내림차
 
 
 */
