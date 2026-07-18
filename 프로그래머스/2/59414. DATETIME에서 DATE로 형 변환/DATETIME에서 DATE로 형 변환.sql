327bed0cd9f5b879ec6d7c18f81afe10398ade7d
noah <104671487+gratisreise@users.noreply.github.com>
2026-07-18 19:00:24 +0900

    [level 2] Title: DATETIME에서 DATE로 형 변환, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/59414. DATETIME에서 DATE로 형 변환/DATETIME에서 DATE로 형 변환.sql
+++ 프로그래머스/2/59414. DATETIME에서 DATE로 형 변환/DATETIME에서 DATE로 형 변환.sql
@@ -1,10 +1,5 @@
-select animal_id, name, date_format(datetime, '%Y-%m-%d') as `날짜`
+select 
+    animal_id,
+    name,
+    date_format(datetime,'%Y-%m-%d') as 날짜
 from animal_ins
-order by animal_id
-
-/*
-아이디순 
-
-동물의 아이디, 이름, 들어온 날짜 
-
-*/
