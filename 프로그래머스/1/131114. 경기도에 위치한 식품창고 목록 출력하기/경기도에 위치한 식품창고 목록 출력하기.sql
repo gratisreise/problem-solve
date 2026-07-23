ef05d000ddb5a1099f12c8827e182dc11c3e5d35
noah <104671487+gratisreise@users.noreply.github.com>
2026-07-23 20:29:43 +0900

    [level 1] Title: 경기도에 위치한 식품창고 목록 출력하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/1/131114. 경기도에 위치한 식품창고 목록 출력하기/경기도에 위치한 식품창고 목록 출력하기.sql
+++ 프로그래머스/1/131114. 경기도에 위치한 식품창고 목록 출력하기/경기도에 위치한 식품창고 목록 출력하기.sql
@@ -1,15 +1,17 @@
-select warehouse_id, warehouse_name, address, ifnull(freezer_yn, 'N') as freezer_yn
+select warehouse_id, warehouse_name, address, 
+    case 
+        when freezer_yn is null then 'N'
+        else freezer_yn
+    end as freezer_yn
 from food_warehouse
-where address like '경기도%'
+where address like '%경기도%'
 order by warehouse_id
 
 
 
 /*
 경기도, 
-여부 == NULL => 'N' 출력
-오름차순 정렬
-
-창고의id, 이름, 주소, 냉동시설 여부
+시설여부 null => 'N'
+창고id 오름차
 
 */
