bce79964d0d0c73d45b280e8e4539e30a4b21641
noah <104671487+gratisreise@users.noreply.github.com>
2026-07-23 20:34:43 +0900

    [level 1] Title: 조건에 맞는 회원수 구하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/1/131535. 조건에 맞는 회원수 구하기/조건에 맞는 회원수 구하기.sql
+++ 프로그래머스/1/131535. 조건에 맞는 회원수 구하기/조건에 맞는 회원수 구하기.sql
@@ -1,13 +1,10 @@
-select count(*) as users
+select count(*)
 from user_info
-where year(joined) = 2021 and age >= 20 and age <= 29
-
+where year(joined) = 2021 and age between 20 and 29
 
 
 
 /*
-2021년 가입, 나이 20~29 
-
-몇명
-
+2021년 가입, 
+20~29 회원수
 */
