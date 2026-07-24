6bca15dedcfff42297e9262dc0530f6f1c203322
noah <104671487+gratisreise@users.noreply.github.com>
2026-07-24 22:07:50 +0900

    [level 3] Title: 있었는데요 없었습니다, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/3/59043. 있었는데요 없었습니다/있었는데요 없었습니다.sql
+++ 프로그래머스/3/59043. 있었는데요 없었습니다/있었는데요 없었습니다.sql
@@ -1,14 +1,11 @@
 select i.animal_id, i.name
 from animal_ins i
- join animal_outs o
-    on i.animal_id = o.animal_id
-where i.datetime > o.datetime
+join animal_outs o
+on i.animal_id = o.animal_id 
+where i.datetime > o.datetime 
 order by i.datetime
 
-
 /*
-보호 시작일 > 입양일 
+보호시작일 > 입양일
 보호시작일 오름차
-아이디, 이름
-
 */
