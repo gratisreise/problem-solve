6668c928ac342422ae437811827fc06f23057a5e
noah <104671487+gratisreise@users.noreply.github.com>
2026-07-25 18:08:41 +0900

    [level 1] Title: Python 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/1/276013. Python 개발자 찾기/Python 개발자 찾기.sql
+++ 프로그래머스/1/276013. Python 개발자 찾기/Python 개발자 찾기.sql
@@ -1,10 +1,9 @@
 select id, email, first_name, last_name
 from developer_infos
-where concat_ws(' ', skill_1, skill_2, skill_3) like '%Python%' 
+where concat_ws(' ', skill_1, skill_2, skill_3) like '%Python%'
 order by id
 
-
 /*
-python 스킬을 가진 개발자의 정보 조회
-
+python 스킬가짐 
+id 오름차
 */
