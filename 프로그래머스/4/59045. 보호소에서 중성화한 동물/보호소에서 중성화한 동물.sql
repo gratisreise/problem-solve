5fc8ad6a774612e569624a2fc41bc5fd8a365203
noah <104671487+gratisreise@users.noreply.github.com>
2026-07-25 17:09:54 +0900

    [level 4] Title: 보호소에서 중성화한 동물, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
+++ 프로그래머스/4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
@@ -1,18 +1,15 @@
 select i.animal_id, i.animal_type, i.name
 from animal_ins i
- join animal_outs o
-  on i.animal_id = o.animal_id
-where i.sex_upon_intake like 'Intact%' and o.sex_upon_outcome regexp 'Spayed|Neutered'
+join animal_outs o
+on i.animal_id = o.animal_id 
+where i.sex_upon_intake like '%Intact%'  and o.sex_upon_outcome not like '%Intact%'
 order by i.animal_id
 
-
 /*
-중성화 수술한 동물정보
+중성화 수술 거친 동물
 들어올 때 중성화x, 나갈 때 중성화O
-아이디 오름차
-
 아이디, 생물종, 이름
-
+아이디 오름차
 
 
 */
