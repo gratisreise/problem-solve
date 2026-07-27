fa694a03b8e0027f1e01766e5ca710ac4ef42b57
noah <104671487+gratisreise@users.noreply.github.com>
2026-07-27 20:21:28 +0900

    [level 3] Title: 없어진 기록 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
+++ 프로그래머스/3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
@@ -1,13 +1,14 @@
 select o.animal_id, o.name
-from animal_outs o
-    left join animal_ins i 
-    on o.animal_id = i.animal_id 
+from animal_ins i
+right join animal_outs o
+on i.animal_id = o.animal_id
 where i.animal_id is null
 order by o.animal_id
 
 
 /*
-입양은 감 들어온기록x
-동물id, 이름 
+outO, inX, 
+id 오름차
+
 
 */
