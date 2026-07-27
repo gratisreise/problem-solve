3ae1da5069d26873f05bd46b40310c889c091bfc
noah <104671487+gratisreise@users.noreply.github.com>
2026-07-27 20:12:45 +0900

    [level 3] Title: 조건에 맞는 사용자 정보 조회하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/3/164670. 조건에 맞는 사용자 정보 조회하기/조건에 맞는 사용자 정보 조회하기.sql
+++ 프로그래머스/3/164670. 조건에 맞는 사용자 정보 조회하기/조건에 맞는 사용자 정보 조회하기.sql
@@ -1,20 +1,20 @@
-select 
-    u.user_id,
+select
+    u.user_id, 
     u.nickname,
-    concat_ws(' ', u.city, u.street_address1,u.street_address2) as 전체주소,
-    concat_ws('-', left(u.tlno, 3), substr(u.tlno, 4,4), right(u.tlno, 4)) as 전화번호
-from used_goods_user u
-where (
-    select count(*)
-    from used_goods_board
-    where writer_id = u.user_id
-) >= 3
+    concat_ws(' ', city, street_address1, street_address2) as 전체주소,
+    concat_ws('-', left(tlno, 3), substr(tlno, 4, 4), right(tlno, 4)) as 전화번호
+from used_goods_board b
+join used_goods_user u
+on b.writer_id = u.user_id 
+group by u.user_id 
+having count(*) >= 3
 order by u.user_id desc
 
 /*
-중고거래 게시물 3건이상 등록
-
-사용자id, 닉네임, 전체주소, 전화번호
+중고거래 3건 이상,
+시+도로명+상세,
+xxx-xxxx-xxxx
+회원id 내림차
 
 
 */
