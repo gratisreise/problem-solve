2a283681c81eb793c9577fcd93c654c775b83e8a
noah <104671487+gratisreise@users.noreply.github.com>
2026-07-31 22:08:01 +0900

    [level 4] Title: 우유와 요거트가 담긴 장바구니, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/62284. 우유와 요거트가 담긴 장바구니/우유와 요거트가 담긴 장바구니.sql
+++ 프로그래머스/4/62284. 우유와 요거트가 담긴 장바구니/우유와 요거트가 담긴 장바구니.sql
@@ -1,14 +1,12 @@
 select cart_id
 from cart_products
 group by cart_id
-having sum(name = 'Milk') > 0  and sum(name = 'Yogurt') > 0
+having sum(name = 'Milk') > 0 and sum(name = 'Yogurt') > 0
 order by cart_id
 
+
 /*
-우유 & 요거트 동시 구입했나?
-id 오름차
-
-장바구니id,
-
+우유, 요거트 동시에 구입한 장바구니 확인
+아이디 오름차
 
 */
