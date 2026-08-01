04ed805dd3f36ce57edc19145ea9cc4fd52746d4
noah <104671487+gratisreise@users.noreply.github.com>
2026-08-01 13:24:26 +0900

    [level 3] Title: 물고기 종류 별 대어 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/3/293261. 물고기 종류 별 대어 찾기/물고기 종류 별 대어 찾기.sql
+++ 프로그래머스/3/293261. 물고기 종류 별 대어 찾기/물고기 종류 별 대어 찾기.sql
@@ -1,20 +1,19 @@
-select id, fish_name, length
+select i.id, ni.fish_name, i.length 
 from fish_info i
-join fish_name_info n
-on i.fish_type = n.fish_type
-where length = (
+join fish_name_info ni
+on i.fish_type = ni.fish_type
+where i.length = (
     select max(length)
     from fish_info
     where fish_type = i.fish_type
 )
+order by i.id asc
+
 
 
 /*
-물고기 종류별, max크기
-id 오름차, 
-
-ID, 이름, 길이
-
-
-
+물고기 종류별 가장 큰 물고기
+id asc, 
+id, 이름, 길이 
+종류별로 max는 한명, 10cm max는 없음
 */
