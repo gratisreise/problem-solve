d47a9d146cac1f93402a00cc3091509261f4ffea
noah <104671487+gratisreise@users.noreply.github.com>
2026-08-01 13:07:15 +0900

    [level 4] Title: 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/284528. 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기/연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기.sql
+++ 프로그래머스/4/284528. 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기/연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기.sql
@@ -1,26 +1,33 @@
-select e.emp_no, e.emp_name, 
+select
+    e.emp_no,
+    e.emp_name,
     case
-        when avg(score) >= 96 then 'S'
-        when avg(score) >= 90 then 'A'
-        when avg(score) >= 80 then 'B'
+        when avg(g.score) >= 96 then 'S'
+        when avg(g.score) >= 90 then 'A'
+        when avg(g.score) >= 80 then 'B'
         else 'C'
     end as grade,
-    case
-        when avg(score) >= 96 then sal*0.2
-        when avg(score) >= 90 then sal*0.15
-        when avg(score) >= 80 then sal*0.1
-        else 0
+    case 
+        when avg(g.score) >= 96 then sal * 0.2
+        when avg(g.score) >= 90 then sal * 0.15
+        when avg(g.score) >= 80 then sal * 0.1
+        else sal * 0
     end as bonus
-from hr_grade g
-join hr_employees e
-on g.emp_no = e.emp_no
-group by emp_no 
+from hr_employees e
+join hr_grade g
+on e.emp_no = g.emp_no
+group by e.emp_no 
 
 
 
 /*
-
-사번, 성명, 평가 등급(grade), 성과금(bonus)
+사원별 성과금 정보
+평가 점수별 등급
+등급에 따른 성과금정보
 사번 오름차
+
+사번, 성명, 평가등급(grade), 성과금(bonus)
+
+
 
 */
