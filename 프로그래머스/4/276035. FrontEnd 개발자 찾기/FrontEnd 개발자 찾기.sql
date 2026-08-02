b05f08b27f6499825bc2a79cb46fe1be477d0ace
noah <104671487+gratisreise@users.noreply.github.com>
2026-08-02 16:06:50 +0900

    [level 4] Title: FrontEnd 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/276035. FrontEnd 개발자 찾기/FrontEnd 개발자 찾기.sql
+++ 프로그래머스/4/276035. FrontEnd 개발자 찾기/FrontEnd 개발자 찾기.sql
@@ -1,15 +1,13 @@
-select distinct(id) as id,  email, first_name, last_name 
+select d.id, d.email, d.first_name, d.last_name
 from developers d
 join skillcodes s
-on d.skill_code & s.code >= 1
-where s.category = 'Front End'
-order by id
-
+on d.skill_code & s.code >= 1 and s.category = 'Front End'
+group by d.id
+order by d.id 
 
 
 /*
-front end 스킬 가진 개발자 정보 조회
-id오름차
-
+front end 스킬 가진 개발자 정보
+id 오름차
 
 */
