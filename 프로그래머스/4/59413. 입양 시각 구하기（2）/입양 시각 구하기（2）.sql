af1fc8d8b870f6444e7070d1b53971f7ea980500
noah <104671487+gratisreise@users.noreply.github.com>
2026-08-02 15:27:41 +0900

    [level 4] Title: 입양 시각 구하기(2), Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/59413. 입양 시각 구하기（2）/입양 시각 구하기（2）.sql
+++ 프로그래머스/4/59413. 입양 시각 구하기（2）/입양 시각 구하기（2）.sql
@@ -1,31 +1,22 @@
-with recursive time_slots as (
+with recursive times as (
     select 0 as hour
+    
     union all
+    
     select hour + 1
-    from time_slots
-    where hour + 1 <= 23
+    from times
+    where hour < 23
 )
 
-select t.hour, ifnull(count, 0) as count
-from (
-    select hour(datetime) as hour, count(*) as count
-    from animal_outs 
-    group by hour(datetime)
-) as ao
-right join time_slots t
-on ao.hour = t.hour
+select t.hour, count(o.animal_id) as "count"
+from animal_outs o
+right join times t
+on t.hour = hour(o.datetime)
+group by t.hour 
 order by t.hour
 
 
-
 /*
-몇시에 입양이 가장 활발?
-
-0~ 23
-시간대별로 입양이 몇 건이나 발생하는지 조회
-
-0~23 까지의 테이블
-해당 테이블에 맞게 group by 해서 조인
 
 
 */
