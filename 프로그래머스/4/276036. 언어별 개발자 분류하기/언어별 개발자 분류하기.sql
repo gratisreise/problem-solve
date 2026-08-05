bb31d284642429c0e58ae304011ef6c5de803245
noah <104671487+gratisreise@users.noreply.github.com>
2026-08-05 21:35:53 +0900

    [level 4] Title: 언어별 개발자 분류하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/276036. 언어별 개발자 분류하기/언어별 개발자 분류하기.sql
+++ 프로그래머스/4/276036. 언어별 개발자 분류하기/언어별 개발자 분류하기.sql
@@ -1,24 +1,22 @@
 select 
     case
-        when group_concat(s.name) like '%Python%' and group_concat(category) like '%Front End%' then 'A'
-        when group_concat(s.name) like '%C#%' then 'B'
+        when group_concat(category) like '%Front End%' and group_concat(name) like '%Python%' then 'A'
+        when group_concat(name) like '%C#%' then 'B'
         when group_concat(category) like '%Front End%' then 'C'
         else null
     end as grade,
-    id,
-    email
+    d.id,
+    d.email
 from developers d
 join skillcodes s
-on d.skill_code & s.code >= 1
-group by id 
+on d.skill_code & s.code = s.code
+group by d.id 
 having grade is not null
-order by grade, id
-/*
-A: Front End and Python 
-B: C#
-C: 그외 front end
+order by grade asc,  d.id asc
 
-skill_code로 조인하고
-category를 sum하고 
+
+
+/*
+
 
 */
