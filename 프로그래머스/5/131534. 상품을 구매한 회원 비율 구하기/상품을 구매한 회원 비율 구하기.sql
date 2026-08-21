8dbfe9dd87dc335288279e73a233482f943ff5de
noah <104671487+gratisreise@users.noreply.github.com>
2026-08-21 16:26:39 +0900

    [level 5] Title: 상품을 구매한 회원 비율 구하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/5/131534. 상품을 구매한 회원 비율 구하기/상품을 구매한 회원 비율 구하기.sql
+++ 프로그래머스/5/131534. 상품을 구매한 회원 비율 구하기/상품을 구매한 회원 비율 구하기.sql
@@ -1,22 +1,23 @@
-select
-    year(sales_date) year,
-    month(sales_date) as month,
+select year(sales_date) as year, month(sales_date) as month,
     count(distinct(o.user_id)) as purchased_users,
-    round(count(distinct(o.user_id)) / (select count(*) from user_info where year(joined) = 2021), 1) as purchased_ratio
-from user_info u
-join online_sale o
-on u.user_id = o.user_id
+    round(
+        count(distinct(o.user_id)) / (
+        select count(*) from user_info
+        where year(joined) = 2021
+    ), 1
+    ) as purchased_ratio
+from online_sale o
+join user_info u
+on o.user_id = u.user_id
 where year(u.joined) = 2021
-group by year(o.sales_date), month(o.sales_date)
-order by year asc, month asc
-
+group by year(sales_date), month(sales_date)
 
 
 /*
-상품을 구매한 회원의 비율
-년, 월별, 
+2021년에 가입한 회원 중 
+상품을 구매한 회원수,
+년,월별, 
+상품구매회원비율(소수둘째반올림), 
+년 오름차, 월 오름차
 
-상품을 구매한 회원수, 상품을 구매한 회원의 비율
-
-조인해서 그룹화 해서 년, 월, 그룹화 해서 구입 여부 
 */
