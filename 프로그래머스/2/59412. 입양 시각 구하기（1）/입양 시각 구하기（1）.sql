87b214f4803e9c386c3047470581e2ed4cd357ff
noah <104671487+gratisreise@users.noreply.github.com>
2026-08-23 15:21:12 +0900

    [level 2] Title: 입양 시각 구하기(1), Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/59412. 입양 시각 구하기（1）/입양 시각 구하기（1）.sql
+++ 프로그래머스/2/59412. 입양 시각 구하기（1）/입양 시각 구하기（1）.sql
@@ -1,14 +1,7 @@
-select
-    hour(datetime) as hour,
-    count(*) as count
+# 9~19, 입양건수 조회, 결과 시간순 정렬
+
+select hour(datetime) as hour, count(*) as `count`
 from animal_outs
 where hour(datetime) >= 9 and hour(datetime) <= 19
-group by hour
+group by hour(datetime) 
 order by hour
-
-
-/*
-몇시에 입양이 가장 활발 
-09 ~ 19
-
-*/
