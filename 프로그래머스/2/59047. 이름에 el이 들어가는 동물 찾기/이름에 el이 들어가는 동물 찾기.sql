3677ed889680049b34616bef8da6d9915bb77c68
noah <104671487+gratisreise@users.noreply.github.com>
2026-08-23 14:35:29 +0900

    [level 2] Title: 이름에 el이 들어가는 동물 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/59047. 이름에 el이 들어가는 동물 찾기/이름에 el이 들어가는 동물 찾기.sql
+++ 프로그래머스/2/59047. 이름에 el이 들어가는 동물 찾기/이름에 el이 들어가는 동물 찾기.sql
@@ -1,11 +1,5 @@
+# 이름에 el, 이름순,아이디순정렬, 이름대소구분x, 개
 select animal_id, name
 from animal_ins
-where lower(name) like '%el%' and animal_type = 'Dog'
-order by name, animal_id
-
-/*
-이름 el 있음
-이름순, 아이디순
-
-
-*/
+where lower(name) like  '%el%' and animal_type = 'Dog'
+order by name asc, animal_id asc
