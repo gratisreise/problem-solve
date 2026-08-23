ef4ec49eddcde902cea4434185664fedd93e972c
noah <104671487+gratisreise@users.noreply.github.com>
2026-08-23 15:37:10 +0900

    [level 1] Title: 조건에 맞는 도서 리스트 출력하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/1/144853. 조건에 맞는 도서 리스트 출력하기/조건에 맞는 도서 리스트 출력하기.sql
+++ 프로그래머스/1/144853. 조건에 맞는 도서 리스트 출력하기/조건에 맞는 도서 리스트 출력하기.sql
@@ -1,11 +1,5 @@
+# 2021년 출판, 인문 카테고리, 
 select book_id, published_date
 from book 
-where year(published_date) = 2021 and category = '인문'
-order by published_date
-
-/*
-2021년 출판 
-인문 카테고리 
-출판일 오름차
-
-*/
+where category = '인문' and year(published_date) = 2021
+order by published_date asc
