d714960b7c0b85d55bf3dc8dfced39535c026953
noah <104671487+gratisreise@users.noreply.github.com>
2026-08-23 14:49:18 +0900

    [level 1] Title: 흉부외과 또는 일반외과 의사 목록 출력하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/1/132203. 흉부외과 또는 일반외과 의사 목록 출력하기/흉부외과 또는 일반외과 의사 목록 출력하기.sql
+++ 프로그래머스/1/132203. 흉부외과 또는 일반외과 의사 목록 출력하기/흉부외과 또는 일반외과 의사 목록 출력하기.sql
@@ -1,11 +1,5 @@
+# cs or gs, 고용일자 내림차, 이름 오름차
 select dr_name, dr_id, mcdp_cd, hire_ymd
 from doctor
-where mcdp_cd IN ('CS', 'GS')
+where mcdp_cd = 'CS' or mcdp_cd = 'GS'
 order by hire_ymd desc, dr_name asc
-
-
-/*
-진료과 = CS or GS
-고용일자 내림차, 이름 오름차
-
-*/
