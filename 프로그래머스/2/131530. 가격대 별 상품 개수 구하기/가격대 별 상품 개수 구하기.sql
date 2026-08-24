0feadc391effac155b202bf16751bc37df281417
noah <104671487+gratisreise@users.noreply.github.com>
2026-08-24 21:12:35 +0900

    [level 2] Title: 가격대 별 상품 개수 구하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/131530. 가격대 별 상품 개수 구하기/가격대 별 상품 개수 구하기.sql
+++ 프로그래머스/2/131530. 가격대 별 상품 개수 구하기/가격대 별 상품 개수 구하기.sql
@@ -1,14 +1,14 @@
-with processed_data as (
-    select floor(price/10000) * 10000 as price_group 
+# 만원 단위의 가격대 별, 상품 개수
+with cte1 as (
+    select 
+        *,
+        floor(price / 10000) * 10000 as price_group
     from product
 )
-select price_group, count(*) as products
-from processed_data
-group by price_group
-order by price_group 
 
-
-/*
-가격대 별, 상품갯수, 
-가격대 오름차
-*/
+select 
+    price_group,
+    count(*) as proudcts
+from cte1
+group by price_group 
+order by price_group asc
