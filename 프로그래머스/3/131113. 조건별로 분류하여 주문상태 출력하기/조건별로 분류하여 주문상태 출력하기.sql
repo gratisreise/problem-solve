ccfd132590569e602c98981c733edf1101b2141c
noah <104671487+gratisreise@users.noreply.github.com>
2026-08-24 20:45:07 +0900

    [level 3] Title: 조건별로 분류하여 주문상태 출력하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/3/131113. 조건별로 분류하여 주문상태 출력하기/조건별로 분류하여 주문상태 출력하기.sql
+++ 프로그래머스/3/131113. 조건별로 분류하여 주문상태 출력하기/조건별로 분류하여 주문상태 출력하기.sql
@@ -1,14 +1,13 @@
-# 2022년 5월 1일 기준
-# 5월 1일까지 출고완료 이후 날짜 출고 대기, 미정이면 미정
-# 주문id 오름차
+# 2022년 5월 1일, 
+# 5월 1일 포함이전 출고완료, 이후 출고 대기, 미정 = 출고 미정
 select 
     order_id,
     product_id,
     out_date,
     case 
-        when out_date <'2022-05-02' then '출고완료'
+        when out_date < '2022-05-02' then '출고완료'
         when out_date is null then '출고미정'
         else '출고대기'
-    end '출고여부'
-from food_order 
-order by order_id asc 
+    end as '출고여부'
+from food_order
+order by order_id asc
