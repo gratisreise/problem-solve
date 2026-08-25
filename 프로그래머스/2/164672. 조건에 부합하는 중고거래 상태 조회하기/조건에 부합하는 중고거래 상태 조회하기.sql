ca31029e6a11a7aba3b4996bcf52f556c41abe09
noah <104671487+gratisreise@users.noreply.github.com>
2026-08-25 21:41:24 +0900

    [level 2] Title: 조건에 부합하는 중고거래 상태 조회하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/164672. 조건에 부합하는 중고거래 상태 조회하기/조건에 부합하는 중고거래 상태 조회하기.sql
+++ 프로그래머스/2/164672. 조건에 부합하는 중고거래 상태 조회하기/조건에 부합하는 중고거래 상태 조회하기.sql
@@ -1,21 +1,12 @@
-select
-    board_id,
-    writer_id,
-    title,
-    price,
-    case
-        when status = 'SALE' then '판매중'
-        when status = 'DONE' then '거래완료'
+# 2022년 10월 5일에 등록
+# 판매중, 예약중, 거래완료, 게시글ID DESC
+
+select board_id, writer_id, title, price,
+    case status
+        when 'DONE' then '거래완료'
+        when 'SALE' then '판매중'
         else '예약중'
-    end as status
-from used_goods_board
+    end as `status`
+from  used_goods_board
 where created_date = '2022-10-05'
-order by board_id desc
-
-/*
-2022년 10월 5일 등록
-거래상태 sale => 판매중, reserved => 예약중, done => 거래완료
-게시글id 내림차
-
-
-*/
+order by board_id desc 
