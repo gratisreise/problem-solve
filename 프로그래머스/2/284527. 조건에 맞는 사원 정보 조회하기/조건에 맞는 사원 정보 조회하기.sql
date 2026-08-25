67b55f4f2776b7fa4da511f977d02fd2d274b8fa
noah <104671487+gratisreise@users.noreply.github.com>
2026-08-25 22:14:10 +0900

    [level 2] Title: 조건에 맞는 사원 정보 조회하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/284527. 조건에 맞는 사원 정보 조회하기/조건에 맞는 사원 정보 조회하기.sql
+++ 프로그래머스/2/284527. 조건에 맞는 사원 정보 조회하기/조건에 맞는 사원 정보 조회하기.sql
@@ -1,21 +1,12 @@
-select 
-    sum(g.score) as score,
-    e.emp_no,
-    e.emp_name,
-    e.position,
-    e.email
-from hr_employees e
-join hr_grade g
-on e.emp_no = g.emp_no 
-group by e.emp_no
-order by sum(g.score) desc
-limit 1
+# 2022년, 평가점수max, 사원들?, 
+with cte1 as(
+    select emp_no, sum(score) as score
+    from hr_grade
+    group by emp_no
+    order by score desc
+    limit 1
+)
 
-
-
-/*
-2022년 한해 평가 점수 max 
-사원정보
-
-
-*/
+select c1.score, e.emp_no, e.emp_name, e.position, e.email
+from hr_employees e join cte1 c1
+on e.emp_no = c1.emp_no
