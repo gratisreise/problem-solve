7a6f9e4bff52dedd00817cd22775d5596e1fa174
noah <104671487+gratisreise@users.noreply.github.com>
2026-08-26 22:28:57 +0900

    [level 1] Title: 가장 큰 물고기 10마리 구하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/1/298517. 가장 큰 물고기 10마리 구하기/가장 큰 물고기 10마리 구하기.sql
+++ 프로그래머스/1/298517. 가장 큰 물고기 10마리 구하기/가장 큰 물고기 10마리 구하기.sql
@@ -1,12 +1,6 @@
+# max 물고기, 길이 내림차, id 오름차, 
 select id, length
 from fish_info
+where length is not null
 order by length desc, id asc
 limit 10
-
-
-/*
-가장 큰 물고기 10마리 
-ID, 길이
-길이 내림차, id 오름차 ,10cm이하 x
-
-*/
