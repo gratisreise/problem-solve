b6e7e733b3949fb3448b83f9bfe6ac873f089c75
noah <104671487+gratisreise@users.noreply.github.com>
2026-08-28 19:53:45 +0900

    [level 4] Title: 자동차 대여 기록 별 대여 금액 구하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/151141. 자동차 대여 기록 별 대여 금액 구하기/자동차 대여 기록 별 대여 금액 구하기.sql
+++ 프로그래머스/4/151141. 자동차 대여 기록 별 대여 금액 구하기/자동차 대여 기록 별 대여 금액 구하기.sql
@@ -1,37 +1,27 @@
-with pre1 as(
+# 종류 = '트럭', 대여금액 내림차, 대여기록id 내림차
+# 차 - 히스토리 가격, 히스토리별 - 할인 구간
+with cte1 as ( # 차 - 히스토리 - 가격
+    select h.history_id, c.car_type, 
+    (datediff(end_date, start_date) + 1) * c.daily_fee as totals, 
+    datediff(end_date, start_date) + 1 as days
+    from car_rental_company_rental_history h
+    join car_rental_company_car c
+    on h.car_id = c.car_id
+    where c.car_type= '트럭'
+),
+cte2 as ( # 히스토리 - 할인구간
     select 
-        history_id, 
-        datediff(end_date, start_date) + 1 as days,
-        (datediff(end_date, start_date)+1) * c.daily_fee as sum_fee
-    from car_rental_company_car c
-    join car_rental_company_rental_history h
-    on c.car_id = h.car_id
-    where car_type = '트럭'
-), pre2 as(
-    select 
-        trim(replace(duration_type, '일 이상', '')) + 0 as days,
-        (1-discount_rate/100) as discount
-    from car_rental_company_discount_plan
-    where car_type = '트럭'
+        c1.history_id, 
+        max(ifnull(p.discount_rate, 0)) as rate
+    from cte1 c1 left join car_rental_company_discount_plan p
+    on c1.car_type = p.car_type 
+    and days >= replace(duration_type, '일 이상', '')
+    group by c1.history_id 
 )
 
-# select * from pre2
-
 select 
-    p1.history_id,
-    sum_fee * ifnull(min(discount), 1) as fee
-from pre1 p1
-left join pre2 p2
-on p1.days >= p2.days 
-group by p1.history_id 
-order by fee desc , p1.history_id desc
-
-
-
-/*
-자동차 종류 = 트럭
-대여기록별  대여금액, 
-대여 금액 내림차, 기록id 내림차
-
-
-*/
+    c1.history_id, 
+    c1.totals * (1 - rate / 100) as fee
+from cte1 c1 join cte2 c2 
+on c1.history_id = c2.history_id
+order by fee desc, c1.history_id desc
