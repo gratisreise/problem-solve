6b1aee5ab47481fda7293e1fd7c4f53c0135959e
noah <104671487+gratisreise@users.noreply.github.com>
2026-08-29 19:30:19 +0900

    [level 4] Title: 특정 세대의 대장균 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/301650. 특정 세대의 대장균 찾기/특정 세대의 대장균 찾기.sql
+++ 프로그래머스/4/301650. 특정 세대의 대장균 찾기/특정 세대의 대장균 찾기.sql
@@ -1,28 +1,16 @@
-with recursive tree as (
-    select id, 1 as generation
+# 3세대 
+
+with recursive tree as(
+    select id, 1 as gen
     from ecoli_data
     where parent_id is null
     
     union all
     
-    select e.id, t.generation + 1
-    from ecoli_data e
-    join tree t
-    on e.parent_id = t.id
+    select e.id, t.gen + 1
+    from ecoli_data e join tree t
+    on e.parent_id = t.id 
 )
-
-select id from tree
-where generation = 3
-order by id
-
-
-
-/*
-3세대 대장균
-ID
-오름차
-부모의 id랑 id랑 엮고 부모의 id가 null이 아닌 것들 중에 
-parent가 null이 아닌 것들을 찾으면 3세대?
-나의 부모의 부모가 null이 아니면 3세대
-
-*/
+select id from tree 
+where gen = 3
+order by id asc
