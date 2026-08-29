0050502f1e5d700deb630f34c6f7faac96b7f425
noah <104671487+gratisreise@users.noreply.github.com>
2026-08-29 19:13:03 +0900

    [level 2] Title: 조건에 맞는 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/276034. 조건에 맞는 개발자 찾기/조건에 맞는 개발자 찾기.sql
+++ 프로그래머스/2/276034. 조건에 맞는 개발자 찾기/조건에 맞는 개발자 찾기.sql
@@ -1,14 +1,7 @@
+# Python or C# 개발자, id 오름차
 select d.id, d.email, d.first_name, d.last_name
-from developers d
-left join skillcodes s
-on d.skill_code & s.code >= 1 
-group by d.id
-having sum(s.name = 'Python') >= 1 or sum(s.name = 'C#') >= 1
-order by id asc
+from developers d join skillcodes s
+on d.skill_code & s.code = s.code and (s.name = 'Python' or s.name = 'C#')
+group by d.id 
+order by d.id asc
 
-
-/*
-python or C#
-id 오름차
-
-*/
