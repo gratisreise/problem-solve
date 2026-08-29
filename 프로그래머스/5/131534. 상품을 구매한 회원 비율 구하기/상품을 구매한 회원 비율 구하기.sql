eaf04c6d5a0bcd8fc9ea46d3837c9d9e4d6846f1
noah <104671487+gratisreise@users.noreply.github.com>
2026-08-29 18:13:29 +0900

    [level 5] Title: 상품을 구매한 회원 비율 구하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/5/131534. 상품을 구매한 회원 비율 구하기/상품을 구매한 회원 비율 구하기.sql
+++ 프로그래머스/5/131534. 상품을 구매한 회원 비율 구하기/상품을 구매한 회원 비율 구하기.sql
@@ -1,23 +1,19 @@
-select year(sales_date) as year, month(sales_date) as month,
-    count(distinct(o.user_id)) as purchased_users,
-    round(
-        count(distinct(o.user_id)) / (
-        select count(*) from user_info
-        where year(joined) = 2021
-    ), 1
-    ) as purchased_ratio
-from online_sale o
-join user_info u
-on o.user_id = u.user_id
-where year(u.joined) = 2021
+# 2021년, 년,월 별 .1f, 년 오름차, 월 오름차
+# 2021년 
+with cte1 as ( # 2021년 가입한 놈과 그때 가입한 회원의 수 
+    select 
+        *,
+        count(*) over(partition by year(joined)) as totals
+    from user_info
+    where year(joined) = 2021
+)
+
+select 
+    year(sales_date) as year,
+    month(sales_date) as month,
+    count(distinct os.user_id) as purchased_users,
+    round((count(distinct os.user_id) / totals), 1) as purchased_ratio
+from online_sale os join cte1 c1
+on os.user_id = c1.user_id
 group by year(sales_date), month(sales_date)
-
-
-/*
-2021년에 가입한 회원 중 
-상품을 구매한 회원수,
-년,월별, 
-상품구매회원비율(소수둘째반올림), 
-년 오름차, 월 오름차
-
-*/
+order by year asc, month asc
