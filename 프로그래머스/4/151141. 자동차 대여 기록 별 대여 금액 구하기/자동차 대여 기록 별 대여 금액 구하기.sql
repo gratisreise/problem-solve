29d339a6491da60405f64cada5242005360b34c8
noah <104671487+gratisreise@users.noreply.github.com>
2026-08-29 19:06:22 +0900

    [level 4] Title: 자동차 대여 기록 별 대여 금액 구하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/151141. 자동차 대여 기록 별 대여 금액 구하기/자동차 대여 기록 별 대여 금액 구하기.sql
+++ 프로그래머스/4/151141. 자동차 대여 기록 별 대여 금액 구하기/자동차 대여 기록 별 대여 금액 구하기.sql
@@ -1,27 +1,29 @@
-# 종류 = '트럭', 대여금액 내림차, 대여기록id 내림차
-# 차 - 히스토리 가격, 히스토리별 - 할인 구간
-with cte1 as ( # 차 - 히스토리 - 가격
-    select h.history_id, c.car_type, 
-    (datediff(end_date, start_date) + 1) * c.daily_fee as totals, 
-    datediff(end_date, start_date) + 1 as days
-    from car_rental_company_rental_history h
-    join car_rental_company_car c
-    on h.car_id = c.car_id
-    where c.car_type= '트럭'
-),
-cte2 as ( # 히스토리 - 할인구간
+# 대여기록별 대여금액, 종류 = '트럭', 대여 금액 내림차, 대여 기록ID 내림차
+# 기록, 타입, 금액 | 기록 할인율 
+with 
+prices as (
     select 
-        c1.history_id, 
-        max(ifnull(p.discount_rate, 0)) as rate
-    from cte1 c1 left join car_rental_company_discount_plan p
-    on c1.car_type = p.car_type 
-    and days >= replace(duration_type, '일 이상', '')
-    group by c1.history_id 
+        history_id, car_type,
+        (datediff(end_date, start_date) + 1) * daily_fee as prices,
+        datediff(end_date, start_date) + 1,
+        daily_fee
+    from car_rental_company_car c join car_rental_company_rental_history h
+    on c.car_id = h.car_id
+    where car_type = '트럭'
+),
+rates as (
+    select 
+        history_id, 
+        (1 - (max(ifnull(discount_rate, 0))/100)) rate
+    from car_rental_company_rental_history h left join car_rental_company_discount_plan p
+    on (datediff(end_date, start_date) + 1) >= replace(p.duration_type, '일 이상', '') and p.car_type = '트럭'
+    group by history_id
 )
 
+
 select 
-    c1.history_id, 
-    c1.totals * (1 - rate / 100) as fee
-from cte1 c1 join cte2 c2 
-on c1.history_id = c2.history_id
-order by fee desc, c1.history_id desc
+    p.history_id,
+    floor(prices * rate) as fee 
+from prices p join rates r
+on p.history_id = r.history_id 
+order by fee desc, p.history_id desc 
