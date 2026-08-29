3889c292fcd3f41f810e8469545802bf01e750f6
noah <104671487+gratisreise@users.noreply.github.com>
2026-08-29 19:55:30 +0900

    [level 1] Title: 조건에 부합하는 중고거래 댓글 조회하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/1/164673. 조건에 부합하는 중고거래 댓글 조회하기/조건에 부합하는 중고거래 댓글 조회하기.sql
+++ 프로그래머스/1/164673. 조건에 부합하는 중고거래 댓글 조회하기/조건에 부합하는 중고거래 댓글 조회하기.sql
@@ -1,12 +1,8 @@
+# 2022년 10월 작성
+# 댓글 작성일 오름차, 제목 오름차
+
 select b.title, b.board_id, r.reply_id, r.writer_id, r.contents, r.created_date
-from used_goods_board b
-join used_goods_reply r
+from used_goods_board b join used_goods_reply r
 on b.board_id = r.board_id 
 where b.created_date like '2022-10%'
-order by r.created_date asc, b.title 
-
-
-/*
-2022년 10월 작성된 게시글 
-댓글 작성일 기준 오름차, 게시글 제목 오름차
-*/
+order by r.created_date asc, b.title asc
