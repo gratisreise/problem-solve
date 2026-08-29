7162bf1d54c8e61c350266d3d7f0b693bf1acff8
noah <104671487+gratisreise@users.noreply.github.com>
2026-08-29 18:16:02 +0900

    [level 1] Title: 특정 형질을 가지는 대장균 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/1/301646. 특정 형질을 가지는 대장균 찾기/특정 형질을 가지는 대장균 찾기.sql
+++ 프로그래머스/1/301646. 특정 형질을 가지는 대장균 찾기/특정 형질을 가지는 대장균 찾기.sql
@@ -1,10 +1,4 @@
-select count(*) as "COUNT"
-from ecoli_data
-where genotype & 2 = 0 and (genotype & 1 >= 1 or genotype & 4 >= 1)
-
-
-
-/*
-2번 x (1 or 3) 대장균 개체수
-
-*/
+# 2번X and 1or3O 개체수, 
+select count(*) as `count`
+from ecoli_data 
+where genotype & 2 = 0 and (genotype & 1 = 1 or genotype & 4 = 4)
